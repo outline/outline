317d071c68bb115226e5bc96763085983898ec03
Jori Lallo <jtlallo@gmail.com>
2016-05-27 00:11:51 -0700

    Removed debug statement

--- server/middlewares/subdomainRedirect.js
+++ server/middlewares/subdomainRedirect.js
@@ -1,7 +1,5 @@
 export default function subdomainRedirect(options) {
   return async function subdomainRedirectMiddleware(ctx, next) {
-    console.log(ctx.headers);
-
     if (ctx.headers.host === 'beautifulatlas.com') {
       ctx.redirect('https://www.' + ctx.headers.host + ctx.path);
     }
