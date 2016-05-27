a679814782ebe10e3fa7d10aa8d127ec75b686e4
Jori Lallo <jtlallo@gmail.com>
2016-05-27 00:11:28 -0700

    Counting is hard

--- server/middlewares/subdomainRedirect.js
+++ server/middlewares/subdomainRedirect.js
@@ -3,7 +3,7 @@ export default function subdomainRedirect(options) {
     console.log(ctx.headers);
 
     if (ctx.headers.host === 'beautifulatlas.com') {
-      ctx.redirect('https://wwww.' + ctx.headers.host + ctx.path);
+      ctx.redirect('https://www.' + ctx.headers.host + ctx.path);
     }
     else {
       return next();
