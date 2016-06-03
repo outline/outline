68d935f6b2ad305150cf2c5242f040fca35782c0
Jori Lallo <jtlallo@gmail.com>
2016-06-02 22:21:46 -0700

    sup

--- server/api/documents.js
+++ server/api/documents.js
@@ -21,7 +21,7 @@ router.post('documents.info', auth({ require: false }), async (ctx) => {
 
   // Don't expose private documents outside the team
   if (document.private) {
-    // if (!ctx.state.user) throw httpErrors.NotFound();
+    if (!ctx.state.user) throw httpErrors.NotFound();
 
     const team = await ctx.state.user.getTeam();
     if (document.teamId !== team.id) {
