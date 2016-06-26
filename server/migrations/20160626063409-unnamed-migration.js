2bcf37fd83e4680e2cca8bf43f007b5904541aba
Jori Lallo <jtlallo@gmail.com>
2016-06-25 23:40:08 -0700

    Added indexes

---
+++ server/migrations/20160626063409-unnamed-migration.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    queryInterface.addIndex('documents', ['urlId']);
+    queryInterface.addIndex('documents', ['id', 'atlasId']);
+    queryInterface.addIndex('documents', ['id', 'teamId']);
+    queryInterface.addIndex('documents', ['parentDocumentId', 'atlasId']);
+
+    queryInterface.addIndex('atlases', ['id', 'teamId']);
+
+    queryInterface.addIndex('teams', ['slackId']);
+
+    queryInterface.addIndex('users', ['slackId']);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    queryInterface.removeIndex('documents', ['urlId']);
+    queryInterface.removeIndex('documents', ['id', 'atlasId']);
+    queryInterface.removeIndex('documents', ['id', 'teamId']);
+    queryInterface.removeIndex('documents', ['parentDocumentId', 'atlasId']);
+
+    queryInterface.removeIndex('atlases', ['id', 'teamId']);
+
+    queryInterface.removeIndex('teams', ['slackId']);
+
+    queryInterface.removeIndex('users', ['slackId']);
+  }
+};
