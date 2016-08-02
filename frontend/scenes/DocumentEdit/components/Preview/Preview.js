9de77e15240b90c67a36e07050b98585e75e7aed
Jori Lallo <jtlallo@gmail.com>
2016-08-02 12:30:41 +0300

    Hide title anchors on edit mode preview

--- frontend/scenes/DocumentEdit/components/Preview/Preview.js
+++ frontend/scenes/DocumentEdit/components/Preview/Preview.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import { DocumentHtml } from 'components/Document';
 
-import styles from '../DocumentEdit.scss';
+import styles from './Preview.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 const Preview = (props) => {
   return (
-    <div className={ styles.preview }>
+    <div className={ cx(styles.container) }>
       <DocumentHtml html={ props.html } />
     </div>
   );
---
+++ frontend/scenes/DocumentEdit/components/Preview/index.js
@@ -0,0 +1,2 @@
+import Preview from './Preview';
+export default Preview;
