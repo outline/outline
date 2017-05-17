f70e2326c0571dfc000b046e28c77c51e0d63da3
Jori Lallo <jtlallo@gmail.com>
2017-05-17 00:09:47 -0700

    Fixed source maps

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,18 +1,15 @@
+/* eslint-disable */
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 commonWebpackConfig = require('./webpack.config');
 
 productionWebpackConfig = Object.assign(commonWebpackConfig, {
   cache: true,
   devtool: 'cheap-module-source-map',
-  entry: [
-    'babel-polyfill',
-    'babel-regenerator-runtime',
-    './frontend/index',
-  ],
+  entry: ['babel-polyfill', 'babel-regenerator-runtime', './frontend/index'],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.[hash].js',
@@ -21,25 +18,34 @@ productionWebpackConfig = Object.assign(commonWebpackConfig, {
 });
 productionWebpackConfig.module.loaders.push({
   test: /\.s?css$/,
-  loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass?sourceMap')
+  loader: ExtractTextPlugin.extract(
+    'style-loader',
+    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass?sourceMap'
+  ),
 });
-productionWebpackConfig.plugins.push(new HtmlWebpackPlugin({
-  template: 'server/static/index.html'
-}));
-productionWebpackConfig.plugins.push(new ExtractTextPlugin('styles.[hash].css'));
-productionWebpackConfig.plugins.push(new webpack.optimize.OccurenceOrderPlugin());
+productionWebpackConfig.plugins.push(
+  new HtmlWebpackPlugin({
+    template: 'server/static/index.html',
+  })
+);
+productionWebpackConfig.plugins.push(
+  new ExtractTextPlugin('styles.[hash].css')
+);
+productionWebpackConfig.plugins.push(
+  new webpack.optimize.OccurenceOrderPlugin()
+);
 productionWebpackConfig.plugins.push(
   new webpack.optimize.UglifyJsPlugin({
     compress: {
-        warnings: false
-    }
+      warnings: false,
+    },
   })
 );
 productionWebpackConfig.plugins.push(
   new webpack.DefinePlugin({
     'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
+      NODE_ENV: JSON.stringify('production'),
+    },
   })
 );
 
