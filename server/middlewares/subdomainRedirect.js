044486b9a6531ad5d77395735691a4f56970f628
Tom Moor <tom.moor@gmail.com>
2017-05-27 11:22:42 -0700

    Cleanup

--- server/middlewares/subdomainRedirect.js
+++ server/middlewares/subdomainRedirect.js
@@ -1,7 +1,7 @@
 export default function subdomainRedirect(options) {
   return async function subdomainRedirectMiddleware(ctx, next) {
     if (ctx.headers.host === 'beautifulatlas.com') {
-      ctx.redirect('https://www.' + ctx.headers.host + ctx.path);
+      ctx.redirect(`https://www.${ctx.headers.host}${ctx.path}`);
     } else {
       return next();
     }
