dbcfe1199e233238bb7ced12ed7fc93151f347e3
Jori Lallo <jtlallo@gmail.com>
2017-07-07 00:14:00 -0700

    Positioned document loading state with the document content

--- frontend/components/PreviewLoading/PreviewLoading.js
+++ frontend/components/PreviewLoading/PreviewLoading.js
@@ -11,7 +11,7 @@ const randomValues = Array.from(
   () => `${randomInteger(85, 100)}%`
 );
 
-export default () => {
+export default (props: {}) => {
   return (
     <ReactCSSTransitionGroup
       transitionName="fadeIn"
@@ -22,7 +22,7 @@ export default () => {
       transitionEnterTimeout={0}
       transitionLeaveTimeout={0}
     >
-      <Flex column auto>
+      <Flex column auto {...props}>
         <Mask style={{ width: randomValues[0] }} header />
         <Mask style={{ width: randomValues[1] }} />
         <Mask style={{ width: randomValues[2] }} />
