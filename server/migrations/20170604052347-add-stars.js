8d54f79849bc10e1914e7349bba8be00b284eabc
Jori Lallo <jtlallo@gmail.com>
2017-07-08 21:02:44 -0700

    use promises in migrations

--- server/migrations/20170604052347-add-stars.js
+++ server/migrations/20170604052347-add-stars.js
@@ -1,35 +1,39 @@
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    queryInterface.createTable('stars', {
-      id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true,
-      },
-      documentId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-      },
-      userId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    });
-    queryInterface.addIndex('stars', ['documentId', 'userId'], {
-      indicesType: 'UNIQUE',
-    });
+    queryInterface
+      .createTable('stars', {
+        id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          primaryKey: true,
+        },
+        documentId: {
+          type: Sequelize.UUID,
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.UUID,
+          allowNull: false,
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      })
+      .then(() => {
+        queryInterface.addIndex('stars', ['documentId', 'userId'], {
+          indicesType: 'UNIQUE',
+        });
+      });
   },
 
   down: function(queryInterface, Sequelize) {
-    queryInterface.removeIndex('stars', ['documentId', 'userId']);
-    queryInterface.dropTable('stars');
+    queryInterface.removeIndex('stars', ['documentId', 'userId']).then(() => {
+      queryInterface.dropTable('stars');
+    });
   },
 };
