b6616cd05accb87485ca050680246f3df4309da2
Tom Moor <tom.moor@gmail.com>
2017-07-17 09:03:29 -0700

    Various editor fixes (#160)
    * Tab should move to body from heading
    
    * Focus editor on mount, closes #156
    
    * Closes #154 - Just went for the simple approach here, considered something more complex but this is clear
    
    * Added body placeholder

---
+++ frontend/components/Editor/components/Paragraph.js
@@ -0,0 +1,29 @@
+// @flow
+import React from 'react';
+import { Document } from 'slate';
+import type { Props } from '../types';
+import styles from '../Editor.scss';
+
+export default function Link({
+  attributes,
+  editor,
+  node,
+  parent,
+  children,
+}: Props) {
+  const parentIsDocument = parent instanceof Document;
+  const firstParagraph = parent && parent.nodes.get(1) === node;
+  const lastParagraph = parent && parent.nodes.last() === node;
+  const showPlaceholder =
+    parentIsDocument && firstParagraph && lastParagraph && !node.text;
+
+  return (
+    <p>
+      {children}
+      {showPlaceholder &&
+        <span className={styles.placeholder} contentEditable={false}>
+          {editor.props.bodyPlaceholder}
+        </span>}
+    </p>
+  );
+}
