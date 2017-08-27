eddbcccc5e88638835a874c1cb67dbdbb9c0db07
Tom Moor <tom.moor@gmail.com>
2017-08-27 11:40:36 -0700

    Add missing database constraints

---
+++ server/migrations/20170827182423-improve-references.js
@@ -0,0 +1,56 @@
+module.exports = {
+  up: async function(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('documents', 'atlasId', {
+      type: Sequelize.UUID,
+      allowNull: true,
+      onDelete: 'cascade',
+      references: {
+        model: 'collections',
+      },
+    });
+    await queryInterface.changeColumn('documents', 'userId', {
+      type: Sequelize.UUID,
+      allowNull: true,
+      references: {
+        model: 'users',
+      },
+    });
+    await queryInterface.changeColumn('documents', 'parentDocumentId', {
+      type: Sequelize.UUID,
+      allowNull: true,
+      references: {
+        model: 'documents',
+      },
+    });
+    await queryInterface.changeColumn('documents', 'teamId', {
+      type: Sequelize.UUID,
+      allowNull: true,
+      onDelete: 'cascade',
+      references: {
+        model: 'teams',
+      },
+    });
+  },
+
+  down: async function(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(
+      'ALTER TABLE documents DROP CONSTRAINT "atlasId_foreign_idx";'
+    );
+    await queryInterface.removeIndex('documents', 'atlasId_foreign_idx');
+    await queryInterface.sequelize.query(
+      'ALTER TABLE documents DROP CONSTRAINT "userId_foreign_idx";'
+    );
+    await queryInterface.removeIndex('documents', 'userId_foreign_idx');
+    await queryInterface.sequelize.query(
+      'ALTER TABLE documents DROP CONSTRAINT "parentDocumentId_foreign_idx";'
+    );
+    await queryInterface.removeIndex(
+      'documents',
+      'parentDocumentId_foreign_idx'
+    );
+    await queryInterface.sequelize.query(
+      'ALTER TABLE documents DROP CONSTRAINT "teamId_foreign_idx";'
+    );
+    await queryInterface.removeIndex('documents', 'teamId_foreign_idx');
+  },
+};
