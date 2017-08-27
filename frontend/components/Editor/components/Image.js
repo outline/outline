5f514f559ccb982a2f69d0bf88d8000b89b55037
Tom Moor <tom.moor@gmail.com>
2017-08-27 09:51:37 -0700

    Show selected state

--- frontend/components/Editor/components/Image.js
+++ frontend/components/Editor/components/Image.js
@@ -1,16 +1,27 @@
 // @flow
 import React from 'react';
 import type { Props } from '../types';
+import { color } from 'styles/constants';
 import styled from 'styled-components';
 
-const LoadingImage = styled.img`
-  opacity: .5;
+const StyledImg = styled.img`
+  box-shadow: ${props => (props.active ? `0 0 0 3px ${color.slate}` : '0')};
+  opacity: ${props => (props.loading ? 0.5 : 1)};
 `;
 
-export default function Image({ attributes, node }: Props) {
+export default function Image({ attributes, state, node }: Props) {
   const loading = node.data.get('loading');
-  const Component = loading ? LoadingImage : 'img';
-  const src = node.data.get('inlineSrc') || node.data.get('src');
+  const alt = node.data.get('alt');
+  const src = node.data.get('src');
+  const active = state.isFocused && state.selection.hasEdgeIn(node);
 
-  return <Component {...attributes} src={src} alt={node.data.get('alt')} />;
+  return (
+    <StyledImg
+      {...attributes}
+      src={src}
+      alt={alt}
+      active={active}
+      loading={loading}
+    />
+  );
 }
