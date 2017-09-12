f3cb17046d38a4bd0d9402b63e2fc37dcd358300
Jori Lallo <jtlallo@gmail.com>
2017-09-11 23:32:26 -0700

    Fix to webpack plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 commonWebpackConfig = require('./webpack.config');
 
