98175c3d99b6d76dae87aef2b96b3a3052584ea9
Jori Lallo <jtlallo@gmail.com>
2017-09-11 22:08:47 -0700

    make the star smaller

--- frontend/components/DocumentPreview/DocumentPreview.js
+++ frontend/components/DocumentPreview/DocumentPreview.js
@@ -19,6 +19,9 @@ const StyledStar = styled(({ solid, ...props }) => <Icon {...props} />).attrs({
   type: 'Star',
   color: color.text,
 })`
+  width: 16px;
+  height: 16px;
+  top: 1px;
   margin-left: 4px;
   opacity: ${props => (props.solid ? '1 !important' : 0)};
   transition: opacity 100ms ease-in-out;
