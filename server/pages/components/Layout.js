5cccd11d7b084e3afe2a945ee227da447c769f21
Tom Moor <tom.moor@gmail.com>
2017-11-09 23:14:03 -0800

    Added favicon, social tags to SSR pages

--- server/pages/components/Layout.js
+++ server/pages/components/Layout.js
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import Navigation from './Navigation';
 import Analytics from '../../../shared/components/Analytics';
 import globalStyles from '../../../shared/styles/globals';
+import { color } from '../../../shared/styles/constants';
 
 type Props = {
   children?: React$Element<*>,
@@ -17,7 +18,19 @@ export default function Layout({ children }: Props) {
       <head>
         <Helmet>
           <title>Outline</title>
+          <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="referrer" content="origin" />
+          <meta name="description" content="Your team’s knowledge base" />
+          <meta name="og:site_name" content="Outline" />
+          <meta name="og:type" content="website" />
+          <meta name="theme-color" content={color.primary} />
+
+          <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+          <link
+            rel="dns-prefetch"
+            href={process.env.AWS_S3_UPLOAD_BUCKET_URL}
+          />
         </Helmet>
         <Analytics />
 
