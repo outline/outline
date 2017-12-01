a4b10912a4da7b3ee22350c4f1f004b3b0df5c9a
anchepiece <anchepiece@gmail.com>
2017-12-01 16:40:17 -0500

    Address failing startup without .env #459

--- index.js
+++ index.js
@@ -12,18 +12,19 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-const app = require('./server').default;
-const http = require('http');
-
 if (
+  !process.env.SECRET_KEY ||
   process.env.SECRET_KEY ===
-  'F0E5AD933D7F6FD8F4DBB3E038C501C052DC0593C686D21ACB30AE205D2F634B'
+    'F0E5AD933D7F6FD8F4DBB3E038C501C052DC0593C686D21ACB30AE205D2F634B'
 ) {
   console.error(
     'Please set SECRET_KEY env variable with output of `openssl rand -hex 32`'
   );
   process.exit(1);
 }
+
+const app = require('./server').default;
+const http = require('http');
 
 const server = http.createServer(app.callback());
 server.listen(process.env.PORT || '3000');
