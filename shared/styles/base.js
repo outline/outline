787fb63f09a3f30d5352c0749f0d392fa5fd716e
Jori Lallo <jtlallo@gmail.com>
2017-12-17 17:14:16 -0800

    Added privacy policy

--- shared/styles/base.js
+++ shared/styles/base.js
@@ -68,4 +68,11 @@ export default `
     margin-top: 1em;
     margin-bottom: 1em;
   }
+
+  hr {
+    border: 0;
+  height: 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border - bottom: 1px solid rgba(255, 255, 255, 0.3);
+}   
 `;
