3b303aefc4972fe4bdc7ea4fc31dfda366b22cb1
Tom Moor <tom.moor@gmail.com>
2017-12-28 12:49:34 +0000

    Flow

--- app/models/Document.js
+++ app/models/Document.js
@@ -190,10 +190,10 @@ class Document extends BaseModel {
           data.parentDocument = this.parentDocument;
         }
         res = await client.post('/documents.create', data);
-        invariant(res && res.data, 'Data should be available');
-        this.emit('documents.create', res.data);
+        if (res && res.data) this.emit('documents.create', res.data);
       }
       runInAction('Document#save', () => {
+        invariant(res && res.data, 'Data should be available');
         this.updateData(res.data);
         this.hasPendingChanges = false;
       });
