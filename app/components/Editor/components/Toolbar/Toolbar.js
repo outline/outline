1e38e70987dced28f6022dc821c1bad2de4d77e8
Jori Lallo <jtlallo@gmail.com>
2018-01-01 11:58:35 -0800

    Delay toolbar visibility a bit

--- app/components/Editor/components/Toolbar/Toolbar.js
+++ app/components/Editor/components/Toolbar/Toolbar.js
@@ -167,6 +167,7 @@ const Menu = styled.div`
   transform: scale(0.95);
   transition: opacity 150ms cubic-bezier(0.175, 0.885, 0.32, 1.275),
     transform 150ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  transition-delay: 250ms;
   line-height: 0;
   height: 40px;
   min-width: 260px;
