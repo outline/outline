eb1ba1a6be252bf4d7171e2b46b2b64482eb56e6
Jori Lallo <jtlallo@gmail.com>
2018-01-17 22:16:13 -0800

    changed copy

--- server/utils/onboarding.js
+++ server/utils/onboarding.js
@@ -1,33 +1,31 @@
 // @flow
-const template = `# Welcome to Outline
+export const welcomeMessage = (collectionId: string) =>
+  `# Welcome to Outline
 
-Outline a place for your team to build your knowledge base. This can include:
+Outline is a place for your team to build your knowledge base. This can include:
 
 * Team wiki
 * Documentation
 * Playbooks
 * Employee onboarding
+* ...or anything you can think of
 
-...and anything you can think of.
-
-## 🖋 Editor to fit your needs
+## 🖋 A powerful editor
 
 ![Text formatting in Outline](https://s3.amazonaws.com/dev.beautifulatlas.com/uploads/e2b85962-ca66-4e4c-90d3-b32d30f0610c/754830c0-2aca-467c-82de-2fd6e990b696/Group.png)
 
-Outline's editor is build to be fast and extensible. You can easily format your documents with keyboard shortcuts or simply highlighting the text and making your selections. To add images, just drag and drop them to your canvas.
-
-Like many developers, we love [Markdown](http://commonmark.org/help/) and you can format your Outline documents by writing Markdown it will get formatted without the need for previews.
+Outline's editor lets you easily format your documents with keyboard shortcuts, Markdown syntax or by simply highlighting the text and making your selections. To add images, just drag and drop them to your canvas.
 
 ## 👩‍💻 Developer friendly
 
-Outline features an [open API](https://www.getoutline.com/developers) and syntax highlighting which makes it ideal for software teams. To create your first document using the API, create an API key in [settings](https://www.getoutline.com/settings/tokens) and  run the following Javascript code. Just remember to update your API key and keep it secure!
+Outline features an [API](https://www.getoutline.com/developers) for programatic document creation. To create your first document using the API, simply write it in Markdown and make a call to add it into your collection:
 
 \`\`\`
 const newDocument = {
   title: 'Getting started with codebase',
   text: 'All the information needed in Markdown',
-  collection: 'COLLECTION_ID',
-  token: 'API_KEY', // Replace with a key from https://www.getoutline.com/settings/tokens
+  collection: '${collectionId}',
+  token: 'API_KEY', // Replace with a value from https://www.getoutline.com/settings/tokens
 };
 
 fetch('https://www.getoutline.com/api/documents.create', {
@@ -39,10 +37,7 @@ fetch('https://www.getoutline.com/api/documents.create', {
 });
 \`\`\`
 
-## 💬 Say hi to the team
+## 👋 Say hi to the team
 
 Outline is build by a small team and we would love to get to get to know our users. Drop by at [our Spectrum community](https://spectrum.chat/outline) or [drop us an email](mailto:hello@getoutline.com).
 `;
-
-export const welcomeMessage = collectionId =>
-  template.replace('COLLECTION_ID', collectionId);
