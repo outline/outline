7b25bcd7ffbb0d98d974270ea1aeea880a72c038
Tom Moor <tom.moor@gmail.com>
2018-02-04 12:01:41 -0800

    gtm > ga

--- shared/components/Analytics.js
+++ shared/components/Analytics.js
@@ -5,21 +5,18 @@ function Analytics() {
   const id = process.env.GOOGLE_ANALYTICS_ID;
   if (!id) return null;
 
-  return (
-    <span>
-      <script src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${id}');
-        `,
-        }}
-      />
-    </span>
-  );
+  return [
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+        ga('create', ${id}, 'auto');
+        ga('send', 'pageview');
+      `,
+      }}
+    />,
+    <script async src="https://www.google-analytics.com/analytics.js" />,
+  ];
 }
 
 export default Analytics;
