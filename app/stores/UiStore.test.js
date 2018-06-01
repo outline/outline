140afc8a51dfa60dfd70c17bcaa86afd8266c033
Tom Moor <tom.moor@gmail.com>
2018-06-01 00:27:18 -0400

    :shirt:

--- app/stores/UiStore.test.js
+++ app/stores/UiStore.test.js
@@ -6,22 +6,23 @@ describe('UiStore', () => {
   let store;
 
   beforeEach(() => {
-    store = new stores.UiStore();
+    store = stores.ui;
   });
 
   test('#add should add errors', () => {
-    expect(store.data.length).toBe(0);
+    expect(store.toasts.length).toBe(0);
     store.showToast('first error');
     store.showToast('second error');
     expect(store.toasts.length).toBe(2);
   });
 
   test('#remove should remove errors', () => {
+    store.toasts = [];
     store.showToast('first error');
     store.showToast('second error');
     expect(store.toasts.length).toBe(2);
     store.removeToast(0);
     expect(store.toasts.length).toBe(1);
-    expect(store.toasts[0]).toBe('second error');
+    expect(store.toasts[0].message).toBe('second error');
   });
 });
