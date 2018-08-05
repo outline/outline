cad83f4e7bc6eb9a6b0f3b699565a97b46535ee6
Tom Moor <tom.moor@gmail.com>
2018-08-05 11:12:59 -0700

    Tighten context snippet in slash command results

--- server/presenters/slackAttachment.js
+++ server/presenters/slackAttachment.js
@@ -5,7 +5,7 @@ function present(document: Document, context?: string) {
   // the context contains <b> tags around search terms, we convert them here
   // to the markdown format that slack expects to receive.
   const text = context
-    ? context.replace(/<\/?b>/g, '*')
+    ? context.replace(/<\/?b>/g, '*').replace('\n', '')
     : document.getSummary();
 
   return {
