0280f4f80537afc7609ac957f319c050dfb9eabe
ivy <597055914@qq.com>
2019-01-08 15:14:21 +0800

    Modify authing options

--- public/scripts/login-form.js
+++ public/scripts/login-form.js
@@ -1,34 +1,25 @@
 window.onload = () => {
+    let options = {
+        clientId: '5bf36ad6a9e256000132cebf',
+        secret: 'c15331f0f3361acd83005871801ec613',
+        mountId: 'authing-login-form-wrapper',
+        hideUP: true,
+        title: '大事记',
+        hideOAuth: true,
+        hideClose: true,
+        logo: 'https://user-images.githubusercontent.com/31465/34380645-bd67f474-eb0b-11e7-8d03-0151c1730654.png'
+    }
     if (document.querySelector('#authing-login-form-wrapper')) {
-        new AuthingForm({ 
-            clientId: '5bf36ad6a9e256000132cebf',
-            secret: 'c15331f0f3361acd83005871801ec613',
-            mountId: 'authing-login-form-wrapper',
-            hideUP: true,
-            title: '大事记',
-            hideOAuth: false,
-            hideClose: true,
-            logo: 'https://user-images.githubusercontent.com/31465/34380645-bd67f474-eb0b-11e7-8d03-0151c1730654.png'
-        });
+        new AuthingForm(options);
     }
 
     if (document.querySelector('#authing-login-form-wrapper-invite')) {
-        const form = new AuthingForm({
-            clientId: '5bf36ad6a9e256000132cebf',
-            secret: 'c15331f0f3361acd83005871801ec613',
-            mountId: 'authing-login-form-wrapper-invite',
-
-            hideUP: true,
-            hideClose: true,
-            hideOAuth: true,
-
-            qrcodeScanning: {
-                redirect: false
-            },
-
-            title: '大事记',
-            logo: 'https://user-images.githubusercontent.com/31465/34380645-bd67f474-eb0b-11e7-8d03-0151c1730654.png'
-        });
+        options['hideOAuth'] = true;
+        options['qrcodeScanning'] = {
+            redirect: false
+        };
+        options['mountId'] = 'authing-login-form-wrapper-invite';
+        const form = new AuthingForm(options);
 
         form.on('scanning', function (data) {
             console.log('on scanning success', data);
