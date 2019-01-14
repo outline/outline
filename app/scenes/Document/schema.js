1fbcdba080594ba59dfb7e98fcac84cdbb964516
Tom Moor <tom.moor@gmail.com>
2019-01-13 22:04:49 -0800

    Upgrade Slate

--- app/scenes/Document/schema.js
+++ app/scenes/Document/schema.js
@@ -1,32 +1,23 @@
 // @flow
 import { cloneDeep } from 'lodash';
-import { Block, Change, Node, Mark } from 'slate';
+import { Block, SlateError, Editor } from 'slate';
 import { schema as originalSchema } from 'rich-markdown-editor';
 
 const schema = cloneDeep(originalSchema);
 
 // add rules to the schema to ensure the first node is a heading
-schema.document.nodes.unshift({ types: ['heading1'], min: 1, max: 1 });
-schema.document.normalize = (
-  change: Change,
-  reason: string,
-  {
-    node,
-    child,
-    mark,
-    index,
-  }: { node: Node, mark?: Mark, child: Node, index: number }
-) => {
-  switch (reason) {
+// schema.document.nodes.unshift({ match: { type: 'heading1' }, min: 1, max: 1 });
+schema.document.normalize = (editor: Editor, error: SlateError) => {
+  switch (error.code) {
     case 'child_type_invalid': {
-      return change.setNodeByKey(
-        child.key,
-        index === 0 ? 'heading1' : 'paragraph'
+      return editor.setNodeByKey(
+        error.child.key,
+        error.index === 0 ? 'heading1' : 'paragraph'
       );
     }
     case 'child_required': {
-      const block = Block.create(index === 0 ? 'heading1' : 'paragraph');
-      return change.insertNodeByKey(node.key, index, block);
+      const block = Block.create(error.index === 0 ? 'heading1' : 'paragraph');
+      return editor.insertNodeByKey(error.node.key, error.index, block);
     }
     default:
   }
