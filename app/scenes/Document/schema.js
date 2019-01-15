57af7d21ffbd0ec62e826b19f1b92681ef705c61
Tom Moor <tom.moor@gmail.com>
2019-01-14 23:16:53 -0800

    Normalization

--- app/scenes/Document/schema.js
+++ app/scenes/Document/schema.js
@@ -6,18 +6,21 @@ import { schema as originalSchema } from 'rich-markdown-editor';
 const schema = cloneDeep(originalSchema);
 
 // add rules to the schema to ensure the first node is a heading
-// schema.document.nodes.unshift({ match: { type: 'heading1' }, min: 1, max: 1 });
+schema.document.nodes.unshift({ match: { type: 'heading1' }, min: 1, max: 1 });
 schema.document.normalize = (editor: Editor, error: SlateError) => {
   switch (error.code) {
-    case 'child_type_invalid': {
+    case 'child_max_invalid': {
       return editor.setNodeByKey(
         error.child.key,
         error.index === 0 ? 'heading1' : 'paragraph'
       );
     }
-    case 'child_required': {
-      const block = Block.create(error.index === 0 ? 'heading1' : 'paragraph');
-      return editor.insertNodeByKey(error.node.key, error.index, block);
+    case 'child_min_invalid': {
+      const missingTitle = error.index === 0;
+      const block = Block.create(missingTitle ? 'heading1' : 'paragraph');
+      editor.insertNodeByKey(error.node.key, error.index, block);
+      if (missingTitle) setImmediate(() => editor.moveFocusToStartOfDocument());
+      return editor;
     }
     default:
   }
