6f1016bd6a52c837e451d848e48610117a8b3fe4
Tom Moor <tom.moor@gmail.com>
2019-01-19 22:35:11 -0800

    Upgrade dependencies, reduce warnings

--- setupJest.js
+++ setupJest.js
@@ -1,13 +1,4 @@
 /* eslint-disable */
-import * as React from 'react';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import localStorage from './__mocks__/localStorage';
 
-const snap = children => {
-  const wrapper = shallow(children);
-  expect(toJson(wrapper)).toMatchSnapshot();
-};
-
 global.localStorage = localStorage;
-global.snap = snap;
