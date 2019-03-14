3ef507c1374fc58ae0156125a3f7040ff1040ef5
Tom Moor <tom.moor@gmail.com>
2019-03-13 22:59:25 -0700

    Fixes #918 - Add extra at bottom of document

--- app/components/ClickablePadding.js
+++ app/components/ClickablePadding.js
@@ -2,6 +2,7 @@
 import styled from 'styled-components';
 
 const ClickablePadding = styled.div`
+  min-height: 6em;
   cursor: ${({ onClick }) => (onClick ? 'text' : 'default')};
   ${({ grow }) => grow && `flex-grow: 100;`};
 `;
