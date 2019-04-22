a84f89a1c7e6e04b974621e0ad907e2f801ffb28
Lennard Rottjers <Lennard@rottjers.nl>
2019-04-22 12:23:50 +0200

    Added support for custom logout url

--- server/middlewares/ldapReverseProxy.js
+++ server/middlewares/ldapReverseProxy.js
@@ -2,11 +2,14 @@
 import type { Context } from 'koa';
 
 export default function ldapReverseProxy() {
+  const logoutURL = process.env.LDAP_LOGOUT_URL || '';
   return async function ldapReverseProxyMiddleware(
     ctx: Context,
     next: () => Promise<*>
   ) {
-    if (
+    if (logoutURL !== '' && ctx.request.query.done) {
+      ctx.redirect(logoutURL);
+    } else if (
       ctx.headers[process.env.LDAP_REVERSE_PROXY_HEADER.toLocaleLowerCase()]
     ) {
       const user =
