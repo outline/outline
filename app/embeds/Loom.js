d5f6311bbc515b53c7ebdaf5e4ac14505366e457
Tom Moor <tom.moor@gmail.com>
2019-05-07 19:32:03 -0700

    Fixed: Added support for new loom domain name

--- app/embeds/Loom.js
+++ app/embeds/Loom.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import Frame from './components/Frame';
 
-const URL_REGEX = /^https:\/\/(www\.)?useloom.com\/(embed|share)\/(.*)$/;
+const URL_REGEX = /^https:\/\/(www\.)?(use)?loom.com\/(embed|share)\/(.*)$/;
 
 type Props = {
   url: string,
--- app/embeds/Loom.test.js
+++ app/embeds/Loom.test.js
@@ -7,6 +7,9 @@ describe('Loom', () => {
   const match = Loom.ENABLED[0];
   test('to be enabled on share link', () => {
     expect(
+      'https://www.loom.com/share/55327cbb265743f39c2c442c029277e0'.match(match)
+    ).toBeTruthy();
+    expect(
       'https://www.useloom.com/share/55327cbb265743f39c2c442c029277e0'.match(
         match
       )
@@ -14,6 +17,9 @@ describe('Loom', () => {
   });
 
   test('to be enabled on embed link', () => {
+    expect(
+      'https://www.loom.com/embed/55327cbb265743f39c2c442c029277e0'.match(match)
+    ).toBeTruthy();
     expect(
       'https://www.useloom.com/embed/55327cbb265743f39c2c442c029277e0'.match(
         match
