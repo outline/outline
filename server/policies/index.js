970721739edc2df5cc5d5c461693753db1f5d67e
Tom Moor <tom.moor@gmail.com>
2019-08-10 19:10:03 -0700

    add policy serialize method

--- server/policies/index.js
+++ server/policies/index.js
@@ -1,4 +1,5 @@
 // @flow
+import { Team, User, Collection, Document } from '../models';
 import policy from './policy';
 import './apiKey';
 import './collection';
@@ -8,5 +9,25 @@ import './notificationSetting';
 import './share';
 import './user';
 import './team';
+
+const { can, abilities } = policy;
+
+export function serialize(model: User, target: Team | Collection | Document) {
+  let output = {};
+
+  abilities.forEach(ability => {
+    if (model instanceof ability.model && target instanceof ability.target) {
+      let response = true;
+      try {
+        response = can(model, ability.action, target);
+      } catch (err) {
+        response = false;
+      }
+      output[ability.action] = response;
+    }
+  });
+
+  return output;
+}
 
 export default policy;
