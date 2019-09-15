4ea4bd41cdfd0926dd6b9df208c9f812a4e26dcd
Sherriff Agboola <rylxes@gmail.com>
2019-09-15 23:28:11 +0100

    changed lastActiveAt boolean to string

--- app/models/User.js
+++ app/models/User.js
@@ -7,7 +7,7 @@ class User extends BaseModel {
   name: string;
   email: string;
   isAdmin: boolean;
-  lastActiveAt: boolean;
+  lastActiveAt: string;
   isSuspended: boolean;
   createdAt: string;
 }
