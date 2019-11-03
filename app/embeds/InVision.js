14f6e6abadea9af0e3b3c5e8b66132ece64b07f2
Tom Moor <tom.moor@gmail.com>
2019-11-03 15:32:43 -0800

    feat: Add support for InVision live images closes #946

--- app/embeds/InVision.js
+++ app/embeds/InVision.js
@@ -1,9 +1,14 @@
 // @flow
 import * as React from 'react';
+import ImageZoom from 'react-medium-image-zoom';
 import Frame from './components/Frame';
 
-const URL_REGEX = new RegExp(
+const IFRAME_REGEX = new RegExp(
   '^https://(invis.io/.*)|(projects.invisionapp.com/share/.*)$'
+);
+
+const IMAGE_REGEX = new RegExp(
+  '^https://(opal.invisionapp.com/static-signed/live-embed/.*)$'
 );
 
 type Props = {
@@ -11,9 +16,24 @@ type Props = {
 };
 
 export default class InVision extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
+  static ENABLED = [IFRAME_REGEX, IMAGE_REGEX];
 
   render() {
+    if (IMAGE_REGEX.test(this.props.url)) {
+      return (
+        <ImageZoom
+          image={{
+            src: this.props.url,
+            alt: 'InVision Embed',
+            style: {
+              maxWidth: '100%',
+              maxHeight: '75vh',
+            },
+          }}
+          shouldRespectMaxDimension
+        />
+      );
+    }
     return <Frame src={this.props.url} title="InVision Embed" />;
   }
 }
