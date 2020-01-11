64563a1c21976aa779b0a6d8960929bc8eab87cd
Tom Moor <tom.moor@gmail.com>
2020-01-11 15:25:43 -0800

    Use progressive rendering on PaginatedList component Move drafts and starred views to paginated

--- app/scenes/Starred.js
+++ app/scenes/Starred.js
@@ -3,11 +3,10 @@ import * as React from 'react';
 import { observer, inject } from 'mobx-react';
 
 import CenteredContent from 'components/CenteredContent';
-import { ListPlaceholder } from 'components/LoadingPlaceholder';
 import Empty from 'components/Empty';
 import PageTitle from 'components/PageTitle';
 import Heading from 'components/Heading';
-import DocumentList from 'components/DocumentList';
+import PaginatedDocumentList from 'components/PaginatedDocumentList';
 import InputSearch from 'components/InputSearch';
 import Tabs from 'components/Tabs';
 import Tab from 'components/Tab';
@@ -22,46 +21,31 @@ type Props = {
 
 @observer
 class Starred extends React.Component<Props> {
-  componentDidMount() {
-    this.props.documents.fetchStarred();
-  }
-
   render() {
-    const {
-      isLoaded,
-      isFetching,
-      starred,
-      starredAlphabetical,
-    } = this.props.documents;
+    const { fetchStarred, starred, starredAlphabetical } = this.props.documents;
     const { sort } = this.props.match.params;
-    const showLoading = !isLoaded && isFetching;
-    const showEmpty = isLoaded && !starred.length;
 
     return (
       <CenteredContent column auto>
         <PageTitle title="Starred" />
         <Heading>Starred</Heading>
-        {showEmpty ? (
-          <Empty>You’ve not starred any documents yet.</Empty>
-        ) : (
-          <React.Fragment>
-            <Tabs>
-              <Tab to="/starred" exact>
-                Recently Updated
-              </Tab>
-              <Tab to="/starred/alphabetical" exact>
-                Alphabetical
-              </Tab>
-            </Tabs>
-            <DocumentList
-              documents={
-                sort === 'alphabetical' ? starredAlphabetical : starred
-              }
-              showCollection
-            />
-          </React.Fragment>
-        )}
-        {showLoading && <ListPlaceholder />}
+        <React.Fragment>
+          <Tabs>
+            <Tab to="/starred" exact>
+              Recently Updated
+            </Tab>
+            <Tab to="/starred/alphabetical" exact>
+              Alphabetical
+            </Tab>
+          </Tabs>
+          <PaginatedDocumentList
+            empty={<Empty>You’ve not starred any documents yet.</Empty>}
+            fetch={fetchStarred}
+            documents={sort === 'alphabetical' ? starredAlphabetical : starred}
+            showCollection
+          />
+        </React.Fragment>
+
         <Actions align="center" justify="flex-end">
           <Action>
             <InputSearch />
