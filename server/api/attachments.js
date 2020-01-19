9a1f52fb5b5825f6a3a9e4d56df31de1e7ac62d3
Huss <huss@9fin.com>
2020-01-19 21:27:16 +0000

    basic checking of document read permission to allow attachment viewing

--- server/api/attachments.js
+++ server/api/attachments.js
@@ -1,15 +1,25 @@
 // @flow
 import Router from 'koa-router';
 import auth from '../middlewares/authentication';
-import { Attachment } from '../models';
+import { Attachment, Document } from '../models';
 import { getSignedImageUrl } from '../utils/s3';
 
+import policy from '../policies';
+
+const { authorize } = policy;
 const router = new Router();
 
 router.post('attachments.redirect', auth(), async ctx => {
   const { id } = ctx.body;
   ctx.assertPresent(id, 'id is required');
+
   const attachment = await Attachment.findByPk(id);
+  const user = ctx.state.user;
+  const document = await Document.findByPk(attachment.documentId, {
+    userId: user.id,
+  });
+  authorize(user, 'read', document);
+
   const accessUrl = attachment.isPrivate
     ? await getSignedImageUrl(attachment.key)
     : attachment.url;
