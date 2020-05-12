6f7e141334cf8762541032602ef8b21830edcf06
Tom Moor <tom.moor@gmail.com>
2020-05-11 22:40:09 -0700

    fix: Editor update auto-reload Bump RME

--- server/api/middlewares/editor.js
+++ server/api/middlewares/editor.js
@@ -1,17 +1,12 @@
 // @flow
 import { type Context } from 'koa';
 import pkg from 'rich-markdown-editor/package.json';
+import { EditorUpdateError } from '../../errors';
 
 export default function editor() {
   return async function editorMiddleware(ctx: Context, next: () => Promise<*>) {
     if (ctx.headers['x-editor-version'] !== pkg.version) {
-      ctx.body = {
-        error: 'editor_update_required',
-        message: 'The client editor is out of date and must be reloaded',
-        status: 400,
-        ok: false,
-      };
-      return;
+      throw new EditorUpdateError();
     }
     return next();
   };
