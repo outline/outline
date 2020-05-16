1a1e9be154a19e9dd21e901ca1c25afda47f3f9c
Tom Moor <tom.moor@gmail.com>
2020-05-15 20:04:35 -0700

    Bump RME

--- app/components/Editor/Editor.js
+++ app/components/Editor/Editor.js
@@ -113,15 +113,6 @@ const StyledEditor = styled(RichMarkdownEditor)`
       }
     }
   }
-
-  h1:first-child,
-  h2:first-child,
-  h3:first-child,
-  h4:first-child,
-  h5:first-child,
-  h6:first-child {
-    margin-top: 0;
-  }
 `;
 
 const EditorTooltip = ({ children, ...props }) => (
