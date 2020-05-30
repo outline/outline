a60ca46949fa0d1289d046806c7cf9ec29a4f8ef
Tom Moor <tom.moor@gmail.com>
2020-05-30 11:15:46 -0700

    feat: Invert collection icon color when dark in dark mode

--- app/components/CollectionIcon.js
+++ app/components/CollectionIcon.js
@@ -1,5 +1,7 @@
 // @flow
 import * as React from 'react';
+import { inject, observer } from 'mobx-react';
+import { invert, getLuminance } from 'polished';
 import {
   PrivateCollectionIcon,
   CollectionIcon,
@@ -15,6 +17,7 @@ import {
   SunIcon,
 } from 'outline-icons';
 import Collection from 'models/Collection';
+import UiStore from 'stores/UiStore';
 
 const icons = {
   collection: CollectionIcon,
@@ -34,27 +37,29 @@ type Props = {
   collection: Collection,
   expanded?: boolean,
   size?: number,
+  ui: UiStore,
 };
 
-export default function ResolvedCollectionIcon({
-  collection,
-  expanded,
-  size,
-}: Props) {
+function ResolvedCollectionIcon({ collection, expanded, size, ui }: Props) {
+  // If the chosen icon color is very dark then we invert it in dark mode
+  // otherwise it will be impossible to see against the dark background.
+  const color =
+    ui.resolvedTheme === 'dark'
+      ? getLuminance(collection.color) > 0.1
+        ? collection.color
+        : invert(collection.color)
+      : collection.color;
+
   if (collection.icon) {
     const Component = icons[collection.icon];
-    return <Component color={collection.color} size={size} />;
+    return <Component color={color} size={size} />;
   } else if (collection.private) {
     return (
-      <PrivateCollectionIcon
-        color={collection.color}
-        expanded={expanded}
-        size={size}
-      />
+      <PrivateCollectionIcon color={color} expanded={expanded} size={size} />
     );
   }
 
-  return (
-    <CollectionIcon color={collection.color} expanded={expanded} size={size} />
-  );
+  return <CollectionIcon color={color} expanded={expanded} size={size} />;
 }
+
+export default inject('ui')(observer(ResolvedCollectionIcon));
