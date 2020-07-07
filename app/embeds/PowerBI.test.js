d785825e5a103d0bf1de3fbf8d128ef4a38eb848
Matheus Breguêz <matheus.breguez@rockcontent.com>
2020-07-07 09:15:54 -0300

    Add Tests

---
+++ app/embeds/PowerBI.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+import PowerBI from "./PowerBI";
+
+describe("PowerBI", () => {
+  const match = PowerBI.ENABLED[0];
+  test("to be enabled on dashboard", () => {
+    expect(
+      "https://app.powerbi.com/view?r=eyJrIjoiMjcxNDIyNjAtOGM0Yi00ZWJhLWJkNmEtNjFiOTI0MWVlYjNiIiwidCI6IjI1NmNiNTA1LTAzOWYtNGZiMi04NWE2LWEzZTgzMzI4NTU3OCIsImMiOjh9"
+      .match(match)
+    ).toBeTruthy();
+  });
+
+  test("to not be enabled elsewhere", () => {
+    expect("https://powerbi.com".match(match)).toBe(null);
+    expect("https://app.powerbi.com".match(match)).toBe(null);
+  });
+});
