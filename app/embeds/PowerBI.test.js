52f2395d9a935d7bba2b3f7ba445e51abf0ca762
Matheus Breguêz <matheus.breguez@rockcontent.com>
2020-07-07 09:18:37 -0300

    Fix Lint

--- app/embeds/PowerBI.test.js
+++ app/embeds/PowerBI.test.js
@@ -5,8 +5,7 @@ describe("PowerBI", () => {
   const match = PowerBI.ENABLED[0];
   test("to be enabled on dashboard", () => {
     expect(
-      "https://app.powerbi.com/view?r=eyJrIjoiMjcxNDIyNjAtOGM0Yi00ZWJhLWJkNmEtNjFiOTI0MWVlYjNiIiwidCI6IjI1NmNiNTA1LTAzOWYtNGZiMi04NWE2LWEzZTgzMzI4NTU3OCIsImMiOjh9"
-      .match(match)
+      "https://app.powerbi.com/view?r=eyJrIjoiMjcxNDIyNjAtOGM0Yi00ZWJhLWJkNmEtNjFiOTI0MWVlYjNiIiwidCI6IjI1NmNiNTA1LTAzOWYtNGZiMi04NWE2LWEzZTgzMzI4NTU3OCIsImMiOjh9".match(match)
     ).toBeTruthy();
   });
 
