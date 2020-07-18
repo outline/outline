c05e42487f6ade57f104d43b70c8d99c102ffb8f
Tom Moor <tom.moor@gmail.com>
2020-07-18 13:24:58 -0700

    chore: Remove links and mentions to Spectrum community

--- shared/utils/routeHelpers.js
+++ shared/utils/routeHelpers.js
@@ -34,20 +34,8 @@ export function githubIssuesUrl(): string {
   return "https://www.github.com/outline/outline/issues";
 }
 
-export function slackAppStoreUrl(): string {
-  return "https://goabstract.slack.com/apps/A0W3UMKBQ-outline";
-}
-
-export function blogUrl(): string {
-  return "https://medium.com/getoutline";
-}
-
 export function twitterUrl(): string {
   return "https://twitter.com/outlinewiki";
-}
-
-export function spectrumUrl(): string {
-  return "https://spectrum.chat/outline";
 }
 
 export function mailToUrl(): string {
