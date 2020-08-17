9fea90b9e1231378cdd76d00237ef8eb0a359c17
Tom Moor <tom.moor@gmail.com>
2020-08-16 23:54:53 -0700

    fix: Improve tabs and overflow on mobile

--- app/components/Tabs.js
+++ app/components/Tabs.js
@@ -6,6 +6,8 @@ const Tabs = styled.nav`
   border-bottom: 1px solid ${(props) => props.theme.divider};
   margin-top: 22px;
   margin-bottom: 12px;
+  overflow-y: auto;
+  white-space: nowrap;
 `;
 
 export const Separator = styled.span`
