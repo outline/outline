a5a219ea06a5f879e2be4b7b56ad31e512ae343e
Matheus Breguêz <matheus.breguez@rockcontent.com>
2020-08-20 13:55:41 -0300

    Fix RegExp

--- app/embeds/ClickUp.js
+++ app/embeds/ClickUp.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import Frame from "./components/Frame";
 
 const URL_REGEX = new RegExp(
-  "^https://share.clickup.com/b/h/(.*)/(.*)$"
+  "^https://share.clickup.com/[a-z]/[a-z]/(.*)/(.*)$"
 );
 
 type Props = {|
