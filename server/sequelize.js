c446a91f7db228243664115e9e6638eaee7c0e1e
Tom Moor <tom.moor@gmail.com>
2020-08-22 08:27:42 -0700

    fix: Restore Postgres SSL support on Heroku https://github.com/brianc/node-postgres/issues/2009

--- server/sequelize.js
+++ server/sequelize.js
@@ -3,6 +3,8 @@ import debug from "debug";
 import Sequelize from "sequelize";
 import EncryptedField from "sequelize-encrypted";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const encryptedFields = () =>
   EncryptedField(Sequelize, process.env.SECRET_KEY);
 
@@ -10,7 +12,14 @@ export const DataTypes = Sequelize;
 export const Op = Sequelize.Op;
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  // logging: console.log,
   logging: debug("sql"),
   typeValidation: true,
+  dialectOptions: {
+    ssl: isProduction
+      ? {
+          // Ref.: https://github.com/brianc/node-postgres/issues/2009
+          rejectUnauthorized: false,
+        }
+      : false,
+  },
 });
