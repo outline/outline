2f590e2bab98d1a6d242bf6af22609c18b30cd4d
Tom Moor <tom.moor@gmail.com>
2020-11-02 21:39:27 -0800

    Add domain to database Add redirects to team domain when present

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,7 +1,11 @@
 // @flow
 import Router from "koa-router";
 import { reject } from "lodash";
-import { parseDomain, isCustomSubdomain } from "../../shared/utils/domains";
+import {
+  parseDomain,
+  isCustomDomain,
+  isCustomSubdomain,
+} from "../../shared/utils/domains";
 import { signin } from "../../shared/utils/routeHelpers";
 import auth from "../middlewares/authentication";
 import { Team } from "../models";
@@ -68,11 +72,29 @@ router.post("auth.config", async (ctx) => {
     }
   }
 
+  if (isCustomDomain(ctx.request.hostname)) {
+    const team = await Team.findOne({
+      where: { domain: ctx.request.hostname },
+    });
+
+    if (team) {
+      ctx.body = {
+        data: {
+          name: team.name,
+          hostname: ctx.request.hostname,
+          services: filterServices(team),
+        },
+      };
+      return;
+    }
+  }
+
   // If subdomain signin page then we return minimal team details to allow
   // for a custom screen showing only relevant signin options for that team.
   if (
     process.env.SUBDOMAINS_ENABLED === "true" &&
-    isCustomSubdomain(ctx.request.hostname)
+    isCustomSubdomain(ctx.request.hostname) &&
+    !isCustomDomain(ctx.request.hostname)
   ) {
     const domain = parseDomain(ctx.request.hostname);
     const subdomain = domain ? domain.subdomain : undefined;
