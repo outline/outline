491cbb04474bfa7f5c309dc57bdc6fd4bee18e66
Ruben Moya <ruben@acidtango.com>
2021-01-11 20:55:32 +0100

    refactor: use env variable instead of en_US

--- server/presenters/user.js
+++ server/presenters/user.js
@@ -24,7 +24,7 @@ export default (user: User, options: Options = {}): ?UserPresentation => {
   userData.isAdmin = user.isAdmin;
   userData.isSuspended = user.isSuspended;
   userData.avatarUrl = user.avatarUrl;
-  userData.language = user.language || "en_US";
+  userData.language = user.language || process.env.DEFAULT_LANGUAGE || "en_US";
 
   if (options.includeDetails) {
     userData.email = user.email;
