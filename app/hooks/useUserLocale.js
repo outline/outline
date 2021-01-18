27fca284509878475e9b3a5fffde289d05b58a71
Tom Moor <tom.moor@gmail.com>
2021-01-17 22:19:54 -0800

    fix: Account for rehydrated old users before language closes #1819

--- app/hooks/useUserLocale.js
+++ app/hooks/useUserLocale.js
@@ -4,7 +4,7 @@ import useStores from "./useStores";
 export default function useUserLocale() {
   const { auth } = useStores();
 
-  if (!auth.user) {
+  if (!auth.user || !auth.user.language) {
     return undefined;
   }
 
