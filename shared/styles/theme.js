24ecaa8ce40ceac961a4f52e7fe0dab54bad8e3d
Tom Moor <tom.moor@gmail.com>
2021-01-20 23:07:48 -0800

    chore: Reduce default menu width

--- shared/styles/theme.js
+++ shared/styles/theme.js
@@ -47,7 +47,7 @@ const spacing = {
   padding: "1.5vw 1.875vw",
   vpadding: "1.5vw",
   hpadding: "1.875vw",
-  sidebarWidth: 280,
+  sidebarWidth: 260,
   sidebarCollapsedWidth: 16,
   sidebarMinWidth: 200,
   sidebarMaxWidth: 400,
