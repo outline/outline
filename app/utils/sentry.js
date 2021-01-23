6a0f8abcc82ded5e008a19ff89dd5d3bdcceb31f
Tom Moor <tom.moor@gmail.com>
2021-01-22 20:21:49 -0800

    Hook up performance monitoring

---
+++ app/utils/sentry.js
@@ -0,0 +1,29 @@
+// @flow
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import { type RouterHistory } from "react-router-dom";
+import env from "env";
+
+export function initSentry(history: RouterHistory) {
+  Sentry.init({
+    dsn: env.SENTRY_DSN,
+    integrations: [
+      new Integrations.BrowserTracing({
+        // Can also use reactRouterV4Instrumentation
+        routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+      }),
+    ],
+    tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1,
+    ignoreErrors: [
+      "ResizeObserver loop limit exceeded",
+      "AuthorizationError",
+      "BadRequestError",
+      "NetworkError",
+      "NotFoundError",
+      "OfflineError",
+      "ServiceUnavailableError",
+      "UpdateRequiredError",
+      "ChunkLoadError",
+    ],
+  });
+}
