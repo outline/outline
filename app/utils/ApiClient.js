ec5a1a1b7203bbe179e8d707f6728c44a528b433
Yaroslav Zhavoronkov <yaroslav.zh@gmail.com>
2021-02-06 20:35:43 +0300

    fix: Pass credentials with ApiClient requests if behind Cloudflare Access

--- app/utils/ApiClient.js
+++ app/utils/ApiClient.js
@@ -3,6 +3,7 @@ import invariant from "invariant";
 import { map, trim } from "lodash";
 import stores from "stores";
 import download from "./download";
+import { getCookie } from "tiny-cookie";
 import {
   AuthorizationError,
   BadRequestError,
@@ -17,6 +18,11 @@ import {
 type Options = {
   baseUrl?: string,
 };
+
+// authorization cookie set by a Cloudflare Access proxy
+const CF_AUTHORIZATION = getCookie("CF_Authorization");
+// if the cookie is set, we must pass it with all ApiClient requests
+const CREDENTIALS = CF_AUTHORIZATION ? "same-origin" : "omit";
 
 class ApiClient {
   baseUrl: string;
@@ -91,7 +97,7 @@ class ApiClient {
         body,
         headers,
         redirect: "follow",
-        credentials: "omit",
+        credentials: CREDENTIALS,
         cache: "no-cache",
       });
     } catch (err) {
