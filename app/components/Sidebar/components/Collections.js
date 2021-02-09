2eb1aa1475f1e38bcf050bd0bfb52d7c3a0a50ee
Saumya Pandey <sp160899@gmail.com>
2021-02-10 02:08:54 +0530

    Covert Collections.js to a functional component

--- app/components/Sidebar/components/Collections.js
+++ app/components/Sidebar/components/Collections.js
@@ -1,82 +1,66 @@
 // @flow
-import { observer, inject } from "mobx-react";
+import { observer } from "mobx-react";
 import { PlusIcon } from "outline-icons";
 import * as React from "react";
-import { withTranslation, type TFunction } from "react-i18next";
-import CollectionsStore from "stores/CollectionsStore";
-import DocumentsStore from "stores/DocumentsStore";
-import PoliciesStore from "stores/PoliciesStore";
-import UiStore from "stores/UiStore";
+import { useTranslation } from "react-i18next";
 import Fade from "components/Fade";
 import Flex from "components/Flex";
+import useStores from "../../../hooks/useStores";
 import CollectionLink from "./CollectionLink";
 import CollectionsLoading from "./CollectionsLoading";
 import Header from "./Header";
 import SidebarLink from "./SidebarLink";
 
 type Props = {
-  policies: PoliciesStore,
-  collections: CollectionsStore,
-  documents: DocumentsStore,
   onCreateCollection: () => void,
-  ui: UiStore,
-  t: TFunction,
 };
 
-@observer
-class Collections extends React.Component<Props> {
-  isPreloaded: boolean = !!this.props.collections.orderedData.length;
-
-  componentDidMount() {
-    const { collections } = this.props;
-
+function Collections({ onCreateCollection }: Props) {
+  const { ui, policies, documents, collections } = useStores();
+  const isPreloaded: boolean = !!collections.orderedData.length;
+  const { t } = useTranslation();
+  React.useEffect(() => {
     if (!collections.isFetching && !collections.isLoaded) {
       collections.fetchPage({ limit: 100 });
     }
-  }
+  });
 
-  render() {
-    const { collections, ui, policies, documents, t } = this.props;
-
-    const content = (
-      <>
-        {collections.orderedData.map((collection) => (
-          <CollectionLink
-            key={collection.id}
-            collection={collection}
-            activeDocument={documents.active}
-            prefetchDocument={documents.prefetchDocument}
-            canUpdate={policies.abilities(collection.id).update}
-            ui={ui}
-          />
-        ))}
-        <SidebarLink
-          to="/collections"
-          onClick={this.props.onCreateCollection}
-          icon={<PlusIcon color="currentColor" />}
-          label={`${t("New collection")}…`}
-          exact
+  const content = (
+    <>
+      {collections.orderedData.map((collection) => (
+        <CollectionLink
+          key={collection.id}
+          collection={collection}
+          activeDocument={documents.active}
+          prefetchDocument={documents.prefetchDocument}
+          canUpdate={policies.abilities(collection.id).update}
+          ui={ui}
         />
-      </>
-    );
+      ))}
+      <SidebarLink
+        to="/collections"
+        onClick={onCreateCollection}
+        icon={<PlusIcon color="currentColor" />}
+        label={`${t("New collection")}…`}
+        exact
+      />
+    </>
+  );
 
-    return (
-      <Flex column>
-        <Header>{t("Collections")}</Header>
-        {collections.isLoaded ? (
-          this.isPreloaded ? (
-            content
-          ) : (
-            <Fade>{content}</Fade>
-          )
+  return (
+    <Flex column>
+      <Header>{t("Collections")}</Header>
+      {collections.isLoaded ? (
+        isPreloaded ? (
+          content
         ) : (
-          <CollectionsLoading />
-        )}
-      </Flex>
-    );
-  }
+          <Fade>{content}</Fade>
+        )
+      ) : (
+        <CollectionsLoading />
+      )}
+    </Flex>
+  );
 }
 
-export default withTranslation()<Collections>(
-  inject("collections", "ui", "documents", "policies")(Collections)
-);
+export default observer(Collections);
