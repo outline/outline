86d9a14c5c6be7a5d967667a6ca8e9460c37686c
Tom Moor <tom.moor@gmail.com>
2021-02-16 23:51:29 -0800

    fix: is virtual host

--- server/utils/s3.js
+++ server/utils/s3.js
@@ -17,7 +17,11 @@ const s3 = new AWS.S3({
   accessKeyId: AWS_ACCESS_KEY_ID,
   secretAccessKey: AWS_SECRET_ACCESS_KEY,
   region: AWS_REGION,
-  endpoint: new AWS.Endpoint(process.env.AWS_S3_UPLOAD_BUCKET_URL),
+  endpoint: process.env.AWS_S3_UPLOAD_BUCKET_URL.includes(
+    AWS_S3_UPLOAD_BUCKET_NAME
+  )
+    ? undefined
+    : new AWS.Endpoint(process.env.AWS_S3_UPLOAD_BUCKET_URL),
   signatureVersion: "v4",
 });
 
