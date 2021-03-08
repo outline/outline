b5921be007357c58844fa41a79265ab74790bd1c
Saumya Pandey <sp160899@gmail.com>
2021-03-09 00:30:08 +0530

    Assign index in the natural order

--- server/utils/collectionIndexing.js
+++ server/utils/collectionIndexing.js
@@ -1,43 +1,22 @@
 // @flow
 import fractionalIndex from "fractional-index";
+import naturalSort from "../../shared/utils/naturalSort";
 import { Collection } from "../models";
 
 export default async function collectionIndexing(teamId: string) {
   const collections = await Collection.findAll({
     where: { teamId, deletedAt: null }, //no point in maintaining index of deleted collections.
-    attributes: ["id", "index"],
+    attributes: ["id", "index", "name"],
   });
 
   let sortableCollections = collections.map((collection) => {
     return [collection, collection.index];
   });
 
-  // a set to store the index value of collections.
-  const indexSet = new Set();
-
-  // make the index null, if there is index collision
-  sortableCollections = sortableCollections.map((collection) => {
-    if (collection[1] === null) {
-      return collection;
-    } else if (indexSet.has(collection[1])) {
-      collection[1] = null;
-      return collection;
-    }
-    indexSet.add(collection[1]);
-    return collection;
-  });
-
-  //sort the collection in ASC and such that null values are the end
-  sortableCollections.sort((a, b) => {
-    if (a[1] === b[1]) {
-      return 0;
-    } else if (a[1] === null) {
-      return 1;
-    } else if (b[1] === null) {
-      return -1;
-    }
-    return a[1] < b[1] ? -1 : 1;
-  });
+  sortableCollections = naturalSort(
+    sortableCollections,
+    (collection) => collection[0].name
+  );
 
   //for each collection with null index, use previous collection index to create new index
   let previousCollectionIndex = null;
