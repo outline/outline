4228f6010cc9723abd6ac5f510548de662a96244
Saumya Pandey <sp160899@gmail.com>
2021-03-09 15:26:05 +0530

    Update app/components/Sidebar/components/Collections.js
    Co-authored-by: Tom Moor <tom.moor@gmail.com>

--- app/components/Sidebar/components/Collections.js
+++ app/components/Sidebar/components/Collections.js
@@ -37,7 +37,7 @@ const Collections = ({ onCreateCollection }: Props) => {
     if (!can.update) return;
 
     history.push(newDocumentUrl(activeCollectionId));
-  });
+  }, [ui.activeCollectionId]);
 
   const content = (
     <>
