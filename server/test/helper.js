1393d1950ebcabfe8685a794a0224f728ec0e673
Tom Moor <tom.moor@gmail.com>
2021-03-10 12:04:42 -0800

    chore: Test performance and warnings (#1946)
    * test: Do not request mailer account in test environment
    
    * test: Dupe migrations

--- server/test/helper.js
+++ server/test/helper.js
@@ -5,27 +5,5 @@ require("dotenv").config({ silent: true });
 process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
 process.env.NODE_ENV = "test";
 
-const Sequelize = require("sequelize");
-const Umzug = require("umzug");
-const sequelize = require("../sequelize").sequelize;
-
-const queryInterface = sequelize.getQueryInterface();
-
-function runMigrations() {
-  const umzug = new Umzug({
-    storage: "sequelize",
-    storageOptions: {
-      sequelize,
-    },
-    migrations: {
-      params: [queryInterface, Sequelize],
-      path: "./server/migrations",
-    },
-  });
-  return umzug.up();
-}
-
-runMigrations();
-
 // This is needed for the relative manual mock to be picked up
 jest.mock("../events");
