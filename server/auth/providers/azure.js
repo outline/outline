edf822599d9483340a5e5e5b736b9d005a3763a5
Tom Moor <tom.moor@gmail.com>
2021-03-28 23:04:15 -0700

    test

--- server/auth/providers/azure.js
+++ server/auth/providers/azure.js
@@ -34,98 +34,100 @@ export const config = {
   enabled: !!AZURE_CLIENT_ID,
 };
 
-const strategy = new AzureStrategy(
-  {
-    clientID: AZURE_CLIENT_ID,
-    clientSecret: AZURE_CLIENT_SECRET,
-    callbackURL: `${env.URL}/auth/azure.callback`,
-    useCommonEndpoint: true,
-    passReqToCallback: true,
-    resource: AZURE_RESOURCE_APP_ID,
-    store: new StateStore(),
-    scope: scopes,
-  },
-  async function (req, accessToken, refreshToken, params, _, done) {
-    try {
-      // see docs for what the fields in profile represent here:
-      // https://docs.microsoft.com/en-us/azure/active-directory/develop/access-tokens
-      const profile = jwt.decode(params.id_token);
+if (AZURE_CLIENT_ID) {
+  const strategy = new AzureStrategy(
+    {
+      clientID: AZURE_CLIENT_ID,
+      clientSecret: AZURE_CLIENT_SECRET,
+      callbackURL: `${env.URL}/auth/azure.callback`,
+      useCommonEndpoint: true,
+      passReqToCallback: true,
+      resource: AZURE_RESOURCE_APP_ID,
+      store: new StateStore(),
+      scope: scopes,
+    },
+    async function (req, accessToken, refreshToken, params, _, done) {
+      try {
+        // see docs for what the fields in profile represent here:
+        // https://docs.microsoft.com/en-us/azure/active-directory/develop/access-tokens
+        const profile = jwt.decode(params.id_token);
 
-      // Load the users profile from the Microsoft Graph API
-      // https://docs.microsoft.com/en-us/graph/api/resources/users?view=graph-rest-1.0
-      const profileResponse = await request(
-        `https://graph.microsoft.com/v1.0/me`,
-        accessToken
-      );
-      if (!profileResponse) {
-        throw new MicrosoftGraphError(
-          "Unable to load user profile from Microsoft Graph API"
+        // Load the users profile from the Microsoft Graph API
+        // https://docs.microsoft.com/en-us/graph/api/resources/users?view=graph-rest-1.0
+        const profileResponse = await request(
+          `https://graph.microsoft.com/v1.0/me`,
+          accessToken
         );
-      }
+        if (!profileResponse) {
+          throw new MicrosoftGraphError(
+            "Unable to load user profile from Microsoft Graph API"
+          );
+        }
 
-      // Load the organization profile from the Microsoft Graph API
-      // https://docs.microsoft.com/en-us/graph/api/organization-get?view=graph-rest-1.0
-      const organizationResponse = await request(
-        `https://graph.microsoft.com/v1.0/organization`,
-        accessToken
-      );
-      if (!organizationResponse) {
-        throw new MicrosoftGraphError(
-          "Unable to load organization info from Microsoft Graph API"
+        // Load the organization profile from the Microsoft Graph API
+        // https://docs.microsoft.com/en-us/graph/api/organization-get?view=graph-rest-1.0
+        const organizationResponse = await request(
+          `https://graph.microsoft.com/v1.0/organization`,
+          accessToken
         );
+        if (!organizationResponse) {
+          throw new MicrosoftGraphError(
+            "Unable to load organization info from Microsoft Graph API"
+          );
+        }
+
+        const organization = organizationResponse.value[0];
+        const email = profile.email || profileResponse.mail;
+        if (!email) {
+          throw new MicrosoftGraphError(
+            "'email' property is required but could not be found in user profile."
+          );
+        }
+
+        const domain = email.split("@")[1];
+        const subdomain = domain.split(".")[0];
+        const teamName = organization.displayName;
+
+        const result = await accountProvisioner({
+          ip: req.ip,
+          team: {
+            name: teamName,
+            domain,
+            subdomain,
+          },
+          user: {
+            name: profile.name,
+            email,
+            avatarUrl: profile.picture,
+          },
+          authenticationProvider: {
+            name: providerName,
+            providerId: profile.tid,
+          },
+          authentication: {
+            providerId: profile.oid,
+            accessToken,
+            refreshToken,
+            scopes,
+          },
+        });
+        return done(null, result.user, result);
+      } catch (err) {
+        return done(err, null);
       }
-
-      const organization = organizationResponse.value[0];
-      const email = profile.email || profileResponse.mail;
-      if (!email) {
-        throw new MicrosoftGraphError(
-          "'email' property is required but could not be found in user profile."
-        );
-      }
-
-      const domain = email.split("@")[1];
-      const subdomain = domain.split(".")[0];
-      const teamName = organization.displayName;
-
-      const result = await accountProvisioner({
-        ip: req.ip,
-        team: {
-          name: teamName,
-          domain,
-          subdomain,
-        },
-        user: {
-          name: profile.name,
-          email,
-          avatarUrl: profile.picture,
-        },
-        authenticationProvider: {
-          name: providerName,
-          providerId: profile.tid,
-        },
-        authentication: {
-          providerId: profile.oid,
-          accessToken,
-          refreshToken,
-          scopes,
-        },
-      });
-      return done(null, result.user, result);
-    } catch (err) {
-      return done(err, null);
     }
-  }
-);
+  );
 
-strategy.name = providerName;
-passport.use(strategy);
+  strategy.name = providerName;
+  passport.use(strategy);
 
-router.get("azure", passport.authenticate(providerName));
+  router.get("azure", passport.authenticate(providerName));
 
-router.get(
-  "azure.callback",
-  auth({ required: false }),
-  passportMiddleware(providerName)
-);
+  router.get(
+    "azure.callback",
+    auth({ required: false }),
+    passportMiddleware(providerName)
+  );
+}
 
 export default router;
