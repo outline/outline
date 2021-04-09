326518873e36f0ff8367fb548d0c2d2a6e5e5746
Tom Moor <tom.moor@gmail.com>
2021-04-08 21:04:34 -0700

    fix: Logout suspended users immediately

--- app/utils/ApiClient.js
+++ app/utils/ApiClient.js
@@ -154,6 +154,10 @@ class ApiClient {
     }
 
     if (response.status === 403) {
+      if (error.error === "user_suspended") {
+        stores.auth.logout();
+        return;
+      }
       throw new AuthorizationError(error.message);
     }
 
