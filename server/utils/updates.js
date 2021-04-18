09a93f0dac8c53fd60af85688f854cc15b3fcedf
Tom Moor <tom.moor@gmail.com>
2021-04-17 20:18:50 -0700

    lint

--- server/utils/updates.js
+++ server/utils/updates.js
@@ -1,7 +1,7 @@
 // @flow
 import crypto from "crypto";
-import invariant from "invariant";
 import fetch from "fetch-with-proxy";
+import invariant from "invariant";
 import packageInfo from "../../package.json";
 
 import { User, Team, Collection, Document } from "../models";
