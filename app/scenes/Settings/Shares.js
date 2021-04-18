479cf493f04307ef0b97f98d359a07dc7c53a1f0
Tom Moor <tom.moor@gmail.com>
2021-04-18 00:17:39 -0700

    translations, admin pagination

--- app/scenes/Settings/Shares.js
+++ app/scenes/Settings/Shares.js
@@ -1,12 +1,13 @@
 // @flow
 import { observer } from "mobx-react";
 import * as React from "react";
+import { useTranslation, Trans } from "react-i18next";
 import { Link } from "react-router-dom";
 import CenteredContent from "components/CenteredContent";
 import Empty from "components/Empty";
 import HelpText from "components/HelpText";
-import List from "components/List";
 import PageTitle from "components/PageTitle";
+import PaginatedList from "components/PaginatedList";
 import Subheading from "components/Subheading";
 import ShareListItem from "./components/ShareListItem";
 import useCurrentTeam from "hooks/useCurrentTeam";
@@ -14,43 +15,40 @@ import useStores from "hooks/useStores";
 
 function Shares() {
   const team = useCurrentTeam();
+  const { t } = useTranslation();
   const { shares, auth, policies } = useStores();
   const canShareDocuments = auth.team && auth.team.sharing;
-  const hasSharedDocuments = shares.orderedData.length > 0;
   const can = policies.abilities(team.id);
-
-  React.useEffect(() => {
-    shares.fetchPage({ limit: 100 });
-  }, [shares]);
 
   return (
     <CenteredContent>
-      <PageTitle title="Share Links" />
-      <h1>Share Links</h1>
+      <PageTitle title={t("Share Links")} />
+      <h1>{t("Share Links")}</h1>
       <HelpText>
-        Documents that have been shared are listed below. Anyone that has the
-        public link can access a read-only version of the document until the
-        link has been revoked.
+        <Trans>
+          Documents that have been shared are listed below. Anyone that has the
+          public link can access a read-only version of the document until the
+          link has been revoked.
+        </Trans>
       </HelpText>
       {can.manage && (
         <HelpText>
           {!canShareDocuments && (
-            <strong>Sharing is currently disabled.</strong>
+            <strong>{t("Sharing is currently disabled.")}</strong>
           )}{" "}
-          You can turn {canShareDocuments ? "off" : "on"} public document
-          sharing in <Link to="/settings/security">security settings</Link>.
+          <Trans
+            defaults="You can globally enable and disable public document sharing in the <em>security settings</em>."
+            components={{ em: <Link to="/settings/security" /> }}
+          />
         </HelpText>
       )}
-      <Subheading>Shared Documents</Subheading>
-      {hasSharedDocuments ? (
-        <List>
-          {shares.published.map((share) => (
-            <ShareListItem key={share.id} share={share} />
-          ))}
-        </List>
-      ) : (
-        <Empty>No share links, yet.</Empty>
-      )}
+      <Subheading>{t("Shared documents")}</Subheading>
+      <PaginatedList
+        items={shares.published}
+        empty={<Empty>{t("No share links, yet.")}</Empty>}
+        fetch={shares.fetchPage}
+        renderItem={(item) => <ShareListItem key={item.id} share={item} />}
+      />
     </CenteredContent>
   );
 }
