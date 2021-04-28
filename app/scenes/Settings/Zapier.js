4d988e744bd925583246badfc3f8a9a0722efe67
Tom Moor <tom.moor@gmail.com>
2021-04-27 18:06:16 -0700

    Remaining settings scenes

--- app/scenes/Settings/Zapier.js
+++ app/scenes/Settings/Zapier.js
@@ -1,15 +1,15 @@
 // @flow
 import * as React from "react";
 import Button from "components/Button";
-import CenteredContent from "components/CenteredContent";
+import Heading from "components/Heading";
 import HelpText from "components/HelpText";
-import PageTitle from "components/PageTitle";
+import Scene from "components/Scene";
+import ZapierIcon from "components/ZapierIcon";
 
 function Zapier() {
   return (
-    <CenteredContent>
-      <PageTitle title="Zapier" />
-      <h1>Zapier</h1>
+    <Scene title="Zapier" icon={<ZapierIcon color="currentColor" />}>
+      <Heading>Zapier</Heading>
       <HelpText>
         Zapier is a platform that allows Outline to easily integrate with
         thousands of other business tools. Head over to Zapier to setup a "Zap"
@@ -24,7 +24,7 @@ function Zapier() {
           Open Zapier →
         </Button>
       </p>
-    </CenteredContent>
+    </Scene>
   );
 }
 
