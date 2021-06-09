33c468d2be43c37044dcefb239dba2d54e67a208
Tom Moor <tom.moor@gmail.com>
2021-06-08 21:47:01 -0700

    fix: Allow Dropbox to only show when APP_KEY is present fix: Add correct icon

--- app/embeds/Dropbox.js
+++ app/embeds/Dropbox.js
@@ -1,5 +1,7 @@
 // @flow
 import * as React from "react";
+import styled from "styled-components";
+import env from "env";
 
 const URL_REGEX = new RegExp("^https://www.dropbox.com/s/(.*)$");
 
@@ -20,25 +22,29 @@ class Dropbox extends React.Component<Props> {
     var tag = document.createElement("script");
     tag.async = false;
     tag.id = "dropboxjs";
-    tag.setAttribute("data-app-key", "zuf12mnhan22y62");
+    tag.setAttribute("data-app-key", env.DROPBOX_APP_KEY);
     tag.src = "https://www.dropbox.com/static/api/2/dropins.js";
     document.body?.appendChild(tag);
   }
 
   render() {
     return (
-      <div>
+      <Rounded
+        className={this.props.isSelected ? "ProseMirror-selectednode" : ""}
+      >
         <a
           ref={this.container}
           href={this.props.attrs.href}
           className="dropbox-embed"
           data-height="400px"
-        >
-          Dropbox Embed
-        </a>
-      </div>
+        />
+      </Rounded>
     );
   }
 }
+
+const Rounded = styled.div`
+  border-radius: 3px;
+`;
 
 export default Dropbox;
