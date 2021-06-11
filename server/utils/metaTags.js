094492cc70b57648daf739af94d453847ba68402
Saulo Vallory <saulo@toki.life>
2021-06-11 15:57:05 -0300

    fix: metaTags title param (definitely)

--- server/utils/metaTags.js
+++ server/utils/metaTags.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import ReactDOMServer from "react-dom/server";
 import env from "../env";
 
-export default function metaTags(title: string = null) {
+export default function metaTags(title: string | null = null) {
   return ReactDOMServer.renderToStaticMarkup([
     <meta name="description" content={env.SITE_DESCRIPTION} />,
     <title>{title || env.SITE_NAME}</title>,
