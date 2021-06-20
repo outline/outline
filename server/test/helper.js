ca74908dc5be9ae9844a125845b82d69916ff397
Tom Moor <tom.moor@gmail.com>
2021-06-20 00:20:37 -0700

    test

--- server/test/helper.js
+++ server/test/helper.js
@@ -2,6 +2,7 @@
 require("dotenv").config({ silent: true });
 
 // test environment variables
+process.env.URL = "http://localhost:3000";
 process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
 process.env.NODE_ENV = "test";
 process.env.GOOGLE_CLIENT_ID = "123";
