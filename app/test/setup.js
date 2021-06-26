3e1d2efc1721935b89633b9297069565a53d0d64
Tom Moor <tom.moor@gmail.com>
2021-06-26 12:01:35 -0700

    test: Add enzyme and component test

---
+++ app/test/setup.js
@@ -0,0 +1,10 @@
+// @flow
+/* eslint-disable */
+import localStorage from '../../__mocks__/localStorage';
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import "../stores";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+global.localStorage = localStorage;
--- setupJest.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-import localStorage from './__mocks__/localStorage';
-
-global.localStorage = localStorage;
