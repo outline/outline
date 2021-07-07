e98f94c02d4d15f600a749d489a3277723767e74
Matheus Breguêz <matbrgz@gmail.com>
2021-07-07 14:29:49 -0300

    feat: Add Google Calendar Embed

---
+++ app/embeds/GoogleCalendar.js
@@ -0,0 +1,37 @@
+// @flow
+import * as React from "react";
+import Image from "components/Image";
+import Frame from "./components/Frame";
+
+const URL_REGEX = new RegExp("^https?://calendar.google.com/calendar/(.*)$");
+
+type Props = {|
+  attrs: {|
+    href: string,
+    matches: string[],
+  |},
+|};
+
+export default class GoogleCalendar extends React.Component<Props> {
+  static ENABLED = [URL_REGEX];
+
+  render() {
+    return (
+      <Frame
+        {...this.props}
+        src={this.props.attrs.href}
+        icon={
+          <Image
+            src="/images/google-calendar.png"
+            alt="Google Calendar Icon"
+            width={16}
+            height={16}
+          />
+        }
+        canonicalUrl={this.props.attrs.href}
+        title="Google Calendar"
+        border
+      />
+    );
+  }
+}
