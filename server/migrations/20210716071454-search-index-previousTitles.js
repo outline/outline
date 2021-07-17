1e39de1f773b1000c972e8a85c506fec6b5154b2
Saumya Pandey <sp160899@gmail.com>
2021-07-17 11:41:36 +0530

    Update weights

--- server/migrations/20210716071454-search-index-previousTitles.js
+++ server/migrations/20210716071454-search-index-previousTitles.js
@@ -7,8 +7,8 @@ module.exports = {
     begin
       new."searchVector" :=
         setweight(to_tsvector('english', coalesce(new.title, '')),'A') ||
-        setweight(to_tsvector('english', coalesce(array_to_string(new."previousTitles", ' , '),'')),'A') ||
-        setweight(to_tsvector('english', coalesce(new.text, '')), 'C');
+        setweight(to_tsvector('english', coalesce(array_to_string(new."previousTitles", ' , '),'')),'C') ||
+        setweight(to_tsvector('english', coalesce(new.text, '')), 'B');
       return new;
     end
     $$ LANGUAGE plpgsql;
