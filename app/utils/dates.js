4271f78db00987ef6ca9be0e84c650817f9792a5
Tom Moor <tom.moor@gmail.com>
2021-07-18 10:56:21 -0400

    Improve date heading separtors

--- app/utils/dates.js
+++ app/utils/dates.js
@@ -1,11 +1,18 @@
 // @flow
-import { isThisWeek, isToday, isYesterday, subWeeks } from "date-fns";
+import {
+  isThisWeek,
+  isToday,
+  isYesterday,
+  differenceInCalendarWeeks,
+  differenceInCalendarMonths,
+} from "date-fns";
 import * as React from "react";
 import { type TFunction } from "react-i18next";
 import LocaleTime from "components/LocaleTime";
 
 export function dateToHeading(dateTime: string, t: TFunction) {
   const date = Date.parse(dateTime);
+  const now = new Date();
 
   if (isToday(date)) {
     return t("Today");
@@ -15,13 +22,30 @@ export function dateToHeading(dateTime: string, t: TFunction) {
     return t("Yesterday");
   }
 
-  if (isThisWeek(date)) {
+  const weekDiff = differenceInCalendarWeeks(now, date);
+  if (weekDiff === 0) {
     return <LocaleTime dateTime={dateTime} tooltip={false} format="iiii" />;
   }
 
-  const lastWeek = subWeeks(date, 2);
-  if (date > lastWeek) {
+  if (weekDiff === 1) {
     return t("Last week");
+  }
+
+  const monthDiff = differenceInCalendarMonths(now, date);
+  if (monthDiff === 0) {
+    return t("This month");
+  }
+
+  if (monthDiff === 1) {
+    return t("Last month");
+  }
+
+  if (monthDiff <= 12) {
+    return t("This year");
+  }
+
+  if (monthDiff <= 24) {
+    return t("Last year");
   }
 
   return <LocaleTime dateTime={dateTime} tooltip={false} />;
