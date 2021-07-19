2c139bcc880435bd42242b9af272f7596492f2fa
Saumya Pandey <sp160899@gmail.com>
2021-07-20 00:05:19 +0530

    Rename UiStore.test.js to ToastsStore.test.js

--- app/stores/ToastsStore.test.js
+++ app/stores/ToastsStore.test.js
@@ -2,27 +2,27 @@
 import stores from '.';
 
 // Actions
-describe('UiStore', () => {
+describe('ToastsStore', () => {
   let store;
 
   beforeEach(() => {
-    store = stores.ui;
+    store = stores.toasts;
   });
 
   test('#add should add messages', () => {
-    expect(store.orderedToasts.length).toBe(0);
+    expect(store.orderedData.length).toBe(0);
     store.showToast('first error');
     store.showToast('second error');
-    expect(store.orderedToasts.length).toBe(2);
+    expect(store.orderedData.length).toBe(2);
   });
 
   test('#remove should remove messages', () => {
     store.toasts.clear();
     const id = store.showToast('first error');
     store.showToast('second error');
-    expect(store.orderedToasts.length).toBe(2);
-    store.removeToast(id);
-    expect(store.orderedToasts.length).toBe(1);
-    expect(store.orderedToasts[0].message).toBe('second error');
+    expect(store.orderedData.length).toBe(2);
+    store.hideToast(id);
+    expect(store.orderedData.length).toBe(1);
+    expect(store.orderedData[0].message).toBe('second error');
   });
 });
