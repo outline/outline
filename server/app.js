f1f3423031b4c1fbac448a38dd124652bbf68b1c
Greg Linklater <greg@gryphus.io>
2021-07-25 14:42:09 +0200

    fix: correct type annotation

--- server/app.js
+++ server/app.js
@@ -42,7 +42,7 @@ if (env.CDN_URL) {
   defaultSrc.push(env.CDN_URL);
 }
 
-(app.keys: Array<string>) = [process.env.COOKIE_SECRET];
+app.keys = ([process.env.COOKIE_SECRET]: Array<string>);
 
 app.use(compress());
 app.use(session({}, app));
