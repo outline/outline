9867518211a5f7b32d3064a0676a899351a122ec
Saumya Pandey <sp160899@gmail.com>
2021-07-28 01:51:47 +0530

    Convert places where custom icons are used to icon prop

--- app/menus/NewDocumentMenu.js
+++ app/menus/NewDocumentMenu.js
@@ -11,7 +11,6 @@ import CollectionIcon from "components/CollectionIcon";
 import ContextMenu from "components/ContextMenu";
 import Header from "components/ContextMenu/Header";
 import Template from "components/ContextMenu/Template";
-import Flex from "components/Flex";
 import useCurrentTeam from "hooks/useCurrentTeam";
 import useStores from "hooks/useStores";
 import { newDocumentUrl } from "utils/routeHelpers";
@@ -45,7 +44,7 @@ function NewDocumentMenu() {
       <MenuButton {...menu}>
         {(props) => (
           <Button icon={<PlusIcon />} {...props} small>
-            {`${t("New doc")}…`}
+            {`${t("New doc")}`}
           </Button>
         )}
       </MenuButton>
@@ -56,12 +55,8 @@ function NewDocumentMenu() {
           items={collections.orderedData.map((collection) => ({
             to: newDocumentUrl(collection.id),
             disabled: !policies.abilities(collection.id).update,
-            title: (
-              <Flex align="center">
-                <CollectionIcon collection={collection} />
-                <CollectionName>{collection.name}</CollectionName>
-              </Flex>
-            ),
+            title: <CollectionName>{collection.name}</CollectionName>,
+            icon: <CollectionIcon collection={collection} />,
           }))}
         />
       </ContextMenu>
--- app/menus/NewTemplateMenu.js
+++ app/menus/NewTemplateMenu.js
@@ -3,14 +3,13 @@ import { observer } from "mobx-react";
 import { PlusIcon } from "outline-icons";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { useMenuState, MenuButton } from "reakit/Menu";
+import { MenuButton, useMenuState } from "reakit/Menu";
 import styled from "styled-components";
 import Button from "components/Button";
 import CollectionIcon from "components/CollectionIcon";
 import ContextMenu from "components/ContextMenu";
 import Header from "components/ContextMenu/Header";
 import Template from "components/ContextMenu/Template";
-import Flex from "components/Flex";
 import useCurrentTeam from "hooks/useCurrentTeam";
 import useStores from "hooks/useStores";
 import { newDocumentUrl } from "utils/routeHelpers";
@@ -44,12 +43,8 @@ function NewTemplateMenu() {
               template: true,
             }),
             disabled: !policies.abilities(collection.id).update,
-            title: (
-              <Flex align="center">
-                <CollectionIcon collection={collection} />
-                <CollectionName>{collection.name}</CollectionName>
-              </Flex>
-            ),
+            title: <CollectionName>{collection.name}</CollectionName>,
+            icon: <CollectionIcon collection={collection} />,
           }))}
         />
       </ContextMenu>
