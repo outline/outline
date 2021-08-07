92f6e41432f868b383bf91bab0facf5edef26678
Saumya Pandey <sp160899@gmail.com>
2021-08-08 03:47:56 +0530

    Add auto expire

--- server/api/exports.js
+++ server/api/exports.js
@@ -1,12 +1,10 @@
 // @flow
 import Router from "koa-router";
 import auth from "../middlewares/authentication";
-import { Export, Event, User, Collection } from "../models";
-import policy from "../policies";
+import { Export, User, Collection } from "../models";
 import { presentExport } from "../presenters";
 import pagination from "./middlewares/pagination";
 
-const { authorize } = policy;
 const router = new Router();
 
 router.post("exports.list", auth(), pagination(), async (ctx) => {
@@ -44,30 +42,6 @@ router.post("exports.list", auth(), pagination(), async (ctx) => {
       total,
     },
     data: exports.map(presentExport),
-  };
-});
-
-router.post("exports.delete", auth(), async (ctx) => {
-  const { id } = ctx.body;
-  ctx.assertUuid(id, "id is required");
-
-  const user = ctx.state.user;
-  const exportData = await Export.findByPk(id);
-  authorize(user, "delete", exportData);
-
-  await exportData.destroy();
-
-  await Event.create({
-    name: "exports.delete",
-    modelId: exportData.id,
-    teamId: user.teamId,
-    actorId: user.id,
-    data: { key: exportData.key, collectionId: exportData.collectionId },
-    ip: ctx.request.ip,
-  });
-
-  ctx.body = {
-    success: true,
   };
 });
 
--- server/api/utils.js
+++ server/api/utils.js
@@ -4,8 +4,9 @@ import debug from "debug";
 import Router from "koa-router";
 import { documentPermanentDeleter } from "../commands/documentPermanentDeleter";
 import { AuthenticationError } from "../errors";
-import { Document } from "../models";
+import { Document, Export } from "../models";
 import { Op } from "../sequelize";
+import { deleteFromS3 } from "../utils/s3";
 
 const router = new Router();
 const log = debug("utils");
@@ -33,6 +34,27 @@ router.post("utils.gc", async (ctx) => {
   const countDeletedDocument = await documentPermanentDeleter(documents);
 
   log(`Destroyed ${countDeletedDocument} documents`);
+
+  log(`Expiring all the collection export older than 30 days…`);
+
+  const exports = await Export.findAll({
+    where: {
+      createdAt: {
+        [Op.lt]: subDays(new Date(), 30),
+      },
+      state: {
+        [Op.ne]: "expired",
+      },
+    },
+  });
+
+  await Promise.all(
+    exports.map(async (e) => {
+      e.state = "expired";
+      await deleteFromS3(e.key);
+      await e.save();
+    })
+  );
 
   ctx.body = {
     success: true,
