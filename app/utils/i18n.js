77db0c2e95b9543e9d2d9e6d1b7524dd1069e28a
Tom Moor <tom.moor@gmail.com>
2021-08-12 18:24:13 -0400

    fix: Document history event headings (#2433)
    * fix: Document history events from last year but within 12 months shown as 'this year'
    fix: Events older than a year have repeated headings
    
    * lint

---
+++ app/utils/i18n.js
@@ -0,0 +1,36 @@
+// @flow
+import {
+  enUS,
+  de,
+  faIR,
+  fr,
+  es,
+  it,
+  ja,
+  ko,
+  ptBR,
+  pt,
+  zhCN,
+  zhTW,
+  ru,
+} from "date-fns/locale";
+
+const locales = {
+  en_US: enUS,
+  de_DE: de,
+  es_ES: es,
+  fa_IR: faIR,
+  fr_FR: fr,
+  it_IT: it,
+  ja_JP: ja,
+  ko_KR: ko,
+  pt_BR: ptBR,
+  pt_PT: pt,
+  zh_CN: zhCN,
+  zh_TW: zhTW,
+  ru_RU: ru,
+};
+
+export function dateLocale(userLocale: ?string) {
+  return userLocale ? locales[userLocale] : undefined;
+}
