543079bf0c6e8e63668ba7836a5d8e81a6063672
Saumya Pandey <sp160899@gmail.com>
2021-08-13 17:22:39 +0530

    Use PaginatedList

---
+++ app/components/ImportExportListItem.js
@@ -0,0 +1,153 @@
+// @flow
+import { LinkIcon } from "outline-icons";
+import * as React from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+import breakpoint from "styled-components-breakpoint";
+import Export from "models/Export";
+import Avatar from "components/Avatar";
+import Badge from "components/Badge";
+import Button from "components/Button";
+import EventBoundary from "components/EventBoundary";
+import Flex from "components/Flex";
+import Time from "components/Time";
+import useCurrentUser from "hooks/useCurrentUser";
+import useMobile from "hooks/useMobile";
+
+type ImportExportListItemProps = {
+  item: Export,
+};
+
+const ImportExportListItem = ({ item }: ImportExportListItemProps) => {
+  const user = useCurrentUser();
+  const { t } = useTranslation();
+  const isMobile = useMobile();
+
+  return (
+    <ImportExportListItemWrapper>
+      <Flex align="center" column>
+        <AvatarWrapper src={item.user.avatarUrl} size={32} />
+      </Flex>
+      <Content>
+        <Heading>
+          {item.collection ? (
+            <Title>
+              <Link to={item.collection.url}>{item.collection.name}</Link>
+            </Title>
+          ) : (
+            <Title>All collections</Title>
+          )}
+          <Badge>{item.state}</Badge>
+        </Heading>
+        <Container>
+          <Flex>
+            {user.id === item.user.id ? t("You") : item.user.name} created&nbsp;
+            <Time dateTime={item.createdAt} addSuffix shorten />
+          </Flex>
+          {!isMobile && <>&nbsp;•&nbsp;</>}
+          <Flex>{(item.size / (1024 * 1024)).toPrecision(2)}MB</Flex>
+        </Container>
+      </Content>
+
+      {item.url && (
+        <Actions>
+          <Button
+            onClick={() => window.open(item.url, "_blank")}
+            icon={<LinkIcon fill="currentColor" />}
+            neutral
+          >
+            {t("View")}
+          </Button>
+        </Actions>
+      )}
+    </ImportExportListItemWrapper>
+  );
+};
+
+const Actions = styled(EventBoundary)`
+  align-items: center;
+  margin: 8px;
+  flex-shrink: 0;
+  flex-grow: 0;
+
+  ${breakpoint("tablet")`
+    display: flex;
+  `};
+`;
+
+const Content = styled.div`
+  flex-grow: 1;
+  flex-shrink: 1;
+  width: 100%;
+  min-width: 0;
+  margin-left: 0.5em;
+`;
+
+const Container = styled(Flex)`
+  justify-content: "flex-start";
+  color: ${(props) => props.theme.textTertiary};
+  font-size: 13px;
+  white-space: nowrap;
+  overflow: hidden;
+  min-width: 0;
+
+  ${breakpoint("mobile", "tablet")`
+    flex-direction: column;
+  `};
+`;
+
+const ImportExportListItemWrapper = styled(Flex)`
+  align-items: center;
+  margin: 10px -8px;
+  padding: 6px 8px;
+  border-radius: 8px;
+  max-height: 50vh;
+  width: calc(100vw - 8px);
+
+  ${breakpoint("tablet")`
+    width: auto;
+  `};
+
+  &:hover,
+  &:active,
+  &:focus,
+  &:focus-within {
+    background: ${(props) => props.theme.listItemHoverBackground};
+  }
+
+  ${(props) =>
+    props.$menuOpen &&
+    css`
+      background: ${(props) => props.theme.listItemHoverBackground};
+    `}
+`;
+
+const AvatarWrapper = styled(Avatar)`
+  display: none;
+  ${breakpoint("desktop")`
+    display: inherit;
+  `};
+`;
+
+const Title = styled.span`
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
+const Heading = styled.p`
+  font-size: ${(props) => (props.$small ? 14 : 16)}px;
+  font-weight: 500;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  line-height: ${(props) => (props.$small ? 1.3 : 1.2)};
+  margin: 0;
+  height: 24px;
+  margin-bottom: 0.25em;
+  color: ${(props) => props.theme.text};
+`;
+
+export default ImportExportListItem;
--- app/scenes/Settings/ImportExport.js
+++ app/scenes/Settings/ImportExport.js
@@ -3,195 +3,35 @@ import invariant from "invariant";
 import { observer } from "mobx-react";
 import { CollectionIcon, DocumentIcon } from "outline-icons";
 import * as React from "react";
-import { useState } from "react";
 import { useTranslation, Trans } from "react-i18next";
-import { Link, useLocation, useHistory } from "react-router-dom";
 import { VisuallyHidden } from "reakit/VisuallyHidden";
-import scrollIntoView from "smooth-scroll-into-view-if-needed";
 import styled from "styled-components";
 import { parseOutlineExport } from "shared/utils/zip";
-import { PAGINATION_SYMBOL } from "stores/BaseStore";
-import Avatar from "components/Avatar";
 import Button from "components/Button";
-import Flex from "components/Flex";
 import Heading from "components/Heading";
 import HelpText from "components/HelpText";
+import ImportExportListItem from "components/ImportExportListItem";
 import Notice from "components/Notice";
+import PaginatedList from "components/PaginatedList";
 import Scene from "components/Scene";
-import { type Props as TableProps } from "components/Table";
-import Time from "components/Time";
 import useCurrentUser from "hooks/useCurrentUser";
-import useMobile from "hooks/useMobile";
-import useQuery from "hooks/useQuery";
 import useStores from "hooks/useStores";
 import useToasts from "hooks/useToasts";
 import getDataTransferFiles from "utils/getDataTransferFiles";
 import { uploadFile } from "utils/uploadFile";
 
-const Table = React.lazy<TableProps>(() =>
-  import(/* webpackChunkName: "table" */ "components/Table")
-);
-
 function ImportExport() {
   const { t } = useTranslation();
   const user = useCurrentUser();
   const fileRef = React.useRef();
-  const params = useQuery();
   const { exports, collections } = useStores();
   const { showToast } = useToasts();
-  const [data, setData] = useState([]);
   const [isLoading, setLoading] = React.useState(false);
-  const [isExportDataLoading, setExportDataLoading] = React.useState(false);
   const [isImporting, setImporting] = React.useState(false);
   const [isImported, setImported] = React.useState(false);
   const [isExporting, setExporting] = React.useState(false);
   const [file, setFile] = React.useState();
   const [importDetails, setImportDetails] = React.useState();
-  const limit = 15;
-  const key = params.get("key") || "";
-  const page = parseInt(params.get("page") || 0, 10);
-  const sort = params.get("sort") || "createdAt";
-  const direction = (params.get("direction") || "desc").toUpperCase();
-  const [totalPages, setTotalPages] = React.useState(0);
-  const isMobile = useMobile();
-  const location = useLocation();
-  const history = useHistory();
-  const topRef = React.useRef();
-
-  const columns = React.useMemo(
-    () => [
-      {
-        id: "name",
-        Header: t("Name"),
-        accessor: "user",
-        disableSortBy: true,
-        Cell: observer(({ value, row }) => {
-          return (
-            <Flex align="center" gap={8}>
-              {!isMobile && (
-                <Avatar src={row.original.user.avatarUrl} size={32} />
-              )}
-              {value.name} {user.id === row.original.user.id && `(${t("You")})`}
-            </Flex>
-          );
-        }),
-      },
-      {
-        id: "collection",
-        Header: t("Collection"),
-        accessor: "collection",
-        disableSortBy: true,
-        Cell: observer(({ value }) => {
-          if (!value) return "All collections";
-          return <Link to={value.url}>{value.name}</Link>;
-        }),
-      },
-      {
-        id: "state",
-        Header: t("State"),
-        accessor: "state",
-        disableSortBy: true,
-        Cell: observer(({ value }) => value),
-      },
-      {
-        id: "url",
-        Header: t("Url"),
-        accessor: "url",
-        disableSortBy: true,
-        Cell: observer(({ value }) => {
-          if (!value) return value;
-          return (
-            <a href={value} rel="noreferrer" target="_blank">
-              Link
-            </a>
-          );
-        }),
-      },
-      {
-        id: "size",
-        Header: t("Size"),
-        accessor: "size",
-        sortType: "number",
-        Cell: observer(
-          ({ value }) => (value / (1024 * 1024)).toPrecision(2) + "MB"
-        ),
-      },
-      {
-        id: "createdAt",
-        Header: t("Created"),
-        accessor: "createdAt",
-        Cell: observer(
-          ({ value }) => value && <Time dateTime={value} addSuffix />
-        ),
-      },
-    ],
-    [t, isMobile, user.id]
-  );
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setExportDataLoading(true);
-        const response = await exports.fetchPage({
-          offset: page * limit,
-          limit,
-          direction,
-          sort,
-        });
-        setData(response.slice(0, limit));
-        setTotalPages(Math.ceil(response[PAGINATION_SYMBOL].total / limit));
-      } catch (err) {
-        showToast(err.message);
-      } finally {
-        setExportDataLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [direction, exports, page, showToast, sort, exports.orderedData]);
-
-  const handleChangePage = React.useCallback(
-    (page) => {
-      if (page) {
-        params.set("page", page.toString());
-      } else {
-        params.delete("page");
-      }
-      history.replace({
-        pathname: location.pathname,
-        search: params.toString(),
-      });
-
-      if (topRef.current) {
-        scrollIntoView(topRef.current, {
-          scrollMode: "if-needed",
-          behavior: "instant",
-          block: "start",
-        });
-      }
-    },
-    [params, history, location.pathname]
-  );
-
-  const handleChangeSort = React.useCallback(
-    (sort, direction) => {
-      if (sort) {
-        params.set("sort", sort);
-      } else {
-        params.delete("sort");
-      }
-      if (direction === "ASC") {
-        params.set("direction", direction.toLowerCase());
-      } else {
-        params.delete("direction");
-      }
-      history.replace({
-        pathname: location.pathname,
-        search: params.toString(),
-      });
-    },
-    [params, history, location.pathname]
-  );
 
   const handleImport = React.useCallback(
     async (ev) => {
@@ -340,7 +180,6 @@ function ImportExport() {
           {t("Choose File")}…
         </Button>
       )}
-
       <Heading>{t("Export")}</Heading>
       <HelpText>
         <Trans
@@ -364,17 +203,12 @@ function ImportExport() {
       <Heading>
         <Trans>Exports</Trans>
       </Heading>
-      <Table
-        columns={columns}
-        data={data}
-        isLoading={isExportDataLoading}
-        onChangePage={handleChangePage}
-        onChangeSort={handleChangeSort}
-        page={page}
-        totalPages={totalPages}
-        defaultSortDirection="DESC"
-        defaultSort="createdAt"
-        id={key}
+      <PaginatedList
+        items={exports.orderedData}
+        fetch={exports.fetchPage}
+        renderItem={(item) => (
+          <ImportExportListItem key={item.id} item={item} />
+        )}
       />
     </Scene>
   );
