e5eb134ae6ba189e34e549d7d0da8855c9f91fa0
Saumya Pandey <sp160899@gmail.com>
2021-08-27 01:44:51 +0530

    Use newDocumentUrl helper

--- app/components/NewDocumentButton.js
+++ app/components/NewDocumentButton.js
@@ -4,12 +4,13 @@ import * as React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import Button from "components/Button";
+import { newDocumentUrl } from "utils/routeHelpers";
 
 const NewDocumentButton = () => {
   const { t } = useTranslation();
 
   return (
-    <Button as={Link} to={"/doc/new"} icon={<PlusIcon />}>
+    <Button as={Link} to={newDocumentUrl()} icon={<PlusIcon />}>
       {t("New draft doc")}
     </Button>
   );
