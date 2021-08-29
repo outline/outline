e2e82ac8ef8da313a7df3b1712fed91eb8644ecd
Tom Moor <tom.moor@gmail.com>
2021-08-29 13:13:38 -0700

    lint

--- server/routes/api/middlewares/pagination.test.js
+++ server/routes/api/middlewares/pagination.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 import TestServer from "fetch-test-server";
-import webService from "../../services/web";
-import { flushdb, seed } from "../../test/support";
+import webService from "../../../services/web";
+import { flushdb, seed } from "../../../test/support";
 const app = webService();
 const server = new TestServer(app.callback());
 
