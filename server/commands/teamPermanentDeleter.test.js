57917fc081851900e151027c2f2a652110958573
Tom Moor <tom.moor@gmail.com>
2021-09-19 22:57:49 -0700

    test coverage

--- server/commands/teamPermanentDeleter.test.js
+++ server/commands/teamPermanentDeleter.test.js
@@ -1,7 +1,12 @@
 // @flow
 import { subDays } from "date-fns";
-import { Attachment, Document } from "../models";
-import { buildAttachment, buildTeam, buildDocument } from "../test/factories";
+import { Attachment, User, Document, Collection, Team } from "../models";
+import {
+  buildAttachment,
+  buildUser,
+  buildTeam,
+  buildDocument,
+} from "../test/factories";
 import { flushdb } from "../test/support";
 import teamPermanentDeleter from "./teamPermanentDeleter";
 
@@ -20,21 +25,49 @@ describe("teamPermanentDeleter", () => {
     const team = await buildTeam({
       deletedAt: subDays(new Date(), 90),
     });
+    const user = await buildUser({
+      teamId: team.id,
+    });
     await buildDocument({
       teamId: team.id,
+      userId: user.id,
     });
 
     await teamPermanentDeleter(team);
 
+    expect(await Team.count()).toEqual(0);
+    expect(await User.count()).toEqual(0);
     expect(await Document.unscoped().count({ paranoid: false })).toEqual(0);
+    expect(await Collection.unscoped().count({ paranoid: false })).toEqual(0);
+  });
+
+  it("should not destroy unrelated data", async () => {
+    const team = await buildTeam({
+      deletedAt: subDays(new Date(), 90),
+    });
+
+    await buildUser();
+    await buildTeam();
+    await buildDocument();
+
+    await teamPermanentDeleter(team);
+
+    expect(await Team.count()).toEqual(4); // each build command creates a team
+    expect(await User.count()).toEqual(2);
+    expect(await Document.unscoped().count({ paranoid: false })).toEqual(1);
+    expect(await Collection.unscoped().count({ paranoid: false })).toEqual(1);
   });
 
   it("should destroy attachments", async () => {
     const team = await buildTeam({
       deletedAt: subDays(new Date(), 90),
     });
+    const user = await buildUser({
+      teamId: team.id,
+    });
     const document = await buildDocument({
       teamId: team.id,
+      userId: user.id,
     });
     await buildAttachment({
       teamId: document.teamId,
@@ -43,8 +76,11 @@ describe("teamPermanentDeleter", () => {
 
     await teamPermanentDeleter(team);
 
+    expect(await Team.count()).toEqual(0);
+    expect(await User.count()).toEqual(0);
     expect(await Attachment.count()).toEqual(0);
     expect(await Document.unscoped().count({ paranoid: false })).toEqual(0);
+    expect(await Collection.unscoped().count({ paranoid: false })).toEqual(0);
   });
 
   it("should error when trying to destroy undeleted team", async () => {
