c1bd30aac824370e9c7699904f47e9294265c2f7
Tom Moor <tom.moor@gmail.com>
2021-09-24 19:14:00 -0700

    Add user to collaboration logs

--- server/collaboration/logger.js
+++ server/collaboration/logger.js
@@ -1,19 +1,26 @@
 // @flow
 import Logger from "../logging/logger";
+import { User } from "../models";
+
 export default class CollaborationLogger {
-  async onCreateDocument(data: { documentName: string }) {
-    Logger.info("hocuspocus", `Created document "${data.documentName}"`);
+  async onCreateDocument(data: {
+    documentName: string,
+    context: { user: User },
+  }) {
+    Logger.info("hocuspocus", `Created document "${data.documentName}"`, {
+      userId: data.context.user.id,
+    });
   }
 
-  async onConnect(data: { documentName: string }) {
-    Logger.info("hocuspocus", `New connection to "${data.documentName}"`);
+  async onConnect(data: { documentName: string, context: { user: User } }) {
+    Logger.info("hocuspocus", `New connection to "${data.documentName}"`, {
+      userId: data.context.user.id,
+    });
   }
 
-  async onDisconnect(data: { documentName: string }) {
-    Logger.info("hocuspocus", `Connection to "${data.documentName}" closed`);
-  }
-
-  async onUpgrade() {
-    Logger.info("hocuspocus", "Upgrading connection");
+  async onDisconnect(data: { documentName: string, context: { user: User } }) {
+    Logger.info("hocuspocus", `Connection to "${data.documentName}" closed `, {
+      userId: data.context.user.id,
+    });
   }
 }
