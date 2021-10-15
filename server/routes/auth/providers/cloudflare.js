ff6d4cf17bcb9ec85429a4c2311ebdcb5a0c0041
Pratheek Rebala <pratheekrebala@gmail.com>
2021-10-15 19:03:33 -0400

    Fix linting issues.

--- server/routes/auth/providers/cloudflare.js
+++ server/routes/auth/providers/cloudflare.js
@@ -3,14 +3,13 @@ import passport from "@outlinewiki/koa-passport";
 import Router from "koa-router";
 
 import { Strategy as CloudflareStrategy } from "passport-cloudflare";
-import debug from "debug";
 
-import passportMiddleware from "../../../middlewares/passport";
 import accountProvisioner from "../../../commands/accountProvisioner";
+import passportMiddleware from "../../../middlewares/passport";
 
 const router = new Router();
 
-const providerName = 'cloudflare';
+const providerName = "cloudflare";
 
 const audienceId = process.env.CLOUDFLARE_AUDIENCE;
 const teamName = process.env.CLOUDFLARE_TEAM_NAME;
@@ -20,55 +19,54 @@ const teamDomainName = `${teamName}.cloudflareaccess.com`;
 const providerEnabled = !!teamName && !!audienceId;
 
 export const config = {
-    name: providerName,
-    enabled: providerEnabled
-}
+  name: providerName,
+  enabled: providerEnabled,
+};
 
-const strategy = new CloudflareStrategy({
+const strategy = new CloudflareStrategy(
+  {
     teamName: teamName,
     audience: audienceId,
 
     verifyAudience: true,
     verifyIssuer: true,
 
-    passReqToCallback: true
-}, async function (req, response, done) {
-
+    passReqToCallback: true,
+  },
+  async function (req, response, done) {
     try {
-        const { payload, identity } = response;
+      const { identity } = response;
 
-        const result = await accountProvisioner({
-            ip: identity.ip,
-            team: {
-                name: displayName,
-                domain: teamDomainName,
-                subdomain: teamName
-            },
-            user: {
-                name: identity.name,
-                email: identity.email
-            },
-            authenticationProvider: {
-                name: providerName,
-                providerId: teamDomainName
-            },
-            authentication: {
-                providerId: identity.account_id
-            }
-        });
-    
-        return done(null, result.user, result);
+      const result = await accountProvisioner({
+        ip: identity.ip,
+        team: {
+          name: displayName,
+          domain: teamDomainName,
+          subdomain: teamName,
+        },
+        user: {
+          name: identity.name,
+          email: identity.email,
+        },
+        authenticationProvider: {
+          name: providerName,
+          providerId: teamDomainName,
+        },
+        authentication: {
+          providerId: identity.account_id,
+        },
+      });
+
+      return done(null, result.user, result);
     } catch (err) {
-        return done(err, null);
+      return done(err, null);
     }
-});
+  }
+);
 
 strategy.name = providerName;
 passport.use(strategy);
 
-router.get(
-    providerName, 
-    passportMiddleware(providerName)
-);
+router.get(providerName, passportMiddleware(providerName));
 
 export default router;
