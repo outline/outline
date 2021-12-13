6f73cb493955becfe52ee4c7094f0870b9483eb5
Nan Yu <nan@getoutline.com>
2021-12-13 13:21:03 -0800

    fix: various bugs on draft sidebar items (#2856)
    - show children when the only child is the active draft
    - disable drop onto draft items
    - fix danger button hover color

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -72,7 +72,7 @@ const RealButton = styled.button<{
       fill: ${props.iconColor || props.theme.buttonNeutralText};
     }`
     }
-    
+
 
     &:hover:not(:disabled) {
       background: ${
@@ -100,7 +100,7 @@ const RealButton = styled.button<{
       background: ${props.theme.danger};
       color: ${props.theme.white};
 
-      &:hover {
+      &:hover:not(:disabled) {
         background: ${darken(0.05, props.theme.danger)};
       }
   `};
