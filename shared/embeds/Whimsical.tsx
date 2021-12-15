632799fea6ead4bfee29c77bd9f073ca4d7b8cc9
Tom Moor <tom.moor@gmail.com>
2021-12-14 16:06:31 -0800

    feat: Add 'Whimsical' as embed provider

---
+++ shared/embeds/Whimsical.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Frame from "./components/Frame";
+import { EmbedProps as Props } from ".";
+
+const URL_REGEX = /^https?:\/\/whimsical.com\/[0-9a-zA-Z-_~]*-([a-zA-Z0-9]+)\/?$/;
+
+export default class Whimsical extends React.Component<Props> {
+  static ENABLED = [URL_REGEX];
+
+  render() {
+    const { matches } = this.props.attrs;
+    const boardId = matches[1];
+
+    return (
+      <Frame
+        {...this.props}
+        src={`https://whimsical.com/embed/${boardId}`}
+        title="Whimsical"
+      />
+    );
+  }
+}
