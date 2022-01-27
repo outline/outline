76e98c31e31dfc371c723c95f7287dfd0adb3a06
Tom Moor <tom@getoutline.com>
2022-01-26 22:47:26 -0800

    fix: Team logo shows as white in settings (#3015)
    * fix: Team logo shows as white in settings
    fix: Team logo doesnt update in sidebar immediately after updating
    refactor to ImageUpload component
    
    * text

--- app/scenes/Settings/Details.tsx
+++ app/scenes/Settings/Details.tsx
@@ -3,28 +3,26 @@ import { TeamIcon } from "outline-icons";
 import { useRef, useState } from "react";
 import * as React from "react";
 import { useTranslation, Trans } from "react-i18next";
-import styled from "styled-components";
 import Button from "~/components/Button";
-import Flex from "~/components/Flex";
 import Heading from "~/components/Heading";
 import HelpText from "~/components/HelpText";
-import Input, { LabelText } from "~/components/Input";
+import Input from "~/components/Input";
 import Scene from "~/components/Scene";
 import env from "~/env";
 import useCurrentTeam from "~/hooks/useCurrentTeam";
 import useStores from "~/hooks/useStores";
 import useToasts from "~/hooks/useToasts";
-import ImageUpload from "./components/ImageUpload";
+import ImageInput from "./components/ImageInput";
 
 function Details() {
   const { auth } = useStores();
   const { showToast } = useToasts();
-  const team = useCurrentTeam();
   const { t } = useTranslation();
+  const team = useCurrentTeam();
   const form = useRef<HTMLFormElement>(null);
   const [name, setName] = useState(team.name);
   const [subdomain, setSubdomain] = useState(team.subdomain);
-  const [avatarUrl, setAvatarUrl] = useState<string>();
+  const [avatarUrl, setAvatarUrl] = useState<string>(team.avatarUrl);
 
   const handleSubmit = React.useCallback(
     async (event?: React.SyntheticEvent) => {
@@ -64,13 +62,15 @@ function Details() {
     []
   );
 
-  const handleAvatarUpload = React.useCallback(
-    (avatarUrl: string) => {
-      setAvatarUrl(avatarUrl);
-      handleSubmit();
-    },
-    [handleSubmit]
-  );
+  const handleAvatarUpload = async (avatarUrl: string) => {
+    setAvatarUrl(avatarUrl);
+    await auth.updateTeam({
+      avatarUrl,
+    });
+    showToast(t("Logo updated"), {
+      type: "success",
+    });
+  };
 
   const handleAvatarError = React.useCallback(
     (error: string | null | undefined) => {
@@ -90,22 +90,14 @@ function Details() {
         </Trans>
       </HelpText>
 
-      <ProfilePicture column>
-        <LabelText>{t("Logo")}</LabelText>
-        <AvatarContainer>
-          <ImageUpload
-            onSuccess={handleAvatarUpload}
-            onError={handleAvatarError}
-            submitText={t("Crop logo")}
-            borderRadius={0}
-          >
-            <Avatar src={avatarUrl} />
-            <Flex auto align="center" justify="center">
-              <Trans>Upload</Trans>
-            </Flex>
-          </ImageUpload>
-        </AvatarContainer>
-      </ProfilePicture>
+      <ImageInput
+        label={t("Logo")}
+        onSuccess={handleAvatarUpload}
+        onError={handleAvatarError}
+        src={avatarUrl}
+        borderRadius={0}
+      />
+
       <form onSubmit={handleSubmit} ref={form}>
         <Input
           label={t("Name")}
@@ -143,44 +135,5 @@ function Details() {
     </Scene>
   );
 }
-
-const ProfilePicture = styled(Flex)`
-  margin-bottom: 24px;
-`;
-
-const avatarStyles = `
-  width: 80px;
-  height: 80px;
-  border-radius: 8px;
-`;
-
-const AvatarContainer = styled(Flex)`
-  ${avatarStyles};
-  position: relative;
-  box-shadow: 0 0 0 1px #dae1e9;
-  background: ${(props) => props.theme.white};
-
-  div div {
-    ${avatarStyles};
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    opacity: 0;
-    cursor: pointer;
-    transition: all 250ms;
-  }
-
-  &:hover div {
-    opacity: 1;
-    background: rgba(0, 0, 0, 0.75);
-    color: ${(props) => props.theme.white};
-  }
-`;
-
-const Avatar = styled.img`
-  ${avatarStyles};
-`;
 
 export default observer(Details);
