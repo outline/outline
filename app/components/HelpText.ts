08c5c2736ca62ec9196d8b8fd59e03880994e1c4
Louis Phan <louisphan@notreallouisss.local>
2022-01-31 11:32:12 +0900

    fix helptext layout

--- app/components/HelpText.ts
+++ app/components/HelpText.ts
@@ -4,6 +4,8 @@ const HelpText = styled.p<{ small?: boolean }>`
   margin-top: 0;
   color: ${(props) => props.theme.textSecondary};
   font-size: ${(props) => (props.small ? "14px" : "inherit")};
+  word-break: break-all;
+  white-space: normal;
 `;
 
 export default HelpText;
