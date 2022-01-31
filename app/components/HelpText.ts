a728ea7ed3aa98454c8baa849e2dc4f405a8c5bc
Louis Phan <louisphan@notreallouisss.local>
2022-01-31 12:52:41 +0900

    remove unnecessary css property - word-break

--- app/components/HelpText.ts
+++ app/components/HelpText.ts
@@ -4,7 +4,6 @@ const HelpText = styled.p<{ small?: boolean }>`
   margin-top: 0;
   color: ${(props) => props.theme.textSecondary};
   font-size: ${(props) => (props.small ? "14px" : "inherit")};
-  word-break: break-all;
   white-space: normal;
 `;
 
