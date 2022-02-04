30b3b32bc135cc5e0d3dc05a6841d8d7335bcb10
sleepy ramen <rllola80@gmail.com>
2022-02-04 16:00:11 +0100

    Update server/routes/auth/providers/github.js
    Co-authored-by: Tom Moor <tom.moor@gmail.com>

--- server/routes/auth/providers/github.js
+++ server/routes/auth/providers/github.js
@@ -39,7 +39,7 @@ export const config = {
 
 if (GITHUB_CLIENT_ID) {
   if (GITHUB_ORGANIZATION === "") {
-    throw new Error("`GITHUB_ORGANIZATION` required when using Github.");
+    throw new Error("`GITHUB_ORGANIZATION` required when using GitHub.");
   }
 
   const strategy = new GithubStrategy(
