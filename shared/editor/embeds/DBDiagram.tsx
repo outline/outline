351b920a9380dbe09f4f5e36fdb4843105e8abb5
n3n <nonpawit.tee@gmail.com>
2022-02-15 21:50:38 +0700

    clean variable name

--- shared/editor/embeds/DBDiagram.tsx
+++ shared/editor/embeds/DBDiagram.tsx
@@ -3,17 +3,17 @@ import Frame from "./components/Frame";
 import { EmbedProps as Props } from ".";
 
 export default class DBDiagram extends React.Component<Props> {
-  static ENABLED = [new RegExp("https?://dbdiagram.io/(embed|d)/(\\w+)$")];
+  static ENABLED = [new RegExp("https://dbdiagram.io/(embed|d)/(\\w+)$")];
 
   render() {
     const { matches } = this.props.attrs;
-    const id = matches[2];
+    const shareId = matches[2];
 
     return (
       <Frame
         {...this.props}
-        src={`https://dbdiagram.io/embed/${id}`}
-        title={`DBDiagram (${id})`}
+        src={`https://dbdiagram.io/embed/${shareId}`}
+        title={`DBDiagram (${shareId})`}
         width="100%"
         height="400px"
       />
