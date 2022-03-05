8404e2a13250904cbd55f4e9c6be0172b1d3aed1
Tom Moor <tom.moor@gmail.com>
2022-03-05 12:08:28 -0800

    Comment model

---
+++ server/migrations/20220305195830-create-comments.js
@@ -0,0 +1,67 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("comments", {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true
+      },
+      data: {
+        type: Sequelize.JSONB,
+        allowNull: false
+      },
+      documentId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "documents"
+        }
+      },
+      parentCommentId: {
+        type: Sequelize.UUID,
+        allowNull: true,
+        references: {
+          model: "comments"
+        }
+      },
+      createdById: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "users"
+        }
+      },
+      resolvedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      resolvedById: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "users"
+        }
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      }
+    });
+
+    await queryInterface.addIndex("comments", ["documentId"]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("comments");
+  }
+};
---
+++ server/models/Comment.ts
@@ -0,0 +1,50 @@
+import {
+  DataType,
+  BelongsTo,
+  ForeignKey,
+  Column,
+  Table,
+} from "sequelize-typescript";
+import Document from "./Document";
+import User from "./User";
+import ParanoidModel from "./base/ParanoidModel";
+import Fix from "./decorators/Fix";
+
+@Table({ tableName: "comments", modelName: "comment" })
+@Fix
+class Comment extends ParanoidModel {
+  @Column(DataType.JSONB)
+  data: Record<string, any>;
+
+  // associations
+
+  @BelongsTo(() => User, "createdById")
+  createdBy: User;
+
+  @ForeignKey(() => User)
+  @Column(DataType.UUID)
+  createdById: string;
+
+  @BelongsTo(() => User, "resolvedById")
+  resolvedBy: User;
+
+  @ForeignKey(() => User)
+  @Column(DataType.UUID)
+  resolvedById: string;
+
+  @BelongsTo(() => Document, "documentId")
+  document: Document;
+
+  @ForeignKey(() => Document)
+  @Column(DataType.UUID)
+  documentId: string;
+
+  @BelongsTo(() => Comment, "parentCommentId")
+  parentComment: Comment;
+
+  @ForeignKey(() => Comment)
+  @Column(DataType.UUID)
+  parentCommentId: string;
+}
+
+export default Comment;
