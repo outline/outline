36f90b3a468b5740705193dca3ba45cae900b232
Tom Moor <tom.moor@gmail.com>
2022-03-10 18:50:28 -0800

    perf: Additional missing index

--- server/migrations/20220311020825-views-indexes.js
+++ server/migrations/20220311020825-views-indexes.js
@@ -8,10 +8,14 @@ module.exports = {
     await queryInterface.addIndex("views", ["updatedAt"], {
       name: "views_updated_at",
     });
+    await queryInterface.addIndex("collection_users", ["userId"], {
+      name: "collection_users_user_id",
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeIndex("views", "views_updated_at");
     await queryInterface.removeIndex("views", "views_user_id");
+    await queryInterface.removeIndex("collection_users", "collection_users_user_id");
   }
 };
