455998074c3aa6d03e0413b820c765b5b2b4cab3
Saumya Pandey <sp160899@gmail.com>
2022-03-16 00:24:05 +0530

    add tests for collectionImporter command

--- server/commands/collectionImporter.test.ts
+++ server/commands/collectionImporter.test.ts
@@ -82,4 +82,67 @@ describe("collectionImporter", () => {
       "Uploaded file does not contain importable documents"
     );
   });
+
+  it("should generate valid links in documents when the import zip has relatively linked documents", async () => {
+    const user = await buildUser();
+    const name = "Import-Export.zip";
+    const file = new File({
+      name,
+      type: "application/zip",
+      path: path.resolve(__dirname, "..", "test", "fixtures", name),
+    });
+    const response = await collectionImporter({
+      type: "outline",
+      user,
+      file,
+      ip,
+    });
+
+    expect(response.collections.length).toEqual(1);
+    expect(response.documents.length).toEqual(8);
+    expect(response.attachments.length).toEqual(2);
+    expect(await Collection.count()).toEqual(1);
+    expect(await Document.count()).toEqual(8);
+    expect(await Attachment.count()).toEqual(2);
+    const brainStorm = response.documents.find(
+      (doc) => doc.title === "Brainstorm"
+    );
+    const meetingNotes = response.documents.find(
+      (doc) => doc.title === "Meeting Notes"
+    );
+    const tables = response.documents.find((doc) => doc.title === "Tables");
+    const github = response.documents.find((doc) => doc.title === "GitHub");
+    const myfirstDoc = response.documents.find(
+      (doc) => doc.title === "My first document"
+    );
+    const codeTesting = response.documents.find(
+      (doc) => doc.title === "Code Testing"
+    );
+    const testDoc = response.documents.find((doc) => doc.title === "Test Doc");
+
+    expect(brainStorm).toBeDefined();
+    expect(meetingNotes).toBeDefined();
+    expect(tables).toBeDefined();
+    expect(github).toBeDefined();
+    expect(myfirstDoc).toBeDefined();
+    expect(codeTesting).toBeDefined();
+    expect(testDoc).toBeDefined();
+
+    expect(brainStorm?.text).toContain(
+      `- [ ] Add notes in [Meeting Notes](/doc/meeting-notes-${meetingNotes?.urlId}) of the last week all hands.`
+    );
+    expect(brainStorm?.text).toContain(
+      `- [ ] See If we can update the table in [Tables](/doc/tables-${tables?.urlId})`
+    );
+    expect(github?.text).toContain(
+      `We should do some [Code Testing](/doc/code-testing-${codeTesting?.urlId}) of the attached GitHub gist.`
+    );
+    expect(myfirstDoc?.text).toContain(
+      `This is a test link to [Test Doc](/doc/test-doc-${testDoc?.urlId})`
+    );
+    // My first doc has reference to itself, we don't do anything to it.
+    expect(myfirstDoc?.text).toContain(
+      "This is a test link to myself [My first document](/doc/my-first-document-NTU3G1Vbin)"
+    );
+  });
 });
