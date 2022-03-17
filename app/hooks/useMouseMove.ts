fac2f3a98313376f0ab138f793e96364bcbc033b
Saumya Pandey <sp160899@gmail.com>
2022-03-17 11:52:19 +0530

    fix: accept timeout parameter

--- app/hooks/useMouseMove.ts
+++ app/hooks/useMouseMove.ts
@@ -1,21 +1,26 @@
 import * as React from "react";
 
-const useMouseMove = (timeout: number = 5000) => {
+/**
+ * Hook to check if mouse is moving in the window
+ * @param {number} [timeout] - time in ms to wait before marking the mouse as not moving
+ * @returns {boolean} true if the mouse is moving, false otherwise
+ */
+const useMouseMove = (timeout = 5000) => {
   const [isMouseMoving, setIsMouseMoving] = React.useState(false);
-  const timeout = React.useRef<ReturnType<typeof setTimeout>>();
+  const timeoutId = React.useRef<ReturnType<typeof setTimeout>>();
 
-  const onMouseMove = () => {
-    timeout.current && clearTimeout(timeout.current);
+  const onMouseMove = React.useCallback(() => {
+    timeoutId.current && clearTimeout(timeoutId.current);
     setIsMouseMoving(true);
-    timeout.current = setTimeout(() => setIsMouseMoving(false), 5000);
-  };
+    timeoutId.current = setTimeout(() => setIsMouseMoving(false), timeout);
+  }, [timeout]);
 
   React.useEffect(() => {
     document.addEventListener("mousemove", onMouseMove);
     return () => {
       document.removeEventListener("mousemove", onMouseMove);
     };
-  }, []);
+  }, [onMouseMove]);
 
   return isMouseMoving;
 };
