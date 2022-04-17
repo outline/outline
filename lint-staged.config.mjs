af617ea5cd541d9c758b48c94d302ebda77dccf5
Tom Moor <tom.moor@gmail.com>
2022-04-17 12:39:04 -0700

    testing

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,14 +1,15 @@
 export default {
   // Run tsc on changes to TypeScript files
-  "**/*.ts?(x)": "tsc",
+  "**/*.ts?(x)": () => "tsc",
 
   // Efficiently run prettier and translation updates on changes to JS and
   // TypeScript files
   "**/*.[tj]s?(x)": [
     (f) => (f.length > 20 ? `yarn lint --fix` : `eslint ${f.join(" ")} --fix`),
-    (f) => `i18next --silent ${f.join(" ")} && yarn copy:i18n`,
+    (f) => `i18next --silent ${f.map((n) => `"${n}"`).join(" ")}`,
+    () => "yarn copy:i18n",
   ],
 
   // Automatically de-duplicate packages as yarn is terrible at it
-  "(yarn.lock|package.json)": `yarn yarn-deduplicate yarn.lock`,
+  "(yarn.lock|package.json)": () => `yarn yarn-deduplicate yarn.lock`,
 };
