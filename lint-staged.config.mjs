7d2a44301ecff5cc53f3900dbac9c55097e4d30c
Tom Moor <tom.moor@gmail.com>
2022-04-17 12:45:09 -0700

    Remove tsc

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,13 +1,10 @@
 export default {
-  // Run tsc on changes to TypeScript files
-  "**/*.ts?(x)": () => "tsc",
-
   // Efficiently run prettier and translation updates on changes to JS and
   // TypeScript files
   "**/*.[tj]s?(x)": [
     (f) => (f.length > 20 ? `yarn lint --fix` : `eslint ${f.join(" ")} --fix`),
     (f) => `i18next --silent ${f.map((n) => `"${n}"`).join(" ")}`,
-    () => "yarn copy:i18n",
+    () => "git add shared/i18n/locales/en_US/translation.json",
   ],
 
   // Automatically de-duplicate packages as yarn is terrible at it
