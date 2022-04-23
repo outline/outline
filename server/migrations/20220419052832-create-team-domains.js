9558f166cbf750fc3bbfe0de1aaf745e26f5bf64
Tom Moor <tom.moor@gmail.com>
2022-04-23 15:33:01 -0700

    migration

---
+++ server/migrations/20220419052832-create-team-domains.js
@@ -0,0 +1,52 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.createTable("team_domains", {
+        id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          primaryKey: true,
+        },
+        teamId: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          onDelete: "cascade",
+          references: {
+            model: "teams",
+          },
+        },
+        createdById: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          references: {
+            model: "users",
+          },
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      }, {
+        transaction
+      });
+
+      await queryInterface.addIndex("team_domains", ["teamId", "name"], {
+        transaction,
+        user: true,
+      });
+    });
+  },
+  down: async (queryInterface) => {
+    return queryInterface.dropTable("team_domains");
+  },
+};
