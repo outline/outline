a8dca0a0f18ae4563efde6e9130ee31fa1628216
Corey Alexander <coreyja@gmail.com>
2022-05-30 22:49:08 -0400

    Get a few checkboxes ready to go

--- app/scenes/WebhookSubscriptionNew.tsx
+++ app/scenes/WebhookSubscriptionNew.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, UseFormRegister } from "react-hook-form";
 import { useTranslation, Trans } from "react-i18next";
+import styled from "styled-components";
 import Button from "~/components/Button";
 import Flex from "~/components/Flex";
 import { ReactHookWrappedInput } from "~/components/Input";
@@ -12,17 +13,64 @@ type Props = {
   onSubmit: () => void;
 };
 
+type EventString = "*" | "docs.read" | "docs.write";
+
 interface FormData {
   name: string;
+  events: EventString[] | EventString;
+}
+
+const EventCheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+
+  padding: 0.5rem 0;
+`;
+
+const EventCheckboxText = styled.span`
+  margin-right: 0.5rem;
+`;
+
+function EventCheckbox({
+  label,
+  value,
+  onChange,
+  register,
+  disabled,
+}: {
+  label: string;
+  value: EventString;
+  onChange?: React.ChangeEventHandler;
+  register: UseFormRegister<FormData>;
+  disabled?: boolean;
+}) {
+  return (
+    <EventCheckboxLabel>
+      <EventCheckboxText>{label}</EventCheckboxText>
+      <input
+        type="checkbox"
+        value={value}
+        {...register("events", {
+          required: true,
+          disabled: disabled,
+          onChange,
+        })}
+      />
+    </EventCheckboxLabel>
+  );
 }
 
 function WebhookSubscriptionNew({ onSubmit }: Props) {
   const { webhookSubscriptions } = useStores();
   const { showToast } = useToasts();
   const { t } = useTranslation();
-  const { register, handleSubmit: formHandleSubmit, formState } = useForm<
-    FormData
-  >({ mode: "onChange" });
+  const {
+    register,
+    handleSubmit: formHandleSubmit,
+    formState,
+    watch,
+    setValue,
+  } = useForm<FormData>({ mode: "onChange" });
 
   const handleSubmit = React.useCallback(
     async (data: FormData) => {
@@ -37,6 +85,7 @@ function WebhookSubscriptionNew({ onSubmit }: Props) {
         );
         onSubmit();
       } catch (err) {
+        console.log(data, err);
         showToast(err.message, {
           type: "error",
         });
@@ -44,6 +93,21 @@ function WebhookSubscriptionNew({ onSubmit }: Props) {
     },
     [t, showToast, onSubmit, webhookSubscriptions]
   );
+
+  const onAllEventsChange = React.useCallback(() => {
+    const eventsOrig = watch("events");
+    const events = Array.isArray(eventsOrig) ? eventsOrig : [eventsOrig];
+
+    if (events.includes("*")) {
+      setValue("events", ["*"]);
+    }
+  }, [setValue, watch]);
+
+  const lastEventsOrig = watch("events");
+  const lastEvents = Array.isArray(lastEventsOrig)
+    ? lastEventsOrig
+    : [lastEventsOrig];
+  const isAllEventSelected = lastEvents.includes("*");
 
   return (
     <form onSubmit={formHandleSubmit(handleSubmit)}>
@@ -58,6 +122,24 @@ function WebhookSubscriptionNew({ onSubmit }: Props) {
           {...register("name", { required: true })}
         />
       </Flex>
+      <EventCheckbox
+        label={t("All events")}
+        value="*"
+        onChange={onAllEventsChange}
+        register={register}
+      />
+      <EventCheckbox
+        label={t("Read docs")}
+        value="docs.read"
+        register={register}
+        disabled={isAllEventSelected}
+      />
+      <EventCheckbox
+        label={t("Write docs")}
+        value="docs.write"
+        register={register}
+        disabled={isAllEventSelected}
+      />
       <Button
         type="submit"
         disabled={formState.isSubmitting || !formState.isValid}
