15780ebd36b0713b839d8e1a8a2e0dc3ab370f70
Corey Alexander <coreyja@gmail.com>
2022-06-13 00:23:44 -0400

    Get all the events added to the Frontend and work on styling them a bit, still needs some love though

--- app/scenes/WebhookSubscriptionNew.tsx
+++ app/scenes/WebhookSubscriptionNew.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation, Trans } from "react-i18next";
 import styled from "styled-components";
@@ -7,6 +7,7 @@ import Button from "~/components/Button";
 import Flex from "~/components/Flex";
 import { ReactHookWrappedInput } from "~/components/Input";
 import Text from "~/components/Text";
+import useMobile from "~/hooks/useMobile";
 import useStores from "~/hooks/useStores";
 import useToasts from "~/hooks/useToasts";
 
@@ -14,12 +15,10 @@ type Props = {
   onSubmit: () => void;
 };
 
-type EventString = "*" | "documents" | "documents.move" | "documents.delete";
-
 interface FormData {
   name: string;
   url: string;
-  events: EventString[];
+  events: string[];
 }
 
 const EventCheckboxLabel = styled.label`
@@ -27,11 +26,103 @@ const EventCheckboxLabel = styled.label`
   align-items: center;
 
   padding: 0.5rem 0;
+
+  &.group-label {
+    font-weight: bold;
+    font-size: 1.2em;
+  }
 `;
 
 const EventCheckboxText = styled.span`
   margin-right: 0.5rem;
 `;
+
+const WEBHOOK_EVENTS = {
+  users: [
+    "users.create",
+    "users.signin",
+    "users.update",
+    "users.suspend",
+    "users.activate",
+    "users.delete",
+    "users.invite",
+  ],
+  documents: [
+    "documents.create",
+    "documents.publish",
+    "documents.unpublish",
+    "documents.delete",
+    "documents.permanent_delete",
+    "documents.archive",
+    "documents.unarchive",
+    "documents.restore",
+    "documents.star",
+    "documents.unstar",
+    "documents.move",
+    "documents.update",
+    "documents.update.delayed",
+    "documents.update.debounced",
+    "documents.title_change",
+  ],
+  revisions: ["revisions.create"],
+  fileOperations: [
+    "file_operations.create",
+    "file_operations.update",
+    "file_operations.delete",
+  ],
+  collections: [
+    "collections.create",
+    "collections.update",
+    "collections.delete",
+    "collections.add_user",
+    "collections.remove_user",
+    "collections.add_group",
+    "collections.remove_group",
+    "collections.move",
+    "collections.permission_changed",
+  ],
+  groups: [
+    "groups.create",
+    "groups.update",
+    "groups.delete",
+    "groups.add_user",
+    "groups.remove_user",
+  ],
+  integrations: ["integrations.create", "integrations.update"],
+  teams: ["teams.update"],
+  pins: ["pins.create", "pins.update", "pins.delete"],
+};
+
+const FieldSet = styled.fieldset`
+  border: none;
+
+  &.disabled {
+    opacity: 50%;
+  }
+`;
+
+const AllFieldSet = styled(FieldSet)`
+  padding-left: 0;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  &.mobile {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const GroupWrapper = styled.div`
+  padding: 2rem 0;
+
+  &.mobile {
+    padding: 1rem 0;
+  }
+`;
+
+function joinClasses(...classes: (string | boolean | undefined)[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 function WebhookSubscriptionNew({ onSubmit }: Props) {
   const { webhookSubscriptions } = useStores();
@@ -43,7 +134,7 @@ function WebhookSubscriptionNew({ onSubmit }: Props) {
     formState,
     watch,
     setValue,
-  } = useForm<FormData>({ mode: "onTouched", defaultValues: { events: [] } });
+  } = useForm<FormData>({ mode: "all", defaultValues: { events: [] } });
 
   const handleSubmit = React.useCallback(
     async (data: FormData) => {
@@ -73,40 +164,30 @@ function WebhookSubscriptionNew({ onSubmit }: Props) {
 
   const events = watch("events");
   const isAllEventSelected = events && events.includes("*");
-  const isDocsRootSelected = events && events.includes("documents");
+
+  const isGroupAllSelected = useCallback(
+    (group: string) => events && events.includes(group),
+    [events]
+  );
+
+  const isMobile = useMobile();
 
   useEffect(() => {
     if (isAllEventSelected) {
+      console.log("setting value to *");
       setValue("events", ["*"]);
     }
   }, [isAllEventSelected, setValue]);
 
-  useEffect(() => {
-    if (isDocsRootSelected && events.some((e) => e.startsWith("documents."))) {
-      setValue(
-        "events",
-        events.filter((e) => !e.startsWith("documents."))
-      );
-    }
-  }, [isDocsRootSelected, setValue, events]);
-
-  function EventCheckbox({
-    label,
-    value,
-  }: {
-    label: string;
-    value: EventString;
-  }) {
+  function EventCheckbox({ label, value }: { label: string; value: string }) {
     return (
-      <EventCheckboxLabel>
+      <EventCheckboxLabel
+        className={
+          Object.keys(WEBHOOK_EVENTS).includes(value) ? "group-label" : ""
+        }
+      >
         <EventCheckboxText>{label}</EventCheckboxText>
-        <input
-          type="checkbox"
-          value={value}
-          {...register("events", {
-            required: true,
-          })}
-        />
+        <input type="checkbox" value={value} {...register("events", {})} />
       </EventCheckboxLabel>
     );
   }
@@ -133,16 +214,27 @@ function WebhookSubscriptionNew({ onSubmit }: Props) {
         />
       </Flex>
       <EventCheckbox label={t("All events")} value="*" />
-      <fieldset disabled={isAllEventSelected}>
-        <EventCheckbox label={t("All Document Events")} value="documents" />
-        <fieldset disabled={isDocsRootSelected}>
-          <EventCheckbox label={t("Documents Moved")} value="documents.move" />
-          <EventCheckbox
-            label={t("Documents Deleted")}
-            value="documents.delete"
-          />
-        </fieldset>
-      </fieldset>
+      <AllFieldSet
+        disabled={isAllEventSelected}
+        className={joinClasses(
+          isAllEventSelected && "disabled",
+          isMobile && "mobile"
+        )}
+      >
+        {Object.entries(WEBHOOK_EVENTS).map(([group, events], i) => (
+          <GroupWrapper key={i} className={joinClasses(isMobile && "mobile")}>
+            <EventCheckbox label={t(`All ${group} Events`)} value={group} />
+            <FieldSet
+              className={isGroupAllSelected(group) ? "disabled" : ""}
+              disabled={isGroupAllSelected(group)}
+            >
+              {events.map((event) => (
+                <EventCheckbox label={event} value={event} />
+              ))}
+            </FieldSet>
+          </GroupWrapper>
+        ))}
+      </AllFieldSet>
       <Button
         type="submit"
         disabled={formState.isSubmitting || !formState.isValid}
