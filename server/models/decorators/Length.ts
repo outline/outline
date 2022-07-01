d0243b0739eb9cea4c127b5434e95ee60e905cc0
CuriousCorrelation <curiouscorrelation@protonmail.com>
2022-07-01 22:23:09 +0530

    fix: Length decorator function return type

--- server/models/decorators/Length.ts
+++ server/models/decorators/Length.ts
@@ -5,15 +5,23 @@ import { addAttributeOptions } from "sequelize-typescript";
  * A decorator that calculates size of the string based on lodash's size function.
  * particularly useful for strings with unicode characters of variable lengths.
  */
-export function Length({ msg, min, max }: { msg?: string; min: number; max: number }): Function {
-    return (target: any, propertyName: string) =>
-        addAttributeOptions(target, propertyName, {
-            validate: {
-                validLength(value: string) {
-                    if (size(value) > max || size(value) < min) {
-                        throw new Error(msg);
-                    }
-                }
-            },
-        });
+export function Length({
+  msg,
+  min,
+  max,
+}: {
+  msg?: string;
+  min: number;
+  max: number;
+}): (target: any, propertyName: string) => void {
+  return (target: any, propertyName: string) =>
+    addAttributeOptions(target, propertyName, {
+      validate: {
+        validLength(value: string) {
+          if (size(value) > max || size(value) < min) {
+            throw new Error(msg);
+          }
+        },
+      },
+    });
 }
