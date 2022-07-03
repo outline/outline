27978b8fc4797da7ff8e653b26c2faf9f571b5d3
Tom Moor <tom.moor@gmail.com>
2022-07-03 14:16:49 +0200

    fix: Remove teams.create from audit events

--- server/models/Event.ts
+++ server/models/Event.ts
@@ -156,7 +156,6 @@ class Event extends IdModel {
     "shares.create",
     "shares.update",
     "shares.revoke",
-    "teams.create",
     "teams.update",
     "users.create",
     "users.update",
