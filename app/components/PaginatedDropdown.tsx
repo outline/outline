fada7032be4e5cfbbefe391a851d2df91244b6d5
CuriousCorrelation <curiouscorrelation@protonmail.com>
2022-07-10 16:55:07 +0530

    Add `PaginatedDropdown` component
    Based on `FilterOptions.tsx`
    
    TODO: Replace `FilterOptions` with `PaginatedDropdown`
    in `UserFilter.tsx`

---
+++ app/components/PaginatedDropdown.tsx
@@ -0,0 +1,118 @@
+import * as React from "react";
+import { useMenuState, MenuButton } from "reakit/Menu";
+import styled from "styled-components";
+import Button, { Inner } from "~/components/Button";
+import ContextMenu from "~/components/ContextMenu";
+import MenuItem from "~/components/ContextMenu/MenuItem";
+import Text from "~/components/Text";
+import InputSearch from "./InputSearch";
+
+type TFilterOption = {
+  key: string;
+  label: string;
+  note?: string;
+};
+
+type Props = {
+  options: TFilterOption[];
+  activeKey: string | null | undefined;
+  defaultLabel?: string;
+  selectedPrefix?: string;
+  className?: string;
+  onSelect: (key: string | null | undefined) => void;
+};
+
+const PaginatedDropdown = ({
+  options,
+  activeKey = "",
+  defaultLabel = "Filter options",
+  selectedPrefix = "",
+  className,
+  onSelect,
+}: Props) => {
+  const menu = useMenuState({
+    modal: true,
+  });
+  const selected =
+    options.find((option) => option.key === activeKey) || options[0];
+
+  const selectedLabel = selected ? `${selectedPrefix} ${selected.label}` : "";
+
+    const handleOnChange = () => {};
+    const handleOnFocus = () => {};
+
+  return (
+    <Wrapper>
+      <MenuButton {...menu}>
+        {(props) => (
+          <StyledButton {...props} className={className} neutral disclosure>
+            {activeKey ? selectedLabel : defaultLabel}
+          </StyledButton>
+        )}
+      </MenuButton>
+      <ContextMenu aria-label={defaultLabel} {...menu}>
+        <InputSearch onChange={handleOnChange} onFocus={handleOnFocus} />
+        {options.map((option) => (
+          <MenuItem
+            key={option.key}
+            onClick={() => {
+              onSelect(option.key);
+              menu.hide();
+            }}
+            selected={option.key === activeKey}
+            {...menu}
+          >
+            {option.note ? (
+              <LabelWithNote>
+                {option.label}
+                <Note>{option.note}</Note>
+              </LabelWithNote>
+            ) : (
+              option.label
+            )}
+          </MenuItem>
+        ))}
+      </ContextMenu>
+    </Wrapper>
+  );
+};
+
+const Note = styled(Text)`
+  margin-top: 2px;
+  margin-bottom: 0;
+  line-height: 1.2em;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${(props) => props.theme.textTertiary};
+`;
+
+const LabelWithNote = styled.div`
+  font-weight: 500;
+  text-align: left;
+
+  &:hover ${Note} {
+    color: ${(props) => props.theme.white50};
+  }
+`;
+
+const StyledButton = styled(Button)`
+  box-shadow: none;
+  text-transform: none;
+  border-color: transparent;
+  height: auto;
+
+  &:hover {
+    background: transparent;
+  }
+
+  ${Inner} {
+    line-height: 24px;
+    min-height: auto;
+  }
+`;
+
+const Wrapper = styled.div`
+  margin-right: 8px;
+`;
+
+export default PaginatedDropdown;
