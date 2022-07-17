b8b2d3402576af9a22d141e7fbf2082c74378371
Tom Moor <tom.moor@gmail.com>
2022-07-17 17:28:45 +0100

    tidy

--- shared/editor/components/Video.tsx
+++ shared/editor/components/Video.tsx
@@ -2,24 +2,15 @@ import * as React from "react";
 import styled, { DefaultTheme, ThemeProps } from "styled-components";
 
 type Props = {
-  title: React.ReactNode;
-  context?: React.ReactNode;
+  title: string;
   href: string;
   isSelected: boolean;
-  children?: React.ReactNode;
 };
 
 export default function Video(props: Props & ThemeProps<DefaultTheme>) {
   return (
-    <Wrapper
-      className={
-        props.isSelected ? "ProseMirror-selectednode widget" : "widget"
-      }
-    >
-      <NativeVideo src={props.href} controls />
-      <Preview>
-        <Title>{props.title}</Title>
-      </Preview>
+    <Wrapper className={props.isSelected ? "ProseMirror-selectednode" : ""}>
+      <NativeVideo src={props.href} title={props.title} controls />
     </Wrapper>
   );
 }
@@ -27,21 +18,6 @@ export default function Video(props: Props & ThemeProps<DefaultTheme>) {
 const NativeVideo = styled.video`
   max-width: 100%;
   border-radius: 8px;
-`;
-
-const Title = styled.strong`
-  font-weight: 500;
-  font-size: 14px;
-  color: ${(props) => props.theme.text};
-`;
-
-const Preview = styled.div`
-  gap: 8px;
-  display: flex;
-  flex-direction: row;
-  flex-grow: 1;
-  align-items: center;
-  color: ${(props) => props.theme.textTertiary};
 `;
 
 const Wrapper = styled.div`
