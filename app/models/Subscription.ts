7f2b6add0e461aeec55839d918405c5dfc6cdc68
CuriousCorrelation <curiouscorrelation@protonmail.com>
2022-07-20 21:17:51 +0530

    feat(app): Add `Subscription` model

---
+++ app/models/Subscription.ts
@@ -0,0 +1,31 @@
+import { observable } from "mobx";
+import BaseModel from "./BaseModel";
+import Field from "./decorators/Field";
+
+class Subscription extends BaseModel {
+  @Field
+  @observable
+  id: string;
+
+  userId: string;
+
+  documentId: string;
+
+  enabled: boolean;
+
+  createdAt: string;
+
+  updatedAt: string;
+
+  next(): Subscription | undefined {
+    const index = this.store.orderedData.indexOf(this);
+    return this.store.orderedData[index + 1];
+  }
+
+  previous(): Subscription | undefined {
+    const index = this.store.orderedData.indexOf(this);
+    return this.store.orderedData[index + 1];
+  }
+}
+
+export default Subscription;
