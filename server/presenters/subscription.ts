fefeb87119e38301868a94838f243cca4c6bff23
CuriousCorrelation <curiouscorrelation@protonmail.com>
2022-07-21 21:34:41 +0530

    fix(server): `Subscription` presenter missing event

--- server/presenters/subscription.ts
+++ server/presenters/subscription.ts
@@ -5,6 +5,7 @@ export default function present(subscription: Subscription) {
     id: subscription.id,
     userId: subscription.userId,
     documentId: subscription.documentId,
+    event: subscription.event,
     enabled: subscription.enabled,
     createdAt: subscription.createdAt,
     updatedAt: subscription.updatedAt,
