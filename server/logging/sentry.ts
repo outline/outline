2794057738ee75093aecf6fae2774b66ee813380
Tom Moor <tom.moor@gmail.com>
2022-07-26 22:06:47 +0100

    fix: Sequelize rejectOnEmpty should result in 404 status

--- server/logging/sentry.ts
+++ server/logging/sentry.ts
@@ -14,6 +14,7 @@ if (env.SENTRY_DSN) {
       // Validation
       "BadRequestError",
       "SequelizeValidationError",
+      "SequelizeEmptyResultError",
       "ValidationError",
 
       // Authentication
