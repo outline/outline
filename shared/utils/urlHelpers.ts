27e8e04281203611a7ffd8b2b875d50b027b5f27
Frank <frankli0324@hotmail.com>
2022-07-26 11:43:05 +0800

    mount signin

--- shared/utils/urlHelpers.ts
+++ shared/utils/urlHelpers.ts
@@ -49,7 +49,7 @@ export function changelogUrl(): string {
 }
 
 export function signin(service = "slack"): string {
-  return `/auth/${service}`;
+  return `${env.URL}/auth/${service}`;
 }
 
 export const SLUG_URL_REGEX = /^(?:[0-9a-zA-Z-_~]*-)?([a-zA-Z0-9]{10,15})$/;
