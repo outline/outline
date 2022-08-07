1f45fb0d207c3d402861218c58d9e5caaf627e0e
Jonathan Harrington <jonathan@harringtons.me>
2022-08-07 09:52:11 +0100

    Change Grist embed to use function based API

--- shared/editor/embeds/Grist.tsx
+++ shared/editor/embeds/Grist.tsx
@@ -1,17 +1,31 @@
 import * as React from "react";
 import Frame from "../components/Frame";
+import Image from "../components/Image";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = new RegExp(
-  "^https?://([a-z.-]+\\.)?getgrist\\.com/(.*)/p/([0-9]+)\\?embed=true$"
-);
-
-export default class Grist extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    return (
-      <Frame {...this.props} src={this.props.attrs.href} title="Grist Embed" />
-    );
-  }
+function Grist(props: Props) {
+  return (
+    <Frame
+      {...props}
+      src={props.attrs.href}
+      icon={
+        <Image
+          src="/images/grist.png"
+          alt="Grist Icon"
+          width={16}
+          height={16}
+        />
+      }
+      title="Grist Spreadsheet"
+      border
+    />
+  );
 }
+
+Grist.ENABLED = [
+  new RegExp(
+    "^https?://([a-z.-]+\\.)?getgrist\\.com/(.*)/p/([0-9]+)\\?embed=true$"
+  ),
+];
+
+export default Grist;
