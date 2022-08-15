ac3ec941cf8208378261ac8723c995a7a4bfcc6e
Apoorv Mishra <apoorvmishra101092@gmail.com>
2022-08-15 22:51:05 +0530

    chore: refactor server test setup
    * Close dangling redis connections instead of mocking rate limiter
      specific modules
    * Segregate pre and post env test setup

---
+++ server/test/env.ts
@@ -0,0 +1,14 @@
+import env from "../env";
+
+// test environment variables
+env.SMTP_HOST = "smtp.example.com";
+env.ENVIRONMENT = "test";
+env.GOOGLE_CLIENT_ID = "123";
+env.GOOGLE_CLIENT_SECRET = "123";
+env.SLACK_CLIENT_ID = "123";
+env.SLACK_CLIENT_SECRET = "123";
+env.DEPLOYMENT = undefined;
+
+if (process.env.DATABASE_URL_TEST) {
+  env.DATABASE_URL = process.env.DATABASE_URL_TEST;
+}
