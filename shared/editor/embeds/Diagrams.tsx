e1f67f5903dc9a2496686f99f88ba7da7d326eef
Apoorv Mishra <apoorvmishra101092@gmail.com>
2022-08-18 00:37:02 +0530

    chore: refactor into functional component

--- shared/editor/embeds/Diagrams.tsx
+++ shared/editor/embeds/Diagrams.tsx
@@ -3,43 +3,34 @@ import Frame from "../components/Frame";
 import Image from "../components/Image";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = /^https:\/\/viewer\.diagrams\.net\/(?!proxy).*(title=\\w+)?/;
+function Diagrams(props: Props) {
+  const embedUrl = props.attrs.matches[0];
+  const params = new URL(embedUrl).searchParams;
+  const title = params.get("title")
+    ? `Diagrams.net (${params.get("title")})`
+    : "Diagrams.net";
 
-export default class Diagrams extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  get embedUrl() {
-    return this.props.attrs.matches[0];
-  }
-
-  get title() {
-    let title = "Diagrams.net";
-    const url = new URL(this.embedUrl);
-    const documentTitle = url.searchParams.get("title");
-
-    if (documentTitle) {
-      title += ` (${documentTitle})`;
-    }
-
-    return title;
-  }
-
-  render() {
-    return (
-      <Frame
-        {...this.props}
-        src={this.embedUrl}
-        title={this.title}
-        border
-        icon={
-          <Image
-            src="/images/diagrams.png"
-            alt="Diagrams.net"
-            width={16}
-            height={16}
-          />
-        }
-      />
-    );
-  }
+  return (
+    <Frame
+      {...props}
+      src={embedUrl}
+      icon={
+        <Image
+          src="/images/diagrams.png"
+          alt="Diagrams.net"
+          width={16}
+          height={16}
+        />
+      }
+      canonicalUrl={props.attrs.href}
+      title={title}
+      border
+    />
+  );
 }
+
+Diagrams.ENABLED = [
+  /^https:\/\/viewer\.diagrams\.net\/(?!proxy).*(title=\\w+)?/,
+];
+
+export default Diagrams;
