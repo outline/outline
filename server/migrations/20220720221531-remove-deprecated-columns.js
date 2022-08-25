e91cbe0e4108814b5e62183a252a4e347b4f2964
Tom Moor <tom.moor@gmail.com>
2022-08-25 20:39:46 +0200

    Add guard for upgrading past v0.54.0

--- server/migrations/20220720221531-remove-deprecated-columns.js
+++ server/migrations/20220720221531-remove-deprecated-columns.js
@@ -3,6 +3,23 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.sequelize.transaction(async (transaction) => {
+      // This guard prevents the migration from running and destroying this
+      // service data if the script from v0.54.0 has not yet been run.
+      if (process.env.DEPLOYMENT !== "hosted") {
+        const [teams] = await queryInterface.sequelize.query(
+          `SELECT COUNT(*) FROM teams`,
+          { transaction }
+        );
+        const [authenticationProviders] = await queryInterface.sequelize.query(
+          `SELECT COUNT(*) FROM authentication_providers`,
+          { transaction }
+        );
+
+        if (teams[0].count > 0 && authenticationProviders[0].count === 0) {
+          throw Error("Refusing to destroy deprecated columns without authentication providers");
+        }
+      }
+
       await queryInterface.removeColumn("attachments", "url", { transaction });
       await queryInterface.removeColumn("users", "service", { transaction });
       await queryInterface.removeColumn("users", "serviceId", { transaction });
