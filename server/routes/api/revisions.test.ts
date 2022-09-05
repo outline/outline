bfaed5d9cb40c3ac5a62629c7ee17dc69ca4d464
Tom Moor <tom.moor@gmail.com>
2022-09-05 15:38:00 +0200

    test

--- server/routes/api/revisions.test.ts
+++ server/routes/api/revisions.test.ts
@@ -39,6 +39,91 @@ describe("#revisions.info", () => {
   });
 });
 
+describe("#revisions.diff", () => {
+  it("should return the document HTML if no previous revision", async () => {
+    const { user, document } = await seed();
+    const revision = await Revision.createFromDocument(document);
+    const res = await server.post("/api/revisions.diff", {
+      body: {
+        token: user.getJwtToken(),
+        id: revision.id,
+      },
+    });
+    const body = await res.json();
+    expect(res.status).toEqual(200);
+
+    // Can't compare entire HTML output due to generated class names
+    expect(body.data).toContain("<html");
+    expect(body.data).toContain("<style");
+    expect(body.data).toContain("<h1");
+    expect(body.data).not.toContain("<ins");
+    expect(body.data).not.toContain("<del");
+    expect(body.data).toContain(document.title);
+  });
+
+  it("should allow returning HTML directly with accept header", async () => {
+    const { user, document } = await seed();
+    const revision = await Revision.createFromDocument(document);
+    const res = await server.post("/api/revisions.diff", {
+      body: {
+        token: user.getJwtToken(),
+        id: revision.id,
+      },
+      headers: {
+        accept: "text/html",
+      },
+    });
+    const body = await res.text();
+    expect(res.status).toEqual(200);
+
+    // Can't compare entire HTML output due to generated class names
+    expect(body).toContain("<html");
+    expect(body).toContain("<style");
+    expect(body).toContain("<h1");
+    expect(body).not.toContain("<ins");
+    expect(body).not.toContain("<del");
+    expect(body).toContain(document.title);
+  });
+
+  it("should compare to previous revision by default", async () => {
+    const { user, document } = await seed();
+    await Revision.createFromDocument(document);
+
+    await document.update({ text: "New text" });
+    const revision1 = await Revision.createFromDocument(document);
+
+    const res = await server.post("/api/revisions.diff", {
+      body: {
+        token: user.getJwtToken(),
+        id: revision1.id,
+      },
+    });
+    const body = await res.json();
+    expect(res.status).toEqual(200);
+
+    // Can't compare entire HTML output due to generated class names
+    expect(body.data).toContain("<html");
+    expect(body.data).toContain("<style");
+    expect(body.data).toContain("<h1");
+    expect(body.data).toContain("<ins");
+    expect(body.data).toContain("<del");
+    expect(body.data).toContain(document.title);
+  });
+
+  it("should require authorization", async () => {
+    const document = await buildDocument();
+    const revision = await Revision.createFromDocument(document);
+    const user = await buildUser();
+    const res = await server.post("/api/revisions.diff", {
+      body: {
+        token: user.getJwtToken(),
+        id: revision.id,
+      },
+    });
+    expect(res.status).toEqual(403);
+  });
+});
+
 describe("#revisions.list", () => {
   it("should return a document's revisions", async () => {
     const { user, document } = await seed();
