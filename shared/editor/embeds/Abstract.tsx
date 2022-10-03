a9c08206f9172bd0ea68c1d78fb7b1dc8234a97f
Peter Kompasz <kompaszbence@gmail.com>
2022-10-03 09:42:23 +0300

    Refractor Abstract class component to functional

--- shared/editor/embeds/Abstract.tsx
+++ shared/editor/embeds/Abstract.tsx
@@ -2,21 +2,21 @@ import * as React from "react";
 import Frame from "../components/Frame";
 import { EmbedProps as Props } from ".";
 
-export default class Abstract extends React.Component<Props> {
-  static ENABLED = [
-    new RegExp("https?://share\\.(?:go)?abstract\\.com/(.*)$"),
-    new RegExp("https?://app\\.(?:go)?abstract\\.com/(?:share|embed)/(.*)$"),
-  ];
-
-  render() {
-    const { matches } = this.props.attrs;
-    const shareId = matches[1];
-    return (
-      <Frame
-        {...this.props}
-        src={`https://app.goabstract.com/embed/${shareId}`}
-        title={`Abstract (${shareId})`}
-      />
-    );
-  }
+function Abstract(props: Props) {
+  const { matches } = props.attrs;
+  const shareId = matches[1];
+  return (
+    <Frame
+      {...props}
+      src={`https://app.goabstract.com/embed/${shareId}`}
+      title={`Abstract (${shareId})`}
+    />
+  );
 }
+
+Abstract.ENABLED = [
+  new RegExp("https?://share\\.(?:go)?abstract\\.com/(.*)$"),
+  new RegExp("https?://app\\.(?:go)?abstract\\.com/(?:share|embed)/(.*)$"),
+];
+
+export default Abstract;
