86e43f644f3961946fced793d11dae2ed6390cc6
Peter Kompasz <kompaszbence@gmail.com>
2022-10-04 13:08:54 +0300

    Refractor Airtable class component to functional

--- shared/editor/embeds/Airtable.tsx
+++ shared/editor/embeds/Airtable.tsx
@@ -4,19 +4,19 @@ import { EmbedProps as Props } from ".";
 
 const URL_REGEX = new RegExp("https://airtable.com/(?:embed/)?(shr.*)$");
 
-export default class Airtable extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    const { matches } = this.props.attrs;
-    const shareId = matches[1];
-    return (
-      <Frame
-        {...this.props}
-        src={`https://airtable.com/embed/${shareId}`}
-        title={`Airtable (${shareId})`}
-        border
-      />
-    );
-  }
+function Airtable(props: Props) {
+  const { matches } = props.attrs;
+  const shareId = matches[1];
+  return (
+    <Frame
+      {...props}
+      src={`https://airtable.com/embed/${shareId}`}
+      title={`Airtable (${shareId})`}
+      border
+    />
+  );
 }
+
+Airtable.ENABLED = [URL_REGEX];
+
+export default Airtable;
