c8f990018ca2593cb47b958ac80d8b70f3cc702d
pbkompasz <47194071+pbkompasz@users.noreply.github.com>
2022-10-08 16:50:08 +0300

    Refactor DBDiagram class component to functional (#4228)

---
+++ shared/editor/embeds/Berrycast.test.ts
@@ -0,0 +1,8 @@
+import Berrycast from "./Berrycast";
+
+describe("Berrycast", () => {
+  Berrycast.ENABLED[0];
+
+  // TODO
+  test.todo("");
+});
--- shared/editor/embeds/DBDiagram.tsx
+++ shared/editor/embeds/DBDiagram.tsx
@@ -2,21 +2,19 @@ import * as React from "react";
 import Frame from "../components/Frame";
 import { EmbedProps as Props } from ".";
 
-export default class DBDiagram extends React.Component<Props> {
-  static ENABLED = [new RegExp("https://dbdiagram.io/(embed|d)/(\\w+)$")];
+export default function DBDiagram(props: Props) {
+  const { matches } = props.attrs;
+  const shareId = matches[2];
 
-  render() {
-    const { matches } = this.props.attrs;
-    const shareId = matches[2];
-
-    return (
-      <Frame
-        {...this.props}
-        src={`https://dbdiagram.io/embed/${shareId}`}
-        title={`DBDiagram (${shareId})`}
-        width="100%"
-        height="400px"
-      />
-    );
-  }
+  return (
+    <Frame
+      {...props}
+      src={`https://dbdiagram.io/embed/${shareId}`}
+      title={`DBDiagram (${shareId})`}
+      width="100%"
+      height="400px"
+    />
+  );
 }
+
+DBDiagram.ENABLED = [new RegExp("https://dbdiagram.io/(embed|d)/(\\w+)$")];
