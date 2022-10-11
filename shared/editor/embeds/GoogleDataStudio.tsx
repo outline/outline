d3513093d3f43e66c655de801ca7884534dde91f
Quincy Mast <email.quincymast@gmail.com>
2022-10-11 14:42:08 +0000

    Functional Component Refactor: GoogleDataStudio

--- shared/editor/embeds/GoogleDataStudio.tsx
+++ shared/editor/embeds/GoogleDataStudio.tsx
@@ -3,30 +3,30 @@ import Frame from "../components/Frame";
 import Image from "../components/Image";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = new RegExp(
-  "^https?://datastudio\\.google\\.com/(embed|u/0)/reporting/(.*)/page/(.*)(/edit)?$"
-);
-
-export default class GoogleDataStudio extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    return (
-      <Frame
-        {...this.props}
-        src={this.props.attrs.href.replace("u/0", "embed").replace("/edit", "")}
-        icon={
-          <Image
-            src="/images/google-datastudio.png"
-            alt="Google Data Studio Icon"
-            width={16}
-            height={16}
-          />
-        }
-        canonicalUrl={this.props.attrs.href}
-        title="Google Data Studio"
-        border
-      />
-    );
-  }
+function GoogleDataStudio(props: Props) {
+  return (
+    <Frame
+      {...props}
+      src={props.attrs.href.replace("u/0", "embed").replace("/edit", "")}
+      icon={
+        <Image
+          src="/images/google-datastudio.png"
+          alt="Google Data Studio Icon"
+          width={16}
+          height={16}
+        />
+      }
+      canonicalUrl={props.attrs.href}
+      title="Google Data Studio"
+      border
+    />
+  );
 }
+
+GoogleDataStudio.ENABLED = [
+  new RegExp(
+    "^https?://datastudio\\.google\\.com/(embed|u/0)/reporting/(.*)/page/(.*)(/edit)?$"
+  ),
+];
+
+export default GoogleDataStudio;
