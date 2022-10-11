91a0d1afd08187977cd5c7ba1f5487ff953a6145
Quincy Mast <email.quincymast@gmail.com>
2022-10-11 14:57:41 +0000

    Functional Component Refactor: Vimeo

--- shared/editor/embeds/Vimeo.tsx
+++ shared/editor/embeds/Vimeo.tsx
@@ -2,27 +2,27 @@ import * as React from "react";
 import Frame from "../components/Frame";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = /(http|https)?:\/\/(www\.)?vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/([^/]*)\/videos\/|)(\d+)(?:\/|\?)?([\d\w]+)?/;
+function Vimeo(props: Props) {
+  const { matches } = props.attrs;
+  const videoId = matches[4];
+  const hId = matches[5];
 
-export default class Vimeo extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    const { matches } = this.props.attrs;
-    const videoId = matches[4];
-    const hId = matches[5];
-
-    return (
-      <Frame
-        {...this.props}
-        src={`https://player.vimeo.com/video/${videoId}?byline=0${
-          hId ? `&h=${hId}` : ""
-        }`}
-        title={`Vimeo Embed (${videoId})`}
-        height="412px"
-        border={false}
-        referrerPolicy="origin"
-      />
-    );
-  }
+  return (
+    <Frame
+      {...props}
+      src={`https://player.vimeo.com/video/${videoId}?byline=0${
+        hId ? `&h=${hId}` : ""
+      }`}
+      title={`Vimeo Embed (${videoId})`}
+      height="412px"
+      border={false}
+      referrerPolicy="origin"
+    />
+  );
 }
+
+Vimeo.ENABLED = [
+  /(http|https)?:\/\/(www\.)?vimeo\.com\/(?:channels\/(?:\w+\/)?|groups\/([^/]*)\/videos\/|)(\d+)(?:\/|\?)?([\d\w]+)?/,
+];
+
+export default Vimeo;
