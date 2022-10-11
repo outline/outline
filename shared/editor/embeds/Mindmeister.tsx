7bfbaca857d0af758c2863f52d43444f4efe7c3b
Quincy Mast <email.quincymast@gmail.com>
2022-10-11 14:51:24 +0000

    Functional Component Refactor: Mindmeister

--- shared/editor/embeds/Mindmeister.tsx
+++ shared/editor/embeds/Mindmeister.tsx
@@ -2,25 +2,25 @@ import * as React from "react";
 import Frame from "../components/Frame";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = new RegExp(
-  "^https://([w.-]+\\.)?(mindmeister\\.com|mm\\.tt)(/maps/public_map_shell)?/(\\d+)(\\?t=.*)?(/.*)?$"
-);
-
-export default class Mindmeister extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    const chartId =
-      this.props.attrs.matches[4] +
-      (this.props.attrs.matches[5] || "") +
-      (this.props.attrs.matches[6] || "");
-    return (
-      <Frame
-        {...this.props}
-        src={`https://www.mindmeister.com/maps/public_map_shell/${chartId}`}
-        title="Mindmeister Embed"
-        border
-      />
-    );
-  }
+function Mindmeister(props: Props) {
+  const chartId =
+    props.attrs.matches[4] +
+    (props.attrs.matches[5] || "") +
+    (props.attrs.matches[6] || "");
+  return (
+    <Frame
+      {...props}
+      src={`https://www.mindmeister.com/maps/public_map_shell/${chartId}`}
+      title="Mindmeister Embed"
+      border
+    />
+  );
 }
+
+Mindmeister.ENABLED = [
+  new RegExp(
+    "^https://([w.-]+\\.)?(mindmeister\\.com|mm\\.tt)(/maps/public_map_shell)?/(\\d+)(\\?t=.*)?(/.*)?$"
+  ),
+];
+
+export default Mindmeister;
