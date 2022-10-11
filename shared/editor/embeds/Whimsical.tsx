169f0454e815fa51c5fcdae314730d2b438d34ba
Quincy Mast <email.quincymast@gmail.com>
2022-10-11 14:58:00 +0000

    Functional Component Refactor: Whimsical

--- shared/editor/embeds/Whimsical.tsx
+++ shared/editor/embeds/Whimsical.tsx
@@ -2,21 +2,21 @@ import * as React from "react";
 import Frame from "../components/Frame";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = /^https?:\/\/whimsical\.com\/[0-9a-zA-Z-_~]*-([a-zA-Z0-9]+)\/?$/;
+function Whimsical(props: Props) {
+  const { matches } = props.attrs;
+  const boardId = matches[1];
 
-export default class Whimsical extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    const { matches } = this.props.attrs;
-    const boardId = matches[1];
-
-    return (
-      <Frame
-        {...this.props}
-        src={`https://whimsical.com/embed/${boardId}`}
-        title="Whimsical"
-      />
-    );
-  }
+  return (
+    <Frame
+      {...props}
+      src={`https://whimsical.com/embed/${boardId}`}
+      title="Whimsical"
+    />
+  );
 }
+
+Whimsical.ENABLED = [
+  /^https?:\/\/whimsical\.com\/[0-9a-zA-Z-_~]*-([a-zA-Z0-9]+)\/?$/,
+];
+
+export default Whimsical;
