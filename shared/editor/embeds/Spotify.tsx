e726858b65ff8266d3f63a6c1e25637a2e9bb898
Quincy Mast <email.quincymast@gmail.com>
2022-10-11 14:56:28 +0000

    Functional Component Refactor: Spotify

--- shared/editor/embeds/Spotify.tsx
+++ shared/editor/embeds/Spotify.tsx
@@ -1,42 +1,39 @@
 import * as React from "react";
 import Frame from "../components/Frame";
-
-const URL_REGEX = new RegExp("https?://open\\.spotify\\.com/(.*)$");
 import { EmbedProps as Props } from ".";
 
-export default class Spotify extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  get pathname() {
-    try {
-      const parsed = new URL(this.props.attrs.href);
-      return parsed.pathname;
-    } catch (err) {
-      return "";
-    }
+function Spotify(props: Props) {
+  let pathname = "";
+  try {
+    const parsed = new URL(props.attrs.href);
+    pathname = parsed.pathname;
+  } catch (err) {
+    pathname = "";
   }
 
-  render() {
-    const normalizedPath = this.pathname.replace(/^\/embed/, "/");
-    let height;
+  const normalizedPath = pathname.replace(/^\/embed/, "/");
+  let height;
 
-    if (normalizedPath.includes("episode") || normalizedPath.includes("show")) {
-      height = 232;
-    } else if (normalizedPath.includes("track")) {
-      height = 80;
-    } else {
-      height = 380;
-    }
-
-    return (
-      <Frame
-        {...this.props}
-        width="100%"
-        height={`${height}px`}
-        src={`https://open.spotify.com/embed${normalizedPath}`}
-        title="Spotify Embed"
-        allow="encrypted-media"
-      />
-    );
+  if (normalizedPath.includes("episode") || normalizedPath.includes("show")) {
+    height = 232;
+  } else if (normalizedPath.includes("track")) {
+    height = 80;
+  } else {
+    height = 380;
   }
+
+  return (
+    <Frame
+      {...props}
+      width="100%"
+      height={`${height}px`}
+      src={`https://open.spotify.com/embed${normalizedPath}`}
+      title="Spotify Embed"
+      allow="encrypted-media"
+    />
+  );
 }
+
+Spotify.ENABLED = [new RegExp("https?://open\\.spotify\\.com/(.*)$")];
+
+export default Spotify;
