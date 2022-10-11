4be8e13ca399be356c16ea5bfe20272f2e8b17c0
Quincy Mast <email.quincymast@gmail.com>
2022-10-11 14:53:36 +0000

    Functional Component Refactor: Prezi

--- shared/editor/embeds/Prezi.tsx
+++ shared/editor/embeds/Prezi.tsx
@@ -2,15 +2,11 @@ import * as React from "react";
 import Frame from "../components/Frame";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = new RegExp("^https://prezi\\.com/view/(.*)$");
-
-export default class Prezi extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    const url = this.props.attrs.href.replace(/\/embed$/, "");
-    return (
-      <Frame {...this.props} src={`${url}/embed`} title="Prezi Embed" border />
-    );
-  }
+function Prezi(props: Props) {
+  const url = props.attrs.href.replace(/\/embed$/, "");
+  return <Frame {...props} src={`${url}/embed`} title="Prezi Embed" border />;
 }
+
+Prezi.ENABLED = [new RegExp("^https://prezi\\.com/view/(.*)$")];
+
+export default Prezi;
