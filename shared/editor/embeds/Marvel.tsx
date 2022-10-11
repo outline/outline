2fdb20e38341e9ceec9211ae4497cc46f1328a7b
Quincy Mast <email.quincymast@gmail.com>
2022-10-11 14:51:08 +0000

    Functional Component Refactor: Marvel

--- shared/editor/embeds/Marvel.tsx
+++ shared/editor/embeds/Marvel.tsx
@@ -2,19 +2,12 @@ import * as React from "react";
 import Frame from "../components/Frame";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = new RegExp("^https://marvelapp\\.com/([A-Za-z0-9-]{6})/?$");
-
-export default class Marvel extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    return (
-      <Frame
-        {...this.props}
-        src={this.props.attrs.href}
-        title="Marvel Embed"
-        border
-      />
-    );
-  }
+function Marvel(props: Props) {
+  return (
+    <Frame {...props} src={props.attrs.href} title="Marvel Embed" border />
+  );
 }
+
+Marvel.ENABLED = [new RegExp("^https://marvelapp\\.com/([A-Za-z0-9-]{6})/?$")];
+
+export default Marvel;
