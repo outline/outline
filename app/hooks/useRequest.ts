0fc2c9e2fa946e95638bca08c92becabe26d0ed6
Tom Moor <tom.moor@gmail.com>
2022-10-12 19:44:22 -0400

    docs

--- app/hooks/useRequest.ts
+++ app/hooks/useRequest.ts
@@ -1,13 +1,25 @@
 import * as React from "react";
 
+type RequestResponse<T> = {
+  /** The return value of the request function. */
+  data: T | undefined;
+  /** The request error, if any. */
+  error: unknown;
+  /** Whether the request is currently in progress. */
+  loading: boolean;
+  /** Function to start the request. */
+  request: () => Promise<T | undefined>;
+};
+
+/**
+ * A hook to make an API request and track its state within a component.
+ *
+ * @param requestFn The function to call to make the request, it should return a promise.
+ * @returns
+ */
 export default function useRequest<T = unknown>(
   requestFn: () => Promise<T>
-): {
-  data: T | undefined;
-  error: any;
-  loading: boolean;
-  request: () => Promise<void>;
-} {
+): RequestResponse<T> {
   const [data, setData] = React.useState<T>();
   const [loading, setLoading] = React.useState<boolean>(false);
   const [error, setError] = React.useState();
@@ -17,11 +29,14 @@ export default function useRequest<T = unknown>(
     try {
       const response = await requestFn();
       setData(response);
+      return response;
     } catch (err) {
       setError(err);
     } finally {
       setLoading(false);
     }
+
+    return undefined;
   }, [requestFn]);
 
   return { data, loading, error, request };
