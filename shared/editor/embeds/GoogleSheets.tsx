4dc336eeab3cdb684b1ca3c929280db815f5cb21
mastqe <email.quincymast@gmail.com>
2022-10-15 10:00:59 -0400

    Functional Component Refactor: Google Docs, Drive, Sheets, & Slides (#4259)

--- shared/editor/embeds/GoogleSheets.tsx
+++ shared/editor/embeds/GoogleSheets.tsx
@@ -3,30 +3,28 @@ import Frame from "../components/Frame";
 import Image from "../components/Image";
 import { EmbedProps as Props } from ".";
 
-const URL_REGEX = new RegExp(
-  "^https?://docs\\.google\\.com/spreadsheets/d/(.*)$"
-);
-
-export default class GoogleSheets extends React.Component<Props> {
-  static ENABLED = [URL_REGEX];
-
-  render() {
-    return (
-      <Frame
-        {...this.props}
-        src={this.props.attrs.href.replace("/edit", "/preview")}
-        icon={
-          <Image
-            src="/images/google-sheets.png"
-            alt="Google Sheets Icon"
-            width={16}
-            height={16}
-          />
-        }
-        canonicalUrl={this.props.attrs.href}
-        title="Google Sheets"
-        border
-      />
-    );
-  }
+function GoogleSheets(props: Props) {
+  return (
+    <Frame
+      {...props}
+      src={props.attrs.href.replace("/edit", "/preview")}
+      icon={
+        <Image
+          src="/images/google-sheets.png"
+          alt="Google Sheets Icon"
+          width={16}
+          height={16}
+        />
+      }
+      canonicalUrl={props.attrs.href}
+      title="Google Sheets"
+      border
+    />
+  );
 }
+
+GoogleSheets.ENABLED = [
+  new RegExp("^https?://docs\\.google\\.com/spreadsheets/d/(.*)$"),
+];
+
+export default GoogleSheets;
