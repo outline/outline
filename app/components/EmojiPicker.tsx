750055d674c73e7fe43a8b8a31dddc1b1884031f
Apoorv Mishra <apoorvmishra101092@gmail.com>
2022-10-31 22:34:07 +0530

    fix(app): make picker responsive

--- app/components/EmojiPicker.tsx
+++ app/components/EmojiPicker.tsx
@@ -3,26 +3,42 @@ import Picker from "@emoji-mart/react";
 import { CloseIcon } from "outline-icons";
 import * as React from "react";
 import {
+  useDialogState,
+  Dialog as BaseDialog,
+  DialogDisclosure,
+  DialogProps as DProps,
+} from "reakit/Dialog";
+import {
   usePopoverState,
   Popover as ReakitPopover,
   PopoverDisclosure,
   PopoverArrow,
-  PopoverProps,
+  PopoverProps as PProps,
 } from "reakit/Popover";
 import styled from "styled-components";
 import { depths } from "@shared/styles";
+import { Position, Backdrop } from "~/components/ContextMenu";
 import NudeButton from "~/components/NudeButton";
 import Tooltip from "~/components/Tooltip";
+import useMobile from "~/hooks/useMobile";
 
-type Props = PopoverProps & {
+type PopoverProps = PProps & {
   disclosure: any;
   children: React.ReactNode;
   popoverState?: any;
+  dialogState?: any;
   tabIndex?: number;
   showPopoverArrow?: boolean;
 };
 
-const Popover: React.FC<Props> = ({
+type DialogProps = DProps & {
+  disclosure: any;
+  children: (props: any) => React.ReactNode;
+  dialogState?: any;
+  tabIndex?: number;
+};
+
+const Popover: React.FC<PopoverProps> = ({
   disclosure,
   children,
   popoverState,
@@ -48,12 +64,35 @@ const Popover: React.FC<Props> = ({
   );
 };
 
+const Dialog: React.FC<DialogProps> = ({
+  disclosure,
+  children,
+  dialogState,
+  ...props
+}) => {
+  const dialog = useDialogState(dialogState);
+  return (
+    <>
+      <DialogDisclosure {...dialog} ref={disclosure.ref} {...disclosure.props}>
+        {(disclosureProps) => React.cloneElement(disclosure, disclosureProps)}
+      </DialogDisclosure>
+      <BaseDialog {...dialog} {...props}>
+        {children({ dialog })}
+      </BaseDialog>
+    </>
+  );
+};
+
 type EmojiPickerProps = {
   disclosure: React.ReactNode;
   onEmojiSelect: (emoji: string) => void;
   onEmojiRemove: () => void;
   theme: string;
 };
+
+const PickerContainer = React.forwardRef((props: any, ref: any) => {
+  return <Position ref={ref} {...props} />;
+});
 
 const EmojiPicker: React.FC<EmojiPickerProps> = ({
   disclosure,
@@ -62,9 +101,67 @@ const EmojiPicker: React.FC<EmojiPickerProps> = ({
   theme,
   ...pickerOptions
 }) => {
+  const isMobile = useMobile();
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  const [pickerWidth, setPickerWidth] = React.useState<number | undefined>(0);
+
+  const resizeObserver = new ResizeObserver(() => {
+    if (ref.current?.clientWidth !== pickerWidth) {
+      setPickerWidth(ref.current?.clientWidth);
+    }
+  });
+
+  React.useEffect(() => {
+    if (ref.current) {
+      resizeObserver.observe(ref.current);
+    }
+    return () => {
+      resizeObserver.disconnect();
+    };
+  });
+
   const handleEmojiSelect = async (emoji: any) => {
     await onEmojiSelect(emoji.native);
   };
+
+  if (isMobile) {
+    return (
+      <Dialog disclosure={disclosure} baseId="emoji-picker" tabIndex={0}>
+        {({ dialog }: any) => (
+          <>
+            <Backdrop
+              onClick={(ev) => {
+                ev.preventDefault();
+                ev.stopPropagation();
+                dialog.hide();
+              }}
+            />
+            <PickerContainer ref={ref} onClick={dialog.hide}>
+              <Tooltip tooltip="Remove emoji" placement="top">
+                <NudeButton onClick={onEmojiRemove}>
+                  <CloseIcon />
+                </NudeButton>
+              </Tooltip>
+              <PickerStyles onClick={(e) => e.stopPropagation()}>
+                <Picker
+                  data={data}
+                  onEmojiSelect={handleEmojiSelect}
+                  theme={theme}
+                  perLine={
+                    // 28 is picker's width when perLine is set to 0
+                    // and 36 is the default emojiButtonSize
+                    pickerWidth ? Math.floor((pickerWidth - 28) / 36) : 9
+                  }
+                  {...pickerOptions}
+                />
+              </PickerStyles>
+            </PickerContainer>
+          </>
+        )}
+      </Dialog>
+    );
+  }
 
   return (
     <Popover
@@ -90,6 +187,13 @@ const EmojiPicker: React.FC<EmojiPickerProps> = ({
 
 const BasePopover = styled(ReakitPopover)`
   z-index: ${depths.popover};
+`;
+
+const PickerStyles = styled.div`
+  em-emoji-picker {
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
 
 export default EmojiPicker;
