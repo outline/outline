0c372a84c03b723233245e9e337a28db6be98b16
Apoorv Mishra <apoorvmishra101092@gmail.com>
2022-12-07 11:00:59 +0530

    fix: migrations

--- server/migrations/20221206163421-add-share-url-slug.js
+++ server/migrations/20221206163421-add-share-url-slug.js
@@ -2,45 +2,37 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction();
     try {
-      await queryInterface.addColumn(
-        "shares",
-        "urlId",
-        {
+      await queryInterface.sequelize.transaction(async (transaction) => {
+        await queryInterface.addColumn("shares", "urlId", {
           type: Sequelize.STRING,
           allowNull: true,
-        },
-        { transaction }
-      );
+          transaction,
+        });
 
-      await queryInterface.addConstraint("shares", {
-        fields: ["urlId", "teamId"],
-        type: "unique",
-        transaction,
+        await queryInterface.addConstraint("shares", {
+          fields: ["urlId", "teamId"],
+          type: "unique",
+          transaction,
+        });
       });
-
-      await transaction.commit();
     } catch (err) {
-      await transaction.rollback();
       throw err;
     }
   },
 
   async down(queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction();
     try {
-      await queryInterface.removeConstraint(
-        "shares",
-        "shares_urlId_teamId_uk",
-        { transaction }
-      );
+      await queryInterface.sequelize.transaction(async (transaction) => {
+        await queryInterface.removeConstraint(
+          "shares",
+          "shares_urlId_teamId_uk",
+          { transaction }
+        );
 
-      await queryInterface.removeColumn("shares", "urlId", { transaction });
-
-      await transaction.commit();
+        await queryInterface.removeColumn("shares", "urlId", { transaction });
+      });
     } catch (err) {
-      await transaction.rollback();
       throw err;
     }
   },
