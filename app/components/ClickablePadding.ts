a9683f4d53a87ce58b0a1d2435bbc2d056fd29ba
Tom Moor <tom.moor@gmail.com>
2022-12-14 20:44:38 -0500

    fix: Add additional padding at the bottom of documents when editing

--- app/components/ClickablePadding.ts
+++ app/components/ClickablePadding.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const ClickablePadding = styled.div<{ grow?: boolean }>`
-  min-height: 10em;
+  min-height: 50vh;
   cursor: ${({ onClick }) => (onClick ? "text" : "default")};
   ${({ grow }) => grow && `flex-grow: 100;`};
 `;
