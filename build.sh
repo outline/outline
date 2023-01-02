435969cf4baf27ce21736cbbca86e68d0928ab52
Tom Moor <tom.moor@gmail.com>
2023-01-02 16:00:01 -0500

    chore: Refactor build:server to bashfile

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+yarn babel --extensions .ts,.tsx --quiet -d ./build/server ./server
+yarn babel --extensions .ts,.tsx --quiet -d ./build/shared ./shared
+
+# Copy static files
+cp ./server/collaboration/Procfile ./build/server/collaboration/Procfile
+cp package.json ./build
+cp ./server/static/error.dev.html ./build/server/error.dev.html
+cp ./server/static/error.prod.html ./build/server/error.prod.html
+
+# Link webpack config
+ln -sf \"$(pwd)/webpack.config.dev.js\" ./build
