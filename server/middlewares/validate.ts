5efd8458dac51357733860b53717ad0744d43284
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-01-05 12:25:31 +0530

    fix: error handling

--- server/middlewares/validate.ts
+++ server/middlewares/validate.ts
@@ -1,20 +1,20 @@
 import { Next } from "koa";
-import { z } from "zod";
+import { z, ZodError } from "zod";
 import { ValidationError } from "@server/errors";
 import { APIContext, BaseReq } from "@server/types";
 
 export default function validate<T extends z.ZodType<BaseReq>>(schema: T) {
-  return async function validateMiddleware(
-    ctx: APIContext<BaseReq>,
-    next: Next
-  ) {
+  return async function validateMiddleware(ctx: APIContext, next: Next) {
     try {
       ctx.input = schema.parse(ctx.request);
     } catch (err) {
-      const { path, message } = err.issues[0];
-      const errMessage =
-        path.length > 0 ? `${path[path.length - 1]}: ${message}` : message;
-      throw ValidationError(errMessage);
+      if (err instanceof ZodError) {
+        const { path, message } = err.issues[0];
+        const errMessage =
+          path.length > 0 ? `${path[path.length - 1]}: ${message}` : message;
+        throw ValidationError(errMessage);
+      }
+      ctx.throw(err);
     }
     return next();
   };
