f98cac5705034e11eadd0681bd93fe81d12de092
Hans Pagel <mail@hanspagel.com>
2023-01-11 17:19:51 +0100

    add webpack-stats and legacy builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import path from "path";
+import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import nodeGlobals from "rollup-plugin-node-globals";
+import { webpackStats } from "rollup-plugin-webpack-stats";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "./app",
+  root: "./",
   server: {
     port: 3001,
   },
   plugins: [
+    legacy({
+      targets: "> 0.25%, not dead",
+    }),
     react({
       babel: {
         babelrc: true,
@@ -25,10 +30,11 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: "./build/app",
     manifest: true,
     rollupOptions: {
       input: "./app/index.tsx",
-      plugins: [nodeGlobals()],
+      plugins: [nodeGlobals(), webpackStats()],
     },
   },
 });
