010e9c0f02388d78a4ed43b79e67303b642e586d
Tom Moor <tom.moor@gmail.com>
2023-01-12 21:26:04 -0500

    test

--- server/models/Collection.test.ts
+++ server/models/Collection.test.ts
@@ -105,10 +105,11 @@ describe("#addDocumentToStructure", () => {
   test("should add as last element without index", async () => {
     const { collection } = await seed();
     const id = uuidv4();
-    const newDocument = new Document({
+    const newDocument = await buildDocument({
       id,
       title: "New end node",
       parentDocumentId: null,
+      teamId: collection.teamId,
     });
     await collection.addDocumentToStructure(newDocument);
     expect(collection.documentStructure!.length).toBe(2);
@@ -118,10 +119,11 @@ describe("#addDocumentToStructure", () => {
   test("should add with an index", async () => {
     const { collection } = await seed();
     const id = uuidv4();
-    const newDocument = new Document({
+    const newDocument = await buildDocument({
       id,
       title: "New end node",
       parentDocumentId: null,
+      teamId: collection.teamId,
     });
     await collection.addDocumentToStructure(newDocument, 1);
     expect(collection.documentStructure!.length).toBe(2);
@@ -131,10 +133,11 @@ describe("#addDocumentToStructure", () => {
   test("should add as a child if with parent", async () => {
     const { collection, document } = await seed();
     const id = uuidv4();
-    const newDocument = new Document({
+    const newDocument = await buildDocument({
       id,
       title: "New end node",
       parentDocumentId: document.id,
+      teamId: collection.teamId,
     });
     await collection.addDocumentToStructure(newDocument, 1);
     expect(collection.documentStructure!.length).toBe(1);
@@ -145,16 +148,18 @@ describe("#addDocumentToStructure", () => {
 
   test("should add as a child if with parent with index", async () => {
     const { collection, document } = await seed();
-    const newDocument = new Document({
+    const newDocument = await buildDocument({
       id: uuidv4(),
       title: "node",
       parentDocumentId: document.id,
+      teamId: collection.teamId,
     });
     const id = uuidv4();
-    const secondDocument = new Document({
+    const secondDocument = await buildDocument({
       id,
       title: "New start node",
       parentDocumentId: document.id,
+      teamId: collection.teamId,
     });
     await collection.addDocumentToStructure(newDocument);
     await collection.addDocumentToStructure(secondDocument, 0);
@@ -167,10 +172,11 @@ describe("#addDocumentToStructure", () => {
     test("should append supplied json over document's own", async () => {
       const { collection } = await seed();
       const id = uuidv4();
-      const newDocument = new Document({
+      const newDocument = await buildDocument({
         id: uuidv4(),
         title: "New end node",
         parentDocumentId: null,
+        teamId: collection.teamId,
       });
       await collection.addDocumentToStructure(newDocument, undefined, {
         documentJson: {
