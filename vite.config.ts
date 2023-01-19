22008db2ac59cb5561acbc713d775aa2574a6b17
Hans Pagel <mail@hanspagel.com>
2023-01-19 15:16:17 +0100

    configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,44 @@ export default defineConfig({
         ],
       },
     }),
-    VitePWA(),
+    // https://vite-pwa-org.netlify.app/
+    VitePWA({
+      registerType: "autoUpdate",
+      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: "Outline",
+        short_name: "Outline",
+        // description: "My Awesome App description",
+        theme_color: "#fff",
+        background_color: "#fff",
+        start_url: "/",
+        publicPath: "/static/",
+        display: "standalone",
+        // For Chrome, you must provide at least a 192x192 pixel icon, and a 512x512 pixel icon.
+        // If only those two icon sizes are provided, Chrome will automatically scale the icons
+        // to fit the device. If you'd prefer to scale your own icons, and adjust them for
+        // pixel-perfection, provide icons in increments of 48dp.
+        icons: [
+          {
+            src: "images/icon-512.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "images/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          // last one duplicated for purpose: 'any maskable'
+          {
+            src: "images/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
   optimizeDeps: {
     exclude: ["@benrbray/prosemirror-math"],
