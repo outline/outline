7b6d1e1c81019bdef0208b01381c4be2f2aa7ebe
Hans Pagel <mail@hanspagel.com>
2023-01-19 12:49:06 +0100

    fix build issue with hocuspocus provider

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import browserslistToEsbuild from "browserslist-to-esbuild";
-import nodeGlobals from "rollup-plugin-node-globals";
 import { webpackStats } from "rollup-plugin-webpack-stats";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   root: "./",
@@ -17,12 +16,10 @@ export default defineConfig({
         babelrc: true,
       },
     }),
-    VitePWA(),
+    // VitePWA(),
   ],
   optimizeDeps: {
-    // include: ["@hocuspocus/provider"],
     exclude: ["@benrbray/prosemirror-math"],
-    // esbuildOptions: {},
   },
   resolve: {
     alias: {
@@ -37,7 +34,7 @@ export default defineConfig({
     // reportCompressedSize: false,
     rollupOptions: {
       input: "./app/index.tsx",
-      plugins: [nodeGlobals(), webpackStats()],
+      plugins: [webpackStats()],
     },
   },
 });
