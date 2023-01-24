3974674871f8545c11e8855ca28be1c37f65bc16
Hans Pagel <mail@hanspagel.com>
2023-01-24 17:59:06 +0100

    remove the webpack config from the build script

--- build.sh
+++ build.sh
@@ -8,6 +8,3 @@ cp ./server/collaboration/Procfile ./build/server/collaboration/Procfile
 cp package.json ./build
 cp ./server/static/error.dev.html ./build/server/error.dev.html
 cp ./server/static/error.prod.html ./build/server/error.prod.html
-
-# Link webpack config
-ln -sf "$(pwd)/webpack.config.dev.js" ./build
