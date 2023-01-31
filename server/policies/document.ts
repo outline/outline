33dd5831e97cb35c057e49e422e44ee8cc715785
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-01-31 18:12:13 +0530

    fix: viewer should be allowed to subscribe to a document

--- server/policies/document.ts
+++ server/policies/document.ts
@@ -180,36 +180,46 @@ allow(User, "move", Document, (user, document) => {
   return user.teamId === document.teamId;
 });
 
-allow(
-  User,
-  ["pin", "unpin", "subscribe", "unsubscribe"],
-  Document,
-  (user, document) => {
-    if (!document) {
-      return false;
-    }
-    if (document.archivedAt) {
-      return false;
-    }
-    if (document.deletedAt) {
-      return false;
-    }
-    if (document.template) {
-      return false;
-    }
-    if (!document.publishedAt) {
-      return false;
-    }
-    invariant(
-      document.collection,
-      "collection is missing, did you forget to include in the query scope?"
-    );
-    if (cannot(user, "update", document.collection)) {
-      return false;
-    }
-    return user.teamId === document.teamId;
+allow(User, ["pin", "unpin"], Document, (user, document) => {
+  if (!document) {
+    return false;
   }
-);
+  if (document.archivedAt) {
+    return false;
+  }
+  if (document.deletedAt) {
+    return false;
+  }
+  if (document.template) {
+    return false;
+  }
+  if (!document.publishedAt) {
+    return false;
+  }
+  if (document.collection && cannot(user, "update", document.collection)) {
+    return false;
+  }
+  return user.teamId === document.teamId;
+});
+
+allow(User, ["subscribe", "unsubscribe"], Document, (user, document) => {
+  if (!document) {
+    return false;
+  }
+  if (document.archivedAt) {
+    return false;
+  }
+  if (document.deletedAt) {
+    return false;
+  }
+  if (document.template) {
+    return false;
+  }
+  if (!document.publishedAt) {
+    return false;
+  }
+  return user.teamId === document.teamId;
+});
 
 allow(User, ["pinToHome"], Document, (user, document) => {
   if (!document) {
