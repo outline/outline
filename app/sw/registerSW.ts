bbeac9fa312322bb8bf62ed77b9fb2e010d471d0
Hans Pagel <mail@hanspagel.com>
2023-02-08 16:57:46 +0100

    hide the service worker behind a globally registered function

--- app/sw/registerSW.ts
+++ app/sw/registerSW.ts
@@ -1,25 +1,33 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    // see: https://bugs.chromium.org/p/chromium/issues/detail?id=1097616
-    // In some rare (<0.1% of cases) this call can return `undefined`
-    const maybePromise = navigator.serviceWorker.register("/sw.js", {
-      scope: "/",
-    });
-
-    if (maybePromise?.then) {
-      maybePromise
-        .then((registration) => {
-          console.log("[ServiceWorker] Registered.", registration);
-        })
-        .catch((registrationError) => {
-          console.log(
-            "[ServiceWorker] Registration failed. ",
-            registrationError
-          );
-        });
-    }
-  });
+declare global {
+  interface Window {
+    executeServiceWorkerCode: () => void;
+  }
 }
+
+window.executeServiceWorkerCode = () => {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      // see: https://bugs.chromium.org/p/chromium/issues/detail?id=1097616
+      // In some rare (<0.1% of cases) this call can return `undefined`
+      const maybePromise = navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+
+      if (maybePromise?.then) {
+        maybePromise
+          .then((registration) => {
+            console.log("[ServiceWorker] Registered.", registration);
+          })
+          .catch((registrationError) => {
+            console.log(
+              "[ServiceWorker] Registration failed. ",
+              registrationError
+            );
+          });
+      }
+    });
+  }
+};
 
 // Add an empty 'export {}' statement to make it a module.
 export default {};
