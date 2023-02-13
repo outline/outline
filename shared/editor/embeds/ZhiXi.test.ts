9a656eef612b813ebfdf099602a882dd50d4d8e8
ZhengJin <fangzhengjin@gmail.com>
2023-02-13 15:33:04 +0800

    feat: add support ZhiXi MindMap

--- shared/editor/embeds/ZhiXi.test.ts
+++ shared/editor/embeds/ZhiXi.test.ts
@@ -3,8 +3,11 @@ import ZhiXi from "./ZhiXi";
 describe("ZhiXi", () => {
   const match = ZhiXi.ENABLED[0];
 
-  test("to not be enabled for invalid urls", () => {
-    expect("https://www.zhixi.com/embed/1bb899e0".match(match)).toBe(null);
-    expect("https://www.zhixi.com/view/308d9a55".match(match)).toBe(null);
+  test("to be enabled on view link", () => {
+    expect("https://www.zhixi.com/view/308d9a55".match(match)).toBeTruthy();
+  });
+
+  test("to be enabled on embed link", () => {
+    expect("https://www.zhixi.com/embed/1bb899e0".match(match)).toBeTruthy();
   });
 });
