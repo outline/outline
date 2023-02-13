a07edc03afd082d02e7a4e3418f972d68e38eb45
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-02-13 23:52:43 +0530

    fix: make all mention nodes visible in shared doc

--- shared/editor/rules/mention.ts
+++ shared/editor/rules/mention.ts
@@ -22,28 +22,61 @@ function parseUri(uri: string) {
 
 function parseMentions(state: StateCore) {
   const scanRE = /(?:^|\s)@\[[a-zA-Z\s]+\]\(mention:\/\/[a-z]+\/[a-z0-9-]+\)/;
+  const WINDOW_SIZE = 4;
 
   for (let i = 0; i < state.tokens.length; i++) {
     const tok = state.tokens[i];
-    if (tok.type !== "inline") {
-      continue;
-    }
-    if (!scanRE.test(tok.content)) {
+    if (!(tok.type === "inline" && scanRE.test(tok.content) && tok.children)) {
       continue;
     }
 
-    const [prefixToken, openToken, labelToken] = tok.children!.slice(0, 3);
-    prefixToken.content = prefixToken.content.slice(0, -1);
+    const canChunkComposeMentionToken = (chunk: Token[]) => {
+      if (chunk.length < 4) {
+        return false;
+      }
 
-    const href = openToken.attrs![0];
-    const { type, id } = parseUri(href![1]);
+      const [precToken, openToken, textToken, closeToken] = chunk;
+      return (
+        precToken.content.endsWith("@") &&
+        openToken.type === "link_open" &&
+        textToken.content &&
+        closeToken.type === "link_close"
+      );
+    };
 
-    const mentionToken = new Token("mention", "", 0);
-    mentionToken.attrSet("data-type", type);
-    mentionToken.attrSet("data-id", id);
-    mentionToken.content = labelToken.content;
+    const chunkWithMentionToken = (chunk: Token[]) => {
+      const [precToken, openToken, textToken] = chunk;
 
-    state.tokens[i].children!.splice(1, 3, mentionToken);
+      // remove "@" from preceding token
+      precToken.content = precToken.content.slice(0, -1);
+
+      // href must be present, otherwise the scanRE test above would've failed
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const href = openToken.attrs![0];
+      const { type, id } = parseUri(href[1]);
+
+      const mentionToken = new Token("mention", "", 0);
+      mentionToken.attrSet("data-type", type);
+      mentionToken.attrSet("data-id", id);
+      mentionToken.content = textToken.content;
+
+      return [precToken, mentionToken];
+    };
+
+    let newChildren: Token[] = [];
+    let j = 0;
+    while (j < tok.children.length) {
+      const chunk = tok.children.slice(j, j + WINDOW_SIZE);
+      if (canChunkComposeMentionToken(chunk)) {
+        newChildren = newChildren.concat(chunkWithMentionToken(chunk));
+        j += WINDOW_SIZE;
+      } else {
+        newChildren.push(tok.children[j]);
+        j++;
+      }
+    }
+
+    state.tokens[i].children = newChildren;
   }
 }
 
