137154532cdc38738e7626fad8f373ba246059f8
Tom Moor <tom.moor@gmail.com>
2023-02-15 21:19:16 -0500

    Enable SW

--- app/index.tsx
+++ app/index.tsx
@@ -100,14 +100,8 @@ window.addEventListener("load", async () => {
   });
 });
 
-// To activate the Service Worker, execute the following code in the browser console and refresh:
-// localStorage.setItem("installServiceWorker", true)
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    if (!localStorage.getItem("installServiceWorker")) {
-      return;
-    }
-
     // see: https://bugs.chromium.org/p/chromium/issues/detail?id=1097616
     // In some rare (<0.1% of cases) this call can return `undefined`
     const maybePromise = navigator.serviceWorker.register("/static/sw.js", {
