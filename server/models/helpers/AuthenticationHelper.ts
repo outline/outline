6c069f658b9f26acc690a2e1a5af4c36f6e08fcd
Tom Moor <tom.moor@gmail.com>
2023-02-18 13:56:03 -0500

    fix: Do not show authentication provider plugins that aren't enabled

--- server/models/helpers/AuthenticationHelper.ts
+++ server/models/helpers/AuthenticationHelper.ts
@@ -17,13 +17,20 @@ export default class AuthenticationHelper {
     return providerConfigs
       .sort((config) => (config.id === "email" ? 1 : -1))
       .filter((config) => {
-        // guest sign-in is an exception as it does not have an authentication
-        // provider using passport, instead it exists as a boolean option on the team
-        if (config.id === "email") {
-          return team?.emailSigninEnabled;
+        // Don't return authentication methods that are not enabled.
+        if (!config.enabled) {
+          return false;
         }
+
+        // If no team return all possible authentication providers.
         if (!team) {
           return true;
+        }
+
+        // Guest sign-in is an exception as it does not have an authentication
+        // provider using passport, instead it exists as a boolean option.
+        if (config.id === "email") {
+          return team?.emailSigninEnabled;
         }
 
         const authProvider = find(team.authenticationProviders, {
