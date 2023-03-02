c19b75a1194363d5abb010d91a565fd20686cea9
Limezy <arnaudetpia@de-ruffray.fr>
2023-03-02 23:36:07 +0700

    First try

--- shared/editor/embeds/GoogleLookerStudio.tsx
+++ shared/editor/embeds/GoogleLookerStudio.tsx
@@ -3,30 +3,30 @@ import Frame from "../components/Frame";
 import Image from "../components/Image";
 import { EmbedProps as Props } from ".";
 
-function GoogleDataStudio(props: Props) {
+function GoogleLookerStudio(props: Props) {
   return (
     <Frame
       {...props}
       src={props.attrs.href.replace("u/0", "embed").replace("/edit", "")}
       icon={
         <Image
-          src="/images/google-datastudio.png"
-          alt="Google Data Studio Icon"
+          src="/images/google-lookerstudio.png"
+          alt="Google Looker Studio Icon"
           width={16}
           height={16}
         />
       }
       canonicalUrl={props.attrs.href}
-      title="Google Data Studio"
+      title="Google Looker Studio"
       border
     />
   );
 }
 
-GoogleDataStudio.ENABLED = [
+GoogleLookerStudio.ENABLED = [
   new RegExp(
-    "^https?://datastudio\\.google\\.com/(embed|u/0)/reporting/(.*)/page/(.*)(/edit)?$"
+    "^https?://lookerstudio\\.google\\.com/(embed|u/0)/reporting/(.*)/page/(.*)(/edit)?$"
   ),
 ];
 
-export default GoogleDataStudio;
+export default GoogleLookerStudio;
