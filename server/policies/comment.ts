e2dff9afcaf45f604bf7fda4d45b125c6494443f
Tom Moor <tom.moor@gmail.com>
2023-03-11 19:08:12 -0500

    fix: Viewers cannot delete their own comments

--- server/policies/comment.ts
+++ server/policies/comment.ts
@@ -2,7 +2,7 @@ import { Comment, User, Team } from "@server/models";
 import { allow } from "./cancan";
 
 allow(User, "createComment", Team, (user, team) => {
-  if (!team || user.isViewer || user.teamId !== team.id) {
+  if (!team || user.teamId !== team.id) {
     return false;
   }
   return true;
@@ -10,9 +10,6 @@ allow(User, "createComment", Team, (user, team) => {
 
 allow(User, ["read", "update", "delete"], Comment, (user, comment) => {
   if (!comment) {
-    return false;
-  }
-  if (user.isViewer) {
     return false;
   }
   return user?.id === comment.createdById;
