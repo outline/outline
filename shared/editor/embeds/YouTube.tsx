497ab4b89f6888cb9a449ad2cd4273511d26aed8
Tom Moor <tom.moor@gmail.com>
2023-03-17 11:09:31 -0400

    fix: YouTube links with timestamp cannot be embedded, closes #5051

--- shared/editor/embeds/YouTube.tsx
+++ shared/editor/embeds/YouTube.tsx
@@ -5,17 +5,21 @@ import { EmbedProps as Props } from ".";
 function YouTube(props: Props) {
   const { matches } = props.attrs;
   const videoId = matches[1];
+  const queryParameters = matches[2];
+
   return (
     <Frame
       {...props}
-      src={`https://www.youtube.com/embed/${videoId}?modestbranding=1`}
+      src={`https://www.youtube.com/embed/${videoId}?modestbranding=1${
+        queryParameters ? `&${queryParameters}` : ""
+      }`}
       title={`YouTube (${videoId})`}
     />
   );
 }
 
 YouTube.ENABLED = [
-  /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{11})$/i,
+  /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{11})(?:\?)?(.*)?$/i,
 ];
 
 export default YouTube;
