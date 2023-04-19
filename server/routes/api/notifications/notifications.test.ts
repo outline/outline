78d3fa25caac02bedaa795e1aea5953b43153532
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-04-19 17:10:57 +0530

    fix: complete user profile in unnecessary

--- server/routes/api/notifications/notifications.test.ts
+++ server/routes/api/notifications/notifications.test.ts
@@ -61,7 +61,7 @@ describe("#notifications.list", () => {
     expect(body.data.length).toBe(3);
     expect(body.unseenCount).toBe(2);
     expect((randomElement(body.data) as any).actor.id).toBe(actor.id);
-    expect((randomElement(body.data) as any).user.id).toBe(user.id);
+    expect((randomElement(body.data) as any).userId).toBe(user.id);
     const events = body.data.map((n: any) => n.event);
     expect(events).toContain(NotificationEventType.UpdateDocument);
     expect(events).toContain(NotificationEventType.CreateComment);
@@ -118,7 +118,7 @@ describe("#notifications.list", () => {
     expect(body.data.length).toBe(1);
     expect(body.unseenCount).toBe(1);
     expect((randomElement(body.data) as any).actor.id).toBe(actor.id);
-    expect((randomElement(body.data) as any).user.id).toBe(user.id);
+    expect((randomElement(body.data) as any).userId).toBe(user.id);
     const events = body.data.map((n: any) => n.event);
     expect(events).toContain(NotificationEventType.MentionedInComment);
   });
