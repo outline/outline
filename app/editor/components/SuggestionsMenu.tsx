a33d6fce1a3af6186496e87ed3fc969715ac657e
Tom Moor <tom.moor@gmail.com>
2023-05-24 09:12:25 -0400

    fix: Slash trigger character sometimes left

--- app/editor/components/SuggestionsMenu.tsx
+++ app/editor/components/SuggestionsMenu.tsx
@@ -165,8 +165,12 @@ function SuggestionsMenu<T extends MenuItem>(props: Props<T>) {
 
   const handleClearSearch = React.useCallback(() => {
     const { state, dispatch } = view;
-    const poss = state.doc.cut(state.selection.from - 1, state.selection.from);
-    const trimTrigger = poss.textContent === props.trigger;
+    const poss = state.doc.cut(
+      state.selection.from - props.search.length - 1,
+      state.selection.from
+    );
+    const trimTrigger = poss.textContent.startsWith(props.trigger);
+
     if (!props.search && !trimTrigger) {
       return;
     }
