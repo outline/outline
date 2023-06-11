f1d103555016e213936a9a360ff0ae9111518fd2
Tom Moor <tom.moor@gmail.com>
2023-06-11 16:02:15 +0300

    Increase padding on lists

--- shared/editor/components/Styles.ts
+++ shared/editor/components/Styles.ts
@@ -741,7 +741,7 @@ a:hover {
 ul,
 ol {
   margin: ${props.rtl ? "0 -26px 0 0.1em" : "0 0.1em 0 -26px"};
-  padding: ${props.rtl ? "0 44px 0 0" : "0 0 0 44px"};
+  padding: ${props.rtl ? "0 48px 0 0" : "0 0 0 48px"};
 }
 
 ol ol {
