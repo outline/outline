8d69de1be06ec1a185d0af86b44286a23ad233e9
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-06-21 10:38:38 +0530

    chore: request validation for searches (#5460)

---
+++ server/routes/api/searches/schema.ts
@@ -0,0 +1,14 @@
+import { isEmpty } from "lodash";
+import { z } from "zod";
+import BaseSchema from "../BaseSchema";
+
+export const SearchesDeleteSchema = BaseSchema.extend({
+  body: z.object({
+    id: z.string().uuid().optional(),
+    query: z.string().optional(),
+  }),
+}).refine((req) => !(isEmpty(req.body.id) && isEmpty(req.body.query)), {
+  message: "id or query is required",
+});
+
+export type SearchesDeleteReq = z.infer<typeof SearchesDeleteSchema>;
