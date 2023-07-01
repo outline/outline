2b47471d05863c93e30c332b0ec1a9dbc0d78ee1
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-07-01 19:36:44 +0530

    fix: reuse validations

--- server/routes/api/subscriptions/schema.ts
+++ server/routes/api/subscriptions/schema.ts
@@ -1,15 +1,12 @@
-import isUUID from "validator/lib/isUUID";
 import { z } from "zod";
-import { SLUG_URL_REGEX } from "@shared/utils/urlHelpers";
+import { ValidateDocumentId } from "@server/validation";
 import BaseSchema from "../BaseSchema";
 
 export const SubscriptionsListSchema = BaseSchema.extend({
   body: z.object({
-    documentId: z
-      .string()
-      .refine((val) => isUUID(val) || SLUG_URL_REGEX.test(val), {
-        message: "must be uuid or url slug",
-      }),
+    documentId: z.string().refine(ValidateDocumentId.isValid, {
+      message: ValidateDocumentId.message,
+    }),
     event: z.literal("documents.update"),
   }),
 });
@@ -18,11 +15,9 @@ export type SubscriptionsListReq = z.infer<typeof SubscriptionsListSchema>;
 
 export const SubscriptionsInfoSchema = BaseSchema.extend({
   body: z.object({
-    documentId: z
-      .string()
-      .refine((val) => isUUID(val) || SLUG_URL_REGEX.test(val), {
-        message: "must be uuid or url slug",
-      }),
+    documentId: z.string().refine(ValidateDocumentId.isValid, {
+      message: ValidateDocumentId.message,
+    }),
     event: z.literal("documents.update"),
   }),
 });
@@ -31,11 +26,9 @@ export type SubscriptionsInfoReq = z.infer<typeof SubscriptionsInfoSchema>;
 
 export const SubscriptionsCreateSchema = BaseSchema.extend({
   body: z.object({
-    documentId: z
-      .string()
-      .refine((val) => isUUID(val) || SLUG_URL_REGEX.test(val), {
-        message: "must be uuid or url slug",
-      }),
+    documentId: z.string().refine(ValidateDocumentId.isValid, {
+      message: ValidateDocumentId.message,
+    }),
     event: z.literal("documents.update"),
   }),
 });
