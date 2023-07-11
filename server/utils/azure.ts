c56add74c6f2d06d8b5c02e2e24d3cdbe473ce68
Tom Moor <tom@getoutline.com>
2023-07-11 18:59:28 -0400

    fix: Azure single-tenant SSO tokens are unable to refresh (#5551)

--- server/utils/azure.ts
+++ server/utils/azure.ts
@@ -1,4 +1,11 @@
+import JWT from "jsonwebtoken";
+import env from "@server/env";
 import OAuthClient from "./oauth";
+
+type AzurePayload = {
+  /* A GUID that represents the Azure AD tenant that the user is from */
+  tid: string;
+};
 
 export default class AzureClient extends OAuthClient {
   endpoints = {
@@ -6,4 +13,24 @@ export default class AzureClient extends OAuthClient {
     token: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
     userinfo: "https://graph.microsoft.com/v1.0/me",
   };
+
+  async rotateToken(
+    accessToken: string,
+    refreshToken: string
+  ): Promise<{
+    accessToken: string;
+    refreshToken?: string;
+    expiresAt: Date;
+  }> {
+    if (env.isCloudHosted()) {
+      return super.rotateToken(accessToken, refreshToken);
+    }
+
+    const payload = JWT.decode(accessToken) as AzurePayload;
+    return super.rotateToken(
+      accessToken,
+      refreshToken,
+      `https://login.microsoftonline.com/${payload.tid}/oauth2/v2.0/token`
+    );
+  }
 }
