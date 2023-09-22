fa66ee68cdd3fca50537ca233b3cf57338e9c32e
Tom Moor <tom.moor@gmail.com>
2023-09-21 23:31:04 -0400

    Remove unused getVideoThumbnail

--- shared/editor/lib/FileHelper.ts
+++ shared/editor/lib/FileHelper.ts
@@ -39,36 +39,4 @@ export default class FileHelper {
       video.src = URL.createObjectURL(file);
     });
   }
-
-  /**
-   * Returns a thumbnail for a given video file.
-   *
-   * @param file The file to load the thumbnail for
-   * @returns A promise resolving to the thumbnail of the video as a png data uri
-   */
-  static async getVideoThumbnail(file: File) {
-    return new Promise((resolve, reject) => {
-      const canvas = document.createElement("canvas");
-      const video = document.createElement("video");
-
-      // this is important
-      video.autoplay = true;
-      video.muted = true;
-      video.src = URL.createObjectURL(file);
-
-      video.onloadeddata = () => {
-        const ctx = canvas.getContext("2d");
-        if (!ctx) {
-          return reject("No canvas available");
-        }
-
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-
-        ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-        video.pause();
-        return resolve(canvas.toDataURL("image/png"));
-      };
-    });
-  }
 }
