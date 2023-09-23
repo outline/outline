c09c0768bfca255205e1369b396b3d495706fa91
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-09-23 23:53:37 +0530

    fix: create views upon table rename for zero downtime

--- server/migrations/20230921071140-rename-collection-groups.js
+++ server/migrations/20230921071140-rename-collection-groups.js
@@ -4,8 +4,11 @@ const { Op } = require("sequelize");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.renameTable("collection_groups", "group_permissions");
     await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `DROP VIEW IF EXISTS group_permissions`,
+        { transaction }
+      );
       await queryInterface.sequelize.query(
         `ALTER INDEX "collection_groups_collection_id_group_id" RENAME TO "group_permissions_collection_id_group_id"`,
         { transaction }
@@ -20,19 +23,19 @@ module.exports = {
       );
 
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY group_permissions RENAME CONSTRAINT "collection_groups_collectionId_fkey" TO "group_permissions_collectionId_fkey"`,
+        `ALTER TABLE ONLY collection_groups RENAME CONSTRAINT "collection_groups_collectionId_fkey" TO "group_permissions_collectionId_fkey"`,
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY group_permissions RENAME CONSTRAINT "collection_groups_createdById_fkey" TO "group_permissions_createdById_fkey"`,
+        `ALTER TABLE ONLY collection_groups RENAME CONSTRAINT "collection_groups_createdById_fkey" TO "group_permissions_createdById_fkey"`,
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY group_permissions RENAME CONSTRAINT "collection_groups_groupId_fkey" TO "group_permissions_groupId_fkey"`,
+        `ALTER TABLE ONLY collection_groups RENAME CONSTRAINT "collection_groups_groupId_fkey" TO "group_permissions_groupId_fkey"`,
         { transaction }
       );
       await queryInterface.addColumn(
-        "group_permissions",
+        "collection_groups",
         "documentId",
         {
           type: Sequelize.UUID,
@@ -44,12 +47,16 @@ module.exports = {
         },
         { transaction }
       );
+      await queryInterface.sequelize.query(
+        `ALTER TABLE ONLY collection_groups RENAME CONSTRAINT "collection_groups_documentId_fkey" TO "group_permissions_documentId_fkey"`,
+        { transaction }
+      );
       await queryInterface.removeConstraint(
-        "group_permissions",
+        "collection_groups",
         "group_permissions_collectionId_fkey",
         { transaction }
       );
-      await queryInterface.addConstraint("group_permissions", {
+      await queryInterface.addConstraint("collection_groups", {
         fields: ["collectionId"],
         name: "group_permissions_collectionId_fkey",
         type: "foreign key",
@@ -61,7 +68,7 @@ module.exports = {
         transaction,
       });
       await queryInterface.changeColumn(
-        "group_permissions",
+        "collection_groups",
         "collectionId",
         {
           type: Sequelize.UUID,
@@ -70,11 +77,11 @@ module.exports = {
         { transaction }
       );
       await queryInterface.removeConstraint(
-        "group_permissions",
+        "collection_groups",
         "group_permissions_createdById_fkey",
         { transaction }
       );
-      await queryInterface.addConstraint("group_permissions", {
+      await queryInterface.addConstraint("collection_groups", {
         fields: ["createdById"],
         name: "group_permissions_createdById_fkey",
         type: "foreign key",
@@ -85,12 +92,24 @@ module.exports = {
         },
         transaction,
       });
+      await queryInterface.renameTable(
+        "collection_groups",
+        "group_permissions",
+        { transaction }
+      );
+      await queryInterface.sequelize.query(
+        `CREATE VIEW collection_groups AS SELECT * FROM group_permissions`,
+        { transaction }
+      );
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.renameTable("group_permissions", "collection_groups");
     await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `DROP VIEW IF EXISTS collection_groups`,
+        { transaction }
+      );
       await queryInterface.sequelize.query(
         `ALTER INDEX "group_permissions_collection_id_group_id" RENAME TO "collection_groups_collection_id_group_id"`,
         { transaction }
@@ -104,26 +123,26 @@ module.exports = {
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY collection_groups RENAME CONSTRAINT "group_permissions_collectionId_fkey" TO "collection_groups_collectionId_fkey"`,
+        `ALTER TABLE ONLY group_permissions RENAME CONSTRAINT "group_permissions_collectionId_fkey" TO "collection_groups_collectionId_fkey"`,
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY collection_groups RENAME CONSTRAINT "group_permissions_createdById_fkey" TO "collection_groups_createdById_fkey"`,
+        `ALTER TABLE ONLY group_permissions RENAME CONSTRAINT "group_permissions_createdById_fkey" TO "collection_groups_createdById_fkey"`,
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY collection_groups RENAME CONSTRAINT "group_permissions_groupId_fkey" TO "collection_groups_groupId_fkey"`,
+        `ALTER TABLE ONLY group_permissions RENAME CONSTRAINT "group_permissions_groupId_fkey" TO "collection_groups_groupId_fkey"`,
         { transaction }
       );
-      await queryInterface.removeColumn("collection_groups", "documentId", {
+      await queryInterface.removeColumn("group_permissions", "documentId", {
         transaction,
       });
       await queryInterface.removeConstraint(
-        "collection_groups",
+        "group_permissions",
         "collection_groups_collectionId_fkey",
         { transaction }
       );
-      await queryInterface.addConstraint("collection_groups", {
+      await queryInterface.addConstraint("group_permissions", {
         fields: ["collectionId"],
         name: "collection_groups_collectionId_fkey",
         type: "foreign key",
@@ -136,11 +155,11 @@ module.exports = {
       });
       // Delete records where collectionId is null before setting back non null constraint
       await queryInterface.sequelize.query(
-        `DELETE FROM collection_groups WHERE "collectionId" IS NULL`,
+        `DELETE FROM group_permissions WHERE "collectionId" IS NULL`,
         { transaction }
       );
       await queryInterface.changeColumn(
-        "collection_groups",
+        "group_permissions",
         "collectionId",
         {
           type: Sequelize.UUID,
@@ -149,11 +168,11 @@ module.exports = {
         { transaction }
       );
       await queryInterface.removeConstraint(
-        "collection_groups",
+        "group_permissions",
         "collection_groups_createdById_fkey",
         { transaction }
       );
-      await queryInterface.addConstraint("collection_groups", {
+      await queryInterface.addConstraint("group_permissions", {
         fields: ["createdById"],
         name: "collection_groups_createdById_fkey",
         type: "foreign key",
@@ -163,6 +182,15 @@ module.exports = {
         },
         transaction,
       });
+      await queryInterface.renameTable(
+        "group_permissions",
+        "collection_groups",
+        { transaction }
+      );
+      await queryInterface.sequelize.query(
+        `CREATE VIEW group_permissions AS SELECT * FROM collection_groups`,
+        { transaction }
+      );
     });
   },
 };
--- server/migrations/20230922105047-rename-collection-users.js
+++ server/migrations/20230922105047-rename-collection-users.js
@@ -4,8 +4,11 @@ const { Op } = require("sequelize");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.renameTable("collection_users", "user_permissions");
     await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `DROP VIEW IF EXISTS user_permissions`,
+        { transaction }
+      );
       await queryInterface.sequelize.query(
         `ALTER INDEX "collection_users_collection_id_user_id" RENAME TO "user_permissions_collection_id_user_id"`,
         { transaction }
@@ -15,19 +18,19 @@ module.exports = {
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY user_permissions RENAME CONSTRAINT "collection_users_collectionId_fkey" TO "user_permissions_collectionId_fkey"`,
+        `ALTER TABLE ONLY collection_users RENAME CONSTRAINT "collection_users_collectionId_fkey" TO "user_permissions_collectionId_fkey"`,
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY user_permissions RENAME CONSTRAINT "collection_users_createdById_fkey" TO "user_permissions_createdById_fkey"`,
+        `ALTER TABLE ONLY collection_users RENAME CONSTRAINT "collection_users_createdById_fkey" TO "user_permissions_createdById_fkey"`,
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY user_permissions RENAME CONSTRAINT "collection_users_userId_fkey" TO "user_permissions_userId_fkey"`,
+        `ALTER TABLE ONLY collection_users RENAME CONSTRAINT "collection_users_userId_fkey" TO "user_permissions_userId_fkey"`,
         { transaction }
       );
       await queryInterface.addColumn(
-        "user_permissions",
+        "collection_users",
         "documentId",
         {
           type: Sequelize.UUID,
@@ -39,12 +42,16 @@ module.exports = {
         },
         { transaction }
       );
+      await queryInterface.sequelize.query(
+        `ALTER TABLE ONLY collection_users RENAME CONSTRAINT "collection_users_documentId_fkey" TO "user_permissions_documentId_fkey"`,
+        { transaction }
+      );
       await queryInterface.removeConstraint(
-        "user_permissions",
+        "collection_users",
         "user_permissions_collectionId_fkey",
         { transaction }
       );
-      await queryInterface.addConstraint("user_permissions", {
+      await queryInterface.addConstraint("collection_users", {
         fields: ["collectionId"],
         name: "user_permissions_collectionId_fkey",
         type: "foreign key",
@@ -56,7 +63,7 @@ module.exports = {
         transaction,
       });
       await queryInterface.changeColumn(
-        "user_permissions",
+        "collection_users",
         "collectionId",
         {
           type: Sequelize.UUID,
@@ -65,11 +72,11 @@ module.exports = {
         { transaction }
       );
       await queryInterface.removeConstraint(
-        "user_permissions",
+        "collection_users",
         "user_permissions_createdById_fkey",
         { transaction }
       );
-      await queryInterface.addConstraint("user_permissions", {
+      await queryInterface.addConstraint("collection_users", {
         fields: ["createdById"],
         name: "user_permissions_createdById_fkey",
         type: "foreign key",
@@ -80,12 +87,22 @@ module.exports = {
         },
         transaction,
       });
+      await queryInterface.renameTable("collection_users", "user_permissions", {
+        transaction,
+      });
+      await queryInterface.sequelize.query(
+        `CREATE VIEW collection_users AS SELECT * FROM user_permissions`,
+        { transaction }
+      );
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.renameTable("user_permissions", "collection_users");
     await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `DROP VIEW IF EXISTS collection_users`,
+        { transaction }
+      );
       await queryInterface.sequelize.query(
         `ALTER INDEX "user_permissions_collection_id_user_id" RENAME TO "collection_users_collection_id_user_id"`,
         { transaction }
@@ -95,26 +112,26 @@ module.exports = {
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY collection_users RENAME CONSTRAINT "user_permissions_collectionId_fkey" TO "collection_users_collectionId_fkey"`,
+        `ALTER TABLE ONLY user_permissions RENAME CONSTRAINT "user_permissions_collectionId_fkey" TO "collection_users_collectionId_fkey"`,
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY collection_users RENAME CONSTRAINT "user_permissions_createdById_fkey" TO "collection_users_createdById_fkey"`,
+        `ALTER TABLE ONLY user_permissions RENAME CONSTRAINT "user_permissions_createdById_fkey" TO "collection_users_createdById_fkey"`,
         { transaction }
       );
       await queryInterface.sequelize.query(
-        `ALTER TABLE ONLY collection_users RENAME CONSTRAINT "user_permissions_userId_fkey" TO "collection_users_userId_fkey"`,
+        `ALTER TABLE ONLY user_permissions RENAME CONSTRAINT "user_permissions_userId_fkey" TO "collection_users_userId_fkey"`,
         { transaction }
       );
-      await queryInterface.removeColumn("collection_users", "documentId", {
+      await queryInterface.removeColumn("user_permissions", "documentId", {
         transaction,
       });
       await queryInterface.removeConstraint(
-        "collection_users",
+        "user_permissions",
         "collection_users_collectionId_fkey",
         { transaction }
       );
-      await queryInterface.addConstraint("collection_users", {
+      await queryInterface.addConstraint("user_permissions", {
         fields: ["collectionId"],
         name: "collection_users_collectionId_fkey",
         type: "foreign key",
@@ -127,11 +144,11 @@ module.exports = {
       });
       // Delete records where collectionId is null before setting back non null constraint
       await queryInterface.sequelize.query(
-        `DELETE FROM collection_users WHERE "collectionId" IS NULL`,
+        `DELETE FROM user_permissions WHERE "collectionId" IS NULL`,
         { transaction }
       );
       await queryInterface.changeColumn(
-        "collection_users",
+        "user_permissions",
         "collectionId",
         {
           type: Sequelize.UUID,
@@ -140,11 +157,11 @@ module.exports = {
         { transaction }
       );
       await queryInterface.removeConstraint(
-        "collection_users",
+        "user_permissions",
         "collection_users_createdById_fkey",
         { transaction }
       );
-      await queryInterface.addConstraint("collection_users", {
+      await queryInterface.addConstraint("user_permissions", {
         fields: ["createdById"],
         name: "collection_users_createdById_fkey",
         type: "foreign key",
@@ -154,6 +171,13 @@ module.exports = {
         },
         transaction,
       });
+      await queryInterface.renameTable("user_permissions", "collection_users", {
+        transaction,
+      });
+      await queryInterface.sequelize.query(
+        `CREATE VIEW user_permissions AS SELECT * FROM collection_users`,
+        { transaction }
+      );
     });
   },
 };
