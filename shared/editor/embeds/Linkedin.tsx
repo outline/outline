1447090854b44043657f105837be0bfd409a5cd7
Prikshit singh <prikshitsingh79@gmail.com>
2023-10-11 20:12:35 +0530

    Convert LinkedIn post URL to embed URL

--- shared/editor/embeds/Linkedin.tsx
+++ shared/editor/embeds/Linkedin.tsx
@@ -4,10 +4,41 @@ import { EmbedProps as Props } from ".";
 
 function Linkedin(props: Props) {
   const { matches } = props.attrs;
-  const source = matches[0];
-  return <Frame {...props} src={source} title="Linkedin" />;
+  const regex = /(\d+)/gm;
+  let m;
+  let code;
+  while ((m = regex.exec(matches[0])) !== null) {
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    m.forEach((match) => {
+      if (match.length > 3) {
+        code = match;
+      }
+    });
+  }
+  const source = code;
+  const regexUgcPost = /ugcPost-(\d+)/;
+  const match = matches[0].match(regexUgcPost);
+
+  if (match && match[1]) {
+    return (
+      <Frame
+        {...props}
+        src={`https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:${source}`}
+        title="Linkedin"
+      />
+    );
+  }
+  return (
+    <Frame
+      {...props}
+      src={`https://www.linkedin.com/embed/feed/update/urn:li:activity:${source}`}
+      title="Linkedin"
+    />
+  );
 }
 
-Linkedin.ENABLED = [new RegExp("^https://www.linkedin.com/embed/?(.*)$")];
+Linkedin.ENABLED = [new RegExp("^https://www.linkedin.com/posts/?(.*)$")];
 
 export default Linkedin;
