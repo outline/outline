60eb39db32b539836af25572f8aca29de7737860
Tom Moor <tom.moor@gmail.com>
2023-11-19 18:10:30 -0500

    Add backfill script

--- server/scripts/20221008000000-backfill-crdt.ts
+++ server/scripts/20221008000000-backfill-crdt.ts
@@ -27,7 +27,6 @@ export default async function main(exit = false) {
       offset: page * limit,
       where: {
         ...(teamId ? { teamId } : {}),
-        state: null,
       },
       order: [["createdAt", "ASC"]],
       paranoid: false,
