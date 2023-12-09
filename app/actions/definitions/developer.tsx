8a6b8404e4f39960e8d1f04e4830e8d0a515ffbf
Tom Moor <tom.moor@gmail.com>
2023-12-09 13:24:23 -0500

    Add 'Copy ID' developer actions

--- app/actions/definitions/developer.tsx
+++ app/actions/definitions/developer.tsx
@@ -1,4 +1,5 @@
-import { ToolsIcon, TrashIcon, UserIcon } from "outline-icons";
+import copy from "copy-to-clipboard";
+import { CopyIcon, ToolsIcon, TrashIcon, UserIcon } from "outline-icons";
 import * as React from "react";
 import { toast } from "sonner";
 import { createAction } from "~/actions";
@@ -7,6 +8,71 @@ import env from "~/env";
 import { client } from "~/utils/ApiClient";
 import Logger from "~/utils/Logger";
 import { deleteAllDatabases } from "~/utils/developer";
+
+export const copyId = createAction({
+  name: ({ t }) => t("Copy ID"),
+  icon: <CopyIcon />,
+  keywords: "uuid",
+  section: DeveloperSection,
+  children: ({
+    currentTeamId,
+    currentUserId,
+    activeCollectionId,
+    activeDocumentId,
+  }) => {
+    function copyAndToast(text: string | null | undefined) {
+      if (text) {
+        copy(text);
+        toast.success("Copied to clipboard");
+      }
+    }
+
+    return [
+      createAction({
+        name: "Copy User ID",
+        section: DeveloperSection,
+        icon: <CopyIcon />,
+        visible: () => !!currentUserId,
+        perform: () => copyAndToast(currentUserId),
+      }),
+      createAction({
+        name: "Copy Team ID",
+        section: DeveloperSection,
+        icon: <CopyIcon />,
+        visible: () => !!currentTeamId,
+        perform: () => copyAndToast(currentTeamId),
+      }),
+      createAction({
+        name: "Copy Collection ID",
+        icon: <CopyIcon />,
+        section: DeveloperSection,
+        visible: () => !!activeCollectionId,
+        perform: () => copyAndToast(activeCollectionId),
+      }),
+      createAction({
+        name: "Copy Document ID",
+        icon: <CopyIcon />,
+        section: DeveloperSection,
+        visible: () => !!activeDocumentId,
+        perform: () => copyAndToast(activeDocumentId),
+      }),
+      createAction({
+        name: "Copy Team ID",
+        icon: <CopyIcon />,
+        section: DeveloperSection,
+        visible: () => !!currentTeamId,
+        perform: () => copyAndToast(currentTeamId),
+      }),
+      createAction({
+        name: "Copy Release ID",
+        icon: <CopyIcon />,
+        section: DeveloperSection,
+        visible: () => !!env.RELEASE,
+        perform: () => copyAndToast(env.RELEASE),
+      }),
+    ];
+  },
+});
 
 export const clearIndexedDB = createAction({
   name: ({ t }) => t("Delete IndexedDB cache"),
@@ -67,7 +133,13 @@ export const developer = createAction({
   icon: <ToolsIcon />,
   iconInContextMenu: false,
   section: DeveloperSection,
-  children: [clearIndexedDB, toggleDebugLogging, createToast, createTestUsers],
+  children: [
+    copyId,
+    clearIndexedDB,
+    toggleDebugLogging,
+    createToast,
+    createTestUsers,
+  ],
 });
 
 export const rootDeveloperActions = [developer];
