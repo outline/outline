ff284db3f989dd4612980b8a79b383e9232e9305
Tom Moor <tom.moor@gmail.com>
2023-12-09 10:54:37 -0500

    fix: Cannot read properties of undefined on old Android

--- app/utils/isTextInput.ts
+++ app/utils/isTextInput.ts
@@ -1,8 +1,9 @@
 const inputs = ["input", "select", "button", "textarea"]; // detect if node is a text input element
 
-export default function isTextInput(element: HTMLElement): boolean {
-  return (
+export default function isTextInput(element: Element): boolean {
+  return !!(
     element &&
+    element.tagName &&
     (inputs.indexOf(element.tagName.toLowerCase()) !== -1 ||
       element.attributes.getNamedItem("role")?.value === "textbox" ||
       element.attributes.getNamedItem("contenteditable")?.value === "true")
