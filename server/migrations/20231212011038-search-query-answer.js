e315ba9dff02b2bbb7aa141a306d7ab3de15e8da
Tom Moor <tom@getoutline.com>
2023-12-11 17:28:39 -0800

    String -> Text

--- server/migrations/20231212011038-search-query-answer.js
+++ server/migrations/20231212011038-search-query-answer.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn("search_queries", "answer", {
-      type: Sequelize.STRING,
+      type: Sequelize.TEXT,
       allowNull: true,
     });
   },
