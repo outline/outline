ddf0088c8e6da9108df4229b3b889930ab71737f
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-02-02 22:05:11 +0530

    fix: hide behind a flag

--- server/env.ts
+++ server/env.ts
@@ -396,6 +396,12 @@ export class Environment {
     process.env.GITHUB_APP_PRIVATE_KEY
   );
 
+  @IsOptional()
+  @IsBoolean()
+  public GITHUB_INTEGRATION_DISABLED = this.toBoolean(
+    process.env.GITHUB_INTEGRATION_DISABLED ?? "true"
+  );
+
   /**
    * Disable autoredirect to the OIDC login page if there is only one
    * authentication method and that method is OIDC.
