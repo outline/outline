57dddb00cb1172123fd9e94e283a82d41c9af180
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-02-03 17:27:25 +0530

    fix: types

--- shared/types.ts
+++ shared/types.ts
@@ -110,7 +110,7 @@ export enum DocumentPermission {
 
 export type IntegrationSettings<T> = T extends IntegrationType.Embed
   ? {
-      url?: string;
+      url: string;
       github?: {
         installation: {
           id: number;
@@ -125,8 +125,8 @@ export type IntegrationSettings<T> = T extends IntegrationType.Embed
   : T extends IntegrationType.Command
   ? { serviceTeamId: string }
   :
+      | { url: string }
       | {
-          url?: string;
           github?: {
             installation: {
               id: number;
