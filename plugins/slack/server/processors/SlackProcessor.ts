7ed37f904af0bbf1da19a08e5d1a8913cc12fd62
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-02-05 13:06:06 +0530

    fix: IntegrationDeletedProcessor

--- plugins/slack/server/processors/SlackProcessor.ts
+++ plugins/slack/server/processors/SlackProcessor.ts
@@ -2,15 +2,8 @@ import { differenceInMilliseconds } from "date-fns";
 import { Op } from "sequelize";
 import { IntegrationService, IntegrationType } from "@shared/types";
 import { Minute } from "@shared/utils/time";
-import {
-  Document,
-  Integration,
-  Collection,
-  Team,
-  IntegrationAuthentication,
-} from "@server/models";
+import { Document, Integration, Collection, Team } from "@server/models";
 import BaseProcessor from "@server/queues/processors/BaseProcessor";
-import { sequelize } from "@server/storage/database";
 import {
   DocumentEvent,
   IntegrationEvent,
@@ -27,7 +20,6 @@ export default class SlackProcessor extends BaseProcessor {
     "documents.publish",
     "revisions.create",
     "integrations.create",
-    "integrations.delete",
   ];
 
   async perform(event: Event) {
@@ -40,8 +32,6 @@ export default class SlackProcessor extends BaseProcessor {
 
       case "integrations.create":
         return this.integrationCreated(event);
-      case "integrations.delete":
-        return this.integrationDeleted(event);
 
       default:
     }
@@ -152,32 +142,6 @@ export default class SlackProcessor extends BaseProcessor {
           presentMessageAttachment(document, team, document.collection),
         ],
       }),
-    });
-  }
-
-  async integrationDeleted(event: IntegrationEvent) {
-    const integration = (await Integration.findOne({
-      where: {
-        id: event.modelId,
-        service: IntegrationService.Slack,
-      },
-      paranoid: false,
-    })) as Integration<IntegrationType.Embed>;
-    if (!integration) {
-      return;
-    }
-
-    await sequelize.transaction(async (transaction) => {
-      await integration.destroy({ transaction, force: true });
-      // also remove the corresponding authentication if it exists
-      if (integration.authenticationId) {
-        await IntegrationAuthentication.destroy({
-          where: {
-            id: integration.authenticationId,
-          },
-          transaction,
-        });
-      }
     });
   }
 }
