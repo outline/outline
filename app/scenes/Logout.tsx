5ad6732516a09c384658d9a9b4e304c6928beaea
Shuttleu <shuttleu@gmail.com>
2024-02-15 11:55:43 +0000

    Add logout redirect to work on logout route

--- app/scenes/Logout.tsx
+++ app/scenes/Logout.tsx
@@ -1,3 +1,4 @@
+import env from "@shared/env";
 import * as React from "react";
 import { Redirect } from "react-router-dom";
 import useStores from "~/hooks/useStores";
@@ -5,6 +6,8 @@ import useStores from "~/hooks/useStores";
 const Logout = () => {
   const { auth } = useStores();
   void auth.logout();
+  if (env.OIDC_LOGOUT_URI)
+    window.location.replace(env.OIDC_LOGOUT_URI);
   return <Redirect to="/" />;
 };
 
