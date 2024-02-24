99cdef2ad4191541af1608d27e17b28972598508
Tom Moor <tom.moor@gmail.com>
2024-02-23 22:33:47 -0500

    fix: Document history is displayed in reverse order

--- app/stores/EventsStore.ts
+++ app/stores/EventsStore.ts
@@ -13,6 +13,6 @@ export default class EventsStore extends Store<Event> {
 
   @computed
   get orderedData(): Event[] {
-    return orderBy(Array.from(this.data.values()), "createdAt", "asc");
+    return orderBy(Array.from(this.data.values()), "createdAt", "desc");
   }
 }
