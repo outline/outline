b8bf99b52468faa8d8a94e3969d96db5ac5516c5
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-02-29 11:30:41 +0530

    fix: UrlHelper class

--- shared/utils/UrlHelper.ts
+++ shared/utils/UrlHelper.ts
@@ -1,16 +1,10 @@
-const urls = {
-  github: "https://www.github.com/outline/outline/issues",
-  twitter: "https://twitter.com/getoutline",
-  contact: "https://www.getoutline.com/contact",
-  developers: "https://www.getoutline.com/developers",
-  changelog: "https://www.getoutline.com/changelog",
-};
+export class UrlHelper {
+  public static github = "https://www.github.com/outline/outline/issues";
+  public static twitter = "https://twitter.com/getoutline";
+  public static contact = "https://www.getoutline.com/contact";
+  public static developers = "https://www.getoutline.com/developers";
+  public static changelog = "https://www.getoutline.com/changelog";
 
-const SLUG_URL_REGEX = /^(?:[0-9a-zA-Z-_~]*-)?([a-zA-Z0-9]{10,15})$/;
-const SHARE_URL_SLUG_REGEX = /^[0-9a-z-]+$/;
-
-export const UrlHelper = {
-  ...urls,
-  SLUG_URL_REGEX,
-  SHARE_URL_SLUG_REGEX,
-};
+  public static SLUG_URL_REGEX = /^(?:[0-9a-zA-Z-_~]*-)?([a-zA-Z0-9]{10,15})$/;
+  public static SHARE_URL_SLUG_REGEX = /^[0-9a-z-]+$/;
+}
