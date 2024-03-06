a7aac2d8dea2daa76d70d26da511eba74017d529
Pranav Joglekar <pranav2000joglekar@gmail.com>
2024-03-06 09:56:26 +0530

    fix: show complete page without scrollbar in print view (#6635)

--- app/hooks/useMobile.ts
+++ app/hooks/useMobile.ts
@@ -2,5 +2,7 @@ import { breakpoints } from "@shared/styles";
 import useMediaQuery from "~/hooks/useMediaQuery";
 
 export default function useMobile(): boolean {
-  return useMediaQuery(`(max-width: ${breakpoints.tablet - 1}px)`);
+  return useMediaQuery(
+    `(max-width: ${breakpoints.tablet - 1}px) and (not print)`
+  );
 }
