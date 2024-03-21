0a462b0818b72274811bc47ad893622d4f2ef5fa
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-03-21 16:30:35 +0530

    fix: remove extra method

--- plugins/iframely/server/iframely.ts
+++ plugins/iframely/server/iframely.ts
@@ -1,4 +1,3 @@
-import type { Unfurl } from "@shared/types";
 import { InternalError } from "@server/errors";
 import fetch from "@server/utils/fetch";
 import env from "./env";
@@ -7,10 +6,16 @@ class Iframely {
   private static apiUrl = `${env.IFRAMELY_URL}/api`;
   private static apiKey = env.IFRAMELY_API_KEY;
 
-  public static async fetch(url: string, type = "oembed") {
+  /**
+   * Fetches the preview data for the given url using Iframely oEmbed API
+   *
+   * @param url
+   * @returns Preview data for the url
+   */
+  public static unfurl = async (url: string) => {
     try {
       const res = await fetch(
-        `${this.apiUrl}/${type}?url=${encodeURIComponent(url)}&api_key=${
+        `${this.apiUrl}/oembed?url=${encodeURIComponent(url)}&api_key=${
           this.apiKey
         }`
       );
@@ -18,17 +23,7 @@ class Iframely {
     } catch (err) {
       throw InternalError(err);
     }
-  }
-
-  /**
-   * Fetches the preview data for the given url using Iframely oEmbed API
-   *
-   * @param url
-   * @returns Preview data for the url
-   */
-  public static async unfurl(url: string): Promise<Unfurl | false> {
-    return Iframely.fetch(url);
-  }
+  };
 }
 
 export default Iframely;
