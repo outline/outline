945e769dbad4fe9e1903b8bfbae3c8a0e3b3deac
Tom Moor <tom.moor@gmail.com>
2024-03-27 08:40:47 -0400

    migration

--- server/migrations/20240327015248-add-user-role.js
+++ server/migrations/20240327015248-add-user-role.js
@@ -10,24 +10,29 @@ module.exports = {
       return;
     }
 
-    await queryInterface.sequelize.query(
-      `UPDATE users SET role = 'viewer' WHERE "isViewer" = true`,
-      {
-        type: queryInterface.sequelize.QueryTypes.SELECT,
-      }
-    );
-    await queryInterface.sequelize.query(
-      `UPDATE users SET role = 'admin' WHERE "isAdmin" = true`,
-      {
-        type: queryInterface.sequelize.QueryTypes.SELECT,
-      }
-    );
-    await queryInterface.sequelize.query(
-      `UPDATE users SET role = 'member' WHERE role IS NULL`,
-      {
-        type: queryInterface.sequelize.QueryTypes.SELECT,
-      }
-    );
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `UPDATE users SET role = 'viewer' WHERE "isViewer" = true`,
+        {
+          transaction,
+          type: queryInterface.sequelize.QueryTypes.UPDATE,
+        }
+      );
+      await queryInterface.sequelize.query(
+        `UPDATE users SET role = 'admin' WHERE "isAdmin" = true`,
+        {
+          transaction,
+          type: queryInterface.sequelize.QueryTypes.UPDATE,
+        }
+      );
+      await queryInterface.sequelize.query(
+        `UPDATE users SET role = 'member' WHERE role IS NULL`,
+        {
+          transaction,
+          type: queryInterface.sequelize.QueryTypes.UPDATE,
+        }
+      );
+    });
   },
 
   async down (queryInterface) {
