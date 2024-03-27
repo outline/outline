69a31a807517ed51b3e0d9a3df31b135a99376d2
Tom Moor <tom.moor@gmail.com>
2024-03-26 23:30:51 -0400

    Simplify for self-hosted

--- server/migrations/20240327015248-add-user-role.js
+++ server/migrations/20240327015248-add-user-role.js
@@ -6,6 +6,28 @@ module.exports = {
       type: Sequelize.ENUM("admin", "member", "viewer", "guest"),
       allowNull: true,
     });
+    if (process.env.DEPLOYMENT === "hosted") {
+      return;
+    }
+
+    await queryInterface.sequelize.query(
+      `UPDATE users SET role = 'admin' WHERE "isAdmin" = true`,
+      {
+        type: queryInterface.sequelize.QueryTypes.SELECT,
+      }
+    );
+    await queryInterface.sequelize.query(
+      `UPDATE users SET role = 'viewer' WHERE "isViewer" = true`,
+      {
+        type: queryInterface.sequelize.QueryTypes.SELECT,
+      }
+    );
+    await queryInterface.sequelize.query(
+      `UPDATE users SET role = 'member' WHERE role IS NULL`,
+      {
+        type: queryInterface.sequelize.QueryTypes.SELECT,
+      }
+    );
   },
 
   async down (queryInterface) {
