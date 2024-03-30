87547280b88b6e8c1d30cb9ba57302b45242a018
Tom Moor <tom.moor@gmail.com>
2024-03-30 00:54:19 -0400

    policies/integration

--- server/policies/notification.ts
+++ server/policies/notification.ts
@@ -1,9 +1,9 @@
 import { Notification, User } from "@server/models";
 import { allow } from "./cancan";
 
-allow(User, ["read", "update"], Notification, (user, notification) => {
-  if (!notification) {
-    return false;
-  }
-  return user?.id === notification.userId;
-});
+allow(
+  User,
+  ["read", "update"],
+  Notification,
+  (user, notification) => user?.id === notification?.userId
+);
