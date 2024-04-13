63ab2f4615c929d44273cbd93b930a992581733a
Tom Moor <tom.moor@gmail.com>
2024-04-13 12:42:22 -0400

    Add 80+ additional icons from FontAwesome

--- app/components/IconPicker.tsx
+++ app/components/IconPicker.tsx
@@ -11,6 +11,7 @@ import useOnClickOutside from "~/hooks/useOnClickOutside";
 import lazyWithRetry from "~/utils/lazyWithRetry";
 import DelayedMount from "./DelayedMount";
 import { IconLibrary } from "./Icons/IconLibrary";
+import InputSearch from "./InputSearch";
 import Popover from "./Popover";
 
 const icons = IconLibrary.mapping;
@@ -38,11 +39,12 @@ function IconPicker({
   onChange,
   className,
 }: Props) {
+  const [query, setQuery] = React.useState("");
   const { t } = useTranslation();
   const theme = useTheme();
   const popover = usePopoverState({
     gutter: 0,
-    placement: "bottom",
+    placement: "right",
     modal: true,
   });
 
@@ -51,8 +53,14 @@ function IconPicker({
       onOpen?.();
     } else {
       onClose?.();
+      setQuery("");
     }
   }, [onOpen, onClose, popover.visible]);
+
+  const filteredIcons = IconLibrary.findIcons(query);
+  const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value.toLowerCase());
+  };
 
   const styles = React.useMemo(
     () => ({
@@ -112,51 +120,64 @@ function IconPicker({
       </PopoverDisclosure>
       <Popover
         {...popover}
-        width={388}
+        width={552}
         aria-label={t("Choose icon")}
         hideOnClickOutside={false}
       >
-        <Icons>
-          {Object.keys(icons).map((name, index) => (
-            <MenuItem key={name} onClick={() => onChange(color, name)}>
-              {(props) => (
-                <IconButton
-                  style={
-                    {
-                      "--delay": `${index * 8}ms`,
-                    } as React.CSSProperties
-                  }
-                  {...props}
-                >
-                  <Icon
-                    as={IconLibrary.getComponent(name)}
-                    color={color}
-                    size={30}
+        <Flex column gap={12}>
+          <InputSearch
+            value={query}
+            placeholder={`${t("Filter")}…`}
+            onChange={handleFilter}
+            autoFocus
+          />
+          <div>
+            {Object.keys(icons).map((name, index) => (
+              <MenuItem key={name} onClick={() => onChange(color, name)}>
+                {(props) => (
+                  <IconButton
+                    style={
+                      {
+                        opacity: query
+                          ? filteredIcons.includes(name)
+                            ? 1
+                            : 0.3
+                          : undefined,
+                        "--delay": `${index * 4}ms`,
+                      } as React.CSSProperties
+                    }
+                    {...props}
                   >
-                    {initial}
-                  </Icon>
-                </IconButton>
-              )}
-            </MenuItem>
-          ))}
-        </Icons>
-        <Colors>
-          <React.Suspense
-            fallback={
-              <DelayedMount>
-                <Text>{t("Loading")}…</Text>
-              </DelayedMount>
-            }
-          >
-            <ColorPicker
-              color={color}
-              onChange={(color) => onChange(color.hex, icon)}
-              colors={colorPalette}
-              triangle="hide"
-              styles={styles}
-            />
-          </React.Suspense>
-        </Colors>
+                    <Icon
+                      as={IconLibrary.getComponent(name)}
+                      color={color}
+                      size={30}
+                    >
+                      {initial}
+                    </Icon>
+                  </IconButton>
+                )}
+              </MenuItem>
+            ))}
+          </div>
+          <Flex>
+            <React.Suspense
+              fallback={
+                <DelayedMount>
+                  <Text>{t("Loading")}…</Text>
+                </DelayedMount>
+              }
+            >
+              <ColorPicker
+                color={color}
+                onChange={(color) => onChange(color.hex, icon)}
+                colors={colorPalette}
+                triangle="hide"
+                styles={styles}
+              />
+            </React.Suspense>
+          </Flex>
+        </Flex>
       </Popover>
     </>
   );
@@ -165,14 +186,6 @@ function IconPicker({
 const Icon = styled.svg`
   transition: fill 150ms ease-in-out;
   transition-delay: var(--delay);
-`;
-
-const Colors = styled(Flex)`
-  padding: 8px;
-`;
-
-const Icons = styled.div`
-  padding: 8px;
 `;
 
 const IconButton = styled(NudeButton)`
