1f980050caa7c6e20c498e7a4181581f6198820c
Tom Moor <tom.moor@gmail.com>
2024-05-30 18:46:03 -0400

    fix: Incorrect empty check for collection description results in large empty space below title

--- shared/utils/ProsemirrorHelper.ts
+++ shared/utils/ProsemirrorHelper.ts
@@ -53,6 +53,30 @@ export class ProsemirrorHelper {
   }
 
   /**
+   * Returns true if the data looks like an empty document.
+   *
+   * @param data The ProsemirrorData to check.
+   * @returns True if the document is empty.
+   */
+  static isEmptyData(data: ProsemirrorData): boolean {
+    if (data.type !== "doc") {
+      return false;
+    }
+
+    if (data.content.length === 1) {
+      const node = data.content[0];
+      return (
+        node.type === "paragraph" &&
+        (node.content === null ||
+          node.content === undefined ||
+          node.content.length === 0)
+      );
+    }
+
+    return data.content.length === 0;
+  }
+
+  /**
    * Returns the node as plain text.
    *
    * @param node The node to convert.
