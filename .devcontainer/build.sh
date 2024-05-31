99bb538b33b24cee7462f6cd50ba80e1c21ecc03
Tao Chen <42793494+IamTaoChen@users.noreply.github.com>
2024-05-31 08:14:18 +0000

    update env

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -26,6 +26,36 @@ replace_secret() {
   replace_env "$key" "$new_value" "$filename"
 }
 
+remove_env() {
+  local key=$1
+  local filename=$2
+  
+  if [ -z "$filename" ]; then
+      filename=".env"
+  fi
+  
+  sed -i "/^${key}=/d" "$filename"
+}
+
+comment_out_env() {
+  local key=$1
+  local filename=$2
+  
+  if [ -z "$filename" ]; then
+      filename=".env"
+  fi
+  
+  sed -i "s/^${key}=/#&/" "$filename"
+}
+
 replace_secret "SECRET_KEY" "$NEW_ENV_FILENAME"
 replace_secret "UTILS_SECRET" "$NEW_ENV_FILENAME"
 
+
+DATABASE_URL=postgres://user:pass@postgres:5432/outline
+REDIS_URL=redis://redis:6379
+
+remove_env "DATABASE_URL" “$DATABASE_URL” "$NEW_ENV_FILENAME"
+remove_env "REDIS_URL" "$REDIS_URL" "$NEW_ENV_FILENAME"
+
+comment_out_env "IFRAMELY_URL" "$NEW_ENV_FILENAME"
