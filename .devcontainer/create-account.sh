26d80887daf2e9f14b6b8226a4cddeff25e81806
Tao Chen <42793494+IamTaoChen@users.noreply.github.com>
2024-05-31 09:50:25 +0000

    update

---
+++ .devcontainer/create-account.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yarn build 
+AUTH_LINK=$(node build/server/scripts/seed.js my@email.com)
+
+echo "Please visit the following link to create an account: $AUTH_LINK"
+
+echo "# $AUTH_LINK" > .env
+echo "The link has been saved to the .env file"
