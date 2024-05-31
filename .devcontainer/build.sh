b1324bfb2f6ceab3f54c300e7ba1fe02124e3e9b
Tao Chen <42793494+IamTaoChen@users.noreply.github.com>
2024-05-31 08:04:27 +0000

    replace ENV

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -4,5 +4,28 @@
 yarn install-local-ssl
 yarn install --pure-lockfile
 
+NEW_ENV_FILENAME=.env
+cp .env.sample $NEW_ENV_FILENAME
 
-cp .env.sample .env
+replace_env() {
+  local key=$1
+  local value=$2
+  local filename=$3
+  
+  if [ -z "$filename" ]; then
+      filename=".env"
+  fi
+  
+  sed -i "s/^${key}=.*/${key}=${value}/" "$filename"
+}
+
+replace_secret() {
+  local key=$1
+  local filename=$2
+  local new_value=$(openssl rand -hex 32)
+  replace_env "$key" "$new_value" "$filename"
+}
+
+replace_secret "SECRET_KEY" "$NEW_ENV_FILENAME"
+replace_secret "UTILS_SECRET" "$NEW_ENV_FILENAME"
+
