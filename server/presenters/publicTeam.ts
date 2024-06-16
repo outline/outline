1f4e6a6ba99da31e92aac3c53d155cf88c4cc22b
Tom Moor <tom.moor@gmail.com>
2024-06-16 17:24:45 -0400

    fix: Allow sidebar position preference without public branding switch

--- server/presenters/publicTeam.ts
+++ server/presenters/publicTeam.ts
@@ -1,11 +1,20 @@
 import { TOCPosition, TeamPreference } from "@shared/types";
 import { Team } from "@server/models";
 
-export default function presentPublicTeam(team: Team) {
+export default function presentPublicTeam(
+  /** The team to present */
+  team: Team,
+  /** Whether the branding is public */
+  isBrandingPublic: boolean
+) {
   return {
-    name: team.name,
-    avatarUrl: team.avatarUrl,
-    customTheme: team.getPreference(TeamPreference.CustomTheme),
+    ...(isBrandingPublic
+      ? {
+          name: team.name,
+          avatarUrl: team.avatarUrl,
+          customTheme: team.getPreference(TeamPreference.CustomTheme),
+        }
+      : {}),
     tocPosition: team.getPreference(TeamPreference.TocPosition) as TOCPosition,
   };
 }
