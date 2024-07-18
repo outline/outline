4b48a9ebeb46b716017d5bf4cdffeeb31c1ee80e
Tom Moor <tom.moor@gmail.com>
2024-07-17 20:46:04 -0400

    docs

--- app/models/GroupMembership.ts
+++ app/models/GroupMembership.ts
@@ -5,20 +5,23 @@ import Group from "./Group";
 import Model from "./base/Model";
 import Relation from "./decorators/Relation";
 
+/**
+ * Represents a groups's membership to a collection or document.
+ */
 class GroupMembership extends Model {
   static modelName = "GroupMembership";
 
-  /** The group ID that this permission is granted to. */
+  /** The group ID that this membership is granted to. */
   groupId: string;
 
-  /** The group that this permission is granted to. */
+  /** The group that this membership is granted to. */
   @Relation(() => Group, { onDelete: "cascade" })
   group: Group;
 
-  /** The collection ID that this permission is granted to. */
+  /** The collection ID that this membership grants the group access to. */
   collectionId: string | undefined;
 
-  /** The collection that this permission is granted to. */
+  /** The collection that this membership grants the group access to. */
   @Relation(() => Collection, { onDelete: "cascade" })
   collection: Collection | undefined;
 
