19b6ee832b8174d194aa44ac1704e67f5b7ad12c
Tom Moor <tom.moor@gmail.com>
2024-09-02 22:07:26 -0400

    Suppress 'Added to document' notifications when user has existing document membership and added through group

--- server/queues/tasks/DocumentAddGroupNotificationsTask.ts
+++ server/queues/tasks/DocumentAddGroupNotificationsTask.ts
@@ -1,26 +1,51 @@
 import { Op } from "sequelize";
-import { GroupUser } from "@server/models";
+import Logger from "@server/logging/Logger";
+import { GroupUser, UserMembership } from "@server/models";
 import { DocumentGroupEvent } from "@server/types";
 import BaseTask, { TaskPriority } from "./BaseTask";
 import DocumentAddUserNotificationsTask from "./DocumentAddUserNotificationsTask";
 
 export default class DocumentAddGroupNotificationsTask extends BaseTask<DocumentGroupEvent> {
   public async perform(event: DocumentGroupEvent) {
-    const groupUsers = await GroupUser.findAll({
-      where: {
-        groupId: event.modelId,
-        userId: {
-          [Op.ne]: event.actorId,
+    await GroupUser.findAllInBatches<GroupUser>(
+      {
+        where: {
+          groupId: event.modelId,
+          userId: {
+            [Op.ne]: event.actorId,
+          },
         },
+        batchLimit: 10,
       },
-    });
+      async (groupUsers) => {
+        await Promise.all(
+          groupUsers.map(async (groupUser) => {
+            const userMembership = await UserMembership.findOne({
+              where: {
+                userId: groupUser.userId,
+                documentId: event.documentId,
+              },
+            });
+            if (userMembership) {
+              Logger.debug(
+                "task",
+                `Suppressing notification for user ${groupUser.userId} as they are already a member of the document`,
+                {
+                  documentId: event.documentId,
+                  userId: groupUser.userId,
+                }
+              );
+              return;
+            }
 
-    for (const groupUser of groupUsers) {
-      await DocumentAddUserNotificationsTask.schedule({
-        ...event,
-        userId: groupUser.userId,
-      });
-    }
+            await DocumentAddUserNotificationsTask.schedule({
+              ...event,
+              userId: groupUser.userId,
+            });
+          })
+        );
+      }
+    );
   }
 
   public get options() {
