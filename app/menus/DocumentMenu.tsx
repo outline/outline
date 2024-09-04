b2ae225908476b7a317ed2e25744b64d1efc0d48
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-09-04 22:02:55 +0530

    fix: bifurcate

--- app/menus/DocumentMenu.tsx
+++ app/menus/DocumentMenu.tsx
@@ -6,7 +6,12 @@ import { EditIcon, InputIcon, RestoreIcon, SearchIcon } from "outline-icons";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
-import { useMenuState, MenuButton, MenuButtonHTMLProps } from "reakit/Menu";
+import {
+  useMenuState,
+  MenuButton,
+  MenuButtonHTMLProps,
+  MenuStateReturn,
+} from "reakit/Menu";
 import { VisuallyHidden } from "reakit/VisuallyHidden";
 import { toast } from "sonner";
 import styled from "styled-components";
@@ -14,6 +19,7 @@ import breakpoint from "styled-components-breakpoint";
 import { s } from "@shared/styles";
 import { UserPreference } from "@shared/types";
 import { getEventFiles } from "@shared/utils/files";
+import Collection from "~/models/Collection";
 import Document from "~/models/Document";
 import ContextMenu from "~/components/ContextMenu";
 import OverflowMenuButton from "~/components/ContextMenu/OverflowMenuButton";
@@ -74,35 +80,23 @@ type Props = {
   onClose?: () => void;
 };
 
-function DocumentMenu({
+type MenuTriggerProps = {
+  label?: (props: MenuButtonHTMLProps) => React.ReactNode;
+  document: Document;
+  className?: string;
+  menu: MenuStateReturn;
+};
+
+const MenuTrigger: React.FC<MenuTriggerProps> = ({
+  label,
   document,
   className,
-  modal = true,
-  showToggleEmbeds,
-  showDisplayOptions,
-  label,
-  onFindAndReplace,
-  onRename,
-  onOpen,
-  onClose,
-}: Props) {
-  const user = useCurrentUser();
-  const { policies, collections, documents, subscriptions } = useStores();
-  const menu = useMenuState({
-    modal,
-    unstable_preventOverflow: true,
-    unstable_fixed: true,
-    unstable_flip: true,
-  });
-  const history = useHistory();
-  const context = useActionContext({
-    isContextMenu: true,
-    activeDocumentId: document.id,
-    activeCollectionId: document.collectionId ?? undefined,
-  });
+  menu,
+}) => {
   const { t } = useTranslation();
-  const isMobile = useMobile();
-  const file = React.useRef<HTMLInputElement>(null);
+
+  const { subscriptions } = useStores();
+
   const { data, loading, error, request } = useRequest(() =>
     subscriptions.fetchPage({
       documentId: document.id,
@@ -116,6 +110,59 @@ function DocumentMenu({
       void document.loadRelations();
     }
   }, [data, error, loading, request, document]);
+
+  return label ? (
+    <MenuButton {...menu} onMouseEnter={handleMouseEnter}>
+      {label}
+    </MenuButton>
+  ) : (
+    <OverflowMenuButton
+      className={className}
+      aria-label={t("Show menu")}
+      onMouseEnter={handleMouseEnter}
+      {...menu}
+    />
+  );
+};
+
+type MenuContentProps = {
+  document: Document;
+  menu: MenuStateReturn;
+  onOpen?: () => void;
+  onClose?: () => void;
+  collection?: Collection;
+  onFindAndReplace?: () => void;
+  onRename?: () => void;
+  showDisplayOptions?: boolean;
+  showToggleEmbeds?: boolean;
+};
+
+const MenuContent: React.FC<MenuContentProps> = ({
+  document,
+  menu,
+  onOpen,
+  onClose,
+  collection,
+  onFindAndReplace,
+  onRename,
+  showDisplayOptions,
+  showToggleEmbeds,
+}) => {
+  const user = useCurrentUser();
+
+  const can = usePolicy(document);
+
+  const { t } = useTranslation();
+
+  const { policies, collections } = useStores();
+
+  const context = useActionContext({
+    isContextMenu: true,
+    activeDocumentId: document.id,
+    activeCollectionId: document.collectionId ?? undefined,
+  });
+
+  const isMobile = useMobile();
 
   const handleRestore = React.useCallback(
     async (
@@ -134,10 +181,6 @@ function DocumentMenu({
     [t, document]
   );
 
-  const collection = document.collectionId
-    ? collections.get(document.collectionId)
-    : undefined;
-  const can = usePolicy(document);
   const restoreItems = React.useMemo(
     () => [
       ...collections.orderedData.reduce<MenuItem[]>((filtered, collection) => {
@@ -160,6 +203,182 @@ function DocumentMenu({
     ],
     [collections.orderedData, handleRestore, policies]
   );
+
+  return !isEmpty(can) ? (
+    <ContextMenu
+      {...menu}
+      aria-label={t("Document options")}
+      onOpen={onOpen}
+      onClose={onClose}
+    >
+      <Template
+        {...menu}
+        items={[
+          {
+            type: "button",
+            title: t("Restore"),
+            visible:
+              ((document.isWorkspaceTemplate || !!collection) && can.restore) ||
+              !!can.unarchive,
+            onClick: (ev) => handleRestore(ev),
+            icon: <RestoreIcon />,
+          },
+          {
+            type: "submenu",
+            title: t("Restore"),
+            visible:
+              !document.isWorkspaceTemplate &&
+              !collection &&
+              !!can.restore &&
+              restoreItems.length !== 0,
+            style: {
+              left: -170,
+              position: "relative",
+              top: -40,
+            },
+            icon: <RestoreIcon />,
+            hover: true,
+            items: [
+              {
+                type: "heading",
+                title: t("Choose a collection"),
+              },
+              ...restoreItems,
+            ],
+          },
+          actionToMenuItem(starDocument, context),
+          actionToMenuItem(unstarDocument, context),
+          actionToMenuItem(subscribeDocument, context),
+          actionToMenuItem(unsubscribeDocument, context),
+          ...(isMobile ? [actionToMenuItem(shareDocument, context)] : []),
+          {
+            type: "button",
+            title: `${t("Find and replace")}…`,
+            visible: !!onFindAndReplace && isMobile,
+            onClick: () => onFindAndReplace?.(),
+            icon: <SearchIcon />,
+          },
+          {
+            type: "separator",
+          },
+          {
+            type: "route",
+            title: t("Edit"),
+            to: documentEditPath(document),
+            visible:
+              !!can.update && user.separateEditMode && !document.template,
+            icon: <EditIcon />,
+          },
+          {
+            type: "button",
+            title: `${t("Rename")}…`,
+            visible: !!can.update && !user.separateEditMode && !!onRename,
+            onClick: () => onRename?.(),
+            icon: <InputIcon />,
+          },
+          actionToMenuItem(createNestedDocument, context),
+          actionToMenuItem(importDocument, context),
+          actionToMenuItem(createTemplateFromDocument, context),
+          actionToMenuItem(duplicateDocument, context),
+          actionToMenuItem(publishDocument, context),
+          actionToMenuItem(unpublishDocument, context),
+          actionToMenuItem(archiveDocument, context),
+          actionToMenuItem(moveDocument, context),
+          actionToMenuItem(moveTemplate, context),
+          actionToMenuItem(pinDocument, context),
+          actionToMenuItem(createDocumentFromTemplate, context),
+          {
+            type: "separator",
+          },
+          actionToMenuItem(openDocumentComments, context),
+          actionToMenuItem(openDocumentHistory, context),
+          actionToMenuItem(openDocumentInsights, context),
+          actionToMenuItem(downloadDocument, context),
+          actionToMenuItem(copyDocument, context),
+          actionToMenuItem(printDocument, context),
+          actionToMenuItem(searchInDocument, context),
+          {
+            type: "separator",
+          },
+          actionToMenuItem(deleteDocument, context),
+          actionToMenuItem(permanentlyDeleteDocument, context),
+        ]}
+      />
+      {(showDisplayOptions || showToggleEmbeds) && can.update && (
+        <>
+          <Separator />
+          <DisplayOptions>
+            {showToggleEmbeds && (
+              <Style>
+                <ToggleMenuItem
+                  width={26}
+                  height={14}
+                  label={t("Enable embeds")}
+                  labelPosition="left"
+                  checked={!document.embedsDisabled}
+                  onChange={
+                    document.embedsDisabled
+                      ? document.enableEmbeds
+                      : document.disableEmbeds
+                  }
+                />
+              </Style>
+            )}
+            {showDisplayOptions && !isMobile && (
+              <Style>
+                <ToggleMenuItem
+                  width={26}
+                  height={14}
+                  label={t("Full width")}
+                  labelPosition="left"
+                  checked={document.fullWidth}
+                  onChange={(ev) => {
+                    const fullWidth = ev.currentTarget.checked;
+                    user.setPreference(
+                      UserPreference.FullWidthDocuments,
+                      fullWidth
+                    );
+                    void user.save();
+                    document.fullWidth = fullWidth;
+                    void document.save();
+                  }}
+                />
+              </Style>
+            )}
+          </DisplayOptions>
+        </>
+      )}
+    </ContextMenu>
+  ) : null;
+};
+
+function DocumentMenu({
+  document,
+  className,
+  modal = true,
+  showToggleEmbeds,
+  showDisplayOptions,
+  label,
+  onRename,
+  onOpen,
+  onClose,
+}: Props) {
+  const { collections, documents } = useStores();
+  const menu = useMenuState({
+    modal,
+    unstable_preventOverflow: true,
+    unstable_fixed: true,
+    unstable_flip: true,
+  });
+  const history = useHistory();
+
+  const { t } = useTranslation();
+  const file = React.useRef<HTMLInputElement>(null);
+
+  const collection = document.collectionId
+    ? collections.get(document.collectionId)
+    : undefined;
+
   const stopPropagation = React.useCallback((ev: React.SyntheticEvent) => {
     ev.stopPropagation();
   }, []);
@@ -212,165 +431,22 @@ function DocumentMenu({
           />
         </label>
       </VisuallyHidden>
-      {label ? (
-        <MenuButton {...menu} onMouseEnter={handleMouseEnter}>
-          {label}
-        </MenuButton>
-      ) : (
-        <OverflowMenuButton
-          className={className}
-          aria-label={t("Show menu")}
-          onMouseEnter={handleMouseEnter}
-          {...menu}
-        />
-      )}
-      {!isEmpty(can) ? (
-        <ContextMenu
-          {...menu}
-          aria-label={t("Document options")}
-          onOpen={onOpen}
-          onClose={onClose}
-        >
-          <Template
-            {...menu}
-            items={[
-              {
-                type: "button",
-                title: t("Restore"),
-                visible:
-                  ((document.isWorkspaceTemplate || !!collection) &&
-                    can.restore) ||
-                  !!can.unarchive,
-                onClick: (ev) => handleRestore(ev),
-                icon: <RestoreIcon />,
-              },
-              {
-                type: "submenu",
-                title: t("Restore"),
-                visible:
-                  !document.isWorkspaceTemplate &&
-                  !collection &&
-                  !!can.restore &&
-                  restoreItems.length !== 0,
-                style: {
-                  left: -170,
-                  position: "relative",
-                  top: -40,
-                },
-                icon: <RestoreIcon />,
-                hover: true,
-                items: [
-                  {
-                    type: "heading",
-                    title: t("Choose a collection"),
-                  },
-                  ...restoreItems,
-                ],
-              },
-              actionToMenuItem(starDocument, context),
-              actionToMenuItem(unstarDocument, context),
-              actionToMenuItem(subscribeDocument, context),
-              actionToMenuItem(unsubscribeDocument, context),
-              ...(isMobile ? [actionToMenuItem(shareDocument, context)] : []),
-              {
-                type: "button",
-                title: `${t("Find and replace")}…`,
-                visible: !!onFindAndReplace && isMobile,
-                onClick: () => onFindAndReplace?.(),
-                icon: <SearchIcon />,
-              },
-              {
-                type: "separator",
-              },
-              {
-                type: "route",
-                title: t("Edit"),
-                to: documentEditPath(document),
-                visible:
-                  !!can.update && user.separateEditMode && !document.template,
-                icon: <EditIcon />,
-              },
-              {
-                type: "button",
-                title: `${t("Rename")}…`,
-                visible: !!can.update && !user.separateEditMode && !!onRename,
-                onClick: () => onRename?.(),
-                icon: <InputIcon />,
-              },
-              actionToMenuItem(createNestedDocument, context),
-              actionToMenuItem(importDocument, context),
-              actionToMenuItem(createTemplateFromDocument, context),
-              actionToMenuItem(duplicateDocument, context),
-              actionToMenuItem(publishDocument, context),
-              actionToMenuItem(unpublishDocument, context),
-              actionToMenuItem(archiveDocument, context),
-              actionToMenuItem(moveDocument, context),
-              actionToMenuItem(moveTemplate, context),
-              actionToMenuItem(pinDocument, context),
-              actionToMenuItem(createDocumentFromTemplate, context),
-              {
-                type: "separator",
-              },
-              actionToMenuItem(openDocumentComments, context),
-              actionToMenuItem(openDocumentHistory, context),
-              actionToMenuItem(openDocumentInsights, context),
-              actionToMenuItem(downloadDocument, context),
-              actionToMenuItem(copyDocument, context),
-              actionToMenuItem(printDocument, context),
-              actionToMenuItem(searchInDocument, context),
-              {
-                type: "separator",
-              },
-              actionToMenuItem(deleteDocument, context),
-              actionToMenuItem(permanentlyDeleteDocument, context),
-            ]}
-          />
-          {(showDisplayOptions || showToggleEmbeds) && can.update && (
-            <>
-              <Separator />
-              <DisplayOptions>
-                {showToggleEmbeds && (
-                  <Style>
-                    <ToggleMenuItem
-                      width={26}
-                      height={14}
-                      label={t("Enable embeds")}
-                      labelPosition="left"
-                      checked={!document.embedsDisabled}
-                      onChange={
-                        document.embedsDisabled
-                          ? document.enableEmbeds
-                          : document.disableEmbeds
-                      }
-                    />
-                  </Style>
-                )}
-                {showDisplayOptions && !isMobile && (
-                  <Style>
-                    <ToggleMenuItem
-                      width={26}
-                      height={14}
-                      label={t("Full width")}
-                      labelPosition="left"
-                      checked={document.fullWidth}
-                      onChange={(ev) => {
-                        const fullWidth = ev.currentTarget.checked;
-                        user.setPreference(
-                          UserPreference.FullWidthDocuments,
-                          fullWidth
-                        );
-                        void user.save();
-                        document.fullWidth = fullWidth;
-                        void document.save();
-                      }}
-                    />
-                  </Style>
-                )}
-              </DisplayOptions>
-            </>
-          )}
-        </ContextMenu>
-      ) : null}
+      <MenuTrigger
+        label={label}
+        document={document}
+        className={className}
+        menu={menu}
+      />
+      <MenuContent
+        document={document}
+        menu={menu}
+        onOpen={onOpen}
+        onClose={onClose}
+        collection={collection}
+        onRename={onRename}
+        showDisplayOptions={showDisplayOptions}
+        showToggleEmbeds={showToggleEmbeds}
+      />
     </>
   );
 }
