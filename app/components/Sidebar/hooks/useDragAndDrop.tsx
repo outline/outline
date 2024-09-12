21bce979393250e5a729d48fa14e018880505bfd
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-09-12 16:42:14 +0530

    fix: disallow dragging deleted or achived docs, also disallow dropping onto deleted or archived docs

--- app/components/Sidebar/hooks/useDragAndDrop.tsx
+++ app/components/Sidebar/hooks/useDragAndDrop.tsx
@@ -149,6 +149,7 @@ export function useDragDocument(
         icon: icon ? <Icon value={icon} color={color} /> : undefined,
         collectionId: document?.collectionId || "",
       } as DragObject),
+    canDrag: () => !!document?.isActive,
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
@@ -245,6 +246,7 @@ export function useDropToReparentDocument(
       !!pathToNode &&
       !pathToNode.includes(monitor.getItem().id) &&
       item.id !== node.id &&
+      !!document?.isActive &&
       policies.abilities(node.id).update &&
       policies.abilities(item.id).move,
     hover: (_item, monitor) => {
@@ -297,6 +299,8 @@ export function useDropToReorderDocument(
   const { t } = useTranslation();
   const { documents, collections, dialogs, policies } = useStores();
 
+  const document = documents.get(node.id);
+
   return useDrop<
     DragObject,
     Promise<void>,
@@ -304,7 +308,11 @@ export function useDropToReorderDocument(
   >({
     accept: "document",
     canDrop: (item: DragObject) => {
-      if (item.id === node.id || !policies.abilities(item.id)?.move) {
+      if (
+        item.id === node.id ||
+        !policies.abilities(item.id)?.move ||
+        !document?.isActive
+      ) {
         return false;
       }
 
