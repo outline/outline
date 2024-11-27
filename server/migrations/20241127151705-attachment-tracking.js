6ca3200bdb996e32f0387505d2f582b1e5672141
Tom Moor <tom.moor@gmail.com>
2024-11-27 17:44:16 -0500

    fix: Index on incorrect table

--- server/migrations/20241127151705-attachment-tracking.js
+++ server/migrations/20241127151705-attachment-tracking.js
@@ -8,13 +8,13 @@ module.exports = {
         type: Sequelize.BIGINT,
         defaultValue: 0,
       }, { transaction });
-      await queryInterface.addIndex("teams", ["createdAt"], { transaction });
+      await queryInterface.addIndex("attachments", ["createdAt"], { transaction });
     });
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.sequelize.transaction(async transaction => {
-      await queryInterface.removeIndex("teams", ["createdAt"], { transaction });
+      await queryInterface.removeIndex("attachments", ["createdAt"], { transaction });
       await queryInterface.removeColumn("teams", "approximateTotalAttachmentsSize", { transaction });
     });
   },
