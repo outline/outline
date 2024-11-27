5adcccd24825287e6f01cc424c89ff1a9d49ed47
infinite-persistence <inf.persistence@gmail.com>
2024-11-27 15:32:16 +0800

    Allow user to select doc-copy dest
    The change in `documentDuplicator` essentially alters the fallback from "parent" to "top of collection". But there is only 1 place that uses it so far, so I think it's fine to support this PR.
    
    In the next commit, the caller side will restore the default to "parent".

--- app/components/DuplicateDialog.tsx
+++ app/components/DuplicateDialog.tsx
@@ -1,10 +1,21 @@
+import flatten from "lodash/flatten";
 import { observer } from "mobx-react";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import breakpoint from "styled-components-breakpoint";
+import { NavigationNode } from "@shared/types";
 import { DocumentValidation } from "@shared/validations";
 import Document from "~/models/Document";
 import ConfirmationDialog from "~/components/ConfirmationDialog";
-import Input from "./Input";
+import DocumentExplorer, {
+  ListContainer,
+  ListSearch,
+} from "~/components/DocumentExplorer";
+import useCollectionTrees from "~/hooks/useCollectionTrees";
+import useStores from "~/hooks/useStores";
+import { flattenTree } from "~/utils/tree";
+import Input, { LabelText } from "./Input";
 import Switch from "./Switch";
 import Text from "./Text";
 
@@ -16,12 +27,32 @@ type Props = {
 
 function DuplicateDialog({ document, onSubmit }: Props) {
   const { t } = useTranslation();
+  const { policies } = useStores();
+  const collectionTrees = useCollectionTrees();
   const defaultTitle = t(`Copy of {{ documentName }}`, {
     documentName: document.title,
   });
   const [publish, setPublish] = React.useState<boolean>(!!document.publishedAt);
   const [recursive, setRecursive] = React.useState<boolean>(true);
   const [title, setTitle] = React.useState<string>(defaultTitle);
+  const [path, selectPath] = React.useState<NavigationNode | null>(null);
+
+  const items = React.useMemo(() => {
+    const nodes = flatten(collectionTrees.map(flattenTree)).filter((node) =>
+      node.collectionId
+        ? policies.get(node.collectionId)?.abilities.createDocument
+        : true
+    );
+
+    if (document.isTemplate) {
+      return nodes
+        .filter((node) => node.type === "collection")
+        .map((node) => ({ ...node, children: [] }));
+    }
+    return nodes;
+  }, [policies, collectionTrees, document.isTemplate]);
+
+  const noOp = () => {};
 
   const handlePublishChange = React.useCallback(
     (ev: React.ChangeEvent<HTMLInputElement>) => {
@@ -45,16 +76,30 @@ function DuplicateDialog({ document, onSubmit }: Props) {
   );
 
   const handleSubmit = async () => {
-    const result = await document.duplicate({
-      publish,
-      recursive,
-      title,
-    });
-    onSubmit(result);
+    if (path) {
+      const result = await document.duplicate({
+        publish,
+        recursive,
+        title,
+        collectionId: path.collectionId,
+        ...(path.type === "document" ? { parentDocumentId: path.id } : {}),
+      });
+      onSubmit(result);
+    }
   };
 
   return (
-    <ConfirmationDialog onSubmit={handleSubmit} submitText={t("Duplicate")}>
+    <ConfirmationDialog
+      onSubmit={handleSubmit}
+      submitText={t("Duplicate")}
+      disabled={!path}
+    >
+      <LabelText>{t("Destination")}</LabelText>
+      <StyledDocumentExplorer
+        items={items}
+        onSubmit={noOp}
+        onSelect={selectPath}
+      />
       <Input
         autoFocus
         autoSelect
@@ -93,5 +138,19 @@ function DuplicateDialog({ document, onSubmit }: Props) {
     </ConfirmationDialog>
   );
 }
+
+const StyledDocumentExplorer = styled(DocumentExplorer)`
+  ${ListSearch} {
+    padding: 0;
+  }
+
+  ${ListContainer} {
+    // Reduce height to make room for title input
+    height: 40vh;
+    ${breakpoint("tablet")`
+      height: 20vh;
+    `}
+  }
+`;
 
 export default observer(DuplicateDialog);
