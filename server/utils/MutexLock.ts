b2a7815b41f5d22ba946cbeaa20dfea93a1841e1
Tom Moor <tom.moor@gmail.com>
2024-11-27 22:42:10 -0500

    fix: The operation was unable to achieve a quorum during its retry window

--- server/utils/MutexLock.ts
+++ server/utils/MutexLock.ts
@@ -2,8 +2,8 @@ import Redlock from "redlock";
 import Redis from "@server/storage/redis";
 
 export class MutexLock {
-  // Default expiry time for qcuiring lock in milliseconds
-  public static defaultLockTimeout = 5000;
+  // Default expiry time for acquiring lock in milliseconds
+  public static defaultLockTimeout = 4000;
 
   /**
    * Returns the redlock instance
@@ -11,6 +11,8 @@ export class MutexLock {
   public static get lock(): Redlock {
     this.redlock ??= new Redlock([Redis.defaultClient], {
       retryJitter: 10,
+      retryCount: 20,
+      retryDelay: 200,
     });
 
     return this.redlock;
