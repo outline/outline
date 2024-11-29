36db19b87d2d1dc4f8a2fa931cff9d225ba2ac6f
hmacr <hmac.devo@gmail.com>
2024-11-29 15:13:26 +0530

    handle app load

--- app/scenes/Document/index.tsx
+++ app/scenes/Document/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { StaticContext } from "react-router";
+import { StaticContext, useHistory } from "react-router";
 import { RouteComponentProps } from "react-router-dom";
+import { SidebarContextType } from "~/components/Sidebar/components/SidebarContext";
 import { useLastVisitedPath } from "~/hooks/useLastVisitedPath";
 import useStores from "~/hooks/useStores";
 import DataLoader from "./components/DataLoader";
@@ -16,12 +17,14 @@ type LocationState = {
   title?: string;
   restore?: boolean;
   revisionId?: string;
+  sidebarContext?: SidebarContextType;
 };
 
 type Props = RouteComponentProps<Params, StaticContext, LocationState>;
 
 export default function DocumentScene(props: Props) {
   const { ui } = useStores();
+  const history = useHistory();
   const { documentSlug, revisionId } = props.match.params;
   const currentPath = props.location.pathname;
   const [, setLastVisitedPath] = useLastVisitedPath();
@@ -31,6 +34,16 @@ export default function DocumentScene(props: Props) {
   }, [currentPath, setLastVisitedPath]);
 
   React.useEffect(() => () => ui.clearActiveDocument(), [ui]);
+
+  React.useEffect(() => {
+    // When opening a document directly on app load, sidebarContext will not be set.
+    if (!props.location.state?.sidebarContext) {
+      history.replace({
+        ...props.location,
+        state: { ...props.location.state, sidebarContext: "collections" }, // optimistic preference of "collections"
+      });
+    }
+  }, [props.location, history]);
 
   // the urlId portion of the url does not include the slugified title
   // we only want to force a re-mount of the document component when the
