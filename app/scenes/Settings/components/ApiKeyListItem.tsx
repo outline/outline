4b833b3e2edd102537def2f0a16922e551e7b280
Tom Moor <tom@getoutline.com>
2024-12-02 21:10:36 -0500

    Add screen for API key management (#8049)
    * API keys
    
    * Add api key list for admins
    
    * permissions

--- app/scenes/Settings/components/ApiKeyListItem.tsx
+++ app/scenes/Settings/components/ApiKeyListItem.tsx
@@ -1,6 +1,8 @@
+import { observer } from "mobx-react";
 import { CopyIcon } from "outline-icons";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
+import { toast } from "sonner";
 import ApiKey from "~/models/ApiKey";
 import Button from "~/components/Button";
 import CopyToClipboard from "~/components/CopyToClipboard";
@@ -8,24 +10,29 @@ import Flex from "~/components/Flex";
 import ListItem from "~/components/List/Item";
 import Text from "~/components/Text";
 import Time from "~/components/Time";
+import useCurrentUser from "~/hooks/useCurrentUser";
 import useUserLocale from "~/hooks/useUserLocale";
 import ApiKeyMenu from "~/menus/ApiKeyMenu";
 import { dateToExpiry } from "~/utils/date";
 
 type Props = {
+  /** The API key to display */
   apiKey: ApiKey;
-  isCopied: boolean;
-  onCopy: (keyId: string) => void;
 };
 
-const ApiKeyListItem = ({ apiKey, isCopied, onCopy }: Props) => {
+const ApiKeyListItem = ({ apiKey }: Props) => {
   const { t } = useTranslation();
   const userLocale = useUserLocale();
+  const user = useCurrentUser();
 
   const subtitle = (
     <>
       <Text type="tertiary">
-        {t(`Created`)} <Time dateTime={apiKey.createdAt} addSuffix /> &middot;{" "}
+        {t(`Created`)} <Time dateTime={apiKey.createdAt} addSuffix />{" "}
+        {apiKey.userId === user.id
+          ? ""
+          : t(`by {{ name }}`, { name: user.name })}{" "}
+        &middot;{" "}
       </Text>
       {apiKey.lastActiveAt && (
         <Text type={"tertiary"}>
@@ -41,9 +48,19 @@ const ApiKeyListItem = ({ apiKey, isCopied, onCopy }: Props) => {
     </>
   );
 
+  const [copied, setCopied] = React.useState<boolean>(false);
+  const copyTimeoutIdRef = React.useRef<ReturnType<typeof setTimeout>>();
+
   const handleCopy = React.useCallback(() => {
-    onCopy(apiKey.id);
-  }, [apiKey.id, onCopy]);
+    if (copyTimeoutIdRef.current) {
+      clearTimeout(copyTimeoutIdRef.current);
+    }
+    setCopied(true);
+    copyTimeoutIdRef.current = setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+    toast.message(t("API key copied to clipboard"));
+  }, [t]);
 
   return (
     <ListItem
@@ -52,10 +69,10 @@ const ApiKeyListItem = ({ apiKey, isCopied, onCopy }: Props) => {
       subtitle={subtitle}
       actions={
         <Flex align="center" gap={8}>
-          {apiKey.value && (
+          {apiKey.value && handleCopy && (
             <CopyToClipboard text={apiKey.value} onCopy={handleCopy}>
               <Button type="button" icon={<CopyIcon />} neutral borderOnHover>
-                {isCopied ? t("Copied") : t("Copy")}
+                {copied ? t("Copied") : t("Copy")}
               </Button>
             </CopyToClipboard>
           )}
@@ -74,4 +91,4 @@ const ApiKeyListItem = ({ apiKey, isCopied, onCopy }: Props) => {
   );
 };
 
-export default ApiKeyListItem;
+export default observer(ApiKeyListItem);
