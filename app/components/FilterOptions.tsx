9c71566d66ac843eae62cc5c0d33825390f8a0b5
Tom Moor <tom.moor@gmail.com>
2025-01-05 08:53:51 -0500

    fix: Filter input divider in light theme

--- app/components/FilterOptions.tsx
+++ app/components/FilterOptions.tsx
@@ -229,7 +229,7 @@ const SearchInput = styled(Input)`
   ${Outline} {
     border: none;
     border-radius: 0;
-    border-bottom: 1px solid rgb(34 40 52);
+    border-bottom: 1px solid ${s("divider")};
     background: ${s("menuBackground")};
   }
 
