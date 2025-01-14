1aaabf113b8892dd6d50454240738d6919e059d7
Tom Moor <tom@getoutline.com>
2025-01-14 09:35:43 -0500

    fix: Incorrect plain text serialization in exportTable (#8234)

--- server/models/helpers/DocumentHelper.tsx
+++ server/models/helpers/DocumentHelper.tsx
@@ -3,6 +3,7 @@ import { Node } from "prosemirror-model";
 import { updateYFragment, yDocToProsemirrorJSON } from "y-prosemirror";
 import * as Y from "yjs";
 import textBetween from "@shared/editor/lib/textBetween";
+import { getTextSerializers } from "@shared/editor/lib/textSerializers";
 import { EditorStyleHelper } from "@shared/editor/styles/EditorStyleHelper";
 import { IconType, ProsemirrorData } from "@shared/types";
 import { determineIconType } from "@shared/utils/icon";
@@ -487,9 +488,5 @@ export class DocumentHelper {
     );
   }
 
-  private static textSerializers = Object.fromEntries(
-    Object.entries(schema.nodes)
-      .filter(([, n]) => n.spec.toPlainText)
-      .map(([name, n]) => [name, n.spec.toPlainText])
-  );
+  private static textSerializers = getTextSerializers(schema);
 }
