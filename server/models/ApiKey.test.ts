8b3115be9a28e44f1a532eea6869ea549eb7c168
Tom Moor <tom.moor@gmail.com>
2025-01-25 00:30:00 -0500

    test

--- server/models/ApiKey.test.ts
+++ server/models/ApiKey.test.ts
@@ -4,26 +4,26 @@ import ApiKey from "./ApiKey";
 
 describe("#ApiKey", () => {
   describe("match", () => {
-    test("should match an API secret", async () => {
+    it("should match an API secret", async () => {
       const apiKey = await buildApiKey();
       expect(ApiKey.match(apiKey.value!)).toBe(true);
       expect(ApiKey.match(`${randomstring.generate(38)}`)).toBe(true);
     });
 
-    test("should not match non secrets", async () => {
+    it("should not match non secrets", async () => {
       expect(ApiKey.match("123")).toBe(false);
       expect(ApiKey.match("1234567890")).toBe(false);
     });
   });
 
   describe("lastActiveAt", () => {
-    test("should update lastActiveAt", async () => {
+    it("should update lastActiveAt", async () => {
       const apiKey = await buildApiKey();
       await apiKey.updateActiveAt();
       expect(apiKey.lastActiveAt).toBeTruthy();
     });
 
-    test("should not update lastActiveAt within 5 minutes", async () => {
+    it("should not update lastActiveAt within 5 minutes", async () => {
       const apiKey = await buildApiKey();
       await apiKey.updateActiveAt();
       expect(apiKey.lastActiveAt).toBeTruthy();
@@ -35,13 +35,58 @@ describe("#ApiKey", () => {
   });
 
   describe("findByToken", () => {
-    test("should find by hash", async () => {
+    it("should find by hash", async () => {
       const apiKey = await buildApiKey({
         name: "Dev",
       });
       const found = await ApiKey.findByToken(apiKey.value!);
       expect(found?.id).toEqual(apiKey.id);
       expect(found?.last4).toEqual(apiKey.value!.slice(-4));
+    });
+  });
+
+  describe("canAccess", () => {
+    it("should return true for all resources if no scope", async () => {
+      const apiKey = await buildApiKey({
+        name: "Dev",
+      });
+
+      expect(apiKey.canAccess("documents.info")).toBe(true);
+      expect(apiKey.canAccess("collections.create")).toBe(true);
+      expect(apiKey.canAccess("apiKeys.list")).toBe(true);
+    });
+
+    it("should return false if no matching scope", async () => {
+      const apiKey = await buildApiKey({
+        name: "Dev",
+        scope: "documents.info",
+      });
+
+      expect(apiKey.canAccess("documents.info")).toBe(true);
+      expect(apiKey.canAccess("collections.create")).toBe(false);
+      expect(apiKey.canAccess("apiKeys.list")).toBe(false);
+    });
+
+    it("should allow wildcard methods", async () => {
+      const apiKey = await buildApiKey({
+        name: "Dev",
+        scope: "documents.*",
+      });
+
+      expect(apiKey.canAccess("documents.info")).toBe(true);
+      expect(apiKey.canAccess("documents.create")).toBe(true);
+      expect(apiKey.canAccess("collections.create")).toBe(false);
+    });
+
+    it("should allow wildcard namespaces", async () => {
+      const apiKey = await buildApiKey({
+        name: "Dev",
+        scope: "*.info",
+      });
+
+      expect(apiKey.canAccess("documents.info")).toBe(true);
+      expect(apiKey.canAccess("documents.create")).toBe(false);
+      expect(apiKey.canAccess("collections.create")).toBe(false);
     });
   });
 });
