96de47eb18391ffa3a38000dadd3430117db0ef5
Tom Moor <tom.moor@gmail.com>
2025-02-05 22:51:32 -0500

    Copy as Markdown

--- app/editor/extensions/ClipboardTextSerializer.ts
+++ app/editor/extensions/ClipboardTextSerializer.ts
@@ -1,7 +1,5 @@
 import { Plugin, PluginKey } from "prosemirror-state";
 import Extension from "@shared/editor/lib/Extension";
-import textBetween from "@shared/editor/lib/textBetween";
-import { getTextSerializers } from "@shared/editor/lib/textSerializers";
 
 /**
  * A plugin that allows overriding the default behavior of the editor to allow
@@ -14,19 +12,15 @@ export default class ClipboardTextSerializer extends Extension {
   }
 
   get plugins() {
-    const textSerializers = getTextSerializers(this.editor.schema);
-
     return [
       new Plugin({
         key: new PluginKey("clipboardTextSerializer"),
         props: {
-          clipboardTextSerializer: () => {
-            const { doc, selection } = this.editor.view.state;
-            const { ranges } = selection;
-            const from = Math.min(...ranges.map((range) => range.$from.pos));
-            const to = Math.max(...ranges.map((range) => range.$to.pos));
-
-            return textBetween(doc, from, to, textSerializers);
+          clipboardTextSerializer: (slice) => {
+            const serializer = this.editor.extensions.serializer();
+            return serializer.serialize(slice.content, {
+              softBreak: true,
+            });
           },
         },
       }),
