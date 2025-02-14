a1c502316aac65fff184d094a67bb2a3c407a727
Tom Moor <tom.moor@gmail.com>
2025-02-13 19:12:41 -0500

    fix: Support forward slash in mention search, closes #8406

--- app/editor/extensions/Suggestion.ts
+++ app/editor/extensions/Suggestion.ts
@@ -19,7 +19,9 @@ export default class Suggestion extends Extension {
     super(options);
 
     this.openRegex = new RegExp(
-      `(?:^|\\s|\\()${escapeRegExp(this.options.trigger)}(${`[\\p{L}\\p{M}\\d${
+      `(?:^|\\s|\\()${escapeRegExp(
+        this.options.trigger
+      )}(${`[\\p{L}\/\\p{M}\\d${
         this.options.allowSpaces ? "\\s{1}" : ""
       }\\.]+`})${this.options.requireSearchTerm ? "" : "?"}$`,
       "u"
