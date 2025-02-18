f4c3741000bf356e01412c071dbe34303064f80e
Tom Moor <tom.moor@gmail.com>
2025-02-17 23:29:39 -0500

    change index

--- server/migrations/20250217230810-add-team-previous-subdomains.js
+++ server/migrations/20250217230810-add-team-previous-subdomains.js
@@ -8,13 +8,19 @@ module.exports = {
         type: Sequelize.ARRAY(Sequelize.STRING),
         allowNull: true,
       }, { transaction });
-      await queryInterface.addIndex("teams", ["previousSubdomains"], { transaction });
+      await queryInterface.sequelize.query(
+        `CREATE INDEX teams_previous_subdomains ON teams USING GIN ("previousSubdomains");`,
+        { transaction }
+      );
     });
   },
 
   async down(queryInterface) {
     await queryInterface.sequelize.transaction(async transaction => {
-      await queryInterface.removeIndex("teams", ["previousSubdomains"], { transaction });
+      await queryInterface.sequelize.query(
+        `DROP INDEX teams_previous_subdomains;`,
+        { transaction }
+      );
       await queryInterface.removeColumn("teams", "previousSubdomains", { transaction });
     });
   },
