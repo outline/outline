42dfe7027ff26a553319b5d488e819f7d95c0b44
Tom Moor <tom@getoutline.com>
2025-02-22 10:54:15 -0500

    chore: CircleCI -> GitHub (#8534)

--- .circleci/run-build-locally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=<ENTER COMMIT SHA HERE>\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/outline/outline/tree/master
