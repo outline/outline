59abc3355ce65dff8b6f57b7681b0c0d80ccb247
YouLL <xyoullx01@gmail.com>
2025-02-23 02:07:02 +0100

    feat: change notice type (#8533)
    * feat: change notice type
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Moor <tom.moor@gmail.com>
    
    * refactor: change enum naming
    
    * fix: notice creation
    
    * fix: menu name
    
    * refactor: put notice type in the menu label
    
    ---------
    
    Co-authored-by: Tom Moor <tom.moor@gmail.com>

---
+++ app/editor/menus/notice.tsx
@@ -0,0 +1,64 @@
+import { capitalize } from "lodash";
+import {
+  DoneIcon,
+  ExpandedIcon,
+  InfoIcon,
+  StarredIcon,
+  WarningIcon,
+} from "outline-icons";
+import { EditorState } from "prosemirror-state";
+import * as React from "react";
+import { NoticeTypes } from "@shared/editor/nodes/Notice";
+import { MenuItem } from "@shared/editor/types";
+import { Dictionary } from "~/hooks/useDictionary";
+
+export default function noticeMenuItems(
+  state: EditorState,
+  readOnly: boolean | undefined,
+  dictionary: Dictionary
+): MenuItem[] {
+  const { selection } = state;
+  const { $from } = selection;
+  const node = $from.node(-1);
+
+  const currentStyle = node.attrs.style;
+
+  return [
+    {
+      name: "container_notice",
+      visible: !readOnly,
+      label: capitalize(currentStyle),
+      icon: <ExpandedIcon />,
+      children: [
+        {
+          name: NoticeTypes.Info,
+          icon: <InfoIcon />,
+          label: dictionary.infoNotice,
+          visible: true,
+          active: () => currentStyle === NoticeTypes.Info,
+        },
+        {
+          name: NoticeTypes.Success,
+          icon: <DoneIcon />,
+          label: dictionary.successNotice,
+          visible: true,
+          active: () => currentStyle === NoticeTypes.Success,
+        },
+        {
+          name: NoticeTypes.Warning,
+          icon: <WarningIcon />,
+          label: dictionary.warningNotice,
+          visible: true,
+          active: () => currentStyle === NoticeTypes.Warning,
+        },
+        {
+          name: NoticeTypes.Tip,
+          icon: <StarredIcon />,
+          label: dictionary.tipNotice,
+          visible: true,
+          active: () => currentStyle === NoticeTypes.Tip,
+        },
+      ],
+    },
+  ];
+}
