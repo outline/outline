402506e441f6e32b09a045b268014190a9dc07d8
hmacr <hmac.devo@gmail.com>
2025-02-24 14:59:31 +0530

    fix: Horizontal scroll in workspace details page

--- app/scenes/Settings/components/ActionRow.tsx
+++ app/scenes/Settings/components/ActionRow.tsx
@@ -1,5 +1,6 @@
 import { transparentize } from "polished";
 import styled from "styled-components";
+import breakpoint from "styled-components-breakpoint";
 import { s } from "@shared/styles";
 
 /**
@@ -8,8 +9,9 @@ import { s } from "@shared/styles";
 export const ActionRow = styled.div`
   position: sticky;
   bottom: 0;
-  padding: 16px 50vw;
-  margin: 0 -50vw;
+  width: 100vw;
+  padding: 16px 12px;
+  margin-left: -12px;
 
   background: ${s("background")};
 
@@ -17,4 +19,8 @@ export const ActionRow = styled.div`
     backdrop-filter: blur(20px);
     background: ${(props) => transparentize(0.2, props.theme.background)};
   }
+
+  ${breakpoint("tablet")`
+    width: auto;
+  `}
 `;
