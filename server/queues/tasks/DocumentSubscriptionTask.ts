2aad3b618bf674c601ee30ee74c5450630b2fd41
hmacr <hmac.devo@gmail.com>
2025-03-05 05:57:53 +0530

    fix: Skip auto creating subscriptions when user/group is added to a document

--- server/queues/tasks/DocumentSubscriptionTask.ts
+++ server/queues/tasks/DocumentSubscriptionTask.ts
@@ -1,6 +1,5 @@
 import { Transaction } from "sequelize";
 import { SubscriptionType } from "@shared/types";
-import subscriptionCreator from "@server/commands/subscriptionCreator";
 import { createContext } from "@server/context";
 import { Subscription, User } from "@server/models";
 import { sequelize } from "@server/storage/database";
@@ -10,44 +9,17 @@ import BaseTask from "./BaseTask";
 export default class DocumentSubscriptionTask extends BaseTask<DocumentUserEvent> {
   public async perform(event: DocumentUserEvent) {
     const user = await User.findByPk(event.userId);
-    if (!user) {
+
+    if (!user || event.name !== "documents.remove_user") {
       return;
     }
 
-    switch (event.name) {
-      case "documents.add_user":
-        return this.addUser(event, user);
-
-      case "documents.remove_user":
-        return this.removeUser(event, user);
-
-      default:
-    }
-  }
-
-  private async addUser(event: DocumentUserEvent, user: User) {
-    await sequelize.transaction(async (transaction) => {
-      await subscriptionCreator({
-        ctx: createContext({
-          user,
-          authType: event.authType,
-          ip: event.ip,
-          transaction,
-        }),
-        documentId: event.documentId,
-        event: SubscriptionType.Document,
-        resubscribe: false,
-      });
-    });
-  }
-
-  private async removeUser(event: DocumentUserEvent, user: User) {
     await sequelize.transaction(async (transaction) => {
       const subscription = await Subscription.findOne({
         where: {
           userId: user.id,
           documentId: event.documentId,
-          event: "documents.update",
+          event: SubscriptionType.Document,
         },
         transaction,
         lock: Transaction.LOCK.UPDATE,
