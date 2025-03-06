e447b9f53d1606d5c196b4cab6dafc415aa5bbd1
Tom Moor <tom.moor@gmail.com>
2025-03-05 21:58:41 -0500

    fix: Add more tldraw url support

--- shared/editor/embeds/index.tsx
+++ shared/editor/embeds/index.tsx
@@ -589,7 +589,7 @@ const embeds: EmbedDescriptor[] = [
     title: "Tldraw",
     keywords: "draw schematics diagrams",
     regexMatch: [
-      new RegExp("^https?://(beta|www|old)\\.tldraw\\.com/[rsvo]+/(.*)"),
+      new RegExp("^https?://(beta|www|old)\\.tldraw\\.com/[rsvopf]+/(.*)"),
     ],
     transformMatch: (matches: RegExpMatchArray) => matches[0],
     icon: <Img src="/images/tldraw.png" alt="Tldraw" />,
