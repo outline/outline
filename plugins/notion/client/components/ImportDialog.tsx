5244dcd901a290a372695c654eb35e2947df00d5
hmacr <hmac.devo@gmail.com>
2025-03-06 23:42:46 +0530

    basic table UI

--- plugins/notion/client/components/ImportDialog.tsx
+++ plugins/notion/client/components/ImportDialog.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { toast } from "sonner";
+import styled from "styled-components";
+import { s } from "@shared/styles";
+import { CollectionPermission } from "@shared/types";
+import Button from "~/components/Button";
+import { Emoji } from "~/components/Emoji";
+import Flex from "~/components/Flex";
+import InputSelectPermission from "~/components/InputSelectPermission";
+import Text from "~/components/Text";
 import useQuery from "~/hooks/useQuery";
 import useRequest from "~/hooks/useRequest";
+import { EmptySelectValue } from "~/types";
 import { client } from "~/utils/ApiClient";
 import { Page } from "plugins/notion/shared/types";
 
+type PageWithPermission = Page & {
+  permission?: CollectionPermission;
+};
+
 export function ImportDialog() {
+  const { t } = useTranslation();
   const queryParams = useQuery();
-  const integrationId = queryParams.get("integrationId"); // integration id will be available here since dialog renders only in success case.
+  const integrationId = queryParams.get("integrationId"); // integration id will be available here since dialog renders only in success flow.
+  const [pagesWithPermission, setPagesWithPermission] =
+    React.useState<PageWithPermission[]>();
 
   const loadRootPages = React.useCallback(async () => {
     const res = await client.post("/notion.search", {
@@ -22,20 +39,105 @@ export function ImportDialog() {
     loading,
   } = useRequest<Page[]>(loadRootPages, true);
 
-  if (loading) {
-    return <div>Loading data</div>;
-  }
+  const handlePermissionChange = React.useCallback(
+    (id: string, permission?: CollectionPermission) => {
+      setPagesWithPermission((prev) =>
+        prev?.map((page) => {
+          if (page.id === id) {
+            page.permission = permission;
+          }
+          return page;
+        })
+      );
+    },
+    []
+  );
+
+  const handleStartImport = React.useCallback(() => {
+    console.log("start import");
+  }, []);
+
+  React.useEffect(() => {
+    if (pages?.length) {
+      setPagesWithPermission(pages);
+    } else {
+      setPagesWithPermission([]);
+    }
+  }, [pages]);
 
   if (error) {
     toast.error("Error while fetching page info from Notion");
     return <div>Error: {error}</div>;
   }
 
+  if (loading || !pagesWithPermission) {
+    return <div>Loading data</div>;
+  }
+
   return (
-    <div>
-      {pages?.map((page) => (
-        <div>{page.name}</div>
-      ))}
-    </div>
+    <Flex column gap={8}>
+      <div>
+        {pagesWithPermission?.map((page) => (
+          <PageItem
+            key={page.id}
+            page={page}
+            onPermissionChange={handlePermissionChange}
+          />
+        ))}
+      </div>
+      <Flex justify="flex-end">
+        <Button onClick={handleStartImport}>{t("Start import")}</Button>
+      </Flex>
+    </Flex>
   );
 }
+
+function PageItem({
+  page,
+  onPermissionChange,
+}: {
+  page: PageWithPermission;
+  onPermissionChange: (id: string, permission?: CollectionPermission) => void;
+}) {
+  const handlePermissionChange = React.useCallback(
+    (value: CollectionPermission | typeof EmptySelectValue) =>
+      onPermissionChange(
+        page.id,
+        value === EmptySelectValue ? undefined : value
+      ),
+    [onPermissionChange]
+  );
+
+  return (
+    <Row align="center" justify="space-between" gap={4}>
+      <Column $width="75%" $border>
+        <Flex gap={6} align="center">
+          {page.emoji && <Emoji>{page.emoji}</Emoji>}
+          <Text size="small">{page.name}</Text>
+        </Flex>
+      </Column>
+      <Column>
+        <InputSelectPermission
+          onChange={handlePermissionChange}
+          value={page.permission}
+          labelHidden
+          nude
+          style={{ margin: 0 }}
+        />
+      </Column>
+    </Row>
+  );
+}
+
+const Row = styled(Flex)`
+  border: 1px solid ${s("divider")};
+  border-collapse: collapse;
+  margin-bottom: auto;
+`;
+
+const Column = styled.div<{ $width?: string; $border?: boolean }>`
+  width: ${({ $width }) => $width || "auto"};
+  padding: 4px 6px;
+  border-right: 1px solid
+    ${({ $border }) => ($border ? s("divider") : "transparent")};
+`;
