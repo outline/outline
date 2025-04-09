62ed9baea34e797ff881a2d3e79e6342e2eb2f04
hunmac9 <hunmac9@gmail.com>
2025-04-09 03:55:05 +0000

    did not work, reverting.

--- app/editor/menus/pdf.ts
+++ app/editor/menus/pdf.ts
@@ -1,23 +1,21 @@
-import { DownloadIcon, ReplaceIcon, TrashIcon } from "outline-icons";
 import { EditorState } from "prosemirror-state";
-import * as React from "react";
 
 export default function getPdfMenuItems(state: EditorState, dictionary: any) {
   return [
     {
       name: "downloadPdfAttachment",
       title: dictionary.download,
-      icon: <DownloadIcon />,
+      icon: "DownloadIcon",
     },
     {
       name: "replacePdfAttachment",
       title: dictionary.replace,
-      icon: <ReplaceIcon />,
+      icon: "ReplaceIcon",
     },
     {
       name: "deletePdfAttachment",
       title: dictionary.delete,
-      icon: <TrashIcon />,
+      icon: "TrashIcon",
       dangerous: true,
     },
   ];
