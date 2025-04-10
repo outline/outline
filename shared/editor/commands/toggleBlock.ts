2c3ec0289db42fa24c09a134f5dd7d5d496b5dd0
Apoorv Mishra <apoorvmishra101092@gmail.com>
2025-04-10 15:43:59 +0530

    feat: `Tab` into a toggle block, `Shift-Tab` out of toggle block
    As part of this, we've modified handling `Enter` within a toggle block
    in a way that prevents it to trigger `liftEmptyBlock`, so that the
    cursor remains within the toggle block body and is only taken out of it
    when pressing `Shift-Tab` combo.

--- shared/editor/commands/toggleBlock.ts
+++ shared/editor/commands/toggleBlock.ts
@@ -1,13 +1,21 @@
+import filter from "lodash/filter";
 import isNull from "lodash/isNull";
+import isUndefined from "lodash/isUndefined";
 import some from "lodash/some";
-import { Node, ResolvedPos, Slice } from "prosemirror-model";
+import {
+  Node,
+  NodeType,
+  ResolvedPos,
+  Slice,
+  Fragment,
+} from "prosemirror-model";
 import {
   Command,
   EditorState,
   TextSelection,
   Transaction,
 } from "prosemirror-state";
-import { liftTarget } from "prosemirror-transform";
+import { liftTarget, ReplaceAroundStep } from "prosemirror-transform";
 import ToggleBlock from "../nodes/ToggleBlock";
 
 export const lift: Command = (state, dispatch) => {
@@ -26,8 +34,10 @@ export const lift: Command = (state, dispatch) => {
   return true;
 };
 
+const isToggleBlock = (node: Node) => node.type.name === "toggle_block";
+
 const withinToggleBlock = ($cursor: ResolvedPos | null) =>
-  $cursor && $cursor.node($cursor.depth - 1).type.name === "toggle_block";
+  $cursor && some(ancestors($cursor), isToggleBlock);
 
 const withinToggleBlockHead = ($cursor: ResolvedPos | null) =>
   withinToggleBlock($cursor) && $cursor!.index($cursor!.depth - 1) === 0;
@@ -154,3 +164,117 @@ export const liftAfter: Command = (state, dispatch) => {
 
   return true;
 };
+
+const prevSibling = ($from: ResolvedPos, depth?: number) => {
+  const ancestor = $from.node(depth);
+  const index = $from.index(depth);
+  if (index === 0) {
+    return null;
+  }
+  return ancestor.child(index - 1);
+};
+
+export const sinkBlockInto =
+  (type: NodeType): Command =>
+  (state, dispatch) => {
+    const { $from } = state.selection;
+
+    let before = -1;
+    for (let depth = $from.depth; depth >= 0; depth--) {
+      const nodeBefore = prevSibling($from, depth);
+      if (nodeBefore && nodeBefore.type === type) {
+        // before of nodeBefore
+        before = $from.posAtIndex($from.index(depth) - 1, depth);
+        break;
+      }
+    }
+
+    if (before === -1) {
+      return false;
+    }
+
+    const slice = new Slice(Fragment.from(type.create()), 1, 0);
+
+    const from = before + state.doc.nodeAt(before)!.nodeSize;
+    const to = from + state.doc.nodeAt(from)!.nodeSize;
+    const step = new ReplaceAroundStep(from - 1, to, from, to, slice, 0, true);
+
+    const tr = state.tr.step(step).scrollIntoView();
+    if (dispatch) {
+      dispatch(tr);
+    }
+
+    return true;
+  };
+
+const ancestors = (
+  $from: ResolvedPos,
+  pred?: ($cursor: ResolvedPos, ancestor: Node, depth: number) => boolean
+) => {
+  const anc = [];
+
+  // Notice that ancestors are arranged in increasing order of depth
+  // within the array, which implies that the index of an ancestor
+  // within the array actually represents its depth within the document.
+  for (let depth = 0; depth <= $from.depth; depth++) {
+    anc.push($from!.node(depth));
+  }
+
+  if (pred) {
+    return filter(anc, (ancestor, index) =>
+      // `index` represents the depth of the ancestor within the document,
+      // so we simply pass it as `depth` to the predicate function.
+      pred($from, ancestor, index)
+    );
+  }
+
+  return anc;
+};
+
+const suchThat = (
+  pred: (...args: any[]) => boolean
+): ((...args: any[]) => boolean) => pred;
+
+const nearest = (ancestors: Node[]) =>
+  // Since the ancestors are arranged in increasing order of depth,
+  // the last element of the array is the nearest ancestor.
+  ancestors.pop();
+
+export const liftLastBlockOutOf =
+  (type: NodeType): Command =>
+  (state, dispatch) => {
+    const { $from, $to } = state.selection as TextSelection;
+
+    const ancestor = nearest(
+      ancestors(
+        $from,
+        suchThat(
+          ($fr, anc, depth) =>
+            anc.type === type &&
+            anc.childCount > 0 &&
+            $fr.index(depth) === anc.childCount - 1
+        )
+      )
+    );
+
+    if (isUndefined(ancestor)) {
+      return false;
+    }
+
+    const range = $from.blockRange($to, (node) => node.eq(ancestor));
+    if (isNull(range)) {
+      return false;
+    }
+
+    const target = liftTarget(range);
+    if (isNull(target)) {
+      return false;
+    }
+
+    const tr = state.tr;
+    tr.lift(range, target);
+    if (dispatch) {
+      dispatch(tr);
+    }
+    return true;
+  };
