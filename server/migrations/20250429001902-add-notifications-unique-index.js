9ac39a6c522eb5c09824db8bdcd28f2752afd158
Tom Moor <tom@getoutline.com>
2025-04-28 20:23:40 -0400

    Add migration

---
+++ server/migrations/20250429001902-add-notifications-unique-index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addIndex("notifications", ["userId", "commentId"], {
+      unique: true,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeIndex("notifications", ["userId", "commentId"]);
+  }
+};
