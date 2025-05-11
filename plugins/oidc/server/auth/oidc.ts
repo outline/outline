f28ec8e0fb105f5cf745725ec004c568c0824d1f
Adam Roe <adam.roe@me.com>
2025-05-11 12:39:13 +1000

    Fall back to id_token if profile does not contain username or email

--- plugins/oidc/server/auth/oidc.ts
+++ plugins/oidc/server/auth/oidc.ts
@@ -1,4 +1,5 @@
 import passport from "@outlinewiki/koa-passport";
+import JWT from "jsonwebtoken";
 import type { Context } from "koa";
 import Router from "koa-router";
 import get from "lodash/get";
@@ -58,7 +59,7 @@ if (
         ctx: Context,
         accessToken: string,
         refreshToken: string,
-        params: { expires_in: number },
+        params: { expires_in: number; id_token: string },
         _profile: unknown,
         done: (
           err: Error | null,
@@ -78,14 +79,25 @@ if (
             accessToken
           );
 
-          if (!profile.email) {
+          // Some providers, namely ADFS, don't provide anything more than the `sub` claim in the userinfo endpoint
+          // So, we'll decode the params.id_token and see if that contains what we need.
+          const id_token = JWT.decode(params.id_token);
+          const token = id_token as {
+            email?: string;
+            preferred_username?: string;
+            sub?: string;
+          };
+
+          const email = profile.email ?? token.email ?? null;
+
+          if (!email) {
             throw AuthenticationError(
-              `An email field was not returned in the profile parameter, but is required.`
+              `An email field was not returned in the profile or id_token parameter, but is required.`
             );
           }
           const team = await getTeamFromContext(ctx);
           const client = getClientFromContext(ctx);
-          const { domain } = parseEmail(profile.email);
+          const { domain } = parseEmail(email);
 
           // Only a single OIDC provider is supported – find the existing, if any.
           const authenticationProvider = team
@@ -118,13 +130,15 @@ if (
 
           // Claim name can be overriden using an env variable.
           // Default is 'preferred_username' as per OIDC spec.
-          const username = get(profile, env.OIDC_USERNAME_CLAIM);
+          const username = get(profile, env.OIDC_USERNAME_CLAIM)
+            ? get(profile, env.OIDC_USERNAME_CLAIM)
+            : get(token, env.OIDC_USERNAME_CLAIM);
           const name = profile.name || username || profile.username;
           const profileId = profile.sub ? profile.sub : profile.id;
 
           if (!name) {
             throw AuthenticationError(
-              `Neither a name or username was returned in the profile parameter, but at least one is required.`
+              `Neither a ${env.OIDC_USERNAME_CLAIM}, name or username was returned in the profile parameter, but at least one is required.`
             );
           }
 
@@ -138,7 +152,7 @@ if (
             },
             user: {
               name,
-              email: profile.email,
+              email,
               avatarUrl: profile.picture,
             },
             authenticationProvider: {
