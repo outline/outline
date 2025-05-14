93bedd6863f17ab90474a1dd90982fe795576177
Tom Moor <tom@getoutline.com>
2025-05-13 22:47:48 -0400

    fix: Add encoding meta tag to exported HTML (#9207)
    closes #9194

--- server/models/helpers/ProsemirrorHelper.tsx
+++ server/models/helpers/ProsemirrorHelper.tsx
@@ -490,7 +490,7 @@ export class ProsemirrorHelper {
     // Render the Prosemirror document using virtual DOM and serialize the
     // result to a string
     const dom = new JSDOM(
-      `<!DOCTYPE html>${
+      `<!DOCTYPE html><meta charset="utf-8">${
         options?.includeStyles === false ? "" : styleTags
       }${html}`
     );
