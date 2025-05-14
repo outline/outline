a964c3042789ad3d7bc8bf0b4d914a5b1ac3b57b
Adam Roe <adam.roe@me.com>
2025-05-14 11:06:14 +1000

    fix: fall back to id_token when OIDC userinfo endpoint is sparse (#9172)
    * Fall back to id_token if profile does not contain username or email
    
    * More comments
    
    * Add error handling to id_token decode
    
    * simplify username fallback logic using nullish coalescing
    
    Co-authored-by: Tom Moor <tom.moor@gmail.com>
    
    * make id_token decoding more tolerant of malformed or invalid tokens
    
    ---------
    
    Co-authored-by: Tom Moor <tom.moor@gmail.com>

--- plugins/oidc/server/auth/oidc.ts
+++ plugins/oidc/server/auth/oidc.ts
@@ -1,4 +1,5 @@
 import passport from "@outlinewiki/koa-passport";
+import JWT from "jsonwebtoken";
 import type { Context } from "koa";
 import Router from "koa-router";
 import get from "lodash/get";
@@ -9,6 +10,7 @@ import {
   OIDCMalformedUserInfoError,
   AuthenticationError,
 } from "@server/errors";
+import Logger from "@server/logging/Logger";
 import passportMiddleware from "@server/middlewares/passport";
 import { AuthenticationProvider, User } from "@server/models";
 import { AuthenticationResult } from "@server/types";
@@ -58,7 +60,7 @@ if (
         ctx: Context,
         accessToken: string,
         refreshToken: string,
-        params: { expires_in: number },
+        params: { expires_in: number; id_token: string },
         _profile: unknown,
         done: (
           err: Error | null,
@@ -78,14 +80,39 @@ if (
             accessToken
           );
 
-          if (!profile.email) {
+          // Some providers, namely ADFS, don't provide anything more than the `sub` claim in the userinfo endpoint
+          // So, we'll decode the params.id_token and see if that contains what we need.
+          const token = (() => {
+            try {
+              const decoded = JWT.decode(params.id_token);
+
+              if (!decoded || typeof decoded !== "object") {
+                Logger.warn("Decoded id_token is not a valid object");
+                return {};
+              }
+
+              return decoded as {
+                email?: string;
+                preferred_username?: string;
+                sub?: string;
+              };
+            } catch (err) {
+              Logger.error("id_token decode threw error: ", err);
+              return {};
+            }
+          })();
+
+          const email = profile.email ?? token.email ?? null;
+
+          if (!email) {
             throw AuthenticationError(
-              `An email field was not returned in the profile parameter, but is required.`
+              `An email field was not returned in the profile or id_token parameter, but is required.`
             );
           }
+
           const team = await getTeamFromContext(ctx);
           const client = getClientFromContext(ctx);
-          const { domain } = parseEmail(profile.email);
+          const { domain } = parseEmail(email);
 
           // Only a single OIDC provider is supported – find the existing, if any.
           const authenticationProvider = team
@@ -118,13 +145,16 @@ if (
 
           // Claim name can be overriden using an env variable.
           // Default is 'preferred_username' as per OIDC spec.
-          const username = get(profile, env.OIDC_USERNAME_CLAIM);
+          // This will default to the profile.preferred_username, but will fall back to preferred_username from the id_token
+          const username =
+            get(profile, env.OIDC_USERNAME_CLAIM) ??
+            get(token, env.OIDC_USERNAME_CLAIM);
           const name = profile.name || username || profile.username;
           const profileId = profile.sub ? profile.sub : profile.id;
 
           if (!name) {
             throw AuthenticationError(
-              `Neither a name or username was returned in the profile parameter, but at least one is required.`
+              `Neither a ${env.OIDC_USERNAME_CLAIM}, name or username was returned in the profile parameter, but at least one is required.`
             );
           }
 
@@ -138,7 +168,7 @@ if (
             },
             user: {
               name,
-              email: profile.email,
+              email,
               avatarUrl: profile.picture,
             },
             authenticationProvider: {
