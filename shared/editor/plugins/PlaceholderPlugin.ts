0381cebec5a69dcb95d3a3bf98fd2e145877aa5a
Tom Moor <tom@getoutline.com>
2025-05-22 23:22:25 -0400

    perf: Utilize plugin state in placeholder plugin

--- shared/editor/plugins/PlaceholderPlugin.ts
+++ shared/editor/plugins/PlaceholderPlugin.ts
@@ -25,24 +25,29 @@ export class PlaceholderPlugin extends Plugin {
 
   constructor(config: Config) {
     super({
+      state: {
+        init: (_, state: EditorState) => ({
+          decorations: this.createDecorations(state, config),
+        }),
+        apply: (tr, pluginState, oldState, newState) => {
+          // Only recompute if doc or selection changed
+          if (tr.docChanged || tr.selectionSet) {
+            return { decorations: this.createDecorations(newState, config) };
+          }
+          return pluginState;
+        },
+      },
       props: {
         decorations: (state) => {
-          const decorations: Decoration[] = map(
-            this.placeholders(state),
-            (placeholder) =>
-              Decoration.node(placeholder.from, placeholder.to, {
-                class: "placeholder",
-                "data-empty-text": placeholder.text,
-              })
-          );
-          return DecorationSet.create(state.doc, decorations);
+          const pluginState = this.getState(state);
+          return pluginState ? pluginState.decorations : null;
         },
       },
     });
     this.config = config;
   }
 
-  private placeholders(state: EditorState) {
+  private createDecorations(state: EditorState, config: Config) {
     const paras: Array<{
       node: Node;
       $start: ResolvedPos;
@@ -53,21 +58,24 @@ export class PlaceholderPlugin extends Plugin {
         paras.push({ node, $start: state.doc.resolve(pos + 1), parent });
       }
     });
-    return filter(
+    const decorations: Decoration[] = filter(
       map(paras, (para) => {
-        const condMet = find(this.config, (conf) =>
+        const condMet = find(config, (conf) =>
           conf.condition(para.node, para.$start, para.parent, state)
         );
-
         return condMet
-          ? {
-              from: para.$start.pos - 1,
-              to: para.$start.pos - 1 + para.node.nodeSize,
-              text: condMet.text,
-            }
+          ? Decoration.node(
+              para.$start.pos - 1,
+              para.$start.pos - 1 + para.node.nodeSize,
+              {
+                class: "placeholder",
+                "data-empty-text": condMet.text,
+              }
+            )
           : undefined;
       }),
-      (placeholder) => placeholder !== undefined
+      (decoration) => decoration !== undefined
     );
+    return DecorationSet.create(state.doc, decorations);
   }
 }
