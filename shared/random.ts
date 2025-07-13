7445bfef40f559271934aa021fe8dea5fdda22c4
Tom Moor <tom@getoutline.com>
2025-07-13 09:13:52 -0400

    fix: Last usage of randomstring, docs

--- shared/random.ts
+++ shared/random.ts
@@ -1,13 +1,54 @@
+/**
+ * Generate a random integer within a given range.
+ *
+ * @param min - The minimum value of the range.
+ * @param max - The maximum value of the range.
+ * @returns A random integer within the range.
+ */
 export const randomInteger = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
+/**
+ * Choose a random element from an array.
+ *
+ * @param arr - The array to choose from.
+ * @returns A random element from the array.
+ */
 export const randomElement = <T>(arr: T[]): T =>
   arr[randomInteger(0, arr.length - 1)];
 
-export const randomString = (length: number) => {
+/**
+ * Generate a random string of a given length and charset.
+ *
+ * @param options - The length of the string or an object with length and charset properties.
+ * @returns A random string.
+ */
+export const randomString = (
+  options:
+    | number
+    | {
+        length: number;
+        charset: "lowercase" | "uppercase" | "numeric" | "alphanumeric";
+      }
+) => {
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numeric = "0123456789";
+  const alphanumeric = lowercase + uppercase + numeric;
+
   const chars =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const array = new Uint8Array(length);
+    typeof options === "number"
+      ? alphanumeric
+      : options.charset === "lowercase"
+        ? lowercase
+        : options.charset === "uppercase"
+          ? uppercase
+          : options.charset === "numeric"
+            ? numeric
+            : alphanumeric;
+  const array = new Uint8Array(
+    typeof options === "number" ? options : options.length
+  );
   crypto.getRandomValues(array);
   return Array.from(array, (x) => chars[x % chars.length]).join("");
 };
