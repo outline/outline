94a9a696cde018998f580de55b5c4637ad3cb347
Tom Moor <tom@getoutline.com>
2025-07-16 21:29:31 -0400

    Revert "chore: Keep classnames, Sentry depends on consistency (#9649)" (#9658)
    This reverts commit cd83f412946419fc47e782b4d38929ca7b6c9ff4.

--- vite.config.ts
+++ vite.config.ts
@@ -169,7 +169,6 @@ export default () =>
       target: browserslistToEsbuild(),
       reportCompressedSize: false,
       rollupOptions: {
-        keepNames: true,
         onwarn(warning, warn) {
           // Suppress noisy warnings about module-level directives, e.g. "use client"
           if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
