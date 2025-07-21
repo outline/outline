547e467cc01d000491a8b64530de8a9ab8ce4308
Cameron Clark <cameron.clark@hey.com>
2025-07-21 10:35:13 +0100

    chore(linter): improve oxlint setup

--- server/routes/api/authenticationProviders/authenticationProviders.ts
+++ server/routes/api/authenticationProviders/authenticationProviders.ts
@@ -3,7 +3,7 @@ import { UserRole } from "@shared/types";
 import auth from "@server/middlewares/authentication";
 import { transaction } from "@server/middlewares/transaction";
 import validate from "@server/middlewares/validate";
-import { AuthenticationProvider, Event } from "@server/models";
+import { AuthenticationProvider } from "@server/models";
 import AuthenticationHelper from "@server/models/helpers/AuthenticationHelper";
 import { authorize } from "@server/policies";
 import {
