18fe554985da74d1054a168536ebf3626790d514
Tom Moor <tom@getoutline.com>
2025-07-23 09:04:39 -0400

    chore: Fix Redis mock not used consistently in tests

--- server/queues/index.ts
+++ server/queues/index.ts
@@ -1,29 +1,60 @@
 import { createQueue } from "@server/queues/queue";
 
-export const globalEventQueue = createQueue("globalEvents", {
-  attempts: 5,
-  backoff: {
-    type: "exponential",
-    delay: 1000,
+let _globalEventQueue: ReturnType<typeof createQueue> | undefined;
+let _processorEventQueue: ReturnType<typeof createQueue> | undefined;
+let _websocketQueue: ReturnType<typeof createQueue> | undefined;
+let _taskQueue: ReturnType<typeof createQueue> | undefined;
+
+export const globalEventQueue = new Proxy(
+  {} as ReturnType<typeof createQueue>,
+  {
+    get(_target, prop) {
+      _globalEventQueue ||= createQueue("globalEvents", {
+        attempts: 5,
+        backoff: {
+          type: "exponential",
+          delay: 1000,
+        },
+      });
+      return _globalEventQueue[prop as keyof typeof _globalEventQueue];
+    },
+  }
+);
+
+export const processorEventQueue = new Proxy(
+  {} as ReturnType<typeof createQueue>,
+  {
+    get(_target, prop) {
+      _processorEventQueue ||= createQueue("processorEvents", {
+        attempts: 5,
+        backoff: {
+          type: "exponential",
+          delay: 10 * 1000,
+        },
+      });
+      return _processorEventQueue[prop as keyof typeof _processorEventQueue];
+    },
+  }
+);
+
+export const websocketQueue = new Proxy({} as ReturnType<typeof createQueue>, {
+  get(_target, prop) {
+    _websocketQueue ||= createQueue("websockets", {
+      timeout: 10 * 1000,
+    });
+    return _websocketQueue[prop as keyof typeof _websocketQueue];
   },
 });
 
-export const processorEventQueue = createQueue("processorEvents", {
-  attempts: 5,
-  backoff: {
-    type: "exponential",
-    delay: 10 * 1000,
-  },
-});
-
-export const websocketQueue = createQueue("websockets", {
-  timeout: 10 * 1000,
-});
-
-export const taskQueue = createQueue("tasks", {
-  attempts: 5,
-  backoff: {
-    type: "exponential",
-    delay: 10 * 1000,
+export const taskQueue = new Proxy({} as ReturnType<typeof createQueue>, {
+  get(_target, prop) {
+    _taskQueue ||= createQueue("tasks", {
+      attempts: 5,
+      backoff: {
+        type: "exponential",
+        delay: 10 * 1000,
+      },
+    });
+    return _taskQueue[prop as keyof typeof _taskQueue];
   },
 });
