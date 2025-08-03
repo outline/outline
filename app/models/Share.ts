outline/outline

commit ba5f0aadeb3ab90c01869095039567ea39304d45

feat: Public sharing of collections (#9529)
* shares.info, collections.info, documents.info

* shares.list, shares.create, shares.update

* shares.sitemap

* parity with existing document shared screen

* collection share popover

* parent share and table

* collection scene

* collection link in sidebar

* sidebar and breadcrumb collection link click

* collection link click in editor

* meta

* more meta + 404 page

* map internal link, remove showLastUpdated option

* fix shares.list pagination

* show last updated

* shareLoader tests

* lint

* sidebar context for collection link

* badge in shares table

* fix existing tests

* tsc

* update failing test snapshot

* env

* signed url for collection attachments

* include collection content in SSR for screen readers

* search

* drafts can be shared

* review

* tsc, remove old shared-doc scene

* tweaks

* DRY

* refactor loader

* Remove share/collection urls

* fix: Collection overview should not be editable when viewing shared link and logged in

* Tweak public breadcrumb

* fix: Deleted documents should never be exposed through share

* empty sharedTree array where includeChildDocuments is false

* revert includeChildDocs guard for logical correctness + SSR bug fix

* fix: check document is part of share

---------

Co-authored-by: Tom Moor <tom@getoutline.com>

app/models/Share.ts
@@ -1,4 +1,7 @@
 import { computed, observable } from "mobx";
+import { NavigationNode, PublicTeam } from "@shared/types";
+import SharesStore from "~/stores/SharesStore";
+import env from "~/env";
 import Collection from "./Collection";
 import Document from "./Document";
 import User from "./User";
@@ -9,6 +12,8 @@ import { Searchable } from "./interfaces/Searchable";
 
 class Share extends Model implements Searchable {
   static modelName = "Share";
+
+  store: SharesStore;
 
   @Field
   @observable
@@ -45,6 +50,12 @@ class Share extends Model implements Searchable {
   domain: string;
 
   @observable
+  sourceTitle: string;
+
+  @observable
+  sourcePath: string;
+
+  @observable
   documentTitle: string;
 
   @observable
@@ -71,9 +82,33 @@ class Share extends Model implements Searchable {
   @Relation(() => User, { onDelete: "null" })
   createdBy: User;
 
+  static sitemapUrl(shareId: string) {
+    return `${env.URL}/api/shares.sitemap?shareId=${shareId}`;
+  }
+
+  @computed
+  get title(): string {
+    return this.sourceTitle ?? this.documentTitle;
+  }
+
+  @computed
+  get sourcePathWithFallback(): string {
+    return this.sourcePath ?? this.documentUrl;
+  }
+
   @computed
   get searchContent(): string[] {
-    return [this.document?.title ?? this.documentTitle];
+    return [this.title];
+  }
+
+  @computed
+  get team(): PublicTeam | undefined {
+    return this.store.sharedCache.get(this.id)?.team;
+  }
+
+  @computed
+  get tree(): NavigationNode | undefined {
+    return this.store.sharedCache.get(this.id)?.sharedTree ?? undefined;
   }
 }
 

app/stores/SharesStore.ts
@@ -3,11 +3,11 @@ import filter from "lodash/filter";
 import find from "lodash/find";
 import isUndefined from "lodash/isUndefined";
 import orderBy from "lodash/orderBy";
-import { action, computed } from "mobx";
-import type { Required } from "utility-types";
-import type { JSONObject } from "@shared/types";
+import { action, computed, observable } from "mobx";
+import type { NavigationNode, PublicTeam } from "@shared/types";
+import Document from "~/models/Document";
 import Share from "~/models/Share";
-import type { Properties } from "~/types";
+import type { PartialExcept } from "~/types";
 import { client } from "~/utils/ApiClient";
 import RootStore from "./RootStore";
 import Store, { RPCAction } from "./base/Store";
@@ -19,6 +19,12 @@ export default class SharesStore extends Store<Share> {
     RPCAction.Create,
     RPCAction.Update,
   ];
+
+  @observable
+  sharedCache: Map<
+    string,
+    { sharedTree: NavigationNode | null; team: PublicTeam } | undefined
+  > = new Map();
 
   constructor(rootStore: RootStore) {
     super(rootStore, Share);
@@ -43,26 +49,73 @@ export default class SharesStore extends Store<Share> {
   };
 
   @action
-  async create(params: Required<Properties<Share>, "documentId">) {
-    const item = this.getByDocumentId(params.documentId);
+  async create(
+    params:
+      | (PartialExcept<Share, "collectionId"> & { type: "collection" })
+      | (PartialExcept<Share, "documentId"> & { type: "document" })
+  ): Promise<Share> {
+    const item =
+      params.type === "collection"
+        ? this.getByCollectionId(params.collectionId)
+        : this.getByDocumentId(params.documentId);
+
     if (item) {
       return item;
     }
+
     return super.create(params);
   }
 
   @action
-  async fetch(documentId: string, options: JSONObject = {}): Promise<any> {
-    const item = this.getByDocumentId(documentId);
-    if (item && !options.force) {
-      return item;
+  async fetch(id: string) {
+    const share = this.get(id);
+    const cache = this.sharedCache.get(id);
+    if (share && cache) {
+      return share;
+    }
+
+    this.isFetching = true;
+    try {
+      const res = await client.post(`/${this.apiEndpoint}.info`, {
+        id,
+      });
+      invariant(res?.data, "Data should be available");
+
+      res.data.shares.map(this.add);
+
+      if (res.data.collection) {
+        this.rootStore.collections.add(res.data.collection);
+      }
+
+      if (res.data.document) {
+        this.rootStore.documents.add(res.data.document);
+      }
+
+      this.sharedCache.set(id, {
+        sharedTree: res.data.sharedTree,
+        team: res.data.team,
+      });
+      this.addPolicies(res.policies);
+
+      return this.data.get(id)!;
+    } finally {
+      this.isFetching = false;
+    }
+  }
+
+  @action
+  async fetchOne(params: { documentId: string } | { collectionId: string }) {
+    const share =
+      "collectionId" in params
+        ? this.getByCollectionId(params.collectionId)
+        : this.getByDocumentId(params.documentId);
+    if (share) {
+      return share;
     }
     this.isFetching = true;
 
     try {
-      const res = await client.post(`/${this.apiEndpoint}.info`, {
-        documentId,
-      });
+      const res = await client.post(`/${this.apiEndpoint}.info`, params);
 
       if (isUndefined(res)) {
         return;
@@ -75,10 +128,13 @@ export default class SharesStore extends Store<Share> {
     }
   }
 
-  getByDocumentParents = (documentId: string): Share | undefined => {
-    const document = this.rootStore.documents.get(documentId);
-    if (!document) {
-      return;
+  getByDocumentParents = (document: Document): Share | undefined => {
+    const collectionShare = document.collectionId
+      ? this.getByCollectionId(document.collectionId)
+      : undefined;
+
+    if (collectionShare?.published) {
+      return collectionShare;
     }
 
     const collection = document.collectionId
@@ -89,7 +145,7 @@ export default class SharesStore extends Store<Share> {
     }
 
     const parentIds = collection
-      .pathToDocument(documentId)
+      .pathToDocument(document.id)
       .slice(0, -1)
       .map((p) => p.id);
 

server/routes/api/shares/schema.ts
@@ -1,28 +1,28 @@
 import isEmpty from "lodash/isEmpty";
-import isUUID from "validator/lib/isUUID";
 import { z } from "zod";
 import { UrlHelper } from "@shared/utils/UrlHelper";
 import { Share } from "@server/models";
+import { zodIdType } from "@server/utils/zod";
 import { BaseSchema } from "../schema";
 
 export const SharesInfoSchema = BaseSchema.extend({
   body: z
     .object({
       id: z.string().uuid().optional(),
-      documentId: z
-        .string()
-        .optional()
-        .refine(
-          (val) =>
-            val ? isUUID(val) || UrlHelper.SLUG_URL_REGEX.test(val) : true,
-          {
-            message: "must be uuid or url slug",
-          }
-        ),
+      collectionId: zodIdType().optional(),
+      documentId: zodIdType().optional(),
     })
-    .refine((body) => !(isEmpty(body.id) && isEmpty(body.documentId)), {
-      message: "id or documentId is required",
-    }),
+    .refine(
+      (body) =>
+        !(
+          isEmpty(body.id) &&
+          isEmpty(body.collectionId) &&
+          isEmpty(body.documentId)
+        ),
+      {
+        message: "one of id, collectionId, or documentId is required",
+      }
+    ),
 });
 
 export type SharesInfoReq = z.infer<typeof SharesInfoSchema>;
@@ -66,23 +66,24 @@ export const SharesUpdateSchema = BaseSchema.extend({
 export type SharesUpdateReq = z.infer<typeof SharesUpdateSchema>;
 
 export const SharesCreateSchema = BaseSchema.extend({
-  body: z.object({
-    documentId: z
-      .string()
-      .refine((val) => isUUID(val) || UrlHelper.SLUG_URL_REGEX.test(val), {
-        message: "must be uuid or url slug",
-      }),
-    published: z.boolean().default(false),
-    allowIndexing: z.boolean().optional(),
-    showLastUpdated: z.boolean().optional(),
-    urlId: z
-      .string()
-      .regex(UrlHelper.SHARE_URL_SLUG_REGEX, {
-        message: "must contain only alphanumeric and dashes",
-      })
-      .optional(),
-    includeChildDocuments: z.boolean().default(false),
-  }),
+  body: z
+    .object({
+      collectionId: zodIdType().optional(),
+      documentId: zodIdType().optional(),
+      published: z.boolean().default(false),
+      allowIndexing: z.boolean().optional(),
+      showLastUpdated: z.boolean().optional(),
+      urlId: z
+        .string()
+        .regex(UrlHelper.SHARE_URL_SLUG_REGEX, {
+          message: "must contain only alphanumeric and dashes",
+        })
+        .optional(),
+      includeChildDocuments: z.boolean().default(false),
+    })
+    .refine((obj) => !(isEmpty(obj.collectionId) && isEmpty(obj.documentId)), {
+      message: "one of collectionId or documentId is required",
+    }),
 });
 
 export type SharesCreateReq = z.infer<typeof SharesCreateSchema>;
@@ -94,3 +95,11 @@ export const SharesRevokeSchema = BaseSchema.extend({
 });
 
 export type SharesRevokeReq = z.infer<typeof SharesRevokeSchema>;
+
+export const SharesSitemapSchema = BaseSchema.extend({
+  query: z.object({
+    id: z.string(),
+  }),
+});
+
+export type SharesSitemapReq = z.infer<typeof SharesSitemapSchema>;

server/models/Share.ts
@@ -37,6 +37,10 @@ import Length from "./validators/Length";
       paranoid: false,
     },
     {
+      association: "collection",
+      required: false,
+    },
+    {
       association: "document",
       required: false,
     },
@@ -49,6 +53,21 @@ import Length from "./validators/Length";
   withCollectionPermissions: (userId: string) => ({
     include: [
       {
+        attributes: [
+          "id",
+          "name",
+          "permission",
+          "sharing",
+          "urlId",
+          "teamId",
+          "deletedAt",
+        ],
+        model: Collection.scope({
+          method: ["withMembership", userId],
+        }),
+        as: "collection",
+      },
+      {
         model: Document.scope([
           "withDrafts",
           {
@@ -59,7 +78,15 @@ import Length from "./validators/Length";
         as: "document",
         include: [
           {
-            attributes: ["id", "permission", "sharing", "teamId", "deletedAt"],
+            attributes: [
+              "id",
+              "name",
+              "permission",
+              "urlId",
+              "sharing",
+              "teamId",
+              "deletedAt",
+            ],
             model: Collection.scope({
               method: ["withMembership", userId],
             }),
@@ -186,12 +213,19 @@ class Share extends IdModel<
   @Column(DataType.UUID)
   teamId: string;
 
+  @BelongsTo(() => Collection, "collectionId")
+  collection: Collection | null;
+
+  @ForeignKey(() => Collection)
+  @Column(DataType.UUID)
+  collectionId: string | null;
+
   @BelongsTo(() => Document, "documentId")
   document: Document | null;
 
   @ForeignKey(() => Document)
   @Column(DataType.UUID)
-  documentId: string;
+  documentId: string | null;
 
   revoke(ctx: APIContext) {
     const { user } = ctx.state.auth;

server/routes/api/collections/schema.ts
@@ -1,10 +1,12 @@
 import isUndefined from "lodash/isUndefined";
+import isUUID from "validator/lib/isUUID";
 import { z } from "zod";
 import {
   CollectionPermission,
   CollectionStatusFilter,
   FileOperationFormat,
 } from "@shared/types";
+import { UrlHelper } from "@shared/utils/UrlHelper";
 import { Collection } from "@server/models";
 import { zodIconType, zodIdType } from "@server/utils/zod";
 import { ValidateColor, ValidateIndex } from "@server/validation";
@@ -50,7 +52,13 @@ export const CollectionsCreateSchema = BaseSchema.extend({
 export type CollectionsCreateReq = z.infer<typeof CollectionsCreateSchema>;
 
 export const CollectionsInfoSchema = BaseSchema.extend({
-  body: BaseIdSchema,
+  body: BaseIdSchema.extend({
+    /** Share Id, if available */
+    shareId: z
+      .string()
+      .refine((val) => isUUID(val) || UrlHelper.SHARE_URL_SLUG_REGEX.test(val))
+      .optional(),
+  }),
 });
 
 export type CollectionsInfoReq = z.infer<typeof CollectionsInfoSchema>;

app/components/SearchListItem.tsx
@@ -10,7 +10,7 @@ import breakpoint from "styled-components-breakpoint";
 import { s, hover, ellipsis } from "@shared/styles";
 import Document from "~/models/Document";
 import Highlight, { Mark } from "~/components/Highlight";
-import { sharedDocumentPath } from "~/utils/routeHelpers";
+import { sharedModelPath } from "~/utils/routeHelpers";
 
 type Props = {
   document: Document;
@@ -51,7 +51,7 @@ function DocumentListItem(
       dir={document.dir}
       to={{
         pathname: shareId
-          ? sharedDocumentPath(shareId, document.url)
+          ? sharedModelPath(shareId, document.url)
           : document.url,
         state: {
           title: document.titleWithDefault,

app/components/Sharing/Collection/AccessControlList.tsx
@@ -5,32 +5,42 @@ import { useTranslation } from "react-i18next";
 import { toast } from "sonner";
 import styled, { useTheme } from "styled-components";
 import Squircle from "@shared/components/Squircle";
+import { s } from "@shared/styles";
 import { CollectionPermission } from "@shared/types";
 import Collection from "~/models/Collection";
+import Share from "~/models/Share";
 import { Avatar, GroupAvatar, AvatarSize } from "~/components/Avatar";
 import InputMemberPermissionSelect from "~/components/InputMemberPermissionSelect";
 import { InputSelectPermission } from "~/components/InputSelectPermission";
 import Scrollable from "~/components/Scrollable";
+import useCurrentTeam from "~/hooks/useCurrentTeam";
 import useMaxHeight from "~/hooks/useMaxHeight";
 import usePolicy from "~/hooks/usePolicy";
 import useRequest from "~/hooks/useRequest";
 import useStores from "~/hooks/useStores";
 import { EmptySelectValue, Permission } from "~/types";
+import { Separator } from "../components";
 import { ListItem } from "../components/ListItem";
 import { Placeholder } from "../components/Placeholder";
+import { PublicAccess } from "./PublicAccess";
 
 type Props = {
   /** Collection to which team members are supposed to be invited */
   collection: Collection;
+  /** The existing share model, if any. */
+  share: Share | null | undefined;
   /** Children to be rendered before the list of members */
   children?: React.ReactNode;
   /** List of users and groups that have been invited during the current editing session */
   invitedInSession: string[];
+  /** Whether the popover is visible. */
+  visible: boolean;
 };
 
 export const AccessControlList = observer(
-  ({ collection, invitedInSession }: Props) => {
+  ({ collection, share, invitedInSession, visible }: Props) => {
     const { memberships, groupMemberships } = useStores();
+    const team = useCurrentTeam();
     const can = usePolicy(collection);
     const { t } = useTranslation();
     const theme = useTheme();
@@ -246,6 +256,12 @@ export const AccessControlList = observer(
               ))}
           </>
         )}
+        {team.sharing && can.share && collection.sharing && visible && (
+          <Sticky>
+            {collection.members.length ? <Separator /> : null}
+            <PublicAccess collection={collection} share={share} />
+          </Sticky>
+        )}
       </ScrollableContainer>
     );
   }
@@ -254,4 +270,10 @@ export const AccessControlList = observer(
 const ScrollableContainer = styled(Scrollable)`
   padding: 12px 24px;
   margin: -12px -24px;
+`;
+
+const Sticky = styled.div`
+  background: ${s("menuBackground")};
+  position: sticky;
+  bottom: 0;
 `;

app/components/Sharing/Collection/PublicAccess.tsx
@@ -2,56 +2,50 @@ import debounce from "lodash/debounce";
 import isEmpty from "lodash/isEmpty";
 import { observer } from "mobx-react";
 import { CopyIcon, GlobeIcon, InfoIcon, QuestionMarkIcon } from "outline-icons";
-import * as React from "react";
-import { Trans, useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { toast } from "sonner";
 import styled, { useTheme } from "styled-components";
-import Flex from "@shared/components/Flex";
 import Squircle from "@shared/components/Squircle";
 import { s } from "@shared/styles";
 import { UrlHelper } from "@shared/utils/UrlHelper";
-import Document from "~/models/Document";
+import Collection from "~/models/Collection";
 import Share from "~/models/Share";
+import { AvatarSize } from "~/components/Avatar";
+import CopyToClipboard from "~/components/CopyToClipboard";
+import Flex from "~/components/Flex";
 import Input, { NativeInput } from "~/components/Input";
+import NudeButton from "~/components/NudeButton";
+import { ResizingHeightContainer } from "~/components/ResizingHeightContainer";
 import Switch from "~/components/Switch";
+import Text from "~/components/Text";
+import Tooltip from "~/components/Tooltip";
 import env from "~/env";
 import usePolicy from "~/hooks/usePolicy";
-import { AvatarSize } from "../../Avatar";
-import CopyToClipboard from "../../CopyToClipboard";
-import NudeButton from "../../NudeButton";
-import { ResizingHeightContainer } from "../../ResizingHeightContainer";
-import Text from "../../Text";
-import Tooltip from "../../Tooltip";
 import { ListItem } from "../components/ListItem";
 
 type Props = {
-  /** The document to share. */
-  document: Document;
+  /** The collection to share. */
+  collection: Collection;
   /** The existing share model, if any. */
   share: Share | null | undefined;
-  /** The existing share parent model, if any. */
-  sharedParent: Share | null | undefined;
-  /** Ref to the Copy Link button */
-  copyButtonRef?: React.RefObject<HTMLButtonElement>;
-  onRequestClose?: () => void;
 };
 
-function PublicAccess({ document, share, sharedParent }: Props) {
+function InnerPublicAccess({ collection, share }: Props) {
   const { t } = useTranslation();
   const theme = useTheme();
-  const [validationError, setValidationError] = React.useState("");
-  const [urlId, setUrlId] = React.useState(share?.urlId);
-  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [validationError, setValidationError] = useState("");
+  const [urlId, setUrlId] = useState(share?.urlId);
+  const inputRef = useRef<HTMLInputElement>(null);
   const can = usePolicy(share);
-  const documentAbilities = usePolicy(document);
-  const canPublish = can.update && documentAbilities.share;
+  const collectionAbilities = usePolicy(collection);
+  const canPublish = can.update && collectionAbilities.share;
 
-  React.useEffect(() => {
+  useEffect(() => {
     setUrlId(share?.urlId);
   }, [share?.urlId]);
 
-  const handleIndexingChanged = React.useCallback(
+  const handleIndexingChanged = useCallback(
     async (checked: boolean) => {
       try {
         await share?.save({
@@ -64,7 +58,7 @@ function PublicAccess({ document, share, sharedParent }: Props) {
     [share]
   );
 
-  const handleShowLastModifiedChanged = React.useCallback(
+  const handleShowLastModifiedChanged = useCallback(
     async (checked: boolean) => {
       try {
         await share?.save({
@@ -77,7 +71,7 @@ function PublicAccess({ document, share, sharedParent }: Props) {
     [share]
   );
 
-  const handlePublishedChange = React.useCallback(
+  const handlePublishedChange = useCallback(
     async (checked: boolean) => {
       try {
         await share?.save({
@@ -90,7 +84,7 @@ function PublicAccess({ document, share, sharedParent }: Props) {
     [share]
   );
 
-  const handleUrlChange = React.useMemo(
+  const handleUrlChange = useMemo(
     () =>
       debounce(async (ev) => {
         if (!share) {
@@ -121,17 +115,13 @@ function PublicAccess({ document, share, sharedParent }: Props) {
     [t, share]
   );
 
-  const handleCopied = React.useCallback(() => {
+  const handleCopied = useCallback(() => {
     toast.success(t("Public link copied to clipboard"));
   }, [t]);
 
-  const shareUrl = sharedParent?.url
-    ? `${sharedParent.url}${document.url}`
-    : (share?.url ?? "");
-
   const copyButton = (
     <Tooltip content={t("Copy public link")} placement="top">
-      <CopyToClipboard text={shareUrl} onCopy={handleCopied}>
+      <CopyToClipboard text={share?.url ?? ""} onCopy={handleCopied}>
         <NudeButton type="button" disabled={!share} style={{ marginRight: 3 }}>
           <CopyIcon color={theme.placeholder} size={18} />
         </NudeButton>
@@ -143,53 +133,26 @@ function PublicAccess({ document, share, sharedParent }: Props) {
     <Wrapper>
       <ListItem
         title={t("Web")}
-        subtitle={
-          <>
-            {sharedParent && !document.isDraft ? (
-              sharedParent.collectionId ? (
-                <Trans>
-                  Anyone with the link can access because the containing
-                  collection,{" "}
-                  <StyledLink to={`/collection/${sharedParent.collectionId}`}>
-                    {sharedParent.sourceTitle}
-                  </StyledLink>
-                  , is shared
-                </Trans>
-              ) : (
-                <Trans>
-                  Anyone with the link can access because the parent document,{" "}
-                  <StyledLink to={`/doc/${sharedParent.documentId}`}>
-                    {sharedParent.sourceTitle}
-                  </StyledLink>
-                  , is shared
-                </Trans>
-              )
-            ) : (
-              t("Allow anyone with the link to access")
-            )}
-          </>
-        }
+        subtitle={<>{t("Allow anyone with the link to access")}</>}
         image={
           <Squircle color={theme.text} size={AvatarSize.Medium}>
             <GlobeIcon color={theme.background} size={18} />
           </Squircle>
         }
         actions={
-          sharedParent && !document.isDraft ? null : (
-            <Switch
-              aria-label={t("Publish to internet")}
-              checked={share?.published ?? false}
-              onChange={handlePublishedChange}
-              disabled={!canPublish}
-              width={26}
-              height={14}
-            />
-          )
+          <Switch
+            aria-label={t("Publish to internet")}
+            checked={share?.published ?? false}
+            onChange={handlePublishedChange}
+            disabled={!canPublish}
+            width={26}
+            height={14}
+          />
         }
       />
 
       <ResizingHeightContainer>
-        {share?.published && !sharedParent?.published && (
+        {!!share?.published && (
           <>
             <ListItem
               title={
@@ -241,51 +204,36 @@ function PublicAccess({ document, share, sharedParent }: Props) {
                 />
               }
             />
+            <ShareLinkInput
+              type="text"
+              ref={inputRef}
+              placeholder={share?.id}
+              onChange={handleUrlChange}
+              error={validationError}
+              defaultValue={urlId}
+              prefix={
+                <DomainPrefix onClick={() => inputRef.current?.focus()}>
+                  {env.URL.replace(/https?:\/\//, "") + "/s/"}
+                </DomainPrefix>
+              }
+            >
+              {copyButton}
+            </ShareLinkInput>
+            <Flex align="flex-start" gap={4}>
+              <StyledInfoIcon size={18} color={theme.textTertiary} />
+              <Text type="tertiary" size="xsmall">
+                {t(
+                  "All documents in this collection will be shared on the web, including any new documents added later"
+                )}
+                .
+              </Text>
+            </Flex>
           </>
         )}
-
-        {sharedParent?.published ? (
-          <ShareLinkInput type="text" disabled defaultValue={shareUrl}>
-            {copyButton}
-          </ShareLinkInput>
-        ) : share?.published ? (
-          <ShareLinkInput
-            type="text"
-            ref={inputRef}
-            placeholder={share?.id}
-            onChange={handleUrlChange}
-            error={validationError}
-            defaultValue={urlId}
-            prefix={
-              <DomainPrefix onClick={() => inputRef.current?.focus()}>
-                {env.URL.replace(/https?:\/\//, "") + "/s/"}
-              </DomainPrefix>
-            }
-          >
-            {copyButton}
-          </ShareLinkInput>
-        ) : null}
-
-        {share?.published && !share.includeChildDocuments ? (
-          <Text as="p" type="tertiary" size="xsmall">
-            <StyledInfoIcon size={18} />
-            <span>
-              {t(
-                "Nested documents are not shared on the web. Toggle sharing to enable access, this will be the default behavior in the future"
-              )}
-              .
-            </span>
-          </Text>
-        ) : null}
       </ResizingHeightContainer>
     </Wrapper>
   );
 }
-
-const StyledInfoIcon = styled(InfoIcon)`
-  vertical-align: bottom;
-  margin-right: 2px;
-`;
 
 const Wrapper = styled.div`
   padding-bottom: 8px;
@@ -310,9 +258,10 @@ const ShareLinkInput = styled(Input)`
   }
 `;
 
-const StyledLink = styled(Link)`
-  color: ${s("textSecondary")};
-  text-decoration: underline;
+const StyledInfoIcon = styled(InfoIcon)`
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
 `;
 
-export default observer(PublicAccess);
+export const PublicAccess = observer(InnerPublicAccess);

commit 343a11902f602d2d8e4abbb5c5a288ceeca92e99

fix: Unable to load share from custom slug
closes #9807
regressed in #9529

app/models/Share.ts
@@ -102,13 +102,21 @@ class Share extends Model implements Searchable {
   }
 
   @computed
+  get sharedCache() {
+    return (
+      this.store.sharedCache.get(this.id) ??
+      this.store.sharedCache.get(this.urlId)
+    );
+  }
+
+  @computed
   get team(): PublicTeam | undefined {
-    return this.store.sharedCache.get(this.id)?.team;
+    return this.sharedCache?.team;
   }
 
   @computed
   get tree(): NavigationNode | undefined {
-    return this.store.sharedCache.get(this.id)?.sharedTree ?? undefined;
+    return this.sharedCache?.sharedTree ?? undefined;
   }
 }
 

app/stores/SharesStore.ts
@@ -165,4 +165,11 @@ export default class SharesStore extends Store<Share> {
 
   getByDocumentId = (documentId: string): Share | null | undefined =>
     find(this.orderedData, (share) => share.documentId === documentId);
+
+  get(id: string): Share | undefined {
+    return (
+      this.data.get(id) ??
+      this.orderedData.find((share) => id.endsWith(share.urlId))
+    );
+  }
 }

server/routes/api/shares/schema.ts
@@ -8,7 +8,7 @@ import { BaseSchema } from "../schema";
 export const SharesInfoSchema = BaseSchema.extend({
   body: z
     .object({
-      id: z.string().uuid().optional(),
+      id: z.string().optional(),
       collectionId: zodIdType().optional(),
       documentId: zodIdType().optional(),
     })