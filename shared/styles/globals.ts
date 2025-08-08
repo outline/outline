efaa3d61d53bc49e2429d318c16e5d49679343c8
Tom Moor <tom@getoutline.com>
2025-08-07 21:37:23 -0400

    Update globals.ts

--- shared/styles/globals.ts
+++ shared/styles/globals.ts
@@ -46,9 +46,7 @@ export default createGlobalStyle<Props>`
     -moz-osx-font-smoothing: grayscale;
     -webkit-font-smoothing: antialiased;
     text-rendering: optimizeLegibility;
-  }
 
-  body {
     width: 100vw;
     overflow-x: hidden;
     padding-right: calc(0 - var(--removed-body-scroll-bar-size)) !important;
