8ab856cb4b82b7a36427293bee766a4e89611c25
Tom Moor <tom@getoutline.com>
2025-08-14 22:03:59 -0400

    fix: Prevent both img parseDOM rules matching
    closes #9930

--- shared/editor/nodes/Image.tsx
+++ shared/editor/nodes/Image.tsx
@@ -137,6 +137,11 @@ export default class Image extends SimpleImage {
         {
           tag: "img",
           getAttrs: (dom: HTMLImageElement) => {
+            // Don't parse images from our own editor with this rule.
+            if (dom.parentElement?.classList.contains("image")) {
+              return false;
+            }
+
             // First try HTML attributes
             let width = dom.getAttribute("width");
             let height = dom.getAttribute("height");
