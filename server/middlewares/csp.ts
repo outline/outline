952860b23bfc68bc45b987fedaf12108ec3df510
Tom Moor <tom@getoutline.com>
2025-08-16 18:10:28 -0400

    chore: More CSP hardening (#9951)

--- server/middlewares/csp.ts
+++ server/middlewares/csp.ts
@@ -41,6 +41,7 @@ export default function createCSPMiddleware() {
 
     return contentSecurityPolicy({
       directives: {
+        baseUri: ["'none'"],
         defaultSrc,
         styleSrc,
         scriptSrc: [
