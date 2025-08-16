81df6c6fa514aab7b9964effc514e4844d94fe3e
Tom Moor <tom@getoutline.com>
2025-08-16 08:18:25 -0400

    fix: Document move/copy/publish dialog behavior (#9947)
    * fix: Document move/copy/publish dialog behavior
    
    * More fixes

--- app/hooks/useCollectionTrees.ts
+++ app/hooks/useCollectionTrees.ts
@@ -73,7 +73,7 @@ export default function useCollectionTrees(): NavigationNode[] {
       parent: null,
     };
 
-    return addParent(addCollectionId(addDepth(addType(collectionNode))));
+    return addParent(addCollectionId(addDepth(addType(collectionNode), 1)));
   };
 
   const key = collections.orderedData.map((o) => o.documents?.length).join("-");
