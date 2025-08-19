3aff344501a67b08527f3ba028a5c1a4d692e481
Tom Moor <tom@getoutline.com>
2025-08-19 08:29:53 -0400

    fix: Unable to use DATABASE_HOST env (#9977)

--- server/env.ts
+++ server/env.ts
@@ -78,7 +78,7 @@ export class Environment {
   /**
    * The url of the database.
    */
-  @IsNotEmpty()
+  @IsOptional()
   @IsUrl({
     require_tld: false,
     allow_underscores: true,
@@ -91,7 +91,7 @@ export class Environment {
     "DATABASE_USER",
     "DATABASE_PASSWORD",
   ])
-  public DATABASE_URL = environment.DATABASE_URL ?? "";
+  public DATABASE_URL = this.toOptionalString(environment.DATABASE_URL);
 
   /**
    * Database host for individual component configuration.
