b858bd9a37d7eebf222b8ae035af2fb7673e5510
Tom Moor <tom@getoutline.com>
2025-11-08 21:53:40 -0500

    fix: Account for reading time longer than an hour (#10601)

--- app/components/ReadingTime.tsx
+++ app/components/ReadingTime.tsx
@@ -13,12 +13,28 @@ const ReadingTime = ({ document }: { document: Document }) => {
   );
   const stats = useTextStats(markdown);
 
+  const readingTimeMinutes = stats.total.readingTime;
+  const hours = Math.floor(readingTimeMinutes / 60);
+  const minutes = readingTimeMinutes % 60;
+
+  let readingTimeText;
+  if (hours > 0) {
+    if (minutes > 0) {
+      readingTimeText = t(`{{ hours }}h {{ minutes }}m read`, {
+        hours,
+        minutes,
+      });
+    } else {
+      readingTimeText = t(`{{ hours }}h read`, { hours });
+    }
+  } else {
+    readingTimeText = t(`{{ minutes }}m read`, { minutes: readingTimeMinutes });
+  }
+
   return (
     <>
       <EyeIcon size={18} />
-      {t(`{{ minutes }}m read`, {
-        minutes: stats.total.readingTime,
-      })}
+      {readingTimeText}
     </>
   );
 };
