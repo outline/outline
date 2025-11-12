6a564a575cf4a1d24b658f0be053b4b11d78c57f
Apoorv Mishra <apoorvmishra101092@gmail.com>
2025-11-12 11:07:57 +0530

    Link bar refactor to use `commands` (#10556)
    * fix: quick refactor
    
    * fix: `state.selection.to` is equivalent to mark's end pos in this case
    
    * fix: get rid of paste handler
    
    * fix: inline actions
    
    * fix: remove and inline enum

--- app/editor/menus/formatting.tsx
+++ app/editor/menus/formatting.tsx
@@ -253,7 +253,7 @@ export default function formattingMenuItems(
       visible: !isCodeBlock,
     },
     {
-      name: "link",
+      name: "addLink",
       tooltip: dictionary.createLink,
       shortcut: `${metaDisplay}+K`,
       icon: <LinkIcon />,
