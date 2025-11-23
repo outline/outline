f11c6e5c3948f92035b45275594803c90448ae84
Tom Moor <tom@getoutline.com>
2025-11-23 10:24:54 -0500

    fix: Mobile treatment

--- app/components/SwatchButton.tsx
+++ app/components/SwatchButton.tsx
@@ -3,7 +3,9 @@ import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import { s } from "@shared/styles";
 import lazyWithRetry from "~/utils/lazyWithRetry";
+import useMobile from "~/hooks/useMobile";
 import DelayedMount from "./DelayedMount";
+import { Drawer, DrawerContent, DrawerTrigger } from "./primitives/Drawer";
 import { Popover, PopoverTrigger, PopoverContent } from "./primitives/Popover";
 import Text from "./Text";
 import { ColorButton } from "./ColorButton";
@@ -35,36 +37,55 @@ export const SwatchButton: React.FC<SwatchButtonProps> = ({
   pickerInModal = true,
 }) => {
   const { t } = useTranslation();
+  const isMobile = useMobile();
+
+  const pickerTrigger = (
+    <ColorButton
+      aria-label={t("Select a color")}
+      className={className}
+      color={color}
+      active={active}
+      size={size}
+    />
+  );
+
+  const pickerContent = (
+    <React.Suspense
+      fallback={
+        <DelayedMount>
+          <Text>{t("Loading")}…</Text>
+        </DelayedMount>
+      }
+    >
+      <StyledColorPicker
+        disableAlpha
+        color={color}
+        onChange={(c) => onChange(c.hex)}
+      />
+    </React.Suspense>
+  );
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger asChild>{pickerTrigger}</DrawerTrigger>
+        <DrawerContent aria-label={t("Select a color")}>
+          {pickerContent}
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
   return (
     <Popover modal={pickerInModal}>
-      <PopoverTrigger>
-        <ColorButton
-          aria-label={t("Select a color")}
-          className={className}
-          color={color}
-          active={active}
-          size={size}
-        />
-      </PopoverTrigger>
+      <PopoverTrigger>{pickerTrigger}</PopoverTrigger>
       <StyledContent
         side="bottom"
         align="end"
         aria-label={t("Select a color")}
         shrink
       >
-        <React.Suspense
-          fallback={
-            <DelayedMount>
-              <Text>{t("Loading")}…</Text>
-            </DelayedMount>
-          }
-        >
-          <StyledColorPicker
-            disableAlpha
-            color={color}
-            onChange={(c) => onChange(c.hex)}
-          />
-        </React.Suspense>
+        {pickerContent}
       </StyledContent>
     </Popover>
   );
@@ -85,6 +106,7 @@ const StyledColorPicker = styled(ColorPicker)`
   border: 0 !important;
   border-radius: 0 !important;
   user-select: none;
+  width: auto !important;
 
   input {
     user-select: text;
