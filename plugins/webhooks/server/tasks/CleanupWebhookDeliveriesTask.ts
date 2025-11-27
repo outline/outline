03e5c6b591095bb6426cf14dd7faa1f4734362fb
Tom Moor <tom@getoutline.com>
2025-11-27 09:28:25 -0500

    fix: Missing partition

--- plugins/webhooks/server/tasks/CleanupWebhookDeliveriesTask.ts
+++ plugins/webhooks/server/tasks/CleanupWebhookDeliveriesTask.ts
@@ -3,17 +3,22 @@ import { Op } from "sequelize";
 import Logger from "@server/logging/Logger";
 import { WebhookDelivery } from "@server/models";
 import { TaskPriority } from "@server/queues/tasks/base/BaseTask";
-import { CronTask, TaskInterval } from "@server/queues/tasks/base/CronTask";
+import {
+  CronTask,
+  TaskInterval,
+  Props,
+} from "@server/queues/tasks/base/CronTask";
 import { Hour } from "@shared/utils/time";
 
 export default class CleanupWebhookDeliveriesTask extends CronTask {
-  public async perform() {
+  public async perform({ partition }: Props) {
     Logger.info("task", `Deleting WebhookDeliveries older than one week…`);
     const count = await WebhookDelivery.unscoped().destroy({
       where: {
         createdAt: {
           [Op.lt]: subDays(new Date(), 7),
         },
+        ...this.getPartitionWhereClause("id", partition),
       },
     });
     Logger.info("task", `${count} old WebhookDeliveries deleted.`);
