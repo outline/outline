305b81fbf49f0cf603ed9a924b27821cd86782af
Tom Moor <tom@getoutline.com>
2025-12-05 08:43:22 -0500

    fix: Badges should never wrap (#10804)

--- app/components/Badge.ts
+++ app/components/Badge.ts
@@ -21,6 +21,7 @@ const Badge = styled.span<{ yellow?: boolean; primary?: boolean }>`
   font-size: 12px;
   font-weight: 500;
   user-select: none;
+  white-space: nowrap;
 `;
 
 export default Badge;
