1493c6f52ac04c7574a8f60f073a1f64b1d4d69c
Tom Moor <tom@getoutline.com>
2025-12-05 22:43:34 -0500

    perf

--- shared/editor/nodes/Heading.ts
+++ shared/editor/nodes/Heading.ts
@@ -181,92 +181,120 @@ export default class Heading extends Node {
   }
 
   get plugins() {
+    const createWidgetDecorations = (doc: ProsemirrorNode): Decoration[] => {
+      const decorations: Decoration[] = [];
+
+      doc.descendants((node, pos) => {
+        if (node.type.name === "heading") {
+          // Create anchor button
+          const anchor = document.createElement("button");
+          anchor.innerText = "#";
+          anchor.type = "button";
+          anchor.className = "heading-anchor";
+          anchor.addEventListener("mousedown", (event) =>
+            this.handleCopyLink(event)
+          );
+
+          // Create fold button
+          const fold = document.createElement("button");
+          fold.innerText = "";
+          fold.innerHTML =
+            '<svg fill="currentColor" width="12" height="24" viewBox="6 0 12 24" xmlns="http://www.w3.org/2000/svg"><path d="M8.23823905,10.6097108 L11.207376,14.4695888 L11.207376,14.4695888 C11.54411,14.907343 12.1719566,14.989236 12.6097108,14.652502 C12.6783439,14.5997073 12.7398293,14.538222 12.792624,14.4695888 L15.761761,10.6097108 L15.761761,10.6097108 C16.0984949,10.1719566 16.0166019,9.54410997 15.5788477,9.20737601 C15.4040391,9.07290785 15.1896811,9 14.969137,9 L9.03086304,9 L9.03086304,9 C8.47857829,9 8.03086304,9.44771525 8.03086304,10 C8.03086304,10.2205442 8.10377089,10.4349022 8.23823905,10.6097108 Z" /></svg>';
+          fold.type = "button";
+          fold.className = `heading-fold ${
+            node.attrs.collapsed ? "collapsed" : ""
+          }`;
+          fold.addEventListener("mousedown", (event) =>
+            this.handleFoldContent(event)
+          );
+
+          // Create container span
+          const container = document.createElement("span");
+          container.contentEditable = "false";
+          container.className = `heading-actions ${
+            node.attrs.collapsed ? "collapsed" : ""
+          }`;
+          container.appendChild(anchor);
+          container.appendChild(fold);
+
+          decorations.push(
+            // Contains the heading actions
+            Decoration.widget(pos + 1, container, {
+              side: -1,
+              ignoreSelection: true,
+              relaxedSide: true,
+              key: pos.toString(),
+            })
+          );
+
+          // Creates a "space" for the caret to move to before the widget.
+          // Without this it is very hard to place the caret at the beginning
+          // of the heading when it begins with an atom element.
+          if (node.firstChild?.isAtom === false) {
+            decorations.push(
+              Decoration.widget(pos + 1, () => document.createElement("span"), {
+                side: -1,
+                ignoreSelection: true,
+                relaxedSide: true,
+                key: "span",
+              })
+            );
+          }
+        }
+      });
+
+      return decorations;
+    };
+
     const widgetsPlugin: Plugin = new Plugin({
+      state: {
+        init(_, { doc }) {
+          return DecorationSet.create(doc, createWidgetDecorations(doc));
+        },
+        apply(tr, oldDecoSet) {
+          if (tr.docChanged) {
+            return DecorationSet.create(
+              tr.doc,
+              createWidgetDecorations(tr.doc)
+            );
+          }
+          return oldDecoSet.map(tr.mapping, tr.doc);
+        },
+      },
       props: {
-        decorations: (state) => {
-          const { doc } = state;
-          const decorations: Decoration[] = [];
-
-          doc.descendants((node, pos) => {
-            if (node.type.name === "heading") {
-              // Create anchor button
-              const anchor = document.createElement("button");
-              anchor.innerText = "#";
-              anchor.type = "button";
-              anchor.className = "heading-anchor";
-              anchor.addEventListener("mousedown", (event) =>
-                this.handleCopyLink(event)
-              );
-
-              // Create fold button
-              const fold = document.createElement("button");
-              fold.innerText = "";
-              fold.innerHTML =
-                '<svg fill="currentColor" width="12" height="24" viewBox="6 0 12 24" xmlns="http://www.w3.org/2000/svg"><path d="M8.23823905,10.6097108 L11.207376,14.4695888 L11.207376,14.4695888 C11.54411,14.907343 12.1719566,14.989236 12.6097108,14.652502 C12.6783439,14.5997073 12.7398293,14.538222 12.792624,14.4695888 L15.761761,10.6097108 L15.761761,10.6097108 C16.0984949,10.1719566 16.0166019,9.54410997 15.5788477,9.20737601 C15.4040391,9.07290785 15.1896811,9 14.969137,9 L9.03086304,9 L9.03086304,9 C8.47857829,9 8.03086304,9.44771525 8.03086304,10 C8.03086304,10.2205442 8.10377089,10.4349022 8.23823905,10.6097108 Z" /></svg>';
-              fold.type = "button";
-              fold.className = `heading-fold ${
-                node.attrs.collapsed ? "collapsed" : ""
-              }`;
-              fold.addEventListener("mousedown", (event) =>
-                this.handleFoldContent(event)
-              );
-
-              // Create container span
-              const container = document.createElement("span");
-              container.contentEditable = "false";
-              container.className = `heading-actions ${
-                node.attrs.collapsed ? "collapsed" : ""
-              }`;
-              container.appendChild(anchor);
-              container.appendChild(fold);
-
-              decorations.push(
-                // Contains the heading actions
-                Decoration.widget(pos + 1, container, {
-                  side: -1,
-                  ignoreSelection: true,
-                  relaxedSide: true,
-                  key: pos.toString(),
-                })
-              );
-
-              // Creates a "space" for the caret to move to before the widget.
-              // Without this it is very hard to place the caret at the beginning
-              // of the heading when it begins with an atom element.
-              if (node.firstChild?.isAtom === false) {
-                decorations.push(
-                  Decoration.widget(
-                    pos + 1,
-                    () => document.createElement("span"),
-                    {
-                      side: -1,
-                      ignoreSelection: true,
-                      relaxedSide: true,
-                      key: "span",
-                    }
-                  )
-                );
-              }
-            }
-          });
-
-          return DecorationSet.create(doc, decorations);
+        decorations(state) {
+          return this.getState(state);
         },
       },
     });
 
     const foldPlugin: Plugin = new Plugin({
-      props: {
-        decorations: (state) => {
-          const { doc } = state;
+      state: {
+        init(_, { doc }) {
           const decorations: Decoration[] = findCollapsedNodes(doc).map(
             (block) =>
               Decoration.node(block.pos, block.pos + block.node.nodeSize, {
                 class: "folded-content",
               })
           );
-
           return DecorationSet.create(doc, decorations);
+        },
+        apply(tr, oldDecoSet) {
+          if (tr.docChanged) {
+            const decorations: Decoration[] = findCollapsedNodes(tr.doc).map(
+              (block) =>
+                Decoration.node(block.pos, block.pos + block.node.nodeSize, {
+                  class: "folded-content",
+                })
+            );
+            return DecorationSet.create(tr.doc, decorations);
+          }
+          return oldDecoSet.map(tr.mapping, tr.doc);
+        },
+      },
+      props: {
+        decorations(state) {
+          return this.getState(state);
         },
       },
     });
