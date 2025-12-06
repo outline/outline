410d18fe26da0ec22a206801ffd3c6bace4e9956
Tom Moor <tom@getoutline.com>
2025-12-05 20:52:59 -0500

    feat: Add upload button in emoji picker (#10809)

---
+++ app/components/IconPicker/components/MenuButton.tsx
@@ -0,0 +1,18 @@
+import { hover, s } from "@shared/styles";
+import styled from "styled-components";
+import NudeButton from "~/components/NudeButton";
+
+export const MenuButton = styled(NudeButton)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+  color: ${s("textSecondary")};
+  border: 1px solid ${s("inputBorder")};
+  padding: 4px;
+
+  &: ${hover} {
+    border: 1px solid ${s("inputBorderFocused")};
+  }
+`;
