93abf55542cc553478322925b2875897e203fbe1
Tom Moor <tom@getoutline.com>
2025-12-12 22:53:45 -0500

    refactor

--- server/utils/removeIndexCollision.ts
+++ server/utils/removeIndexCollision.ts
@@ -1,18 +1,11 @@
 import fractionalIndex from "fractional-index";
-import { QueryTypes, type FindOptions } from "sequelize";
+import { Sequelize, type FindOptions } from "sequelize";
 import Collection from "@server/models/Collection";
-import { sequelize } from "@server/storage/database";
-
-interface CollectionIndexResult {
-  id: string;
-  index: string;
-}
 
 /**
- * Checks for index collision and returns a new index if collision exists.
  *
  * @param teamId The team id whose collections has to be fetched
- * @param index The index for which collision has to be checked
+ * @param index the index for which collision has to be checked
  * @param options Additional options to be passed to the query
  * @returns An index, if there is collision returns a new index otherwise the same index
  */
@@ -20,7 +13,7 @@ export default async function removeIndexCollision(
   teamId: string,
   index: string,
   options: FindOptions = {}
-): Promise<string> {
+) {
   const collection = await Collection.findOne({
     where: {
       teamId,
@@ -34,21 +27,21 @@ export default async function removeIndexCollision(
     return index;
   }
 
-  // Use parameterized query to prevent SQL injection
-  const nextCollection = await sequelize.query<CollectionIndexResult>(
-    `SELECT "id", "index" FROM "collections"
-     WHERE "teamId" = :teamId
-       AND "deletedAt" IS NULL
-       AND "index" collate "C" > :index
-     ORDER BY "index" collate "C", "updatedAt" DESC
-     LIMIT 1`,
-    {
-      replacements: { teamId, index },
-      type: QueryTypes.SELECT,
-      transaction: options.transaction,
-    }
-  );
-
+  const nextCollection = await Collection.findAll({
+    where: {
+      teamId,
+      deletedAt: null,
+      index: Sequelize.literal(`"collection"."index" collate "C" > :index`),
+    },
+    attributes: ["id", "index"],
+    limit: 1,
+    order: [
+      Sequelize.literal('"collection"."index" collate "C"'),
+      ["updatedAt", "DESC"],
+    ],
+    replacements: { index },
+    ...options,
+  });
   const nextCollectionIndex = nextCollection.length
     ? nextCollection[0].index
     : null;
