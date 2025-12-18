6a86e3e697c27b3cfb29a90ce40abc86ba5960d0
Tom Moor <tom@getoutline.com>
2025-12-17 23:22:00 -0500

    fix: Comment.data should not be included in event log

--- server/models/Comment.ts
+++ server/models/Comment.ts
@@ -21,6 +21,7 @@ import { type HookContext } from "./base/Model";
 import ParanoidModel from "./base/ParanoidModel";
 import Fix from "./decorators/Fix";
 import TextLength from "./validators/TextLength";
+import { SkipChangeset } from "./decorators/Changeset";
 
 @DefaultScope(() => ({
   include: [
@@ -51,6 +52,7 @@ class Comment extends ParanoidModel<
     msg: `Comment data is too large`,
   })
   @Column(DataType.JSONB)
+  @SkipChangeset
   data: ProsemirrorData;
 
   @Column(DataType.JSONB)
