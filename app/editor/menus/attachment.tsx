24ab1c3f176fe21f80fe94f43f4ff69c5c912119
Tom Moor <tom@getoutline.com>
2025-12-19 19:14:06 -0500

    Add dimensions to attachment toolbar

--- app/editor/menus/attachment.tsx
+++ app/editor/menus/attachment.tsx
@@ -2,6 +2,7 @@ import { TrashIcon, DownloadIcon, ReplaceIcon } from "outline-icons";
 import { EditorState } from "prosemirror-state";
 import { MenuItem } from "@shared/editor/types";
 import { Dictionary } from "~/hooks/useDictionary";
+import { isNodeActive } from "@shared/editor/queries/isNodeActive";
 
 export default function attachmentMenuItems(
   state: EditorState,
@@ -11,6 +12,11 @@ export default function attachmentMenuItems(
   if (readOnly) {
     return [];
   }
+
+  const { schema } = state;
+  const isAttachmentWithPreview = isNodeActive(schema.nodes.attachment, {
+    preview: true,
+  });
 
   return [
     {
@@ -22,6 +28,15 @@ export default function attachmentMenuItems(
       name: "deleteAttachment",
       tooltip: dictionary.deleteAttachment,
       icon: <TrashIcon />,
+    },
+    {
+      name: "separator",
+    },
+    {
+      name: "dimensions",
+      tooltip: dictionary.dimensions,
+      visible: isAttachmentWithPreview(state),
+      skipIcon: true,
     },
     {
       name: "separator",
