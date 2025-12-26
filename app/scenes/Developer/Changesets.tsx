acc6a3db1aa122f7e9a353afe2428096401b07b4
Tom Moor <tom@getoutline.com>
2025-12-26 09:48:57 -0500

    Add debug screen

---
+++ app/scenes/Developer/Changesets.tsx
@@ -0,0 +1,10 @@
+import Heading from "~/components/Heading";
+import Scene from "~/components/Scene";
+
+export default function Debug() {
+  return (
+    <Scene title="Changesets">
+      <Heading>Changesets</Heading>
+    </Scene>
+  );
+}
