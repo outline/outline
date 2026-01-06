e9bc41188424ee600143030316e3e41bd9be32b2
Tom Moor <tom@getoutline.com>
2026-01-05 23:09:12 -0500

    fix: Remove flash on share popover (#11075)
    * fix: Remove flash on share popover
    
    * Loading flash within share dialog

--- app/components/Sharing/components/Placeholder.tsx
+++ app/components/Sharing/components/Placeholder.tsx
@@ -3,6 +3,7 @@ import { AvatarSize } from "~/components/Avatar";
 import Fade from "~/components/Fade";
 import PlaceholderText from "~/components/PlaceholderText";
 import { ListItem } from "../components/ListItem";
+import DelayedMount from "~/components/DelayedMount";
 
 type Props = {
   count?: number;
@@ -13,34 +14,36 @@ type Props = {
  */
 export function Placeholder({ count = 1 }: Props) {
   return (
-    <Fade>
-      {times(count, (index) => (
-        <ListItem
-          key={index}
-          image={
-            <PlaceholderText
-              width={AvatarSize.Medium}
-              height={AvatarSize.Medium}
-            />
-          }
-          title={
-            <PlaceholderText
-              maxWidth={50}
-              minWidth={30}
-              height={14}
-              style={{ marginTop: 4, marginBottom: 4 }}
-            />
-          }
-          subtitle={
-            <PlaceholderText
-              maxWidth={75}
-              minWidth={50}
-              height={12}
-              style={{ marginBottom: 4 }}
-            />
-          }
-        />
-      ))}
-    </Fade>
+    <DelayedMount delay={500}>
+      <Fade>
+        {times(count, (index) => (
+          <ListItem
+            key={index}
+            image={
+              <PlaceholderText
+                width={AvatarSize.Medium}
+                height={AvatarSize.Medium}
+              />
+            }
+            title={
+              <PlaceholderText
+                maxWidth={50}
+                minWidth={30}
+                height={14}
+                style={{ marginTop: 4, marginBottom: 4 }}
+              />
+            }
+            subtitle={
+              <PlaceholderText
+                maxWidth={75}
+                minWidth={50}
+                height={12}
+                style={{ marginBottom: 4 }}
+              />
+            }
+          />
+        ))}
+      </Fade>
+    </DelayedMount>
   );
 }
--- app/scenes/Document/components/ShareButton.tsx
+++ app/scenes/Document/components/ShareButton.tsx
@@ -35,6 +35,10 @@ function ShareButton({ document }: Props) {
     setOpen(false);
   }, []);
 
+  const handleMouseEnter = useCallback(() => {
+    void document.share();
+  }, [document]);
+
   if (isMobile) {
     return null;
   }
@@ -44,13 +48,14 @@ function ShareButton({ document }: Props) {
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger>
-        <Button icon={icon} neutral>
+        <Button icon={icon} neutral onMouseEnter={handleMouseEnter}>
           {t("Share")} {domain && <>&middot; {domain}</>}
         </Button>
       </PopoverTrigger>
       <PopoverContent
         aria-label={t("Share")}
         width={400}
+        minHeight={175}
         side="bottom"
         align="end"
       >
