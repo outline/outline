2116d9972f5af98a6bb1911700263d3312d06bcf
Tom Moor <tom@getoutline.com>
2026-01-07 21:59:19 -0500

    fix: Event propagation from input in SidebarLink (#11105)
    * fix: Event propagation from input in SidebarLink
    
    closes #11093
    
    * Include keyboard

--- shared/components/EventBoundary.tsx
+++ shared/components/EventBoundary.tsx
@@ -1,47 +1,75 @@
 import * as React from "react";
 
-type Props = {
+/**
+ * Props for the EventBoundary component.
+ */
+export interface Props<T extends React.ElementType> {
+  /** The element or component to render as. */
+  as?: T;
+  /** The children to be rendered within the boundary. */
   children?: React.ReactNode;
+  /** Optional CSS class name. */
   className?: string;
   /**
    * Capture all events, pointer events, or click events.
    * @default "all"
    */
-  captureEvents?: "all" | "pointer" | "click";
-};
+  captureEvents?: "all" | "pointer" | "click" | "mouse" | "keyboard";
+}
 
 /**
  * EventBoundary is a component that prevents events from propagating to parent elements.
  * This is useful for preventing clicks or other interactions from bubbling up the DOM tree.
+ *
+ * @param props - the properties of the component.
+ * @return a React component that captures events.
  */
-const EventBoundary: React.FC<Props> = ({
+export const EventBoundary = <T extends React.ElementType = "span">({
+  as,
   children,
   className,
   captureEvents = "all",
-}: Props) => {
+  ...rest
+}: Props<T> & Omit<React.ComponentPropsWithoutRef<T>, keyof Props<T>>) => {
+  const Component = as || "span";
+
   const stopEvent = React.useCallback((event: React.SyntheticEvent) => {
-    event.preventDefault();
     event.stopPropagation();
   }, []);
 
-  let props = {};
+  const eventHandlers: {
+    onPointerDown?: React.PointerEventHandler;
+    onPointerUp?: React.PointerEventHandler;
+    onClick?: React.MouseEventHandler;
+    onMouseDown?: React.MouseEventHandler;
+    onMouseUp?: React.MouseEventHandler;
+    onKeyDown?: React.KeyboardEventHandler;
+    onKeyUp?: React.KeyboardEventHandler;
+  } = {};
+
+  if (captureEvents === "all" || captureEvents === "keyboard") {
+    eventHandlers.onKeyDown = stopEvent;
+    eventHandlers.onKeyUp = stopEvent;
+  }
+
+  if (captureEvents === "all" || captureEvents === "mouse") {
+    eventHandlers.onMouseDown = stopEvent;
+    eventHandlers.onMouseUp = stopEvent;
+  }
 
   if (captureEvents === "all" || captureEvents === "pointer") {
-    props = {
-      onPointerDown: stopEvent,
-      onPointerUp: stopEvent,
-    };
+    eventHandlers.onPointerDown = stopEvent;
+    eventHandlers.onPointerUp = stopEvent;
   }
+
   if (captureEvents === "all" || captureEvents === "click") {
-    props = {
-      ...props,
-      onClick: stopEvent,
-    };
+    eventHandlers.onClick = stopEvent;
   }
+
   return (
-    <span {...props} className={className}>
+    <Component {...rest} {...eventHandlers} className={className}>
       {children}
-    </span>
+    </Component>
   );
 };
 
