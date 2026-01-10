2e64e1865e026afbe58781d45c9703c6f5c18e9d
Tom Moor <tom@getoutline.com>
2026-01-10 10:59:03 -0500

    docs

--- server/models/User.ts
+++ server/models/User.ts
@@ -579,6 +579,7 @@ class User extends ParanoidModel<
    * in the client browser cookies to remain logged in.
    *
    * @param expiresAt The time the token will expire at
+   * @param service The authentication service used to generate the token, if applicable
    * @returns The session token
    */
   getJwtToken = (expiresAt?: Date, service?: string) =>
@@ -613,6 +614,7 @@ class User extends ParanoidModel<
    * between subdomains or domains. It has a short expiry and can only be used
    * once.
    *
+   * @param The authentication service used to generate the token, if applicable
    * @returns The transfer token
    */
   getTransferToken = (service?: string) =>
@@ -631,6 +633,7 @@ class User extends ParanoidModel<
    * Returns a temporary token that is only used for logging in from an email
    * It can only be used to sign in once and has a medium length expiry
    *
+   * @param ctx The request context, used to get the IP address of the request
    * @returns The email signin token
    */
   getEmailSigninToken = (ctx: Context) =>
