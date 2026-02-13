996490031a77b1de8b953ac89d48908afd939ec0
Tom Moor <tom@getoutline.com>
2026-02-12 21:56:00 -0500

    fix: Potential task queue saturation in Notion importer (#11428)
    * fix: Potential task queue saturation in Notion import
    
    * Reduces concurrent Notion API pressure from 3× the recursive call depth down to 1

--- server/queues/tasks/APIImportTask.ts
+++ server/queues/tasks/APIImportTask.ts
@@ -327,11 +327,7 @@ export default abstract class APIImportTask<
       const uploadItems = Object.entries(urlToAttachment).map(
         ([url, attachment]) => ({ attachmentId: attachment.id, url })
       );
-      // publish task after attachments are persisted in DB.
-      const job = await new UploadAttachmentsForImportTask().schedule(
-        uploadItems
-      );
-      await job.finished();
+      await new UploadAttachmentsForImportTask().schedule(uploadItems);
     } catch (err) {
       // upload attachments failure is not critical enough to fail the whole import.
       Logger.error(
