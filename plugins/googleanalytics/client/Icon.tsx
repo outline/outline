469e13193ba5917963921b233a98e5bdd99a1654
Tom Moor <tom@getoutline.com>
2026-02-21 21:34:43 -0500

    Update integrations directory to color icons

--- plugins/googleanalytics/client/Icon.tsx
+++ plugins/googleanalytics/client/Icon.tsx
@@ -5,10 +5,15 @@ type Props = {
   size?: number;
   /** The color of the icon, defaults to the current text color */
   fill?: string;
-  className?: string;
+  /** Whether to render the monochrome version, defaults to true */
+  monochrome?: boolean;
 };
 
-function GoogleLogo({ size = 24, fill = "currentColor", className }: Props) {
+function GoogleLogo({
+  size = 24,
+  fill = "currentColor",
+  monochrome = true,
+}: Props) {
   return (
     <svg
       fill={fill}
@@ -16,9 +21,19 @@ function GoogleLogo({ size = 24, fill = "currentColor", className }: Props) {
       height={size}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
     >
-      <path d="M19.2312 10.5455H11.8276V13.6364H16.0892C15.6919 15.6 14.0306 16.7273 11.8276 16.7273C9.22733 16.7273 7.13267 14.6182 7.13267 12C7.13267 9.38182 9.22733 7.27273 11.8276 7.27273C12.9472 7.27273 13.9584 7.67273 14.7529 8.32727L17.0643 6C15.6558 4.76364 13.85 4 11.8276 4C7.42159 4 3.88232 7.56364 3.88232 12C3.88232 16.4364 7.42159 20 11.8276 20C15.8002 20 19.4117 17.0909 19.4117 12C19.4117 11.5273 19.3395 11.0182 19.2312 10.5455Z" />
+      <path
+        d="M15.0562 5.7088V18.1034C15.0562 19.4903 16.014 20.2631 17.0281 20.2631C17.9671 20.2631 19 19.6058 19 18.1034V5.8027C19 4.53132 18.061 3.73694 17.0281 3.73694C15.9952 3.73694 15.0562 4.61301 15.0562 5.7088Z"
+        fill={monochrome ? undefined : "#F9AB00"}
+      />
+      <path
+        d="M9.89185 12V18.1034C9.89185 19.4903 10.8496 20.2631 11.8637 20.2631C12.8027 20.2631 13.8356 19.6058 13.8356 18.1034V12.0939C13.8356 10.8225 12.8966 10.0281 11.8637 10.0281C10.8308 10.0281 9.89185 10.9042 9.89185 12Z"
+        fill={monochrome ? undefined : "#E37400"}
+      />
+      <path
+        d="M6.69923 20.2631C7.78827 20.2631 8.67111 19.3802 8.67111 18.2912C8.67111 17.2022 7.78827 16.3193 6.69923 16.3193C5.6102 16.3193 4.72736 17.2022 4.72736 18.2912C4.72736 19.3802 5.6102 20.2631 6.69923 20.2631Z"
+        fill={monochrome ? undefined : "#E37400"}
+      />
     </svg>
   );
 }
