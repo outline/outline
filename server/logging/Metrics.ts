841f27af20e6225753104490256718f2e4a4f5c6
Tom Moor <tom@getoutline.com>
2026-03-05 18:48:30 -0500

    PR feedback

--- server/logging/Metrics.ts
+++ server/logging/Metrics.ts
@@ -2,15 +2,9 @@ import { StatsD } from "hot-shots";
 import env from "@server/env";
 
 class Metrics {
-  enabled = !!env.DD_API_KEY;
-
-  private client: StatsD | undefined;
+  private client: StatsD;
 
   constructor() {
-    if (!this.enabled) {
-      return;
-    }
-
     this.client = new StatsD({
       prefix: "outline.",
       globalTags: { env: process.env.DD_ENV ?? env.ENVIRONMENT },
@@ -21,43 +15,27 @@ class Metrics {
   }
 
   gauge(key: string, value: number, tags?: string[]): void {
-    if (!this.enabled) {
-      return;
-    }
-
-    this.client?.gauge(key, value, tags);
+    this.client.gauge(key, value, tags);
   }
 
   gaugePerInstance(key: string, value: number, tags: string[] = []): void {
-    if (!this.enabled) {
-      return;
-    }
-
     const instanceId =
       process.env.INSTANCE_ID || process.env.HEROKU_DYNO_ID || process.pid;
 
-    this.client?.gauge(key, value, [...tags, `instance:${instanceId}`]);
+    this.client.gauge(key, value, [...tags, `instance:${instanceId}`]);
   }
 
   increment(key: string, tags?: Record<string, string>): void {
-    if (!this.enabled) {
-      return;
-    }
-
     const tagList = tags
       ? Object.entries(tags).map(([k, v]) => `${k}:${v}`)
       : undefined;
 
-    this.client?.increment(key, 1, tagList);
+    this.client.increment(key, 1, tagList);
   }
 
   flush(): Promise<void> {
-    if (!this.enabled || !this.client) {
-      return Promise.resolve();
-    }
-
     return new Promise<void>((resolve) => {
-      this.client?.close(() => {
+      this.client.close(() => {
         resolve();
       });
     });
