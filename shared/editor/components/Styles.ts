a6f29e34d83760f63533e7ddefae01b3cf81767e
Tom Moor <tom@getoutline.com>
2026-04-26 10:10:27 -0400

    looking good

--- shared/editor/components/Styles.ts
+++ shared/editor/components/Styles.ts
@@ -934,7 +934,7 @@ th .image .image-wrapper img {
 
 .ProseMirror.dragging .ProseMirror-selectednode,
 .ProseMirror .dragging-source {
-  opacity: 0.5;
+  opacity: 0.25;
 }
 
 .ProseMirror.dragging *::selection {
@@ -2514,10 +2514,6 @@ table {
   animation: ProseMirror-cursor-blink 1.1s steps(2, start) infinite;
 }
 
-.prosemirror-dropcursor-block,
-.prosemirror-dropcursor-inline {
-  border-radius: 1px;
-}
 
 .folded-content,
 .folded-content + .mermaid-diagram-wrapper {
