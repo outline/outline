58732081ffbb53076172bfbdcb65acf2e859ef6d
Tom Moor <tom@getoutline.com>
2026-04-26 22:43:26 -0400

    Tweak TOC positioning on share

--- app/scenes/Document/components/SharedHeader.tsx
+++ app/scenes/Document/components/SharedHeader.tsx
@@ -7,6 +7,8 @@ import useMeasure from "react-use-measure";
 import styled from "styled-components";
 import Icon from "@shared/components/Icon";
 import useShare from "@shared/hooks/useShare";
+import type { PublicTeam } from "@shared/types";
+import { TOCPosition } from "@shared/types";
 import { altDisplay } from "@shared/utils/keyboard";
 import { Action } from "~/components/Actions";
 import Button from "~/components/Button";
@@ -19,6 +21,7 @@ import {
 } from "~/components/Sharing/components/Actions";
 import HeaderBranding from "~/components/Sharing/components/HeaderBranding";
 import ShareSettingsPopover from "~/components/Sharing/components/ShareSettingsPopover";
+import { useTeamContext } from "~/components/TeamContext";
 import Tooltip from "~/components/Tooltip";
 import env from "~/env";
 import useCurrentUser from "~/hooks/useCurrentUser";
@@ -57,6 +60,8 @@ function SharedDocumentHeader({ document }: Props) {
   const [measureRef, size] = useMeasure();
   const { shareId, sharedTree, allowSubscriptions } = useShare();
   const share = shareId ? shares.get(shareId) : undefined;
+  const team = useTeamContext() as PublicTeam | undefined;
+  const tocPosition = team?.tocPosition ?? TOCPosition.Left;
   const isMobile = isMobileMedia || (size.width > 0 && size.width < 700);
 
   const handleToggle = useCallback(() => {
@@ -101,10 +106,11 @@ function SharedDocumentHeader({ document }: Props) {
       shortcut={`Ctrl+${altDisplay}+h`}
       placement="bottom"
     >
-      <Button
+      <TocButton
         aria-label={t("Show contents")}
         onClick={handleToggle}
         icon={<TableOfContentsIcon />}
+        $flipped={tocPosition === TOCPosition.Right}
         borderOnHover
         neutral
       />
@@ -137,6 +143,7 @@ function SharedDocumentHeader({ document }: Props) {
   );
 
   const hasSidebar = !!(sharedTree && sharedTree.children?.length);
+  const tocInLeft = !isMobile && hasSidebar && tocPosition === TOCPosition.Left;
 
   return (
     <StyledHeader
@@ -166,17 +173,15 @@ function SharedDocumentHeader({ document }: Props) {
             shareId={shareId}
             sharedTree={sharedTree}
           >
-            {hasHeadings ? toc : null}
+            {hasHeadings && tocInLeft ? toc : null}
           </PublicBreadcrumb>
-        ) : (
-          <Flex align="center" gap={8}>
-            {share && <HeaderBranding share={share} />}
-            {hasHeadings ? toc : null}
-          </Flex>
-        )
+        ) : share ? (
+          <HeaderBranding share={share} />
+        ) : null
       }
       actions={
         <>
+          {hasHeadings && !isMobile && !tocInLeft && <Action>{toc}</Action>}
           {allowSubscriptions !== false && !user && env.EMAIL_ENABLED && (
             <SubscribeAction shareId={shareId} documentId={document.id} />
           )}
@@ -203,6 +208,16 @@ function SharedDocumentHeader({ document }: Props) {
 const StyledHeader = styled(Header)<{ $hidden: boolean }>`
   transition: opacity 500ms ease-in-out;
   ${(props) => props.$hidden && "opacity: 0;"}
+`;
+
+const TocButton = styled(Button)<{ $flipped?: boolean }>`
+  ${(props) =>
+    props.$flipped &&
+    `
+    svg {
+      transform: scaleX(-1);
+    }
+  `}
 `;
 
 export default observer(SharedDocumentHeader);
