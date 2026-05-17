1d919cc56ac5d1f39893519441977a66efcfe310
mehmet turac <mehmetturac@gmail.com>
2026-05-18 01:11:31 +0300

    fix: initialize text selection state (#12366)
    * fix: initialize text selection state
    
    * Delete app/hooks/useTextSelection.test.ts
    
    ---------
    
    Co-authored-by: Tom Moor <tom.moor@gmail.com>

--- app/hooks/useTextSelection.ts
+++ app/hooks/useTextSelection.ts
@@ -2,19 +2,26 @@ import { useState } from "react";
 import useEventListener from "./useEventListener";
 
 /**
+ * Returns the current selected text in the document.
+ *
+ * @returns the current selected text.
+ */
+export function getSelectedText() {
+  return window.getSelection()?.toString() ?? "";
+}
+
+/**
  * A hook that returns the currently selected text.
  *
  * @returns The selected text
  */
 export default function useTextSelection() {
-  const [selection, setSelection] = useState<string>("");
+  const [selection, setSelection] = useState(getSelectedText);
 
   useEventListener(
     "selectionchange",
     () => {
-      const selection = window.getSelection();
-      const text = selection?.toString();
-      setSelection(text ?? "");
+      setSelection(getSelectedText());
     },
     document
   );
