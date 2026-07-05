5e4d6f7e4df16daf6ed0d01f1580188fe7534afd
Salihu <salihudickson@gmail.com>
2026-07-05 21:29:37 +0100

    fix trim bug

--- app/editor/extensions/PasteHandler.tsx
+++ app/editor/extensions/PasteHandler.tsx
@@ -140,6 +140,8 @@ export default class PasteHandler extends Extension {
                         if (document) {
                           if (state.schema.nodes.mention) {
                             const { hash } = new URL(trimmedText);
+                            const trimmedHash = hash.substring(1);
+
                             view.dispatch(
                               view.state.tr.replaceWith(
                                 state.selection.from,
@@ -149,8 +151,8 @@ export default class PasteHandler extends Extension {
                                   modelId: document.id,
                                   label: document.titleWithDefault,
                                   id: uuidv4(),
-                                  anchorId: hash.length
-                                    ? hash.substring(1)
+                                  anchorId: trimmedHash.length
+                                    ? trimmedHash
                                     : undefined,
                                 })
                               )
@@ -161,9 +163,9 @@ export default class PasteHandler extends Extension {
                               determineIconType(document.icon) ===
                               IconType.Emoji;
 
-                            const title = `${
-                              hasEmoji ? document.icon + " " : ""
-                            }${document.titleWithDefault}`;
+                            const title = `${hasEmoji ? document.icon + " " : ""}${
+                              document.titleWithDefault
+                            }`;
 
                             this.insertLink(`${document.path}${hash}`, title);
                           }
@@ -206,9 +208,9 @@ export default class PasteHandler extends Extension {
                               determineIconType(collection.icon) ===
                               IconType.Emoji;
 
-                            const title = `${
-                              hasEmoji ? collection.icon + " " : ""
-                            }${collection.name}`;
+                            const title = `${hasEmoji ? collection.icon + " " : ""}${
+                              collection.name
+                            }`;
 
                             this.insertLink(`${collection.path}${hash}`, title);
                           }
