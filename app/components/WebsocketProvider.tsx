f5df735adc002444925aec2e249d6b87f0927594
Tom Moor <tom@getoutline.com>
2026-07-30 21:27:30 -0400

    fix: Remove associated notifications when access is lost

--- app/components/WebsocketProvider.tsx
+++ app/components/WebsocketProvider.tsx
@@ -715,6 +715,10 @@ function useNotificationHandlers() {
       }
     );
 
+    socket.on("notifications.delete", (event: WebsocketEntityDeletedEvent) => {
+      notifications.remove(event.modelId);
+    });
+
     socket.on(
       "subscriptions.create",
       (event: PartialExcept<Subscription, "id">) => {
