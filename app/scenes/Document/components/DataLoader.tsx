e3a5a15d4737060988ff4cbb8930125c33f728dc
Tom Moor <tom@getoutline.com>
2026-08-05 18:43:25 -0400

    perf: Avoid fetching relationships for documents we literally just created

--- app/scenes/Document/components/DataLoader.tsx
+++ app/scenes/Document/components/DataLoader.tsx
@@ -97,6 +97,11 @@ function DataLoader({ match, children }: Props) {
   const location = useLocation<LocationState>();
   const query = useQuery();
   const missingPolicy = !can || Object.keys(can).length === 0;
+  const isJustCreated = React.useMemo(
+    () => !!document?.isJustCreated,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [document?.id]
+  );
 
   useDocumentSidebar();
 
@@ -148,7 +153,12 @@ function DataLoader({ match, children }: Props) {
 
   React.useEffect(() => {
     async function fetchViews() {
-      if (document?.id && !document?.isDeleted && !revisionId) {
+      if (
+        document?.id &&
+        !document?.isDeleted &&
+        !revisionId &&
+        !isJustCreated
+      ) {
         try {
           await views.fetchPage({
             documentId: document.id,
@@ -159,7 +169,7 @@ function DataLoader({ match, children }: Props) {
       }
     }
     void fetchViews();
-  }, [document?.id, document?.isDeleted, revisionId, views]);
+  }, [document?.id, document?.isDeleted, revisionId, views, isJustCreated]);
 
   const onCreateLink = React.useCallback(
     async (params: Properties<Document>, nested?: boolean) => {
@@ -205,7 +215,7 @@ function DataLoader({ match, children }: Props) {
       // Prevents unauthorized request to load share information for the document
       // when viewing a public share link
       if (can.read && !document.isDeleted && !revisionId) {
-        if (team.commentingEnabled) {
+        if (team.commentingEnabled && !isJustCreated) {
           void comments.fetchAll({
             documentId: document.id,
             limit: 100,
@@ -213,11 +223,15 @@ function DataLoader({ match, children }: Props) {
           });
         }
 
-        shares.fetchOne({ documentId: document.id }).catch((err) => {
-          if (!(err instanceof NotFoundError)) {
-            throw err;
-          }
-        });
+        // A newly created document has no share of its own, though it can still inherit one
+        // from a parent.
+        if (!isJustCreated || document.parentDocumentId) {
+          shares.fetchOne({ documentId: document.id }).catch((err) => {
+            if (!(err instanceof NotFoundError)) {
+              throw err;
+            }
+          });
+        }
       }
     }
   }, [
@@ -230,6 +244,7 @@ function DataLoader({ match, children }: Props) {
     shares,
     revisionId,
     missingPolicy,
+    isJustCreated,
   ]);
 
   // Auto-enter presentation mode when ?present=true query param is set
