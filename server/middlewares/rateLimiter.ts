66ae4dd47c2d029aff7d559cf4ce94c47e312cf1
Tom Moor <tom@getoutline.com>
2026-08-25 22:16:35 -0400

    address feedback

--- server/middlewares/rateLimiter.ts
+++ server/middlewares/rateLimiter.ts
@@ -95,7 +95,7 @@ async function consume(
     ctx.set("RateLimit-Reset", `${resetSeconds}`);
 
     Metrics.increment("rate_limit.exceeded", {
-      path: isPathScoped ? fullPath : "default",
+      path: isPathScoped ? "path" : "default",
     });
 
     throw RateLimitExceededError();
