f0f85535218cfb06d09c238e02a99621893661bd
Tom Moor <tom@getoutline.com>
2026-09-18 17:52:16 -0400

    feat: Section header menus in the sidebar (#13801)
    * wip
    
    * feat: Share sidebar actions overlay between headers and links, add quick create button

--- app/components/Sidebar/components/SidebarLink.tsx
+++ app/components/Sidebar/components/SidebarLink.tsx
@@ -2,16 +2,19 @@ import type { LocationDescriptor } from "history";
 import * as React from "react";
 import styled, { useTheme, css } from "styled-components";
 import breakpoint from "styled-components-breakpoint";
-import EventBoundary from "@shared/components/EventBoundary";
 import { ellipsis, hover, s } from "@shared/styles";
 import { isMobile } from "@shared/utils/browser";
-import NudeButton from "~/components/NudeButton";
 import { UnreadBadge } from "~/components/UnreadBadge";
 import useClickIntent from "~/hooks/useClickIntent";
 import { undraggableOnDesktop } from "~/styles";
 import Disclosure from "./Disclosure";
 import type { Props as NavLinkProps } from "./NavLink";
 import NavLink from "./NavLink";
+import {
+  SidebarActions,
+  hoveredOrMenuOpen,
+  revealActionsOnHover,
+} from "./SidebarActions";
 import type { ActionFactory, ActionWithChildren } from "~/types";
 import { ContextMenu } from "~/components/Menu/ContextMenu";
 import { useTranslation } from "react-i18next";
@@ -177,7 +180,9 @@ function SidebarLink(
           {unreadBadge && <UnreadBadge style={unreadStyle} />}
         </Content>
       </ContextMenu>
-      {menu && <Actions $showActions={$showActions}>{menu}</Actions>}
+      {menu && (
+        <SidebarActions $showActions={$showActions}>{menu}</SidebarActions>
+      )}
     </>
   );
 
@@ -245,38 +250,6 @@ const Content = styled.span`
   min-width: 0;
 `;
 
-const Actions = styled(EventBoundary)<{ $showActions?: boolean }>`
-  display: inline-flex;
-  visibility: ${(props) => (props.$showActions ? "visible" : "hidden")};
-
-  [data-drag-active] & {
-    display: none;
-  }
-
-  position: absolute;
-  top: 3px;
-  inset-inline-end: 4px;
-  gap: 4px;
-  color: ${s("textTertiary")};
-  transition: opacity 50ms;
-  height: 24px;
-  background: var(--background);
-
-  svg {
-    color: ${s("textSecondary")};
-    fill: currentColor;
-    opacity: 0.5;
-  }
-
-  &:hover {
-    visibility: visible;
-
-    svg {
-      opacity: 0.75;
-    }
-  }
-`;
-
 const HiddenDisclosure = styled(Disclosure)`
   position: inherit;
   inset-inline-start: initial;
@@ -290,13 +263,11 @@ const Link = styled(NavLink)<{
   $isDraft?: boolean;
   $disabled?: boolean;
 }>`
-  &:hover,
-  &:active,
-  &:has([data-state="open"]) {
+  ${hoveredOrMenuOpen} {
     --background: ${s("sidebarHoverBackground")};
   }
 
-  &[aria-current="page"] ${Actions} {
+  &[aria-current="page"] ${SidebarActions} {
     --background: ${s("sidebarActiveBackground")};
   }
 
@@ -364,32 +335,12 @@ const Link = styled(NavLink)<{
     font-size: 14px;
   `}
 
+  ${revealActionsOnHover}
+
   @media (hover: hover) {
-    &:hover ${Actions},
-    &:active ${Actions},
-    &:has([data-state="open"]) ${Actions} {
-      visibility: visible;
-
-      svg {
-        opacity: 0.75;
-      }
-    }
-
-    &:hover,
-    &:has([data-state="open"]) {
+    ${hoveredOrMenuOpen} {
       color: ${(props) =>
         props.$isActiveDrop ? props.theme.white : props.theme.text};
-    }
-  }
-
-  & ${Actions} {
-    ${NudeButton} {
-      background: transparent;
-
-      &:hover,
-      &[aria-expanded="true"] {
-        background: ${s("sidebarControlHoverBackground")};
-      }
     }
   }
 `;
